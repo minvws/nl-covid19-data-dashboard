b3e43935f50a9f626f356a18b3ee787401e1f54a
Jeroen Reumkens <hello@jeroenreumkens.nl>
2020-06-10 07:52:42 +0200

    Removed React import singe it isn't required for NextJS projects

--- src/components/loadingPlaceholder/index.js
+++ src/components/loadingPlaceholder/index.js
@@ -1,7 +1,5 @@
 import styles from './loadingPlaceholder.module.scss';
 
-import * as React from 'react';
-
 export default function LoadingPlaceholder({ width, children }) {
   return (
     <span className={styles.loadingPlaceholder} aria-hidden="true">
