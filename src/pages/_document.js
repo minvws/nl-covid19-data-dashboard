8e4fc2775a02b920cd6d42c80fd98cf66ef62bdb
VWS Corona Dashboard <66416946+VWSCoronaDashboard@users.noreply.github.com>
2020-06-18 13:14:37 +0200

    Add Piwik integration

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -13,6 +13,37 @@ class MyDocument extends Document {
 
         <body>
           <Main />
+
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+              var _paq = _paq || [];
+              _paq.push(["setCookieDomain", "*.rijksoverheid.nl"]);
+              _paq.push(["setDomains", ["*.rijksoverheid.nl"]]);
+              _paq.push(['enableHeartBeatTimer', 10]);
+              _paq.push(['setLinkTrackingTimer', 750]);
+              _paq.push(['trackPageView']);
+              _paq.push(['enableLinkTracking']);
+              (function() 
+                var u="//statistiek.rijksoverheid.nl/piwik/";
+                _paq.push(['setTrackerUrl', u+'piwik.php']);
+                _paq.push(['setSiteId', '7939']);
+                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+              })();
+          `,
+            }}
+          />
+
+          <noscript>
+            <img
+              src="https://statistiek.rijksoverheid.nl/piwik/piwik.php?idsite=7939&rec=1"
+              style="border:0"
+              alt=""
+            />
+          </noscript>
+
           <NextScript />
         </body>
       </Html>
