915a6e02aa8ae182be72b2e583245bb8d7c919dd
VWS Corona Dashboard <66416946+VWSCoronaDashboard@users.noreply.github.com>
2020-06-24 10:37:11 +0200

    fix: load piwik from external script src

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -14,29 +14,7 @@ class MyDocument extends Document {
         <body>
           <Main />
 
-          <script
-            type="text/javascript"
-            dangerouslySetInnerHTML={{
-              __html: `
-              
-              var _paq = _paq || [];
-              _paq.push(["setCookieDomain", "*.rijksoverheid.nl"]);
-              _paq.push(["setDomains", ["*.rijksoverheid.nl"]]);
-              _paq.push(['enableHeartBeatTimer', 10]);
-              _paq.push(['setLinkTrackingTimer', 750]);
-              _paq.push(['trackPageView']);
-              _paq.push(['enableLinkTracking']);
-
-              (function() {
-                var u="//statistiek.rijksoverheid.nl/piwik/";
-                _paq.push(['setTrackerUrl', u+'piwik.php']);
-                _paq.push(['setSiteId', '7939']);
-                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-                g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-              })();
-          `,
-            }}
-          />
+          <script src="/piwik.js"></script>
 
           <noscript>
             <img
