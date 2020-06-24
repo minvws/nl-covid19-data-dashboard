66418cdd90ad8430a53ecd5f814cccb1584129a3
VWS Corona Dashboard <66416946+VWSCoronaDashboard@users.noreply.github.com>
2020-06-24 08:29:38 +0200

    Add collapse content to Andere Cijfers

--- src/components/iconList/index.js
+++ src/components/iconList/index.js
@@ -1,4 +1,7 @@
 import styles from './iconList.module.scss';
+import { useState } from 'react';
+import useCollapse from 'react-collapsed';
+import Arrow from 'assets/arrow.svg';
 
 export default IconList;
 function IconList(props) {
@@ -6,12 +9,47 @@ function IconList(props) {
 
   return (
     <ul className={styles.iconList}>
-      {list.map((item, index) => (
-        <li key={`iconlist-${index}`} className={styles.iconListItem}>
-          <img src={item.icon} className={styles.iconListImage} alt="" />{' '}
-          <span>{item.text}</span>
-        </li>
+      {list.map((item) => (
+        <IconListItem key={`icon-list-item-${item.text}`} item={item} />
       ))}
     </ul>
+  );
+}
+
+function IconListItem(props) {
+  const { item } = props;
+  const [expanded, setExpanded] = useState(false);
+
+  const { getCollapseProps, getToggleProps } = useCollapse({
+    isExpanded: expanded,
+  });
+
+  const toggle = () => setExpanded((expanded) => !expanded);
+
+  return (
+    <li className={styles.iconListItem}>
+      <div className={styles.content}>
+        <img src={item.icon} className={styles.iconListImage} alt="" />{' '}
+        <h4>
+          <button
+            className={styles.button}
+            aria-expanded={expanded}
+            {...getToggleProps({ onClick: toggle })}
+          >
+            <span>
+              {item.text}
+              <Arrow className={styles.arrow} />
+            </span>
+          </button>
+        </h4>
+      </div>
+      <div
+        className={styles.hiddenContent}
+        hidden={!expanded}
+        {...getCollapseProps()}
+      >
+        <p>{item.content}</p>
+      </div>
+    </li>
   );
 }
