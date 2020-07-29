ac866b6b24446d6374aea5a906851c7af77fecdb
VWS Corona Dashboard <66416946+VWSCoronaDashboard@users.noreply.github.com>
2020-07-29 14:50:45 +0200

    Start to abstract away tiles in their own components

---
+++ src/components/tiles/PostivelyTestedPeople.tsx
@@ -0,0 +1,104 @@
+import BarScale from 'components/barScale';
+import Collapse from 'components/collapse';
+import Metadata from 'components/metadata';
+import GraphContainer from 'components/graphContainer';
+import GraphContent from 'components/graphContent';
+import GraphHeader from 'components/graphHeader';
+import DateReported from 'components/dateReported';
+import Getest from 'assets/test.svg';
+import formatDecimal from 'utils/formatDec';
+import { LineChart, BarChart } from './index';
+
+import siteText from 'locale';
+
+import {
+  InfectedPeopleDeltaNormalized,
+  InfectedPeopleTotal,
+  IntakeShareAgeGroups,
+} from 'types/data';
+
+export interface IPostivelyTestedPeople {
+  delta: InfectedPeopleDeltaNormalized | undefined;
+  total: InfectedPeopleTotal | undefined;
+  age: IntakeShareAgeGroups | undefined;
+  text: typeof siteText.positief_geteste_personen;
+}
+
+export const PostivelyTestedPeople: React.FC = () => {
+  const text = siteText.positief_geteste_personen;
+  const delta = state?.NL?.infected_people_delta_normalized;
+  const age = state?.NL?.intake_share_age_groups;
+  const total = state?.NL?.infected_people_total;
+
+  return (
+    <GraphContainer>
+      <GraphContent>
+        <GraphHeader Icon={Getest} title={text.title.translation} />
+        <p>{text.text.translation}</p>
+        {delta && (
+          <BarScale
+            min={0}
+            max={5}
+            screenReaderText={text.screen_reader_graph_content.translation}
+            value={delta.last_value.infected_daily_increase}
+            id="positief"
+            gradient={[
+              {
+                color: '#3391CC',
+                value: 0,
+              },
+            ]}
+          />
+        )}
+
+        {total && (
+          <h3>
+            {text.metric_title.translation}{' '}
+            <span style={{ color: '#01689b' }}>
+              {formatDecimal(total.last_value.infected_daily_total)}
+            </span>
+          </h3>
+        )}
+
+        {delta?.last_value?.infected_daily_increase !== null && (
+          <DateReported
+            datumsText={text.datums.translation}
+            dateUnix={delta?.last_value?.date_of_report_unix}
+          />
+        )}
+      </GraphContent>
+      <Collapse
+        openText={text.open.translation}
+        sluitText={text.sluit.translation}
+        piwikAction="landelijk"
+        piwikName="Positief geteste mensen"
+      >
+        <h4>{text.fold_title.translation}</h4>
+        <p>{text.fold.translation}</p>
+
+        <h4>{text.linechart_title.translation}</h4>
+        {delta && (
+          <LineChart
+            values={delta.values.map((value) => ({
+              value: value.infected_daily_increase,
+              date: value.date_of_report_unix,
+            }))}
+          />
+        )}
+
+        <h4>{text.graph_title.translation}</h4>
+        {age && (
+          <>
+            <BarChart
+              keys={['0 tot 20', '20 tot 40', '40 tot 60', '60 tot 80', '80+']}
+              data={age.values.map(
+                (value) => value.infected_per_agegroup_increase
+              )}
+            />
+            <Metadata dataSource={text.bron} />
+          </>
+        )}
+      </Collapse>
+    </GraphContainer>
+  );
+};
---
+++ src/components/tiles/ReproductionIndex.tsx
@@ -0,0 +1,113 @@
+import BarScale from 'components/barScale';
+import Collapse from 'components/collapse';
+import Metadata from 'components/metadata';
+import Legenda from 'components/legenda';
+import GraphContainer from 'components/graphContainer';
+import GraphContent from 'components/graphContent';
+import GraphHeader from 'components/graphHeader';
+import DateReported from 'components/dateReported';
+import Repro from 'assets/reproductiegetal.svg';
+import { AreaChart } from './index';
+
+import siteText from 'locale';
+
+import { ReproductionIndex as ReproductionIndexData } from 'types/data';
+
+export interface IReproductionIndex {
+  data: ReproductionIndexData | undefined;
+  lastKnownValidData: ReproductionIndexData | undefined;
+  text: typeof siteText.reproductiegetal;
+}
+
+export const ReproductionIndex: React.FC = () => {
+  const text = siteText.reproductiegetal;
+  const lastKnownValidData = state?.NL?.reproduction_index_last_known_average;
+
+  const data = state?.NL?.reproduction_index;
+
+  return (
+    <GraphContainer>
+      <GraphContent>
+        <GraphHeader Icon={Repro} title={text.title.translation} />
+        <p>{text.text.translation}</p>
+        {data && (
+          <BarScale
+            min={0}
+            max={2}
+            screenReaderText={text.screen_reader_graph_content.translation}
+            kritiekeWaarde={Number(text.signaalwaarde.translation)}
+            value={lastKnownValidData?.last_value?.reproduction_index_avg}
+            id="repro"
+            gradient={[
+              {
+                color: '#69c253',
+                value: 0,
+              },
+              {
+                color: '#69c253',
+                value: 1,
+              },
+              {
+                color: '#D3A500',
+                value: 1.0104,
+              },
+              {
+                color: '#f35065',
+                value: 1.125,
+              },
+            ]}
+          />
+        )}
+
+        <DateReported
+          datumsText={text.datums.translation}
+          dateUnix={lastKnownValidData?.last_value?.date_of_report_unix}
+          dateInsertedUnix={
+            lastKnownValidData?.last_value?.date_of_insertion_unix
+          }
+        />
+      </GraphContent>
+      <Collapse
+        openText={text.open.translation}
+        sluitText={text.sluit.translation}
+        piwikName="Reproductiegetal"
+        piwikAction="landelijk"
+      >
+        <h4>{text.fold_title.translation}</h4>
+        <p>{text.fold.translation}</p>
+
+        <img
+          width={315}
+          height={100}
+          loading="lazy"
+          src="/images/reproductie-explainer.svg"
+          alt="Ondersteunende afbeelding bij bovenstaande uitleg"
+        />
+
+        <h4>{text.graph_title.translation}</h4>
+        {data?.values && (
+          <AreaChart
+            data={data.values.map((value) => ({
+              avg: value.reproduction_index_avg,
+              min: value.reproduction_index_low,
+              max: value.reproduction_index_high,
+              date: value.date_of_report_unix,
+            }))}
+            minY={0}
+            maxY={4}
+            signaalwaarde={1}
+            rangeLegendLabel={text.rangeLegendLabel.translation}
+            lineLegendLabel={text.lineLegendLabel.translation}
+          />
+        )}
+
+        <Legenda>
+          <li className="blue">{text.legenda_r.translation}</li>
+          <li className="gray square">{text.legenda_marge.translation}</li>
+        </Legenda>
+
+        <Metadata dataSource={text.bron} />
+      </Collapse>
+    </GraphContainer>
+  );
+};
