8eb75c4b746529d6be31c7094c83cb059671b97a
VWS Corona Dashboard <66416946+VWSCoronaDashboard@users.noreply.github.com>
2020-08-10 15:32:29 +0200

    WIP

--- src/components/lineChart/index.tsx
+++ src/components/lineChart/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 import formatNumber from 'utils/formatNumber';
@@ -14,8 +14,12 @@ type LineChartProps = {
   signaalwaarde?: number;
 };
 
-const LineChart: React.FC<LineChartProps> = ({ values, signaalwaarde }) => {
-  const options = {
+function getOptions(
+  values: Value[],
+  signaalwaarde?: number | undefined
+): Highcharts.Options {
+  const options: Highcharts.Options = {
+    title: { text: undefined },
     chart: {
       alignTicks: true,
       animation: true,
@@ -25,9 +29,7 @@ const LineChart: React.FC<LineChartProps> = ({ values, signaalwaarde }) => {
       borderWidth: 0,
       className: 'undefined',
       colorCount: 10,
-      defaultSeriesType: 'line',
       displayErrors: true,
-      margin: [null],
       height: 175,
     },
     xAxis: {
@@ -40,16 +42,15 @@ const LineChart: React.FC<LineChartProps> = ({ values, signaalwaarde }) => {
       title: {
         text: null,
       },
-      categories: values.map((value) => value.date),
+      categories: values.map((value) => value.date.toString()),
       labels: {
         align: 'right',
-        rotation: '0',
-        formatter: function (): void | string {
-          // @ts-ignore
+        rotation: 0,
+        formatter: function (): string {
           if (this.isFirst || this.isLast) {
-            // @ts-ignore
             return formatDate(this.value * 1000);
           }
+          return '';
         },
       },
     },
@@ -58,11 +59,9 @@ const LineChart: React.FC<LineChartProps> = ({ values, signaalwaarde }) => {
       borderColor: '#01689B',
       borderRadius: 0,
       formatter: function (): string {
-        // @ts-ignore
         return `${formatDate(this.x * 1000)}: ${formatNumber(this.y)}`;
       },
     },
-    credits: false,
     yAxis: {
       lineColor: '#C4C4C4',
       gridLineColor: '#C4C4C4',
@@ -71,24 +70,20 @@ const LineChart: React.FC<LineChartProps> = ({ values, signaalwaarde }) => {
       },
       labels: {
         formatter: function (): string {
-          // @ts-ignore
           return formatNumber(this.value);
         },
       },
       accessibility: {
         rangeDescription: 'Range: 2010 to 2017',
       },
-      plotLines: [],
-    },
-    title: {
-      text: null,
     },
     series: [
       {
-        data: values.map((value) => value.value),
+        data: values.map((value) => value.value as number),
+        type: 'line',
+        color: '#3391CC',
         name: '',
         showInLegend: false,
-        lineColor: '#3391CC',
         marker: {
           enabled: false,
         },
@@ -97,19 +92,82 @@ const LineChart: React.FC<LineChartProps> = ({ values, signaalwaarde }) => {
   };
 
   if (signaalwaarde) {
-    options.yAxis.plotLines.push({
-      // @ts-ignore
-      value: signaalwaarde,
-      // @ts-ignore
-      dashStyle: 'dash',
-      // @ts-ignore
-      width: 1,
-      // @ts-ignore
-      color: '#4f5458',
-    });
+    // @ts-ignore
+    options.yAxis.plotLines = [
+      {
+        value: signaalwaarde,
+        dashStyle: 'dash',
+        width: 1,
+        color: '#4f5458',
+      },
+    ];
   }
+  return options;
+}
 
-  return <HighchartsReact highcharts={Highcharts} options={options} />;
+const LineChart: React.FC<LineChartProps> = ({ values, signaalwaarde }) => {
+  const [timeframe, setTimeframe] = useState<'all' | 'month' | 'week'>('week');
+
+  const id = useMemo(() => {
+    return Math.random().toString(36).substr(2);
+  }, []);
+
+  const chartOptions = useMemo(() => {
+    const week = 7;
+    const month = 30;
+    const days = values.length;
+
+    if (timeframe === 'all') {
+      return getOptions(values, signaalwaarde);
+    }
+    if (timeframe === 'month') {
+      return getOptions(values.slice(days - month, days), signaalwaarde);
+    }
+    if (timeframe === 'week') {
+      return getOptions(values.slice(days - week, days), signaalwaarde);
+    }
+  }, [values, timeframe, signaalwaarde]);
+
+  return (
+    <>
+      <HighchartsReact
+        key={timeframe}
+        highcharts={Highcharts}
+        options={chartOptions}
+      />
+      <div
+        className="chart-button-group"
+        onChange={(evt: any) => setTimeframe(evt.target.value)}
+      >
+        <input
+          id={`all-${id}`}
+          type="radio"
+          name={`timeframe-${id}`}
+          value="all"
+          checked={timeframe === 'all'}
+        />
+        <label htmlFor={`all-${id}`}>Toon alles</label>
+
+        <input
+          id={`month-${id}`}
+          type="radio"
+          name={`timeframe-${id}`}
+          value="month"
+          checked={timeframe === 'month'}
+        />
+        <label htmlFor={`month-${id}`}>Laatste maand</label>
+
+        <input
+          id={`week-${id}`}
+          type="radio"
+          name={`timeframe-${id}`}
+          value="week"
+          checked={timeframe === 'week'}
+        />
+        <label htmlFor={`week-${id}`}>Laatste week</label>
+      </div>
+    </>
+  );
 };
 
 export default LineChart;
