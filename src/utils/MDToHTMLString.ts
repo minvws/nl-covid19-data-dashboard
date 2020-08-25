5fdada40cf128468c617b1da64cfa29f88a096bc
VWS Corona Dashboard 5 <70140495+VWSCoronaDashboard5@users.noreply.github.com>
2020-08-25 11:15:25 +0200

    support external links

--- src/utils/MDToHTMLString.ts
+++ src/utils/MDToHTMLString.ts
@@ -1,24 +1,18 @@
 import unified from 'unified';
-// import remark from 'remark';
 import markdown from 'remark-parse';
 import rehype from 'remark-rehype';
 import html from 'rehype-stringify';
-// import externalLinks from 'remark-external-links';
+import externalLinks from 'remark-external-links';
+
+const processor = unified()
+  .use(markdown)
+  .use(externalLinks, {
+    target: false,
+    rel: ['noopener', 'noreferrer'],
+  })
+  .use(rehype)
+  .use(html);
 
 export default function MDToHTMLString(str: string): string {
-  const processor = unified().use(markdown).use(rehype).use(html);
-  // const replaceLinks = remark()
-  //   .use(externalLinks, {
-  //     target: false,
-  //     rel: ['noopener', 'noreferrer'],
-  //   })
-  //   .use(html)
-  //   .process(str, function (err, file) {
-  //     if (err) throw err;
-  //     console.log(String(file));
-  //   });
-
-  const output = processor.processSync(str).toString();
-
-  return output;
+  return processor.processSync(str).toString();
 }
