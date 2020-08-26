09b00596d332437ff6042be80ddd08e0425e1825
VWS Corona Dashboard 5 <70140495+VWSCoronaDashboard5@users.noreply.github.com>
2020-08-26 16:27:06 +0200

    refactor iconlist with interface item

--- src/components/iconList/index.tsx
+++ src/components/iconList/index.tsx
@@ -3,15 +3,17 @@ import { useState } from 'react';
 import useCollapse from 'react-collapsed';
 import Arrow from 'assets/arrow.svg';
 
+interface IProps {
+  list: Item[];
+}
+
 interface Item {
   content: string;
   text: string;
   icon: string;
 }
 
-const IconList: React.FC<{ list: Item[] }> = (props) => {
-  const { list } = props;
-
+function IconList({ list }: IProps) {
   return (
     <ul className={styles.iconList}>
       {list.map((item) =>
@@ -26,28 +28,25 @@ const IconList: React.FC<{ list: Item[] }> = (props) => {
       )}
     </ul>
   );
-};
+}
 
-const IconListItem: React.FC<{ item: Item }> = (props) => {
-  const { item } = props;
-
+function IconListItem({ text, icon }: Item) {
   return (
     <li className={styles.iconListItem}>
       <div className={styles.content}>
         <img
-          src={item.icon}
+          src={icon}
           className={styles.iconListImage}
           alt=""
           loading="lazy"
         />{' '}
-        <h4>{item.text}</h4>
+        <h4>{text}</h4>
       </div>
     </li>
   );
-};
+}
 
-const CollapseIconListItem: React.FC<{ item: Item }> = (props) => {
-  const { item } = props;
+function CollapseIconListItem({ icon, text, content }: Item) {
   const [expanded, setExpanded] = useState(false);
 
   const { getCollapseProps, getToggleProps } = useCollapse({
@@ -60,7 +59,7 @@ const CollapseIconListItem: React.FC<{ item: Item }> = (props) => {
     <li className={styles.iconListItem}>
       <div className={styles.content}>
         <img
-          src={item.icon}
+          src={icon}
           className={styles.iconListImage}
           loading="lazy"
           alt=""
@@ -71,17 +70,17 @@ const CollapseIconListItem: React.FC<{ item: Item }> = (props) => {
             {...getToggleProps({ onClick: toggle })}
           >
             <span>
-              {item.text}
+              {text}
               <Arrow className={styles.arrow} width={13} height={8} />
             </span>
           </button>
         </h4>
       </div>
       <div className={styles.hiddenContent} {...getCollapseProps()}>
-        <p>{item.content}</p>
+        <p>{content}</p>
       </div>
     </li>
   );
-};
+}
 
 export default IconList;
