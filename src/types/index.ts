c8565963cbc791110a079861f539cea763bfbe38
VWS Corona Dashboard 5 <70140495+VWSCoronaDashboard5@users.noreply.github.com>
2020-08-27 10:14:47 +0200

    specify disable eslint

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,5 @@
 // https://fettblog.eu/typescript-react-why-i-dont-use-react-fc/
-// Slightly altered version of the WithChildren type found in source
-// Added the {} in order for it to allow multiple children, simply wrapping it in a Fragment didn't work
-// eslint-disable-next-line
+// eslint-disable-next-line @typescript-eslint/ban-types
 export type WithChildren<T = {}> = T & {
   children?: React.ReactNode;
 };
