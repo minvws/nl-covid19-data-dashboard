e8712eba3c756986738fbea79f8bd84ac6f51884
VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2020-08-31 18:35:26 +0200

    Add schema for municipalities data that will be used to draw the chloropleths

--- schema/validator/validate-json.js
+++ schema/validator/validate-json.js
@@ -12,6 +12,7 @@ const schemaToJsonLookup = {
   ranges: ['RANGES.json'],
   regional: filterFilenames(allJsonFiles, new RegExp('^VR[0-9]+\\.json$')),
   municipal: filterFilenames(allJsonFiles, new RegExp('^GM[0-9]+\\.json$')),
+  municipalities: ['municipalities.json'],
 };
 
 // The validations are asynchronous so this reducer gathers all the Promises in one array.
