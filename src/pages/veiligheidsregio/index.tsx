f024005332f2003c6d35070adf22e057a2af4fc5
VWS Corona Dashboard 5 <70140495+VWSCoronaDashboard5@users.noreply.github.com>
2020-09-03 15:57:45 +0200

    place safety region map on /veiligheidsregio

--- src/pages/veiligheidsregio/index.tsx
+++ src/pages/veiligheidsregio/index.tsx
@@ -1,8 +1,25 @@
+// import { useState } from 'react';
+
 import { FCWithLayout } from 'components/layout';
 import { getSafetyRegionLayout } from 'components/layout/SafetyRegionLayout';
+// import MunicipalityMap from 'components/mapChart/MunicipalityMap';
+import SafetyRegionMap from 'components/mapChart/SafetyRegionMap';
+// import ChartRegionControls from 'components/chartRegionControls';
+
+// import siteText from 'locale';
+
+// const text: typeof siteText.positief_geteste_personen =
+//   siteText.positief_geteste_personen;
 
 const SafetyRegion: FCWithLayout = () => {
-  return null;
+  return (
+    <div className="column-item column-item-extra-margin">
+      <SafetyRegionMap
+        metric="positive_tested_people"
+        gradient={['#9DDEFE', '#0290D6']}
+      />
+    </div>
+  );
 };
 
 SafetyRegion.getLayout = getSafetyRegionLayout();
