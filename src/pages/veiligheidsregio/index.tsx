f5208d8c599b07fe9800b98e2365149a057ae60e
VWS Corona Dashboard 5 <70140495+VWSCoronaDashboard5@users.noreply.github.com>
2020-09-03 18:05:29 +0200

    remove comments

--- src/pages/veiligheidsregio/index.tsx
+++ src/pages/veiligheidsregio/index.tsx
@@ -1,15 +1,6 @@
-// import { useState } from 'react';
-
 import { FCWithLayout } from 'components/layout';
 import { getSafetyRegionLayout } from 'components/layout/SafetyRegionLayout';
-// import MunicipalityMap from 'components/mapChart/MunicipalityMap';
 import SafetyRegionMap from 'components/mapChart/SafetyRegionMap';
-// import ChartRegionControls from 'components/chartRegionControls';
-
-// import siteText from 'locale';
-
-// const text: typeof siteText.positief_geteste_personen =
-//   siteText.positief_geteste_personen;
 
 const SafetyRegion: FCWithLayout = () => {
   return (
