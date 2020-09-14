af99c2b3ac8e75de3c1ec4b5d7bcef3bbf2159c8
VWS Corona Dashboard <66416946+VWSCoronaDashboard@users.noreply.github.com>
2020-09-14 17:03:55 +0200

    Fix displayname

--- src/components/comboBox/index.tsx
+++ src/components/comboBox/index.tsx
@@ -52,7 +52,13 @@ function ComboBox<Option extends TOption>(props: TProps<Option>) {
   }
 
   function onSelect(name: string): void {
-    const option = options.find((option) => option.name === name);
+    if (!name) {
+      return;
+    }
+
+    const option = options.find(
+      (option) => option.name === name || option.displayName === name
+    );
 
     inputRef?.current?.blur();
 
@@ -76,7 +82,10 @@ function ComboBox<Option extends TOption>(props: TProps<Option>) {
         {results.length > 0 ? (
           <ComboboxList>
             {results.map((option) => (
-              <ComboboxOption key={option.name} value={option.name} />
+              <ComboboxOption
+                key={option.name}
+                value={option.displayName || option.name}
+              />
             ))}
           </ComboboxList>
         ) : (
