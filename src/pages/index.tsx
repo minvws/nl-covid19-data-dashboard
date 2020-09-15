972f67a8b576f4300a48f1c5d210f958401d6db0
VWS Corona Dashboard <66416946+VWSCoronaDashboard@users.noreply.github.com>
2020-09-15 11:02:17 +0200

    Map on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,8 +9,19 @@ import styles from './index.module.scss';
 
 import text from 'locale';
 import TitleWithIcon from 'components/titleWithIcon';
+import ChartRegionControls from 'components/chartRegionControls';
+import MunicipalityChloropleth from 'components/chloropleth/MunicipalityChloropleth';
+import SafetyRegionChloropleth from 'components/chloropleth/SafetyRegionChloropleth';
+import positiveTestedPeopleTooltip from 'components/chloropleth/tooltips/municipal/positiveTestedPeopleTooltip';
+import positiveTestedPeopleTooltipRegion from 'components/chloropleth/tooltips/region/positiveTestedPeopleTooltip';
+import { useState } from 'react';
+import Link from 'next/link';
 
 const Home: FCWithLayout<INationalData> = () => {
+  const [selectedMap, setSelectedMap] = useState<'municipal' | 'region'>(
+    'municipal'
+  );
+
   return (
     <>
       <TitleWithIcon
@@ -32,6 +43,35 @@ const Home: FCWithLayout<INationalData> = () => {
           <ExternalLink />
           <span>{text.notificatie.link.text}</span>
         </a>
+      </article>
+
+      <article className="metric-article layout-two-column">
+        <div className="column-item column-item-extra-margin">
+          <h3>{text.positief_geteste_personen.map_titel}</h3>
+          <p>{text.positief_geteste_personen.map_toelichting}</p>
+          <ChartRegionControls
+            onChange={(val: 'region' | 'municipal') => setSelectedMap(val)}
+          />
+
+          <Link href="/gemeente">
+            <a>{text.laatste_ontwikkelingen.regio_cta}</a>
+          </Link>
+        </div>
+
+        <div className="column-item column-item-extra-margin">
+          {selectedMap === 'municipal' && (
+            <MunicipalityChloropleth
+              metricName="positive_tested_people"
+              tooltipContent={positiveTestedPeopleTooltip}
+            />
+          )}
+          {selectedMap === 'region' && (
+            <SafetyRegionChloropleth
+              metricName="positive_tested_people"
+              tooltipContent={positiveTestedPeopleTooltipRegion}
+            />
+          )}
+        </div>
       </article>
     </>
   );
