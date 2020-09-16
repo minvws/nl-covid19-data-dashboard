a866de3039a692f029ee399946f61e5b01a72f60
VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2020-09-16 15:19:40 +0200

    Put escalation map on region index

--- src/pages/veiligheidsregio/index.tsx
+++ src/pages/veiligheidsregio/index.tsx
@@ -1,20 +1,39 @@
 import { FCWithLayout } from 'components/layout';
 import { getSafetyRegionLayout } from 'components/layout/SafetyRegionLayout';
 import { useRouter } from 'next/router';
-
+import ExclamationMark from 'assets/exclamation-mark-bubble.svg';
 import text from 'locale';
-import styles from 'components/chloropleth/chloropleth.module.scss';
+import styles from './index.module.scss';
 
 import getLastGeneratedData from 'static-props/last-generated-data';
 
 import { ReactNode } from 'react';
 import SafetyRegionChloropleth from 'components/chloropleth/SafetyRegionChloropleth';
 
-const tooltipContent = (context: any): ReactNode => {
+const escalationColors = ['#FCD603', '#F79903', '#F45167'];
+
+const escalationTooltipContent = (context: any): ReactNode => {
+  const type: string = context?.value;
   return (
-    context && (
-      <div className={styles.defaultTooltip}>
-        <strong>{context.vrname}</strong>
+    type && (
+      <div className={styles.escalationTooltip}>
+        <h4>Situatie in {context?.vrname}</h4>
+        {
+          <div className={styles.escalationInfo}>
+            <div className={styles.bubble}>
+              <ExclamationMark
+                className={`${styles[`escalationColor${type}`]}`}
+              />
+            </div>
+            <div>
+              <strong>
+                {(text.escalatie_niveau.types as any)[type].titel}
+              </strong>
+              :&nbsp;
+              {(text.escalatie_niveau.types as any)[type].toelichting}
+            </div>
+          </div>
+        }
       </div>
     )
   );
@@ -46,13 +65,53 @@ const SafetyRegion: FCWithLayout<any> = () => {
           <p className="text-max-width">
             {text.veiligheidsregio_index.selecteer_toelichting}
           </p>
+          <div className={styles.legenda}>
+            <h4 className="text-max-width">
+              {text.escalatie_niveau.legenda.titel}
+            </h4>
+
+            <div className={styles.escalationInfo}>
+              <div className={styles.bubble}>
+                <ExclamationMark className={styles.escalationColor1} />
+              </div>
+              <div>
+                <strong>{text.escalatie_niveau.types['1'].titel}</strong>
+                :&nbsp;
+                {text.escalatie_niveau.types['1'].toelichting}
+              </div>
+            </div>
+
+            <div className={styles.escalationInfo}>
+              <div className={styles.bubble}>
+                <ExclamationMark className={styles.escalationColor2} />
+              </div>
+              <div>
+                <strong>{text.escalatie_niveau.types['2'].titel}</strong>
+                :&nbsp;
+                {text.escalatie_niveau.types['2'].toelichting}
+              </div>
+            </div>
+
+            <div className={styles.escalationInfo}>
+              <div className={styles.bubble}>
+                <ExclamationMark className={styles.escalationColor3} />
+              </div>
+              <div>
+                <strong>{text.escalatie_niveau.types['3'].titel}</strong>
+                :&nbsp;
+                {text.escalatie_niveau.types['3'].toelichting}
+              </div>
+            </div>
+          </div>
         </div>
         <div className="column-item-no-margin column-item">
           <SafetyRegionChloropleth
-            style={{ height: '800px', backgroundColor: 'none' }}
+            metricName="escalation_levels"
+            metricProperty="escalation_level"
+            style={{ height: '500px' }}
+            gradient={escalationColors}
             onSelect={onSelectRegion}
-            gradient={['#ffff', '#ffff']}
-            tooltipContent={tooltipContent}
+            tooltipContent={escalationTooltipContent}
           />
         </div>
       </article>
