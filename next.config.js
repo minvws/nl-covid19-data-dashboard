2bc04c875b91d432da54974cdd6b5b328ceb9d48
R <69849293+VWSCoronaDashboard1@users.noreply.github.com>
2020-09-17 09:56:45 +0200

    Fix/chloropleth legenda thresholds (#340)
    * Latest copy
    
    * Make escalation_levels optional
    
    * Merge branch 'develop' into feature/last-generated-static-props
    
    # Conflicts:
    #	src/components/layout/MunicipalityLayout.tsx
    #	src/components/layout/layout.module.scss
    #	src/pages/over.tsx
    #	src/pages/verantwoording.tsx
    
    * fix lint error
    
    * Fix missing dep
    
    * cherrypick: apply math.round and replace key
    
    * Remove formatting
    
    * Revert "Remove formatting"
    
    This reverts commit 5e12c70701db5b0f8786bf76f280071dfa3fa4dd.
    
    * change key for safety region positively tested people
    
    * 2.0.1
    
    * 2.0.1 (#329)
    
    * Positively tested people: fix key
    
    * Big button (#330)
    
    * Merge branch 'master' of https://github.com/minvws/nl-covid19-data-dashboard
    
    * Turn off legenda for the time being
    
    * correct key
    
    * Switch to quantile distribution for chloropleths and legendas
    
    * Add threshold scale and hardcoded scales for municipality and region chloropleths and legendas
    
    * fix legenda label
    
    * Add official positive tested people thresholds and tweak prettier legenda labels
    
    * Remove random key and replace with item color
    
    Co-authored-by: VWS Corona Dashboard <66416946+VWSCoronaDashboard@users.noreply.github.com>
    Co-authored-by: VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>
    Co-authored-by: VWS Corona Dashboard 5 <70140495+VWSCoronaDashboard5@users.noreply.github.com>
    Co-authored-by: A <69853534+VWSCoronaDashboard4@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -20,8 +20,7 @@ const nextConfig = {
   env: {
     COMMIT_ID: commitHash,
   },
-
-  webpack(config, { dev }) {
+  webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
       use: [
@@ -35,28 +34,28 @@ const nextConfig = {
       },
     });
 
-    if (!dev) {
-      // Move Preact into the framework chunk instead of duplicating in routes:
-      const splitChunks =
-        config.optimization && config.optimization.splitChunks;
-      if (splitChunks) {
-        const cacheGroups = splitChunks.cacheGroups;
-        const test = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/;
-        if (cacheGroups.framework) {
-          cacheGroups.preact = Object.assign({}, cacheGroups.framework, {
-            test,
-          });
-          // if you want to merge the 2 small commons+framework chunks:
-          cacheGroups.commons.name = 'framework';
-        } else {
-          cacheGroups.preact = { name: 'commons', chunks: 'all', test };
-        }
-      }
+    // if (!dev) {
+    //   // Move Preact into the framework chunk instead of duplicating in routes:
+    //   const splitChunks =
+    //     config.optimization && config.optimization.splitChunks;
+    //   if (splitChunks) {
+    //     const cacheGroups = splitChunks.cacheGroups;
+    //     const test = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/;
+    //     if (cacheGroups.framework) {
+    //       cacheGroups.preact = Object.assign({}, cacheGroups.framework, {
+    //         test,
+    //       });
+    //       // if you want to merge the 2 small commons+framework chunks:
+    //       cacheGroups.commons.name = 'framework';
+    //     } else {
+    //       cacheGroups.preact = { name: 'commons', chunks: 'all', test };
+    //     }
+    //   }
 
-      // Install webpack aliases:
-      const aliases = config.resolve.alias || (config.resolve.alias = {});
-      aliases.react = aliases['react-dom'] = 'preact/compat';
-    }
+    //   // Install webpack aliases:
+    //   const aliases = config.resolve.alias || (config.resolve.alias = {});
+    //   aliases.react = aliases['react-dom'] = 'preact/compat';
+    // }
 
     return config;
   },
