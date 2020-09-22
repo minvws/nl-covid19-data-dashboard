21c6b21d83acc46e69586df8a504dc996cfa9816
T <71320230+VWSCoronaDashboard8@users.noreply.github.com>
2020-09-22 15:05:33 +0200

    Use barrel file for 404 page

---
+++ src/components/index.ts
@@ -0,0 +1,15 @@
+export * from './areaChart';
+export * from './barChart';
+export * from './barScale';
+export * from './chartRegionControls';
+export * from './charts';
+export * from './chartTimeControls';
+// export * from './chloropleth';
+export * from './comboBox';
+// export * from './gemeente';
+// export * from './landelijk';
+export * from './layout';
+export * from './legenda';
+export * from './lineChart';
+
+export * from './maxWidth';
