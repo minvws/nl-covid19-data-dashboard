271fbbce41cc70e298fe7fc54b4c89dcbba59eb4
VWS Corona Dashboard 5 <70140495+VWSCoronaDashboard5@users.noreply.github.com>
2020-09-23 16:22:05 +0200

    fix off by one error on index check

--- src/utils/getItemFromArray.ts
+++ src/utils/getItemFromArray.ts
@@ -3,7 +3,7 @@ import { assert } from '~/utils/assert';
 export function getItemFromArray<T>(arr: T[], index: number): T {
   const item = arr[index];
   assert(
-    index > 0 && index < arr.length && item !== undefined,
+    index >= 0 && index < arr.length && item !== undefined,
     `Index ${index} is out of range`
   );
   return item;
