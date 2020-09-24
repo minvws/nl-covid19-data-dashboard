2c820feb8c84a01923b5fa439478267a2e7c0da8
VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2020-09-24 12:49:52 +0200

    First working e2e test

--- cypress/integration/landelijk.positief-geteste-mensen.spec.ts
+++ cypress/integration/landelijk.positief-geteste-mensen.spec.ts
@@ -1,18 +1,26 @@
+import { Context } from 'mocha';
 /// <reference types="cypress" />
+/// <reference types="../../src/types/data" />
 
-import { National } from '~/types/data';
+import { National } from '../../src/types/data';
+import { formatNumber } from '../../src/utils/formatNumber';
 
 context('Landelijk - Positief geteste mensen', () => {
   before(() => {
-    cy.fixture('NL.json').as('national') as National;
+    cy.fixture<National>('NL.json').as('national');
   });
 
   beforeEach(() => {
     cy.visit('http://localhost:3000/landelijk/positief-geteste-mensen');
   });
 
-  it('Should show the correct total infected people', function (this: National) {
-    const infected_daily_total = '1.093';
-    cy.get('[data-cy=infected_daily_total]').contains(infected_daily_total);
+  it('Should show the correct total infected people', function (this: Context & {
+    national: National;
+  }) {
+    const testValue = formatNumber(
+      this.national.infected_people_total.last_value.infected_daily_total
+    );
+
+    cy.get('[data-cy=infected_daily_total]').contains(testValue);
   });
 });
