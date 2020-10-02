d2e451255698efd1d1de74dbf996852b50a24838
VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2020-10-02 14:38:21 +0200

    fix path creation

--- src/tools/validator/getSchemaNames.ts
+++ src/tools/validator/getSchemaNames.ts
@@ -4,7 +4,7 @@ import fs from 'fs';
 /**
  * The fully qualified path to the schema root directory
  */
-export const schemaDirectory = path.join(__dirname, '../../../schema');
+export const schemaDirectory = path.join(__dirname, '..', '..', '..', 'schema');
 
 /**
  * Retrieves a list of available schema names.
