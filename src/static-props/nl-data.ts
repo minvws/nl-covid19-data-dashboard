5cee3afc640012f50a3f44abb95ce156047b904f
T <71320230+VWSCoronaDashboard8@users.noreply.github.com>
2020-10-07 18:15:13 +0200

    Implement data sorting on national level

--- src/static-props/nl-data.ts
+++ src/static-props/nl-data.ts
@@ -1,6 +1,5 @@
 import fs from 'fs';
 import path from 'path';
-
 import { National } from '~/types/data.d';
 
 export interface INationalData {
@@ -19,18 +18,18 @@ interface IProps {
  *
  * Example:
  * ```ts
- * PostivelyTestedPeople.getLayout = getNationalLayout();
+ * PositivelyTestedPeople.getLayout = getNationalLayout();
  *
  * export const getStaticProps = getNlData();
  *
- * export default PostivelyTestedPeople;
+ * export default PositivelyTestedPeople;
  * ```
  *
- * The `INationalData` should be used in conjuction with `FCWithLayout`
+ * The `INationalData` should be used in conjunction with `FCWithLayout`
  *
  * Example:
  * ```ts
- * const PostivelyTestedPeople: FCWithLayout<INationalData> = props => {
+ * const PositivelyTestedPeople: FCWithLayout<INationalData> = props => {
  *   // ...
  * }
  * ```
@@ -43,6 +42,8 @@ export default function getNlData(): () => IProps {
 
     const lastGenerated = data.last_generated;
 
+    sortTimeSeriesInDataInPlace(data);
+
     return {
       props: {
         data,
@@ -50,4 +51,96 @@ export default function getNlData(): () => IProps {
       },
     };
   };
+}
+
+/**
+ * Sort all time series properties in the data in-place, meaning the input
+ * data is mutated.
+ */
+function sortTimeSeriesInDataInPlace(data: National) {
+  const timeSeriesPropertyNames = getTimeSeriesPropertyNames(data);
+  // console.log('+++ timeSeriesPropertyNames', timeSeriesPropertyNames);
+
+  for (const propertyName of timeSeriesPropertyNames) {
+    const timeSeries = data[propertyName] as TimeSeriesData<Timestamped>;
+    (data[propertyName] as TimeSeriesData<
+      Timestamped
+    >).values = sortTimeSeriesValues(timeSeries.values);
+  }
+}
+/**
+ * From the data structure, retrieve all properties that hold a "values" field
+ * in their content. All time series data is kept in this values field.
+ */
+function getTimeSeriesPropertyNames(data: National) {
+  return Object.entries(data).reduce(
+    (acc, [propertyKey, propertyValue]) =>
+      isTimeSeries(propertyValue)
+        ? [...acc, propertyKey as keyof National]
+        : acc,
+    [] as (keyof National)[]
+  );
+}
+
+type Timestamped = ReportTimestamped | WeekTimestamped | MeasurementTimestamped;
+
+interface ReportTimestamped {
+  date_of_report_unix: number;
+}
+
+interface WeekTimestamped {
+  week_unix: number;
+}
+
+interface MeasurementTimestamped {
+  date_measurement_unix: number;
+}
+
+interface TimeSeriesData<T> {
+  values: T[];
+}
+
+function sortTimeSeriesValues(values: Timestamped[]): Timestamped[] {
+  if (isReportTimestamped(values)) {
+    return values.sort((a, b) => a.date_of_report_unix - b.date_of_report_unix);
+  } else if (isWeekTimestamped(values)) {
+    return values.sort((a, b) => a.week_unix - b.week_unix);
+  } else if (isMeasurementTimestamped(values)) {
+    return values.sort(
+      (a, b) => a.date_measurement_unix - b.date_measurement_unix
+    );
+  }
+
+  throw new Error(
+    `Unknown timestamp in value ${JSON.stringify(values[0], null, 2)}`
+  );
+}
+
+function isTimeSeries(
+  value: unknown | TimeSeriesData<Timestamped>
+): value is TimeSeriesData<Timestamped> {
+  return (value as TimeSeriesData<Timestamped>).values !== undefined;
+}
+
+function isReportTimestamped(
+  timeSeries: Timestamped[]
+): timeSeries is ReportTimestamped[] {
+  return (
+    (timeSeries as ReportTimestamped[])[0].date_of_report_unix !== undefined
+  );
+}
+
+function isWeekTimestamped(
+  timeSeries: Timestamped[]
+): timeSeries is WeekTimestamped[] {
+  return (timeSeries as WeekTimestamped[])[0].week_unix !== undefined;
+}
+
+function isMeasurementTimestamped(
+  timeSeries: Timestamped[]
+): timeSeries is MeasurementTimestamped[] {
+  return (
+    (timeSeries as MeasurementTimestamped[])[0].date_measurement_unix !==
+    undefined
+  );
 }
