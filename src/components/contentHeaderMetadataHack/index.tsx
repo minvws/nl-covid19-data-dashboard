9a75fb61c218371ab12c5e67a8df8397b9a8f79c
VWS CoronaDashboard <71320230+VWSCoronaDashboard8@users.noreply.github.com>
2020-10-08 10:23:01 +0200

    Add workaround for content header metadata sources (#549)

---
+++ src/components/contentHeaderMetadataHack/index.tsx
@@ -0,0 +1,61 @@
+import { TitleWithIcon } from '~/components/titleWithIcon';
+import styles from './layout.module.scss';
+import { MetadataHack } from './metadataHack';
+
+/**
+ * An alteration from layout/ContentHeader in order to render two sources in metadata.
+ * This component will be thrown out in the next sprint probably because then
+ * metadata will be taken out and the original is not worth fixing since it
+ * requires a different kind of abstraction.
+ *
+ * The Metadata component is also copied and adjusted here locally for the same
+ * reasons.
+ */
+export function ContentHeaderMetadataHack(props: IContentHeaderProps) {
+  const { category, Icon, title, subtitle, metadata, id } = props;
+
+  const layoutClasses = [styles.contentHeader];
+
+  if (!category) {
+    layoutClasses.push(styles.withoutCategory);
+  }
+  if (!Icon) {
+    layoutClasses.push(styles.withoutIcon);
+  }
+
+  return (
+    <header id={id} className={layoutClasses.join(' ')}>
+      {category && <p className={styles.category}>{category}</p>}
+      <TitleWithIcon Icon={Icon} title={title} as="h2" />
+
+      <div className={styles.text}>
+        <p>{subtitle}</p>
+
+        <div>
+          <MetadataHack {...metadata} />
+        </div>
+      </div>
+    </header>
+  );
+}
+
+interface IContentHeaderProps {
+  title: string;
+  subtitle: string;
+  metadata: {
+    datumsText: string;
+    dateUnix?: number;
+    dateInsertedUnix?: number;
+    dataSourceA: {
+      href: string;
+      text: string;
+    };
+    dataSourceB: {
+      href: string;
+      text: string;
+    };
+  };
+  category?: string;
+  Icon?: React.ComponentType;
+  id?: string;
+}
