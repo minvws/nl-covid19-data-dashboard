31df2c203affa877e2a2ac47552cd53f3a1cd857
VWS Corona Dashboard 5 <70140495+VWSCoronaDashboard5@users.noreply.github.com>
2020-10-12 11:47:33 +0200

    use relative classnames in remote css file

--- src/components/dataWarning/index.tsx
+++ src/components/dataWarning/index.tsx
@@ -4,9 +4,11 @@ import Warning from '~/assets/warning.svg';
 
 export function DataWarning() {
   return (
-    <div className={styles.dataWarning}>
-      <Warning />
-      <p className={styles.dataWarningText}>{text.data_warning_text}</p>
+    <div className={styles.root}>
+      <div>
+        <Warning />
+      </div>
+      <p className={styles.text}>{text.data_warning_text}</p>
     </div>
   );
 }
