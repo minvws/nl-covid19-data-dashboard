9ebfa1e8b726f57f98969557a9fb877fc3345ea3
VWS Corona Dashboard 5 <70140495+VWSCoronaDashboard5@users.noreply.github.com>
2020-10-12 13:32:10 +0200

    place chloropleth map at bottom of page

--- src/pages/landelijk/ziekenhuis-opnames.tsx
+++ src/pages/landelijk/ziekenhuis-opnames.tsx
@@ -75,6 +75,36 @@ const IntakeHospital: FCWithLayout<INationalData> = (props) => {
         </article>
       </div>
 
+      <article className="metric-article">
+        <LineChart
+          title={text.linechart_titel}
+          values={dataIntake.values.map((value: any) => ({
+            value: value.moving_average_hospital,
+            date: value.date_of_report_unix,
+          }))}
+          signaalwaarde={40}
+        />
+        <footer className="article-footer">
+          {siteText.common.metadata.source}:{' '}
+          <a href={text.bronnen.rivm.href}>{text.bronnen.rivm.text}</a>
+        </footer>
+      </article>
+
+      <article className="metric-article">
+        <LineChart
+          title={text.chart_bedbezetting.title}
+          description={text.chart_bedbezetting.description}
+          values={dataBeds.values.map((value) => ({
+            value: value.covid_occupied,
+            date: value.date_of_report_unix,
+          }))}
+        />
+        <footer className="article-footer">
+          {siteText.common.metadata.source}:{' '}
+          <a href={text.bronnen.lnaz.href}>{text.bronnen.lnaz.text}</a>
+        </footer>
+      </article>
+
       <article className="metric-article layout-chloropleth">
         <div className="chloropleth-header">
           <h3>{text.map_titel}</h3>
@@ -118,36 +148,6 @@ const IntakeHospital: FCWithLayout<INationalData> = (props) => {
         <footer className="chloropleth-footer">
           {siteText.common.metadata.source}:{' '}
           <a href={text.bronnen.rivm.href}>{text.bronnen.rivm.text}</a>
-        </footer>
-      </article>
-
-      <article className="metric-article">
-        <LineChart
-          title={text.linechart_titel}
-          values={dataIntake.values.map((value: any) => ({
-            value: value.moving_average_hospital,
-            date: value.date_of_report_unix,
-          }))}
-          signaalwaarde={40}
-        />
-        <footer className="article-footer">
-          {siteText.common.metadata.source}:{' '}
-          <a href={text.bronnen.rivm.href}>{text.bronnen.rivm.text}</a>
-        </footer>
-      </article>
-
-      <article className="metric-article">
-        <LineChart
-          title={text.chart_bedbezetting.title}
-          description={text.chart_bedbezetting.description}
-          values={dataBeds.values.map((value) => ({
-            value: value.covid_occupied,
-            date: value.date_of_report_unix,
-          }))}
-        />
-        <footer className="article-footer">
-          {siteText.common.metadata.source}:{' '}
-          <a href={text.bronnen.lnaz.href}>{text.bronnen.lnaz.text}</a>
         </footer>
       </article>
     </>
