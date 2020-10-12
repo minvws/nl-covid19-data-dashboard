af5c793da55a08f1bfa7d77047ca916aeeb7cc76
T <71320230+VWSCoronaDashboard8@users.noreply.github.com>
2020-10-12 16:55:10 +0200

    Rename use throttle parameter

--- src/utils/useThrottle.ts
+++ src/utils/useThrottle.ts
@@ -1,19 +1,19 @@
 import { useState, useEffect, useRef } from 'react';
 
-export function useThrottle<T>(value: T, intervalMs: number): T {
+export function useThrottle<T>(value: T, delayMs: number): T {
   const [throttledValue, setThrottledValue] = useState<T>(value);
   const lastRan = useRef(Date.now());
 
   useEffect(() => {
     const handler = window.setTimeout(() => {
-      if (Date.now() - lastRan.current >= intervalMs) {
+      if (Date.now() - lastRan.current >= delayMs) {
         setThrottledValue(value);
         lastRan.current = Date.now();
       }
-    }, intervalMs - (Date.now() - lastRan.current));
+    }, delayMs - (Date.now() - lastRan.current));
 
     return () => window.clearTimeout(handler);
-  }, [value, intervalMs]);
+  }, [value, delayMs]);
 
   return throttledValue;
 }
