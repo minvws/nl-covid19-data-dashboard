34ddf3ae1a1e94d6bafcfb8a0934058478e1b107
T <71320230+VWSCoronaDashboard8@users.noreply.github.com>
2020-10-15 10:32:44 +0200

    Clean up other tooltips

--- src/components/chloropleth/tooltips/region/createRegionHospitalAdmissionsTooltip.tsx
+++ src/components/chloropleth/tooltips/region/createRegionHospitalAdmissionsTooltip.tsx
@@ -1,8 +1,9 @@
 import { NextRouter } from 'next/router';
 import { ReactNode } from 'react';
-import { SafetyRegionProperties } from '~/components/chloropleth/shared';
 import { createSelectRegionHandler } from '~/components/chloropleth/selectHandlers/createSelectRegionHandler';
+import { SafetyRegionProperties } from '~/components/chloropleth/shared';
 import { TooltipContent } from '~/components/chloropleth/tooltips/tooltipContent';
+import { formatNumber } from '~/utils/formatNumber';
 
 export const createRegionHospitalAdmissionsTooltip = (router: NextRouter) => (
   context: SafetyRegionProperties & { value: number }
@@ -17,7 +18,7 @@ export const createRegionHospitalAdmissionsTooltip = (router: NextRouter) => (
   return (
     context && (
       <TooltipContent title={context.vrname} onSelect={onSelect}>
-        <strong>{context.value !== undefined ? context.value : '-'}</strong>
+        <strong>{formatNumber(context.value)}</strong>
       </TooltipContent>
     )
   );
