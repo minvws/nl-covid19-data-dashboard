05e12c4e7ed32eecd8eec90e4c2b852d8a72156f
VWS Corona Dashboard 5 <70140495+VWSCoronaDashboard5@users.noreply.github.com>
2020-10-19 13:26:28 +0200

    remove barscale from page

--- src/pages/landelijk/verpleeghuis-sterfte.tsx
+++ src/pages/landelijk/verpleeghuis-sterfte.tsx
@@ -1,6 +1,6 @@
 import CoronaVirus from '~/assets/coronavirus.svg';
 import { LineChart } from '~/components/charts/index';
-import { NursingHomeDeathsBarScale } from '~/components/common/nursing-home-deaths-barscale';
+import { formatNumber } from '~/utils/formatNumber';
 import { FCWithLayout } from '~/components/layout';
 import { ContentHeader } from '~/components/layout/Content';
 import { getNationalLayout } from '~/components/layout/NationalLayout';
@@ -36,10 +36,11 @@ const NursingHomeDeaths: FCWithLayout<INationalData> = (props) => {
         <div className="column-item column-item-extra-margin">
           <h3>{text.barscale_titel}</h3>
 
-          <NursingHomeDeathsBarScale
-            value={data.last_value.deceased_daily}
-            showAxis={true}
-          />
+          <h3>
+            <span className="text-blue kpi" data-cy="infected_daily_total">
+              {formatNumber(data.last_value.deceased_daily)}
+            </span>
+          </h3>
         </div>
 
         <div className="column-item column-item-extra-margin">
