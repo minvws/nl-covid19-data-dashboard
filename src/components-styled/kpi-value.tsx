6a87ae3096e7199a2721a04d8d88b39e6ace8c94
T <71320230+VWSCoronaDashboard8@users.noreply.github.com>
2020-10-19 19:23:59 +0200

    Fix kpi color

--- src/components-styled/kpi-value.tsx
+++ src/components-styled/kpi-value.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { color } from 'styled-system';
 import { isDefined } from 'ts-is-present';
 import { formatNumber, formatPercentage } from '~/utils/formatNumber';
 
@@ -9,18 +10,22 @@ interface KpiValueProps {
 
 /**
  * When we need to style something specific there is no real reason to use the
- * box component and we can style everything directly.
+ * box component and we can style everything directly. Only you can not access
+ * theme variables from the style object, so in this case color need to be set
+ * using a prop.
  */
-const StyledValue = styled.div({
-  /**
-   * This font size is only used in KPI value so left out of them array because
-   * it would mess with the consecutive order of headings.
-   */
-  fontSize: '1.80203rem',
-  fontWeight: 600,
-  fontVariantNumeric: 'tabular-nums',
-  color: 'blue',
-});
+const StyledValue = styled.div(
+  {
+    /**
+     * This font size is only used in KPI value so left out of them array because
+     * it would mess with the consecutive order of headings.
+     */
+    fontSize: '1.80203rem',
+    fontWeight: 600,
+    fontVariantNumeric: 'tabular-nums',
+  },
+  color
+);
 
 /**
  * Display a blue KPI value with optionally a percentage behind it.
@@ -28,11 +33,11 @@ const StyledValue = styled.div({
 export function KpiValue({ absolute, percentage }: KpiValueProps) {
   if (isDefined(percentage)) {
     return (
-      <StyledValue>
+      <StyledValue color="blue">
         {`${formatNumber(absolute)} (${formatPercentage(percentage)}%)`}
       </StyledValue>
     );
   } else {
-    return <StyledValue>{formatNumber(absolute)}</StyledValue>;
+    return <StyledValue color="blue">{formatNumber(absolute)}</StyledValue>;
   }
 }
