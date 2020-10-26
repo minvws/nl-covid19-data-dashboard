d5504d8b536e217b5498e000f4235e8880632d79
VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2020-10-26 15:28:30 +0100

    Remove lodash/get in favor of just manually resolving the data

--- src/components/chloropleth/legenda/hooks/useSafetyRegionLegendaData.ts
+++ src/components/chloropleth/legenda/hooks/useSafetyRegionLegendaData.ts
@@ -7,8 +7,6 @@ import {
 import { useLegendaItems } from './useLegendaItems';
 import { regionThresholds } from '~/components/chloropleth/regionThresholds';
 
-import { get } from 'lodash';
-
 export function getSelectedThreshold(
   metricName?: TRegionMetricName,
   metricValueName?: string
@@ -20,10 +18,10 @@ export function getSelectedThreshold(
   // Even if a metricValueName is passed in, there's not necessarily
   // a threshold defined for this. In that case we fall back to the threshold
   // that exists for the metric name.
-  const thresholdInfo = metricValueName
-    ? get(regionThresholds, `${metricName}.${metricValueName}`) ??
-      regionThresholds[metricName]
-    : regionThresholds[metricName];
+  const thresholdInfo =
+    (metricValueName
+      ? (regionThresholds as any)?.[metricName]?.[metricValueName]
+      : regionThresholds[metricName]) ?? regionThresholds[metricName];
 
   return thresholdInfo as ChoroplethThresholds;
 }
