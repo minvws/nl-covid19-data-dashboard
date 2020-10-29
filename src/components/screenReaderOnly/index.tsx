6261a387a456c0e3f96ab93090409b58d2d08201
VWSCoronaDashboard5 <70140495+VWSCoronaDashboard5@users.noreply.github.com>
2020-10-29 13:37:25 +0100

    replace WithChildren type with ReactNode (#758)
    * replace WithChildren type with ReactNode
    
    * pr feedback
    
    * resolve errors

--- src/components/screenReaderOnly/index.tsx
+++ src/components/screenReaderOnly/index.tsx
@@ -1,11 +1,15 @@
-import { WithChildren } from '~/types/index';
 import styles from './screenReaderOnly.module.scss';
+
+interface IProps {
+  children: React.ReactNode;
+}
 
 /**
  * A small utility component that renders text which is only
  * present for visually impaired users, alias it will be read out
  * loud by screenreaders without being visible on the screen.
  */
-export function ScreenReaderOnly({ children }: WithChildren) {
+export function ScreenReaderOnly(props: IProps) {
+  const { children } = props;
   return <span className={styles.screenReaderOnly}>{children}</span>;
 }
