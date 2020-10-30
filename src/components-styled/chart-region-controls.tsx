479f72f086f4a9801f658521e731ff7f1e5a6960
K <73584448+VWSCoronaDashboard10@users.noreply.github.com>
2020-10-30 11:34:56 +0100

    Prevent unnecessary re-renders

--- src/components-styled/chart-region-controls.tsx
+++ src/components-styled/chart-region-controls.tsx
@@ -6,19 +6,19 @@ export interface ChartRegionControlsProps {
   onChange: (value: 'municipal' | 'region') => void;
 }
 
+const items = [
+  {
+    label: text.charts.region_controls.municipal,
+    value: 'municipal',
+  },
+  {
+    label: text.charts.region_controls.region,
+    value: 'region',
+  },
+];
+
 export function ChartRegionControls(props: ChartRegionControlsProps) {
   const { onChange } = props;
-
-  const items = [
-    {
-      label: text.charts.region_controls.municipal,
-      value: 'municipal',
-    },
-    {
-      label: text.charts.region_controls.region,
-      value: 'region',
-    },
-  ];
 
   return <RadioGroup items={items} onChange={onChange} />;
 }
