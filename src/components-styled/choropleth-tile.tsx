2718082eadc81452939d679684a48ee585245c30
K <73584448+VWSCoronaDashboard10@users.noreply.github.com>
2020-11-02 16:48:10 +0100

    Fix choropleth rendering for both IE as other browsers

--- src/components-styled/choropleth-tile.tsx
+++ src/components-styled/choropleth-tile.tsx
@@ -2,6 +2,7 @@ import {
   ChoroplethLegenda,
   ILegendaItem,
 } from '~/components/choropleth/legenda/ChoroplethLegenda';
+import { useBreakpoints } from '~/utils/useBreakpoints';
 import { Box } from './base';
 import {
   ChartRegionControls,
@@ -22,7 +23,7 @@ interface DataProps {
 
 interface ChoroplethTileProps extends DataProps {
   title: string;
-  description?: string;
+  description?: string | React.ReactNode;
   onChangeControls?: (v: RegionControlOption) => void;
   children: React.ReactNode;
   legend?: {
@@ -38,36 +39,46 @@ export function ChoroplethTile<T>({
   legend,
   children,
 }: ChoroplethTileProps) {
+  const breakpoints = useBreakpoints();
+  const legendComponent = legend && (
+    <ChoroplethLegenda items={legend.items} title={legend.title} />
+  );
+
   return (
-    <Tile
-      mb={4}
-      ml={{ _: -4, sm: 0 }}
-      mr={{ _: -4, sm: 0 }}
-      display={{ _: 'flex', lg: 'grid' }}
-      gridTemplateColumns={'1fr 1fr'}
-      gridTemplateRows={'auto auto 1fr auto'}
-      gridTemplateAreas={`'w w' 'a c' 'b c' 'd c'`}
-    >
-      <Box gridArea="a" mb={[0, 2]}>
-        <Heading level={3}>{title}</Heading>
-        {description && <Text>{description}</Text>}
-        {onChangeControls && (
-          <Box display="flex" justifyContent="flex-start">
-            <ChartRegionControls onChange={onChangeControls} />
-          </Box>
-        )}
-      </Box>
-      <Box gridArea="c">{children}</Box>
-      {legend && (
-        <Box
-          gridArea="b"
-          display="flex"
-          flexDirection="column"
-          alignItems={{ _: 'center', lg: 'flex-start' }}
-        >
-          <ChoroplethLegenda items={legend.items} title={legend.title} />
+    <Tile mb={4} ml={{ _: -4, sm: 0 }} mr={{ _: -4, sm: 0 }}>
+      <Box display="flex" flexDirection={{ _: 'column', lg: 'row' }}>
+        <Box flex={{ lg: 1 }}>
+          <div>
+            <Box mb={[0, 2]}>
+              <Heading level={3}>{title}</Heading>
+              {typeof description === 'string' ? (
+                <Text>{description}</Text>
+              ) : (
+                description
+              )}
+              {onChangeControls && (
+                <Box display="flex" justifyContent="flex-start">
+                  <ChartRegionControls onChange={onChangeControls} />
+                </Box>
+              )}
+            </Box>
+            {legendComponent && breakpoints.lg && (
+              <Box display="flex" flexDirection="row" alignItems="flex-center">
+                {legendComponent}
+              </Box>
+            )}
+          </div>
         </Box>
-      )}
+        <Box flex={{ lg: 1 }}>
+          <div>{children}</div>
+
+          {legendComponent && !breakpoints.lg && (
+            <Box display="flex" justifyContent="center">
+              {legendComponent}
+            </Box>
+          )}
+        </Box>
+      </Box>
     </Tile>
   );
 }
