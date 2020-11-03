42d892333a6040158a14330760e307991165e510
VWS Corona Dashboard 5 <70140495+VWSCoronaDashboard5@users.noreply.github.com>
2020-11-03 10:35:39 +0100

    remove Number casting from props

--- src/components/landelijk/sewer-water-metric.tsx
+++ src/components/landelijk/sewer-water-metric.tsx
@@ -16,14 +16,14 @@ export function SewerWaterMetric(props: {
   if (data === undefined) return null;
 
   const description = replaceVariablesInText(text.dateRangeOfReport, {
-    startDate: formatDateFromSeconds(data?.last_value.week_start_unix, 'axis'),
-    endDate: formatDateFromSeconds(data?.last_value.week_end_unix, 'axis'),
+    startDate: formatDateFromSeconds(data.last_value.week_start_unix, 'axis'),
+    endDate: formatDateFromSeconds(data.last_value.week_end_unix, 'axis'),
   });
 
   return (
     <MetricKPI
       title={title}
-      value={Number(data.last_value.average)}
+      value={data.last_value.average}
       format={formatNumber}
       description={description}
     />
