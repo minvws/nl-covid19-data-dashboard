b0b858abd4d9d1e2fca3f3e73def3f18e66f12f8
VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2020-11-05 14:01:14 +0100

    Add index file with all restriction icon imports

---
+++ src/assets/restrictions/index.ts
@@ -0,0 +1,199 @@
+import AdviseringROTIcon from './advisering ROT.svg';
+import AfstandHoudenIcon from './afstand houden.svg';
+import ApothekerIcon from './apotheker.svg';
+import ArtsIcon from './arts.svg';
+import BacterieDodendeGelIcon from './bacterie dodende gel.svg';
+import BakkerijketensIcon from './bakkerijketens.svg';
+import BasisKantoorgebouwIcon from './basis kantoorgebouw.svg';
+import BasisonderwijsIcon from './basisonderwijs.svg';
+import BegeleidingGehandicaptenIcon from './begeleiding gehandicapten.svg';
+import BenzinestationIcon from './benzinestation.svg';
+import BetaalbaarheidIcon from './betaalbaarheid.svg';
+import BudgetIcon from './budget.svg';
+import BuitenzwembadIcon from './buitenzwembad.svg';
+import BusIcon from './bus.svg';
+import CafeIcon from './cafe.svg';
+import CampingIcon from './camping.svg';
+import CarpoolenIcon from './carpoolen.svg';
+import CoronavirusIcon from './coronavirus.svg';
+import DirecteBelastingenIcon from './directe belastingen.svg';
+import DNAAfnameNeusIcon from './DNA afname neus.svg';
+import DNAAfnameIcon from './DNA afname.svg';
+import DuinIcon from './duin.svg';
+import ElleboogniesIcon from './elleboognies.svg';
+import FestivaltentIcon from './festivaltent.svg';
+import FinancienIcon from './financien.svg';
+import FitgesprekIcon from './fitgesprek.svg';
+import FitnessIcon from './fitness.svg';
+import FlexibelWerkenIcon from './flexibel werken.svg';
+import GeenHandenSchuddenIcon from './geen handen schudden.svg';
+import GemeenteIcon from './gemeente.svg';
+import GezinIcon from './gezin.svg';
+import GezondheidIcon from './gezondheid.svg';
+import GolvenIcon from './golven.svg';
+import GroentenEnFruitIcon from './groenten en fruit.svg';
+import HandenWassenIcon from './handen wassen.svg';
+import HuisvestingIcon from './huisvesting.svg';
+import IjssalonIcon from './ijssalon.svg';
+import IndoorSpeelhallenIcon from './indoor speelhallen.svg';
+import InentingIcon from './inenting.svg';
+import InternetaankopenIcon from './internetaankopen.svg';
+import InvesterenIcon from './investeren.svg';
+import KalenderIcon from './kalender.svg';
+import KapperIcon from './kapper.svg';
+import KindIcon from './kind.svg';
+import KinderenIcon from './kinderen.svg';
+import LandbouwIcon from './landbouw.svg';
+import LaptopIcon from './laptop.svg';
+import LeegWinkelschapIcon from './leeg winkelschap.svg';
+import MedischeHulpmiddelenIcon from './medische hulpmiddelen.svg';
+import OndernemingenIcon from './ondernemingen.svg';
+import OndernemingsklimaatIcon from './ondernemingsklimaat.svg';
+import OndersteuningIcon from './ondersteuning.svg';
+import OppervlaktewaterIcon from './oppervlaktewater.svg';
+import OudeManIcon from './oude man.svg';
+import OudeVrouwIcon from './oude vrouw.svg';
+import ParkIcon from './park.svg';
+import PlantenEnBloemenIcon from './planten en bloemen.svg';
+import PolitieIcon from './politie.svg';
+import PretparkIcon from './pretpark.svg';
+import ProductenEnLevensmiddelenIcon from './producten en levensmiddelen.svg';
+import PubliekIcon from './publiek.svg';
+import ReageerbuisIcon from './reageerbuis.svg';
+import RecreatieIcon from './recreatie.svg';
+import ReisbagageIcon from './reisbagage.svg';
+import ReisdocumentIcon from './reisdocument.svg';
+import RestaurantIcon from './restaurant.svg';
+import RolstoelToegankelijkeWoningIcon from './rolstoel toegankelijke woning.svg';
+import SportIcon from './sport.svg';
+import SupermarktketensIcon from './supermarktketens.svg';
+import TerrasIcon from './terras.svg';
+import ThematischeFinancieringIcon from './thematische financiering.svg';
+import ThuisWerkenIcon from './thuis werken.svg';
+import ThuisblijvenIcon from './thuisblijven.svg';
+import ThuisonderwijsIcon from './thuisonderwijs.svg';
+import TissueboxIcon from './tissuebox.svg';
+import ToerismeIcon from './toerisme.svg';
+import TreinIcon from './trein.svg';
+import UitvalPersoneelGrieppandemieIcon from './uitval personeel grieppandemie.svg';
+import VeiligheidsregioIcon from './veiligheidsregio.svg';
+import VerloskundigeIcon from './verloskundige.svg';
+import VerpleegkundigeIcon from './verpleegkundige.svg';
+import VervelingIcon from './verveling.svg';
+import VerzorgingstehuisIcon from './verzorgingstehuis.svg';
+import VideovergaderenIcon from './videovergaderen.svg';
+import VliegtuigIcon from './vliegtuig.svg';
+import VrouwMetMondkapjeIcon from './vrouw met mondkapje.svg';
+import WerkEnInkomenIcon from './werk en inkomen.svg';
+import WijkagentIcon from './wijkagent.svg';
+import WinkelwagenIcon from './winkelwagen.svg';
+import ZelfMondkapjesMakenIcon from './zelf mondkapjes maken.svg';
+import ZiekenhuisIcon from './ziekenhuis.svg';
+import ZiekteEnHerstelIcon from './ziekte en herstel.svg';
+import ZiektegolfIcon from './ziektegolf.svg';
+import ZintuiglijkIcon from './zintuiglijk.svg';
+import ZonnestudiosIcon from './zonnestudios.svg';
+import ZorgkantoorIcon from './zorgkantoor.svg';
+import ZwangereVrouwIcon from './zwangere vrouw.svg';
+import ZzpIcon from './zzp.svg';
+
+export {
+  AdviseringROTIcon,
+  AfstandHoudenIcon,
+  ApothekerIcon,
+  ArtsIcon,
+  BacterieDodendeGelIcon,
+  BakkerijketensIcon,
+  BasisKantoorgebouwIcon,
+  BasisonderwijsIcon,
+  BegeleidingGehandicaptenIcon,
+  BenzinestationIcon,
+  BetaalbaarheidIcon,
+  BudgetIcon,
+  BuitenzwembadIcon,
+  BusIcon,
+  CafeIcon,
+  CampingIcon,
+  CarpoolenIcon,
+  CoronavirusIcon,
+  DirecteBelastingenIcon,
+  DNAAfnameNeusIcon,
+  DNAAfnameIcon,
+  DuinIcon,
+  ElleboogniesIcon,
+  FestivaltentIcon,
+  FinancienIcon,
+  FitgesprekIcon,
+  FitnessIcon,
+  FlexibelWerkenIcon,
+  GeenHandenSchuddenIcon,
+  GemeenteIcon,
+  GezinIcon,
+  GezondheidIcon,
+  GolvenIcon,
+  GroentenEnFruitIcon,
+  HandenWassenIcon,
+  HuisvestingIcon,
+  IjssalonIcon,
+  IndoorSpeelhallenIcon,
+  InentingIcon,
+  InternetaankopenIcon,
+  InvesterenIcon,
+  KalenderIcon,
+  KapperIcon,
+  KindIcon,
+  KinderenIcon,
+  LandbouwIcon,
+  LaptopIcon,
+  LeegWinkelschapIcon,
+  MedischeHulpmiddelenIcon,
+  OndernemingenIcon,
+  OndernemingsklimaatIcon,
+  OndersteuningIcon,
+  OppervlaktewaterIcon,
+  OudeManIcon,
+  OudeVrouwIcon,
+  ParkIcon,
+  PlantenEnBloemenIcon,
+  PolitieIcon,
+  PretparkIcon,
+  ProductenEnLevensmiddelenIcon,
+  PubliekIcon,
+  ReageerbuisIcon,
+  RecreatieIcon,
+  ReisbagageIcon,
+  ReisdocumentIcon,
+  RestaurantIcon,
+  RolstoelToegankelijkeWoningIcon,
+  SportIcon,
+  SupermarktketensIcon,
+  TerrasIcon,
+  ThematischeFinancieringIcon,
+  ThuisWerkenIcon,
+  ThuisblijvenIcon,
+  ThuisonderwijsIcon,
+  TissueboxIcon,
+  ToerismeIcon,
+  TreinIcon,
+  UitvalPersoneelGrieppandemieIcon,
+  VeiligheidsregioIcon,
+  VerloskundigeIcon,
+  VerpleegkundigeIcon,
+  VervelingIcon,
+  VerzorgingstehuisIcon,
+  VideovergaderenIcon,
+  VliegtuigIcon,
+  VrouwMetMondkapjeIcon,
+  WerkEnInkomenIcon,
+  WijkagentIcon,
+  WinkelwagenIcon,
+  ZelfMondkapjesMakenIcon,
+  ZiekenhuisIcon,
+  ZiekteEnHerstelIcon,
+  ZiektegolfIcon,
+  ZintuiglijkIcon,
+  ZonnestudiosIcon,
+  ZorgkantoorIcon,
+  ZwangereVrouwIcon,
+  ZzpIcon,
+};
