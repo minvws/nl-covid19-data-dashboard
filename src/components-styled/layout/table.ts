8d5d7c7d5d22b41f5767e0fab6db8b6dc5b5945e
VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2020-11-05 22:05:38 +0100

    Tweaking the restrictions table

--- src/components-styled/layout/table.ts
+++ src/components-styled/layout/table.ts
@@ -5,18 +5,32 @@ import {
   padding,
   color,
   space,
+  position,
+  border,
   SpaceProps,
+  BorderProps,
   LayoutProps,
   ColorProps,
   PositionProps,
 } from 'styled-system';
 
-type TableProps = SpaceProps & LayoutProps & ColorProps & PositionProps;
+type TableProps = TableCellProps & PositionProps;
+
+type TableCellProps = TableBodyProps & ColorProps & BorderProps;
+
+type TableBodyProps = SpaceProps & LayoutProps;
 
 export const Table = styled.table<TableProps>(
-  compose(margin, padding, color, space)
+  { borderCollapse: 'collapse', borderSpacing: 0 },
+  compose(margin, padding, color, space, position, border)
 );
 
-export const Tr = styled.tr<TableProps>(compose(margin, padding, color, space));
+export const TableBody = styled.tbody<TableBodyProps>(
+  compose(margin, padding, space)
+);
 
-export const Td = styled.td<TableProps>(compose(margin, padding, color, space));
+export const Row = styled.tr<ColorProps>(compose(color));
+
+export const Cell = styled.td<TableCellProps>(
+  compose(margin, padding, color, space, border)
+);
--- src/components/restrictions/restrictionsTable.tsx
+++ src/components/restrictions/restrictionsTable.tsx
@@ -1,5 +1,5 @@
 import { Box } from '~/components-styled/base';
-import { Table, Td, Tr } from '~/components-styled/layout/table';
+import { Table, Cell, Row, TableBody } from '~/components-styled/layout/table';
 import { Text } from '~/components-styled/typography';
 import { RestrictionsTableData } from '~/utils/useRestrictionsTable';
 import siteText from '~/locale/index';
@@ -14,14 +14,27 @@ export function RestrictionsTable(props: RestrictionsTableProps) {
   const { data } = props;
 
   return (
-    <Table>
-      <tbody>
+    <Table width="100%">
+      <TableBody>
         {data.rows.map((row) => (
-          <Tr key={row.category}>
-            <Td>
-              <Text fontWeight="bold">{text[row.category]}</Text>
-            </Td>
-            <Td>
+          <Row key={row.category}>
+            <Cell
+              borderTop={'1px solid black'}
+              width="20em"
+              pt={3}
+              pb={3}
+              verticalAlign="top"
+            >
+              <Text as="span" fontWeight="bold">
+                {text[row.category]}
+              </Text>
+            </Cell>
+            <Cell
+              borderTop={'1px solid black'}
+              pt={3}
+              pb={3}
+              verticalAlign="top"
+            >
               <Box display="flex" flexDirection="column">
                 {row.restrictions.map((value) => (
                   <Box key={value.text} display="flex" flexDirection="row">
@@ -30,10 +43,10 @@ export function RestrictionsTable(props: RestrictionsTableProps) {
                   </Box>
                 ))}
               </Box>
-            </Td>
-          </Tr>
+            </Cell>
+          </Row>
         ))}
-      </tbody>
+      </TableBody>
     </Table>
   );
 }
