c67901735186cc91e4b615c23812ba7b58a5bbad
K <73584448+VWSCoronaDashboard10@users.noreply.github.com>
2020-11-11 12:37:03 +0100

    Add margins to behavior table tile

--- src/domain/behavior/behavior-table-tile.tsx
+++ src/domain/behavior/behavior-table-tile.tsx
@@ -125,7 +125,23 @@ export function BehaviorTableTile({ text, behavior }: BehaviorTileProps) {
   );
 
   return (
-    <Tile>
+    <Tile
+      /**
+       * The mb here could alternatively be applied using a <Spacer/> in the
+       * page markup. It's a choice, whether we like to include the bottom
+       * margin on all our commonly used components or keep everything flexible
+       * and use spacers in the context where the component is used.
+       */
+      mb={4}
+      /**
+       * The ml and mr negative margins should not be part of this component
+       * ideally, but are the results of the page layout having paddings even on
+       * small screens. We can remove this once we make all page section
+       * elements full-width and remove the padding from the page layout.
+       */
+      ml={{ _: -4, sm: 0 }}
+      mr={{ _: -4, sm: 0 }}
+    >
       <h3>{text.basisregels.title}</h3>
       <Box display="flex" justifyContent="start">
         <BehaviorTypeControls onChange={setBehaviorType} />
