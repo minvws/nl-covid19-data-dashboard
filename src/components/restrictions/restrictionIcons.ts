dba0a3dfca9e91bcf42b27ba222455134310b58e
VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2020-11-12 14:32:36 +0100

    Remove unused restriction icons Create lookup for icons

---
+++ src/components/restrictions/restrictionIcons.ts
@@ -0,0 +1,142 @@
+import AlcoholVerbodIcon from '~/assets/restrictions/AlcoholVerbod.svg';
+import AutoVooraanzichtIcon from '~/assets/restrictions/AutoVooraanzicht.svg';
+import Bezoek1Icon from '~/assets/restrictions/Bezoek1.svg';
+import CafeIcon from '~/assets/restrictions/Cafe.svg';
+import GeenBijeenkomstenIcon from '~/assets/restrictions/GeenBijeenkomsten.svg';
+import GeenEvenementenIcon from '~/assets/restrictions/GeenEvenementen.svg';
+import Groep2PersonenIcon from '~/assets/restrictions/Groep2Personen.svg';
+import HorecaGeslotenIcon from '~/assets/restrictions/HorecaGesloten.svg';
+import KlembordMetVinkjesEnLijnenIcon from '~/assets/restrictions/KlembordMetVinkjesEnLijnen.svg';
+import KlokIcon from '~/assets/restrictions/Klok.svg';
+import LaptopIcon from '~/assets/restrictions/Laptop.svg';
+import NietReizenIcon from '~/assets/restrictions/NietReizenIcon.svg';
+import OudeVrouwIcon from '~/assets/restrictions/OudeVrouw.svg';
+import Personen2Icon from '~/assets/restrictions/Personen2.svg';
+import Personen3Icon from '~/assets/restrictions/Personen3.svg';
+import Personen4Icon from '~/assets/restrictions/Personen4.svg';
+import Personen6Icon from '~/assets/restrictions/Personen6.svg';
+import ReisbagageIcon from '~/assets/restrictions/Reisbagage.svg';
+import RugzakIcon from '~/assets/restrictions/Rugzak.svg';
+import ThuisblijvenIcon from '~/assets/restrictions/Thu.svg';
+import VinkjeIcon from '~/assets/restrictions/V.svg';
+import VerbodSportactiviteitenIcon from '~/assets/restrictions/VerbodSportactiviteiten.svg';
+import VerzorgingstehuisIcon from '~/assets/restrictions/Verzorgingstehuis.svg';
+import VrouwMetMondkapjeIcon from '~/assets/restrictions/VrouwMetMondkapje.svg';
+import WinkelwagenIcon from '~/assets/restrictions/Winkelwagen.svg';
+
+export const restrictionIcons: Record<string, any> = {
+  '4_er_op_uit_1': Personen4Icon,
+  '4_bezoek_2': Personen3Icon,
+  '4_bezoek_3': Bezoek1Icon,
+  '4_samenkomst_4': Groep2PersonenIcon,
+  '4_samenkomst_5': GeenEvenementenIcon,
+  '4_samenkomst_6': '',
+  '4_huwelijk_7': Groep2PersonenIcon,
+  '4_verpleeghuis_8': VerzorgingstehuisIcon,
+  '4_horeca_9': HorecaGeslotenIcon,
+  '4_horeca_10': '',
+  '4_sport_11': '',
+  '4_sport_12': '',
+  '4_sport_13': '',
+  '4_reizen_binnenland_14': AutoVooraanzichtIcon,
+  '4_reizen_buitenland_15': ReisbagageIcon,
+  '4_ov_16': VrouwMetMondkapjeIcon,
+  '4_uitvaart_17': Groep2PersonenIcon,
+  '4_onderwijs_18': RugzakIcon,
+  '4_werk_19': LaptopIcon,
+  '4_winkels_20': KlokIcon,
+  '4_winkels_21': OudeVrouwIcon,
+  '4_alcohol_22': AlcoholVerbodIcon,
+  '4_alcohol_23': '',
+  '1_er_op_uit_32': VinkjeIcon,
+  '1_bezoek_33': VinkjeIcon,
+  '1_samenkomst_34': KlembordMetVinkjesEnLijnenIcon,
+  '1_huwelijk_35': VinkjeIcon,
+  '1_verpleeghuis_36': VerzorgingstehuisIcon,
+  '1_horeca_37': KlembordMetVinkjesEnLijnenIcon,
+  '1_sport_38': VinkjeIcon,
+  '1_reizen_binnenland_39': VinkjeIcon,
+  '1_reizen_buitenland_40': ReisbagageIcon,
+  '1_ov_41': Personen3Icon,
+  '1_uitvaart_42': VrouwMetMondkapjeIcon,
+  '1_onderwijs_43': RugzakIcon,
+  '1_werk_44': LaptopIcon,
+  '1_winkels_45': VrouwMetMondkapjeIcon,
+  '1_alcohol_46': VinkjeIcon,
+  '2_er_op_uit_47': VinkjeIcon,
+  '2_bezoek_48': Personen6Icon,
+  '2_samenkomst_49': Groep2PersonenIcon,
+  '2_huwelijk_50': Groep2PersonenIcon,
+  '2_verpleeghuis_51': VerzorgingstehuisIcon,
+  '2_horeca_52': HorecaGeslotenIcon,
+  '2_sport_53': VinkjeIcon,
+  '2_reizen_binnenland_54': AutoVooraanzichtIcon,
+  '2_reizen_buitenland_55': ReisbagageIcon,
+  '2_ov_56': VrouwMetMondkapjeIcon,
+  '2_uitvaart_57': Groep2PersonenIcon,
+  '2_onderwijs_58': RugzakIcon,
+  '2_werk_59': LaptopIcon,
+  '2_winkels_60': WinkelwagenIcon,
+  '2_winkels_61': OudeVrouwIcon,
+  '2_alcohol_62': KlokIcon,
+  '3_er_op_uit_63': Personen4Icon,
+  '3_bezoek_64': Personen3Icon,
+  '3_samenkomst_65': Groep2PersonenIcon,
+  '3_huwelijk_66': Groep2PersonenIcon,
+  '3_verpleeghuis_67': VerzorgingstehuisIcon,
+  '3_horeca_68': HorecaGeslotenIcon,
+  '3_sport_69': GeenBijeenkomstenIcon,
+  '3_reizen_binnenland_70': AutoVooraanzichtIcon,
+  '3_reizen_buitenland_71': ReisbagageIcon,
+  '3_ov_72': VrouwMetMondkapjeIcon,
+  '3_uitvaart_73': Groep2PersonenIcon,
+  '3_onderwijs_74': RugzakIcon,
+  '3_werk_75': LaptopIcon,
+  '3_winkels_76': KlokIcon,
+  '3_winkels_77': OudeVrouwIcon,
+  '3_alcohol_78': VinkjeIcon,
+  '4_er_op_uit_79': '',
+  '4_bezoek_80': '',
+  '4_bezoek_81': '',
+  '4_samenkomst_82': '',
+  '4_samenkomst_83': '',
+  '4_samenkomst_84': '',
+  '4_huwelijk_85': '',
+  '4_verpleeghuis_86': '',
+  '4_horeca_87': '',
+  '4_horeca_88': '',
+  '4_sport_89': '',
+  '4_sport_90': '',
+  '4_sport_91': '',
+  '4_reizen_binnenland_92': '',
+  '4_reizen_buitenland_93': '',
+  '4_ov_94': '',
+  '4_uitvaart_95': '',
+  '4_onderwijs_96': '',
+  '4_werk_97': '',
+  '4_winkels_98': '',
+  '4_winkels_99': '',
+  '4_alcohol_100': '',
+  '4_alcohol_101': '',
+  '41_er_op_uit_102': Personen2Icon,
+  '41_bezoek_103': ThuisblijvenIcon,
+  '41_samenkomst_104': GeenBijeenkomstenIcon,
+  '41_samenkomst_105': GeenEvenementenIcon,
+  '41_huwelijk_106': Groep2PersonenIcon,
+  '41_verpleeghuis_107': VerzorgingstehuisIcon,
+  '41_horeca_108': HorecaGeslotenIcon,
+  '41_horeca_109': '',
+  '41_sport_110': VerbodSportactiviteitenIcon,
+  '41_sport_111': '',
+  '41_sport_112': '',
+  '41_reizen_binnenland_113': NietReizenIcon,
+  '41_reizen_buitenland_114': ReisbagageIcon,
+  '41_ov_115': VrouwMetMondkapjeIcon,
+  '41_uitvaart_116': Groep2PersonenIcon,
+  '41_onderwijs_117': LaptopIcon,
+  '41_werk_118': LaptopIcon,
+  '41_winkels_119': KlokIcon,
+  '41_winkels_120': OudeVrouwIcon,
+  '41_alcohol_121': CafeIcon,
+  '41_alcohol_122': '',
+};
