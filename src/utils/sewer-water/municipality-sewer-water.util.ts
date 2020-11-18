7ea050c37135517d2c26ec79cfa21172cd72b50f
K <73584448+VWSCoronaDashboard10@users.noreply.github.com>
2020-11-18 15:40:40 +0100

    Add sewer water getter to utils

--- src/utils/sewer-water/municipality-sewer-water.util.ts
+++ src/utils/sewer-water/municipality-sewer-water.util.ts
@@ -217,3 +217,35 @@ export function getSewerWaterBarChartData(
     ],
   };
 }
+
+export function getSewerWaterScatterPlotData(data: Municipal) {
+  const values = data.sewer_per_installation?.values.flatMap(
+    (value) => value.values
+  );
+
+  /**
+   * All individual `value.values`-arrays are already sorted correctly, but
+   * due to merging them into one array the sort might be off.
+   */
+  values?.sort((a, b) => a.date_measurement_unix - b.date_measurement_unix);
+
+  /**
+   * Only return sewer water scatter plot data when there's more than 1
+   * sewer water installation.
+   */
+  if (countUnique(values, (x) => x.rwzi_awzi_code) > 1) {
+    return values;
+  }
+}
+
+export function getInstallationNames(data: Municipal): string[] {
+  return (data.sewer_per_installation?.values || [])
+    .flatMap((value) => value.values)
+    .map((value) => value.rwzi_awzi_name)
+    .filter((value, index, arr) => arr.indexOf(value) === index)
+    .sort((a, b) => a.localeCompare(b));
+}
+
+function countUnique<T>(list: T[] = [], get: (item: T) => unknown) {
+  return list.map(get).filter((x, i, list) => list.indexOf(x) === i).length;
+}
