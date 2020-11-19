86cf4cb53030be987aeed9eaf632b31fd2c3cc58
VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2020-11-19 16:29:28 +0100

    Adjust sewerwater tooltip formatting

--- src/components/choropleth/tooltips/region/create-sewer-regional-tooltip.tsx
+++ src/components/choropleth/tooltips/region/create-sewer-regional-tooltip.tsx
@@ -1,12 +1,12 @@
 import { NextRouter } from 'next/router';
 import { ReactNode } from 'react';
 import { TooltipContent } from '~/components/choropleth/tooltips/tooltipContent';
+import siteText from '~/locale/index';
 import { RegionalSewerValue } from '~/types/data';
 import { formatNumber } from '~/utils/formatNumber';
+import { replaceVariablesInText } from '~/utils/replaceVariablesInText';
 import { createSelectRegionHandler } from '../../select-handlers/create-select-region-handler';
 import { SafetyRegionProperties } from '../../shared';
-import siteText from '~/locale/index';
-import { replaceVariablesInText } from '~/utils/replaceVariablesInText';
 
 const text = siteText.rioolwater_metingen;
 
@@ -23,9 +23,11 @@ export const createSewerRegionalTooltip = (router: NextRouter) => (
   return (
     context && (
       <TooltipContent title={context.vrname} onSelect={onSelect}>
-        <strong>{`${replaceVariablesInText(text.map_tooltip, {
+        <strong>{`${replaceVariablesInText(text.map_tooltip_value, {
           value: formatNumber(context.average),
         })}`}</strong>
+        <br />
+        {text.map_tooltip}
       </TooltipContent>
     )
   );
