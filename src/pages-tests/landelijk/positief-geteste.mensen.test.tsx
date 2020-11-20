7a3ad790208f0b32a29e4978a678249f94663459
VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2020-11-20 15:58:17 +0100

    Delete superfluous textures

--- src/pages-tests/landelijk/positief-geteste.mensen.test.tsx
+++ src/pages-tests/landelijk/positief-geteste.mensen.test.tsx
@@ -38,7 +38,7 @@ describe('National page: PositiefGetesteMensen', () => {
   });
 
   it("should show not show 'last week' option for GGD charts", () => {
-    const { getAllByText } = render(
+    const { getAllByRole } = render(
       <PositiefGetesteMensen data={data as National} lastGenerated="test" />
     );
 
@@ -46,15 +46,15 @@ describe('National page: PositiefGetesteMensen', () => {
      * Currently there are three charts shown on the positief-geteste-mensen page,
      * two of those charts should NOT have the 'Laatste week' option available since
      * the data does not allow for it.
-     * This test therefore checks if there is only ONE label element on the page
-     * with this option.
+     * This test therefore checks if there is only ONE radio element on the page
+     * with a value of 'week'.
      * This test is rather brittle, but for now the only way to do this test as a
      * way of a regression check.
      */
-    const allLabels = getAllByText('Laatste week').filter(
-      (node) => node instanceof HTMLLabelElement
+    const allRadios = getAllByRole('radio').filter(
+      (elm) => (elm as HTMLInputElement).value === 'week'
     );
 
-    expect(allLabels.length).toBe(1);
+    expect(allRadios.length).toBe(1);
   });
 });
