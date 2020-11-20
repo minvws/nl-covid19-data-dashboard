a9502116568e291482b69975fb4e575ce104800b
VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2020-11-20 13:36:52 +0100

    Remove 'last week' option from GGD multiline chart Add regression tests to make sure this will not be re-added in the future.

---
+++ src/pages-tests/landelijk/positief-geteste.mensen.test.tsx
@@ -0,0 +1,69 @@
+import fs from 'fs';
+import path from 'path';
+import React from 'react';
+import { render } from '~/test-utils/render';
+import { National } from '~/types/data';
+import { formatNumber } from '~/utils/formatNumber';
+import PositiefGetesteMensen from '../../pages/landelijk/positief-geteste-mensen';
+
+describe('National page: PositiefGetesteMensen', () => {
+  let data: National | null = null;
+
+  beforeAll(() => {
+    const content = fs.readFileSync(
+      path.join(__dirname, '../../../public/json/NL.json'),
+      { encoding: 'utf8' }
+    );
+    data = JSON.parse(content) as National;
+  });
+
+  it('should use infected_daily_total for Results per Region', () => {
+    const { getByText } = render(
+      <PositiefGetesteMensen
+        data={data as National}
+        lastGenerated="test"
+        text={{}}
+      />
+    );
+
+    const value =
+      formatNumber(
+        data?.infected_people_total.last_value.infected_daily_total
+      ) ?? '';
+
+    getByText(value);
+  });
+
+  it('should show a signaalwaarde', () => {
+    const { getByText } = render(
+      <PositiefGetesteMensen
+        data={data as National}
+        lastGenerated="test"
+        text={{}}
+      />
+    );
+
+    getByText(/signaalwaarde/i);
+  });
+
+  it("should show not show 'last week' option for GGD charts", () => {
+    const { getAllByText } = render(
+      <PositiefGetesteMensen data={data as National} lastGenerated="test" />
+    );
+
+    /**
+     * Currently there are three charts shown on the positief-geteste-mensen page,
+     * two of those charts should NOT have the 'Laatste week' option available since
+     * the data does not allow for it.
+     * This test therefore checks if there is only ONE label element on the page
+     * with this option.
+     * This test is rather brittle, but for now the only way to do this test as a
+     * way of a regression check.
+     */
+    const allLabels = getAllByText('Laatste week').filter(
+      (node) => node instanceof HTMLLabelElement
+    );
+
+    expect(allLabels.length).toBe(1);
+  });
+});
---
+++ src/pages-tests/veiligheidsregio/[code]/positief-geteste.mensen.test.tsx
@@ -0,0 +1,73 @@
+import fs from 'fs';
+import path from 'path';
+import React from 'react';
+import { render } from '~/test-utils/render';
+import { Regionaal } from '~/types/data';
+import { formatNumber } from '~/utils/formatNumber';
+import PositiefGetesteMensen from '../../../pages/veiligheidsregio/[code]/positief-geteste-mensen';
+
+describe('Safety region page: PositiefGetesteMensen', () => {
+  let data: Regionaal | null = null;
+
+  beforeAll(() => {
+    const content = fs.readFileSync(
+      path.join(__dirname, '../../../../public/json/VR13.json'),
+      { encoding: 'utf8' }
+    );
+    data = JSON.parse(content) as Regionaal;
+  });
+
+  it('should use total_reported_increase_per_region for Results per Region', () => {
+    const { getByText } = render(
+      <PositiefGetesteMensen
+        data={data as Regionaal}
+        lastGenerated="test"
+        safetyRegionName="Friesland"
+      />
+    );
+
+    const value =
+      formatNumber(
+        data?.results_per_region.last_value.total_reported_increase_per_region
+      ) ?? '';
+
+    getByText(value.toString());
+  });
+
+  it('should show a signaalwaarde', () => {
+    const { getByText } = render(
+      <PositiefGetesteMensen
+        data={data as Regionaal}
+        lastGenerated="test"
+        safetyRegionName="Friesland"
+      />
+    );
+
+    getByText(/signaalwaarde/i);
+  });
+
+  it("should show not show 'last week' option for GGD charts", () => {
+    const { getAllByText } = render(
+      <PositiefGetesteMensen
+        data={data as Regionaal}
+        lastGenerated="test"
+        safetyRegionName="Friesland"
+      />
+    );
+
+    /**
+     * Currently there are three charts shown on the positief-geteste-mensen page,
+     * two of those charts should NOT have the 'Laatste week' option available since
+     * the data does not allow for it.
+     * This test therefore checks if there is only ONE label element on the page
+     * with this option.
+     * This test is rather brittle, but for now the only way to do this test as a
+     * way of a regression check.
+     */
+    const allLabels = getAllByText('Laatste week').filter(
+      (node) => node instanceof HTMLLabelElement
+    );
+
+    expect(allLabels.length).toBe(1);
+  });
+});
