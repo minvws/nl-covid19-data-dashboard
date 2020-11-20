5ec852426edc4178fc93f49042bb4aebb0866d0a
VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2020-11-20 16:35:27 +0100

    Add filter to fixture copy

--- src/tools/test/copy-fixtures.ts
+++ src/tools/test/copy-fixtures.ts
@@ -18,6 +18,43 @@ const fixtures = [
 
 fixtures.forEach(copyFixture);
 
+filterMunicipalities();
+filterRegions();
+
 function copyFixture(name: string) {
   fs.copyFileSync(path.join(jsonBasePath, name), path.join(fixturePath, name));
+}
+
+function filterMunicipalities() {
+  const filename = path.join(fixturePath, 'MUNICIPALITIES.json');
+  const content = fs.readFileSync(filename, { encoding: 'utf8' });
+  const data = JSON.parse(content);
+
+  for (const prop in data) {
+    if (Array.isArray(data[prop])) {
+      data[prop] = data[prop].filter(
+        (item: any) => item.gmcode === municipalCode
+      );
+    }
+  }
+
+  fs.writeFileSync(filename, JSON.stringify(data, null, '\t'), {
+    encoding: 'utf8',
+  });
+}
+
+function filterRegions() {
+  const filename = path.join(fixturePath, 'REGIONS.json');
+  const content = fs.readFileSync(filename, { encoding: 'utf8' });
+  const data = JSON.parse(content);
+
+  for (const prop in data) {
+    if (Array.isArray(data[prop])) {
+      data[prop] = data[prop].filter((item: any) => item.vrcode === vrcode);
+    }
+  }
+
+  fs.writeFileSync(filename, JSON.stringify(data, null, '\t'), {
+    encoding: 'utf8',
+  });
 }
