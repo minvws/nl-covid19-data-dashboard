111559b5e5c289899d18f77a14e8e737e68d8c2c
K <73584448+VWSCoronaDashboard10@users.noreply.github.com>
2020-11-27 09:36:40 +0100

    Simplify types

--- src/style/utils.ts
+++ src/style/utils.ts
@@ -1,7 +1,6 @@
-import { DefaultTheme } from 'styled-components';
-import { ResponsiveValue, ThemeValue } from 'styled-system';
+import { ResponsiveValue } from 'styled-system';
 
-const responsiveKeys = ['_', 'xs', 'sm', 'md', 'lg', 'xl'];
+const responsiveKeys = ['_', 'xs', 'sm', 'md', 'lg', 'xl'] as const;
 
 /**
  * Styled-system's `css()` function does not support responsive object notation.
@@ -18,16 +17,12 @@ export function asResponsiveArray<T>(
   return isStyleObject(value) ? createArrayValue(value) : value;
 }
 
-type StyleObject<T> = {
-  [key in (ThemeValue<'breakpoints', DefaultTheme> & string) | number]?: T;
-};
+type StyleObject<T> = Record<typeof responsiveKeys[number], T>;
 
 function isStyleObject<T>(value: ResponsiveValue<T>): value is StyleObject<T> {
   return !Array.isArray(value) && value !== null && typeof value === 'object';
 }
 
 function createArrayValue<T>(value: StyleObject<T>) {
-  return responsiveKeys.map(
-    (key) => value[(key as unknown) as number] as T
-  ) as ResponsiveValue<T>;
+  return responsiveKeys.map((key) => value[key]) as ResponsiveValue<T>;
 }
