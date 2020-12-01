8129cfacfc2fabda15fdda033f5422b0f74745d9
K <73584448+VWSCoronaDashboard10@users.noreply.github.com>
2020-12-01 16:45:57 +0100

    Elderly at home national page (#1054)
    * Create elderly at home national page
    
    * Add choropelth to elderly at home national page
    
    * Add date to choropleth

---
+++ src/components/choropleth/tooltips/region/create-region-elderly-at-home-tooltip.tsx
@@ -0,0 +1,25 @@
+import { NextRouter } from 'next/router';
+import { ReactNode } from 'react';
+import { createSelectRegionHandler } from '~/components/choropleth/select-handlers/create-select-region-handler';
+import { SafetyRegionProperties } from '~/components/choropleth/shared';
+import { TooltipContent } from '~/components/choropleth/tooltips/tooltipContent';
+import { formatNumber } from '~/utils/formatNumber';
+
+export const createRegionElderlyAtHomeTooltip = (router: NextRouter) => (
+  context: SafetyRegionProperties & { value: number }
+): ReactNode => {
+  const handler = createSelectRegionHandler(router);
+
+  const onSelect = (event: any) => {
+    event.stopPropagation();
+    handler(context);
+  };
+
+  return (
+    context && (
+      <TooltipContent title={context.vrname} onSelect={onSelect}>
+        <strong>{formatNumber(context.value)}</strong>
+      </TooltipContent>
+    )
+  );
+};
