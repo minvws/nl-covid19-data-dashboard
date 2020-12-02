42d2c88f3ccfae853965ab1857999536f1cb4541
VWS Corona Dashboard <66416946+VWSCoronaDashboard@users.noreply.github.com>
2020-12-02 16:26:31 +0100

    Only error non-whitelisted non-sortable properties

--- src/static-props/data-sorting.ts
+++ src/static-props/data-sorting.ts
@@ -5,7 +5,7 @@ export function sortNationalTimeSeriesInDataInPlace(data: National) {
 
   for (const propertyName of timeSeriesPropertyNames) {
     const timeSeries = data[propertyName] as TimeSeriesData<Timestamped>;
-    timeSeries.values = sortTimeSeriesValues(timeSeries.values);
+    timeSeries.values = sortTimeSeriesValues(timeSeries.values, propertyName);
   }
 }
 
@@ -23,7 +23,7 @@ export function sortRegionalTimeSeriesInDataInPlace(data: Regionaal) {
       >;
 
       nestedSeries.values = nestedSeries.values.map((x) => {
-        x.values = sortTimeSeriesValues(x.values);
+        x.values = sortTimeSeriesValues(x.values, propertyName);
         return x;
       });
 
@@ -32,7 +32,7 @@ export function sortRegionalTimeSeriesInDataInPlace(data: Regionaal) {
     }
 
     const timeSeries = data[propertyName] as TimeSeriesData<Timestamped>;
-    timeSeries.values = sortTimeSeriesValues(timeSeries.values);
+    timeSeries.values = sortTimeSeriesValues(timeSeries.values, propertyName);
   }
 }
 
@@ -50,7 +50,7 @@ export function sortMunicipalTimeSeriesInDataInPlace(data: Municipal) {
       >;
 
       nestedSeries.values = nestedSeries.values.map((x) => {
-        x.values = sortTimeSeriesValues(x.values);
+        x.values = sortTimeSeriesValues(x.values, propertyName);
         return x;
       });
 
@@ -59,7 +59,7 @@ export function sortMunicipalTimeSeriesInDataInPlace(data: Municipal) {
     }
 
     const timeSeries = data[propertyName] as TimeSeriesData<Timestamped>;
-    timeSeries.values = sortTimeSeriesValues(timeSeries.values);
+    timeSeries.values = sortTimeSeriesValues(timeSeries.values, propertyName);
   }
 }
 
@@ -75,7 +75,7 @@ function getTimeSeriesPropertyNames<T>(data: T) {
   );
 }
 
-function sortTimeSeriesValues(values: Timestamped[]) {
+function sortTimeSeriesValues(values: Timestamped[], propertyName: string) {
   /**
    * There are 3 ways in which time series data can be timestamped. We need
    * to detect and handle each of them.
@@ -89,6 +89,18 @@ function sortTimeSeriesValues(values: Timestamped[]) {
       (a, b) => a.date_measurement_unix - b.date_measurement_unix
     );
   }
+
+  if (isWhitelistedProperty(propertyName)) {
+    return values;
+  }
+
+  /**
+   * If none match we throw, since it means an unknown timestamp is used and we
+   * want to be sure we sort all data.
+   */
+  throw new Error(
+    `Unknown timestamp in value ${JSON.stringify(values[0], null, 2)}`
+  );
 
   return values;
 }
@@ -146,4 +158,8 @@ function isMeasurementTimestamped(
     (timeSeries as MeasurementTimestamped[])[0].date_measurement_unix !==
     undefined
   );
+}
+
+function isWhitelistedProperty(propertyName: string) {
+  return ['restrictions'].indexOf(propertyName) !== -1;
 }
