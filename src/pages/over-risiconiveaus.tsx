419477545d6ecc632c01c30e352c6201e84c45e9
VWS Corona Dashboard <66416946+VWSCoronaDashboard@users.noreply.github.com>
2020-12-03 13:37:54 +0100

    risiconiveaus

--- src/pages/over-risiconiveaus.tsx
+++ src/pages/over-risiconiveaus.tsx
@@ -2,26 +2,37 @@ import fs from 'fs';
 import Head from 'next/head';
 import path from 'path';
 
-import BlockContent from '@sanity/block-content-to-react';
-import client, { localize } from '~/lib/sanity';
-
-import { targetLanguage } from '../locale/index';
-
 import { FCWithLayout, getLayoutWithMetadata } from '~/components/layout';
 import { MaxWidth } from '~/components/maxWidth';
-import siteText, { TALLLanguages } from '~/locale/index';
+import siteText from '~/locale/index';
 import { MDToHTMLString } from '~/utils/MDToHTMLString';
 import styles from './over.module.scss';
+
+import { groq } from 'next-sanity';
+import {
+  getClient,
+  usePreviewSubscription,
+  PortableText,
+  localize,
+} from '~/lib/sanity';
+
+import { targetLanguage } from '../locale/index';
 
 interface StaticProps {
   props: OverRisiconiveausProps;
 }
 
 interface OverRisiconiveausProps {
-  text: TALLLanguages;
   lastGenerated: string;
-  overDeRisicoNiveaus: any;
+  risicoData: any;
 }
+
+const risicoQuery = groq`
+  *[_type == 'overRisicoNiveaus']
+  {
+    ...
+  }[0]
+`;
 
 export async function getStaticProps(): Promise<StaticProps> {
   const text = (await import('../locale/index')).default;
@@ -34,22 +45,20 @@ export async function getStaticProps(): Promise<StaticProps> {
   const fileContents = fs.readFileSync(filePath, 'utf8');
   const lastGenerated = JSON.parse(fileContents).last_generated;
 
-  const sanityData = await client.fetch(
-    `
-    *[_id == 'overDeRisicoNiveaus']
-    {
-      ...
-    }[0]
-  `
-  );
+  const risicoData = await getClient(true).fetch(risicoQuery);
 
-  const overDeRisicoNiveaus = localize(sanityData, [targetLanguage, 'nl']);
-
-  return { props: { lastGenerated, overDeRisicoNiveaus } };
+  return { props: { lastGenerated, risicoData } };
 }
 
 const OverRisicoNiveaus: FCWithLayout<OverRisiconiveausProps> = (props) => {
-  const { overDeRisicoNiveaus } = props;
+  const { risicoData } = props;
+
+  const { data: staticOrPreviewData } = usePreviewSubscription(risicoQuery, {
+    initialData: risicoData,
+    enabled: true,
+  });
+
+  const risico = localize(staticOrPreviewData, [targetLanguage, 'nl']);
 
   return (
     <>
@@ -70,11 +79,8 @@ const OverRisicoNiveaus: FCWithLayout<OverRisiconiveausProps> = (props) => {
       <div className={styles.container}>
         <MaxWidth>
           <div className={styles.maxwidth}>
-            <h2>{overDeRisicoNiveaus.title}</h2>
-            <BlockContent blocks={overDeRisicoNiveaus.description} />
-            {overDeRisicoNiveaus.content.map((item: any) => {
-              return <BlockContent key={item['_key']} blocks={item} />;
-            })}
+            <h2>{risico.title}</h2>
+            <PortableText blocks={risico.beschrijving} />
           </div>
         </MaxWidth>
       </div>
