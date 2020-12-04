399b9b43213b46d9781b80f63402788b6bf107b2
R <69849293+VWSCoronaDashboard1@users.noreply.github.com>
2020-12-04 17:15:14 +0100

    place sewerwater aside bar item above huisarts (#1074)
    Co-authored-by: VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>

--- src/components/layout/NationalLayout.tsx
+++ src/components/layout/NationalLayout.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import { Link } from '~/utils/link';
 import { useRouter } from 'next/router';
 import Arrow from '~/assets/arrow.svg';
 import Arts from '~/assets/arts.svg';
@@ -33,6 +32,7 @@ import Layout from '~/components/layout';
 import siteText from '~/locale/index';
 import { NationalPageProps } from '~/static-props/nl-data';
 import theme from '~/style/theme';
+import { Link } from '~/utils/link';
 import { useBreakpoints } from '~/utils/useBreakpoints';
 import { PositiveTestedPeopleMetric } from '../landelijk/positive-tested-people-metric';
 
@@ -348,6 +348,20 @@ function NationalLayout(props: NationalLayoutProps) {
                 </Category>
                 <Menu>
                   <MetricMenuItem>
+                    <Link href="/landelijk/rioolwater">
+                      <a className={getClassName('/landelijk/rioolwater')}>
+                        <TitleWithIcon
+                          icon={<RioolwaterMonitoring />}
+                          title={siteText.rioolwater_metingen.titel_sidebar}
+                        />
+                        <span className="metric-wrapper">
+                          <SewerWaterMetric data={data.sewer} />
+                        </span>
+                      </a>
+                    </Link>
+                  </MetricMenuItem>
+
+                  <MetricMenuItem>
                     <Link href="/landelijk/verdenkingen-huisartsen">
                       <a
                         className={getClassName(
@@ -362,20 +376,6 @@ function NationalLayout(props: NationalLayoutProps) {
                           <SuspectedPatientsMetric
                             data={data.verdenkingen_huisartsen.last_value}
                           />
-                        </span>
-                      </a>
-                    </Link>
-                  </MetricMenuItem>
-
-                  <MetricMenuItem>
-                    <Link href="/landelijk/rioolwater">
-                      <a className={getClassName('/landelijk/rioolwater')}>
-                        <TitleWithIcon
-                          icon={<RioolwaterMonitoring />}
-                          title={siteText.rioolwater_metingen.titel_sidebar}
-                        />
-                        <span className="metric-wrapper">
-                          <SewerWaterMetric data={data.sewer} />
                         </span>
                       </a>
                     </Link>
