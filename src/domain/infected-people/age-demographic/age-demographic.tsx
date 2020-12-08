f742fb96c0ebd0539316447bec8bddd381e47967
A <69853534+VWSCoronaDashboard4@users.noreply.github.com>
2020-12-08 13:27:40 +0100

    Topic/infected age groups keyboard (#1073)

--- src/domain/infected-people/age-demographic/age-demographic.tsx
+++ src/domain/infected-people/age-demographic/age-demographic.tsx
@@ -1,90 +1,67 @@
-import css from '@styled-system/css';
-import { ParentSize } from '@visx/responsive';
-import { MouseEvent, useCallback, useRef, useState } from 'react';
-import styled from 'styled-components';
 import { Box } from '~/components-styled/base';
+import { Tooltip, useTooltip } from '~/components-styled/tooltip';
 import {
   NationalInfectedAgeGroups,
   NationalInfectedAgeGroupsValue,
 } from '~/types/data';
-import { AgeDemographicChart } from './age-demographic-chart';
-import { AgeDemographicTooltip } from './age-demographic-tooltip';
+import { useElementSize } from '~/utils/use-element-size';
+import { useBreakpoints } from '~/utils/useBreakpoints';
+import {
+  AgeDemographicChart,
+  AGE_GROUP_TOOLTIP_WIDTH,
+} from './age-demographic-chart';
+import { useAgeDemographicCoordinates } from './age-demographic-coordinates';
+import { AgeDemographicTooltipContent } from './age-demographic-tooltip-content';
 
 interface AgeDemographicProps {
   data: NationalInfectedAgeGroups;
 }
 
-interface TooltipOptions {
-  left: number;
-  top: number;
-  value: NationalInfectedAgeGroupsValue;
-}
-
-const Wrapper = styled.div(
-  css({
-    position: 'relative',
-  })
-);
-
 export function AgeDemographic({ data }: AgeDemographicProps) {
-  const [tooltip, setTooltip] = useState<TooltipOptions>();
+  const [sizeRef, size] = useElementSize<HTMLDivElement>(400);
+  const breakpoints = useBreakpoints();
+  const isSmallScreen = !breakpoints.xl;
 
-  // @TODO move this tooltip logic elsewhere
-  // And combine with keyboard logic
-  const timer = useRef(-1);
-
-  const debounceSetTooltip = useCallback(
-    (options: TooltipOptions | undefined) => {
-      if (timer.current > -1) {
-        window.clearTimeout(timer.current);
-      }
-
-      timer.current = window.setTimeout(() => setTooltip(options), 100);
-    },
-    []
+  // Calculate graph's coordinates based on the data, the component width and wheher we are on a small screen or not.
+  const coordinates = useAgeDemographicCoordinates(
+    data,
+    isSmallScreen,
+    size.width
   );
 
-  const openTooltip = useCallback(
-    (
-      event: MouseEvent<SVGGElement>,
-      value: NationalInfectedAgeGroupsValue,
-      getTooltipCoordinates: (
-        event: MouseEvent<SVGGElement>,
-        value: NationalInfectedAgeGroupsValue
-      ) => { x: number; y: number }
-    ) => {
-      const { x, y } = getTooltipCoordinates(event, value);
-      const options: TooltipOptions = { left: x, top: y, value };
-      debounceSetTooltip(options);
-    },
-    [debounceSetTooltip]
-  );
-
-  const closeTooltip = useCallback(() => {
-    debounceSetTooltip(undefined);
-  }, [debounceSetTooltip]);
+  // Generate tooltip event handlers and state based on values and tooltip coordinates callback
+  const {
+    openTooltip,
+    closeTooltip,
+    keyboardTooltip,
+    tooltipState,
+  } = useTooltip<NationalInfectedAgeGroupsValue>({
+    values: coordinates.values,
+    getTooltipCoordinates: coordinates.getTooltipCoordinates,
+  });
 
   return (
     <Box mx={-4}>
-      <Wrapper>
-        <ParentSize>
-          {(parent) => (
-            <AgeDemographicChart
-              parentWidth={parent.width}
-              data={data}
-              openTooltip={openTooltip}
-              closeTooltip={closeTooltip}
-            />
-          )}
-        </ParentSize>
-        {tooltip && (
-          <AgeDemographicTooltip
-            left={tooltip.left}
-            top={tooltip.top}
-            value={tooltip.value}
+      <Box position="relative">
+        <div ref={sizeRef}>
+          <AgeDemographicChart
+            coordinates={coordinates}
+            openTooltip={openTooltip}
+            closeTooltip={closeTooltip}
+            keyboardTooltip={keyboardTooltip}
           />
-        )}
-      </Wrapper>
+        </div>
+
+        <Tooltip
+          controls="age-demographic-chart"
+          tooltipState={tooltipState}
+          width={AGE_GROUP_TOOLTIP_WIDTH}
+        >
+          {tooltipState.value && (
+            <AgeDemographicTooltipContent value={tooltipState.value} />
+          )}
+        </Tooltip>
+      </Box>
     </Box>
   );
 }
