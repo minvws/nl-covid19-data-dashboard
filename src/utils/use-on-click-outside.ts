a11d785a4fbeb720c02c986d218b37cc75a269b4
K <73584448+VWSCoronaDashboard10@users.noreply.github.com>
2020-12-09 11:34:24 +0100

    IE11 does not support .contains on svg elements

--- src/utils/use-on-click-outside.ts
+++ src/utils/use-on-click-outside.ts
@@ -11,9 +11,17 @@ export function useOnClickOutside<T extends RefObject<Element>>(
     const refs = Array.isArray(ref) ? ref : [ref];
 
     function listener(event: MouseEvent | TouchEvent) {
-      const clickedInsideRef = refs.find((ref) =>
-        ref.current?.contains(event.target as Node)
-      );
+      const clickedInsideRef = refs.find((ref) => {
+        let el = ref.current as Node | null;
+        /**
+         * IE11 does not support `.contains` on SVG elements. We'll traverse the
+         * DOM to find the first parent which does support that method.
+         */
+        while (el && !el.contains && el !== document.body) {
+          el = el.parentNode;
+        }
+        el?.contains(event.target as Node);
+      });
 
       if (!clickedInsideRef) {
         handlerRef.current(event);
