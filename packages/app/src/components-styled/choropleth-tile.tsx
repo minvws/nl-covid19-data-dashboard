21887df514df8a4dc61940be59181c2bc098b52c
VWS Corona Dashboard <66416946+VWSCoronaDashboard@users.noreply.github.com>
2020-12-21 20:45:17 +0100

    Revert "Fix TS errors"
    This reverts commit 08f55c4848f07f6a6fd78bc206278cc89b8a4270.

--- packages/app/src/components-styled/choropleth-tile.tsx
+++ packages/app/src/components-styled/choropleth-tile.tsx
@@ -33,7 +33,7 @@ interface ChoroplethTileProps extends DataProps {
   metadata?: MetadataProps;
 }
 
-export function ChoroplethTile({
+export function ChoroplethTile<T>({
   title,
   description,
   onChartRegionChange,
