badc55f5c36afe5ed23b0f2579f8dd276b16b0bb
VWS Corona Dashboard <66416946+VWSCoronaDashboard@users.noreply.github.com>
2020-12-22 00:14:42 +0100

    Add a CMS

---
+++ packages/cms/schemas/locale/locale-string.js
@@ -0,0 +1,23 @@
+const supportedLanguages = [
+  { id: "nl", title: "Nederlands", isDefault: true },
+  { id: "en", title: "Engels" },
+];
+
+export default {
+  name: "localeString",
+  type: "object",
+  title: "Locale String Content",
+  fieldsets: [
+    {
+      title: "Vertalingen",
+      name: "translations",
+      options: { collapsible: true },
+    },
+  ],
+  fields: supportedLanguages.map((lang) => ({
+    title: lang.title,
+    name: lang.id,
+    type: "string",
+    fieldset: lang.isDefault ? null : "translations",
+  })),
+};
