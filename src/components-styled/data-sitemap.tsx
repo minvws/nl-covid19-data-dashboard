259bdd26360fc2ee068d14c8919c9a7bc52cd29e
VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2020-12-21 14:30:27 +0100

    Add component and locale entries

---
+++ src/components-styled/data-sitemap.tsx
@@ -0,0 +1,10 @@
+import { Tile } from './tile';
+import { Heading } from './typography';
+
+export function DataSitemap() {
+  return (
+    <Tile>
+      <Heading level={2}>sdfsfd</Heading>
+    </Tile>
+  );
+}
