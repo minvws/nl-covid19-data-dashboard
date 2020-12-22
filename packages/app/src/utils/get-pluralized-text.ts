093776b9d4995398b2b16303a56609339906b120
VWS Corona Dashboard <66416946+VWSCoronaDashboard@users.noreply.github.com>
2020-12-22 16:09:09 +0100

    Amount -> count

--- packages/app/src/utils/get-pluralized-text.ts
+++ packages/app/src/utils/get-pluralized-text.ts
@@ -1,11 +1,11 @@
 export type PluralizationTexts = Record<'zero' | 'singular' | 'plural', string>;
 
-export function getPluralizedText(texts: PluralizationTexts, amount: number): string {
-  const absoluteAmount = Math.abs(amount);
-  if (absoluteAmount === 0) {
+export function getPluralizedText(texts: PluralizationTexts, count: number): string {
+  const absoluteCount = Math.abs(count);
+  if (absoluteCount === 0) {
     return texts.zero;
   }
-  if (absoluteAmount <= 1) {
+  if (absoluteCount <= 1) {
     return texts.singular;
   }
   return texts.plural;
