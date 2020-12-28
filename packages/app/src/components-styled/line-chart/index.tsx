71d26f58d8ba256a8ea2169273538518d1e62445
K <73584448+VWSCoronaDashboard10@users.noreply.github.com>
2020-12-28 13:04:15 +0100

    Fix IE11 issues

--- packages/app/src/components-styled/line-chart/index.tsx
+++ packages/app/src/components-styled/line-chart/index.tsx
@@ -1,7 +1,6 @@
 import { TickFormatter } from '@visx/axis';
-import { useTooltip } from '@visx/tooltip';
 import { extent } from 'd3-array';
-import { useCallback, useMemo } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import { isDefined } from 'ts-is-present';
 import { Box } from '~/components-styled/base';
 import { ValueAnnotation } from '~/components-styled/value-annotation';
@@ -235,4 +234,33 @@ function formatDefaultTooltip<T extends Value & TrendValue>(
   throw new Error(
     `Invalid value passed to format tooltip function: ${JSON.stringify(value)}`
   );
+}
+
+function useTooltip<T>() {
+  const [tooltipData, setTooltipData] = useState<T>();
+  const [tooltipLeft, setTooltipLeft] = useState<number>();
+  const [tooltipTop, setTooltipTop] = useState<number>();
+
+  const showTooltip = useCallback(
+    (x: { tooltipData: T; tooltipLeft: number; tooltipTop: number }) => {
+      setTooltipData(x.tooltipData);
+      setTooltipLeft(x.tooltipLeft);
+      setTooltipTop(x.tooltipTop);
+    },
+    []
+  );
+
+  const hideTooltip = useCallback(() => {
+    setTooltipData(undefined);
+    setTooltipLeft(undefined);
+    setTooltipTop(undefined);
+  }, []);
+
+  return {
+    tooltipData,
+    tooltipLeft,
+    tooltipTop,
+    showTooltip,
+    hideTooltip,
+  };
 }
