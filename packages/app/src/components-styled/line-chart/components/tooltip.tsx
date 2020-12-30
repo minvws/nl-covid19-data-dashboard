87758f74d505028672b44167c54de4501b56ab7c
VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2020-12-30 20:56:54 +0100

    Remove pseudo selector

--- packages/app/src/components-styled/line-chart/components/tooltip.tsx
+++ packages/app/src/components-styled/line-chart/components/tooltip.tsx
@@ -35,16 +35,6 @@ const TooltipContainer = styled.div<TooltipContainerProps>`
     px: 2,
     py: 1,
     fontSize: 1,
-    '&:before': {
-      content: '',
-      position: 'absolute',
-      top: '100%',
-      left: '20px',
-      width: 0,
-      borderTop: '20px solid black',
-      borderLeft: '20px solid transparent',
-      borderRight: '20px solid transparent',
-    },
   })};
   z-index: 100;
 `;
