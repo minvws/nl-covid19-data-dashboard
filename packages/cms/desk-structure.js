791857a486ad49c80f06f0896e29864fc0def37a
J <66416946+VWSCoronaDashboard@users.noreply.github.com>
2021-01-06 13:07:48 +0100

    Clean up Sanity for the first release (#1352)

--- packages/cms/desk-structure.js
+++ packages/cms/desk-structure.js
@@ -14,37 +14,37 @@ import ColorblindPreview from "./previews/colorblind-filter/ColorblindPreview";
 import { assemblePreviewUrl } from "./previews/assemblePreviewUrl";
 
 // Build up the root of the preview URL
-const remoteURL = process.env.SANITY_STUDIO_PREVIEW_SERVER;
-const localURL = "http://localhost:3000";
-const previewURL =
-  window.location.hostname === "localhost" ? localURL : remoteURL;
+// const remoteURL = process.env.SANITY_STUDIO_PREVIEW_SERVER;
+// const localURL = "http://localhost:3000";
+// const previewURL =
+//   window.location.hostname === "localhost" ? localURL : remoteURL;
 
-const WebPreview = ({ options, displayed }) => {
-  const url = assemblePreviewUrl({ displayed, options });
+// const WebPreview = ({ options, displayed }) => {
+//   const url = assemblePreviewUrl({ displayed, options });
 
-  return (
-    <iframe
-      style={{
-        margin: 0,
-        padding: 0,
-      }}
-      width="100%"
-      height="99%"
-      src={url}
-      frameBorder={0}
-    />
-  );
-};
+//   return (
+//     <iframe
+//       style={{
+//         margin: 0,
+//         padding: 0,
+//       }}
+//       width="100%"
+//       height="99%"
+//       src={url}
+//       frameBorder={0}
+//     />
+//   );
+// };
 
-const IFrameMobilePreview = ({ options, displayed }) => {
-  const url = assemblePreviewUrl({ displayed, options });
+// const IFrameMobilePreview = ({ options, displayed }) => {
+//   const url = assemblePreviewUrl({ displayed, options });
 
-  return (
-    <SanityMobilePreview>
-      <iframe src={url} frameBorder={0} width="100%" height="100%" />
-    </SanityMobilePreview>
-  );
-};
+//   return (
+//     <SanityMobilePreview>
+//       <iframe src={url} frameBorder={0} width="100%" height="100%" />
+//     </SanityMobilePreview>
+//   );
+// };
 
 // hiddenDocTypes will filter out all of the content models
 // we expose through other sections in the CMS.
@@ -69,68 +69,68 @@ export default () =>
   S.list()
     .title("Content")
     .items([
-      S.listItem()
-        .title("Instellingen")
-        .schemaType("siteSettings")
-        .icon(GoSettings)
-        .child(
-          S.editor()
-            .schemaType("siteSettings")
-            .title("Site instellingen")
-            .documentId("siteSettings")
-        ),
+      // S.listItem()
+      //   .title("Instellingen")
+      //   .schemaType("siteSettings")
+      //   .icon(GoSettings)
+      //   .child(
+      //     S.editor()
+      //       .schemaType("siteSettings")
+      //       .title("Site instellingen")
+      //       .documentId("siteSettings")
+      //   ),
 
-      S.listItem()
-        .title("Over dit dashboard")
-        .schemaType("overDitDashboard")
-        .icon(GrCircleInformation)
-        .child(
-          S.editor()
-            .title("Over dit dashboard")
-            .schemaType("overDitDashboard")
-            .documentId("overDitDashboard")
-            .views([
-              S.view.form(),
-              S.view
-                .component(WebPreview)
-                .options({ previewURL: `${previewURL}/over` })
-                .title("Web"),
-              S.view
-                .component(IFrameMobilePreview)
-                .options({ previewURL: `${previewURL}/over` })
-                .title("Mobile"),
-              S.view
-                .component(ColorblindPreview)
-                .options({ previewURL: `${previewURL}/over` })
-                .title("Color Blindness"),
-            ])
-        ),
+      // S.listItem()
+      //   .title("Over dit dashboard")
+      //   .schemaType("overDitDashboard")
+      //   .icon(GrCircleInformation)
+      //   .child(
+      //     S.editor()
+      //       .title("Over dit dashboard")
+      //       .schemaType("overDitDashboard")
+      //       .documentId("overDitDashboard")
+      //       .views([
+      //         S.view.form(),
+      //         S.view
+      //           .component(WebPreview)
+      //           .options({ previewURL: `${previewURL}/over` })
+      //           .title("Web"),
+      //         S.view
+      //           .component(IFrameMobilePreview)
+      //           .options({ previewURL: `${previewURL}/over` })
+      //           .title("Mobile"),
+      //         S.view
+      //           .component(ColorblindPreview)
+      //           .options({ previewURL: `${previewURL}/over` })
+      //           .title("Color Blindness"),
+      //       ])
+      //   ),
 
-      S.listItem()
-        .title("Over de risiconiveaus")
-        .schemaType("overRisicoNiveaus")
-        .icon(BsMap)
-        .child(
-          S.editor()
-            .title("Over de risiconiveaus")
-            .schemaType("overRisicoNiveaus")
-            .documentId("overRisicoNiveaus")
-            .views([
-              S.view.form(),
-              S.view
-                .component(WebPreview)
-                .options({ previewURL: `${previewURL}/over-risiconiveaus` })
-                .title("Web"),
-              S.view
-                .component(IFrameMobilePreview)
-                .options({ previewURL: `${previewURL}/over-risiconiveaus` })
-                .title("Mobile"),
-              S.view
-                .component(ColorblindPreview)
-                .options({ previewURL: `${previewURL}/over-risiconiveaus` })
-                .title("Color Blindness"),
-            ])
-        ),
+      // S.listItem()
+      //   .title("Over de risiconiveaus")
+      //   .schemaType("overRisicoNiveaus")
+      //   .icon(BsMap)
+      //   .child(
+      //     S.editor()
+      //       .title("Over de risiconiveaus")
+      //       .schemaType("overRisicoNiveaus")
+      //       .documentId("overRisicoNiveaus")
+      //       .views([
+      //         S.view.form(),
+      //         S.view
+      //           .component(WebPreview)
+      //           .options({ previewURL: `${previewURL}/over-risiconiveaus` })
+      //           .title("Web"),
+      //         S.view
+      //           .component(IFrameMobilePreview)
+      //           .options({ previewURL: `${previewURL}/over-risiconiveaus` })
+      //           .title("Mobile"),
+      //         S.view
+      //           .component(ColorblindPreview)
+      //           .options({ previewURL: `${previewURL}/over-risiconiveaus` })
+      //           .title("Color Blindness"),
+      //       ])
+      //   ),
 
       S.listItem()
         .title("Veelgestelde vragen")
@@ -143,46 +143,46 @@ export default () =>
             .documentId("veelgesteldeVragen")
             .views([
               S.view.form(),
-              S.view
-                .component(WebPreview)
-                .options({ previewURL: `${previewURL}/veelgestelde-vragen` })
-                .title("Web"),
-              S.view
-                .component(IFrameMobilePreview)
-                .options({ previewURL: `${previewURL}/veelgestelde-vragen` })
-                .title("Mobile"),
-              S.view
-                .component(ColorblindPreview)
-                .options({ previewURL: `${previewURL}/veelgestelde-vragen` })
-                .title("Color Blindness"),
+              // S.view
+              //   .component(WebPreview)
+              //   .options({ previewURL: `${previewURL}/veelgestelde-vragen` })
+              //   .title("Web"),
+              // S.view
+              //   .component(IFrameMobilePreview)
+              //   .options({ previewURL: `${previewURL}/veelgestelde-vragen` })
+              //   .title("Mobile"),
+              // S.view
+              //   .component(ColorblindPreview)
+              //   .options({ previewURL: `${previewURL}/veelgestelde-vragen` })
+              //   .title("Color Blindness"),
             ])
         ),
 
-      S.listItem()
-        .title("Cijferverantwoording")
-        .schemaType("cijferVerantwoording")
-        .icon(BsCardChecklist)
-        .child(
-          S.editor()
-            .title("Cijferverantwoording")
-            .schemaType("cijferVerantwoording")
-            .documentId("cijferVerantwoording")
-            .views([
-              S.view.form(),
-              S.view
-                .component(WebPreview)
-                .title("Web")
-                .options({ previewURL: `${previewURL}/verantwoording` }),
-              S.view
-                .component(IFrameMobilePreview)
-                .options({ previewURL: `${previewURL}/verantwoording` })
-                .title("Mobile"),
-              S.view
-                .component(ColorblindPreview)
-                .options({ previewURL: `${previewURL}/verantwoording` })
-                .title("Color Blindness"),
-            ])
-        ),
+      // S.listItem()
+      //   .title("Cijferverantwoording")
+      //   .schemaType("cijferVerantwoording")
+      //   .icon(BsCardChecklist)
+      //   .child(
+      //     S.editor()
+      //       .title("Cijferverantwoording")
+      //       .schemaType("cijferVerantwoording")
+      //       .documentId("cijferVerantwoording")
+      //       .views([
+      //         S.view.form(),
+      //         S.view
+      //           .component(WebPreview)
+      //           .title("Web")
+      //           .options({ previewURL: `${previewURL}/verantwoording` }),
+      //         S.view
+      //           .component(IFrameMobilePreview)
+      //           .options({ previewURL: `${previewURL}/verantwoording` })
+      //           .title("Mobile"),
+      //         S.view
+      //           .component(ColorblindPreview)
+      //           .options({ previewURL: `${previewURL}/verantwoording` })
+      //           .title("Color Blindness"),
+      //       ])
+      //   ),
 
       // Add a visual divider (optional)
       S.divider(),
--- packages/cms/schemas/locale/locale-block.js
+++ packages/cms/schemas/locale/locale-block.js
@@ -7,20 +7,18 @@ export default {
   name: "localeBlock",
   type: "object",
   title: "Locale Block Content",
-  fieldsets: [
-    {
-      title: "Vertalingen",
-      name: "translations",
-      options: { collapsible: true },
-    },
-  ],
+  // fieldsets: [
+  //   {
+  //     title: "Vertalingen",
+  //     name: "translations",
+  //     options: { collapsible: true },
+  //   },
+  // ],
   fields: supportedLanguages.map((lang) => ({
     title: lang.title,
     name: lang.id,
     type: "array",
-    validation: (Rule) => Rule.required(),
-
     of: [{ type: "block" }],
-    fieldset: lang.isDefault ? null : "translations",
+    // fieldset: lang.isDefault ? null : "translations",
   })),
 };
