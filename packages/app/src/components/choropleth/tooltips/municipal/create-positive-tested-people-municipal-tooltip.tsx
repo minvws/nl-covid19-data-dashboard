69eb7c939b6730e1149776d45718f1986a9a54c2
T <71320230+VWSCoronaDashboard8@users.noreply.github.com>
2021-01-06 09:55:12 +0100

    Make gm choropleth positieve test tooltip same as vr

--- packages/app/src/components/choropleth/tooltips/municipal/create-positive-tested-people-municipal-tooltip.tsx
+++ packages/app/src/components/choropleth/tooltips/municipal/create-positive-tested-people-municipal-tooltip.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from 'react';
 import { TooltipContent } from '~/components/choropleth/tooltips/tooltipContent';
 import siteText from '~/locale/index';
 import { MunicipalitiesTestedOverall } from '~/types/data';
+import { formatNumber } from '~/utils/formatNumber';
 import { replaceVariablesInText } from '~/utils/replaceVariablesInText';
 import { MunicipalitySelectionHandler } from '../../select-handlers/create-select-municipal-handler';
 import { MunicipalityProperties } from '../../shared';
@@ -22,27 +23,15 @@ export const createPositiveTestedPeopleMunicipalTooltip = (
   };
 
   return (
-    context && (
-      <TooltipContent title={gemnaam} onSelect={onSelect}>
-        <span>
-          {infected_per_100k !== undefined ? (
-            <>
-              <p className="info-value">
-                {replaceVariablesInText(text.positive_tested_value, {
-                  totalPositiveValue: `${infected_per_100k}`,
-                })}
-              </p>
-              <p className="info-total">
-                {replaceVariablesInText(text.positive_tested_people, {
-                  totalPositiveTestedPeople: `${infected}`,
-                })}
-              </p>
-            </>
-          ) : (
-            '-'
-          )}
-        </span>
-      </TooltipContent>
-    )
+    <TooltipContent title={gemnaam} onSelect={onSelect}>
+      <p className="info-value">
+        {formatNumber(infected_per_100k)} per 100.000
+      </p>
+      <p className="info-total">
+        {replaceVariablesInText(text.positive_tested_people, {
+          totalPositiveTestedPeople: `${infected}`,
+        })}
+      </p>
+    </TooltipContent>
   );
 };
