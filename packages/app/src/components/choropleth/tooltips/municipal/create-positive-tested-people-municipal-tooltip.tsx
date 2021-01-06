249e7f97511cdeae2f61bb74e6fbbe31d71c8777
VWS CoronaDashboard <71320230+VWSCoronaDashboard8@users.noreply.github.com>
2021-01-06 16:40:47 +0100

    Make positive tested choropleth tooltip show decimal values (#1358)

--- packages/app/src/components/choropleth/tooltips/municipal/create-positive-tested-people-municipal-tooltip.tsx
+++ packages/app/src/components/choropleth/tooltips/municipal/create-positive-tested-people-municipal-tooltip.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 import { TooltipContent } from '~/components/choropleth/tooltips/tooltipContent';
 import siteText from '~/locale/index';
 import { MunicipalitiesTestedOverall } from '~/types/data';
-import { formatNumber } from '~/utils/formatNumber';
+import { formatPercentage } from '~/utils/formatNumber';
 import { replaceVariablesInText } from '~/utils/replaceVariablesInText';
 import { MunicipalitySelectionHandler } from '../../select-handlers/create-select-municipal-handler';
 import { MunicipalityProperties } from '../../shared';
@@ -25,7 +25,7 @@ export const createPositiveTestedPeopleMunicipalTooltip = (
   return (
     <TooltipContent title={gemnaam} onSelect={onSelect}>
       <p className="info-value">
-        {formatNumber(infected_per_100k)} per 100.000
+        {formatPercentage(infected_per_100k)} per 100.000
       </p>
       <p className="info-total">
         {replaceVariablesInText(text.positive_tested_people, {
