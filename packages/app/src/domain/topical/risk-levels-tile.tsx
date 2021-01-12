72f1a15a67b4e949b8ff721ca860513ad390dcc2
VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2021-01-12 13:18:11 +0100

    simplify prop

--- packages/app/src/domain/topical/risk-levels-tile.tsx
+++ packages/app/src/domain/topical/risk-levels-tile.tsx
@@ -26,10 +26,7 @@ function EscalationLevelExplanation(props: EscalationLevelExplanationProps) {
 export function RiskLevelsTile() {
   return (
     <Tile bg="white">
-      <Collapsable
-        summary={siteText.escalatie_niveau.tile_title}
-        hideBorder={true}
-      >
+      <Collapsable summary={siteText.escalatie_niveau.tile_title} hideBorder>
         <Box my={3}>
           <EscalationLevelExplanation
             level={1}
