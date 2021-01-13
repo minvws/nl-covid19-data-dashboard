2f0b285ae4ccc5cbb78b060a9555912120997eca
T <71320230+VWSCoronaDashboard8@users.noreply.github.com>
2021-01-13 15:37:32 +0100

    Use date of reporting in all hospital charts

--- packages/app/src/components/choropleth/municipal-thresholds.ts
+++ packages/app/src/components/choropleth/municipal-thresholds.ts
@@ -86,6 +86,8 @@ export const municipalThresholds = {
   tested_overall: {
     infected_per_100k: positiveTestedThresholds,
   },
-  hospital_nice: { admissions_moving_average: hospitalAdmissionsThresholds },
+  hospital_nice: {
+    admissions_on_date_of_reporting: hospitalAdmissionsThresholds,
+  },
   elderly_at_home: elderlyAtHomeThresholds,
 } as const;
