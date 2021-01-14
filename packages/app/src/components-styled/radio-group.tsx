c01ca2304cd6c4a64b22c19bd266fc1875f0c4f5
VWSCoronaDashboard14 <76471292+VWSCoronaDashboard14@users.noreply.github.com>
2021-01-14 13:13:11 +0100

    Reduce padding of the labels on smaller screens (#1410)
    * Reduced the spadding of the labels on smaller screens
    
    * use as responsive array to use theme breakpoints

--- packages/app/src/components-styled/radio-group.tsx
+++ packages/app/src/components-styled/radio-group.tsx
@@ -2,7 +2,7 @@ import { Fragment, useRef, useState } from 'react';
 import { Box } from './base';
 import { css } from '@styled-system/css';
 import styled from 'styled-components';
-
+import { asResponsiveArray } from '~/style/utils';
 interface RadioGroupItem<T extends string> {
   label: string;
   value: T;
@@ -26,14 +26,13 @@ const StyledLabel = styled.label(
   css({
     flex: '0 1 auto',
     color: 'button',
-    fontSize: 1,
+    fontSize: asResponsiveArray({ _: 1, sm: 2 }),
     textAlign: 'center',
-    p: '0.2em 1.5em',
+    p: asResponsiveArray({ _: '0.25em 0.5em', xs: '0.2em 1.5em' }),
     borderStyle: 'solid',
     borderColor: 'button',
     borderWidth: '1px 0 1px 1px',
     whiteSpace: 'nowrap',
-
     cursor: 'pointer',
 
     '&:last-child': {
@@ -42,10 +41,6 @@ const StyledLabel = styled.label(
 
     '&:hover, &:focus': {
       bg: '#cae1ed',
-    },
-
-    '@media (min-width: 768px)': {
-      fontSize: 2,
     },
   })
 );
