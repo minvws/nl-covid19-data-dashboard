eb28477f63125090892a052126379582eb932df7
VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2021-01-15 16:11:42 +0100

    Fix responsiveness

--- packages/app/src/domain/topical/latest-articles.tsx
+++ packages/app/src/domain/topical/latest-articles.tsx
@@ -4,18 +4,26 @@ import { Tile } from '~/components-styled/tile';
 import { Heading } from '~/components-styled/typography';
 import siteText from '~/locale';
 import { LatestArticle } from '~/pages';
+import { useBreakpoints } from '~/utils/useBreakpoints';
 
 type LatestArticlesProps = {
   articles: LatestArticle[];
 };
 
 export function LatestArticles({ articles }: LatestArticlesProps) {
+  const breakpoints = useBreakpoints();
+
   return (
     <Tile>
       <Heading level={2}>
         {siteText.nationaal_actueel.latest_articles.title}
       </Heading>
-      <Box display="flex" spacing={4} spacingHorizontal={true}>
+      <Box
+        display="flex"
+        spacing={4}
+        spacingHorizontal={breakpoints.lg}
+        flexDirection={{ _: 'column', lg: 'row' }}
+      >
         {articles.map((x) => (
           <Box flex="0 0 33%" key={x.slug.current}>
             <ArticleSummary
