b9ad3961d84eb7d1a68b5028bc32ece7c9b74954
VWS Corona Dashboard <66416946+VWSCoronaDashboard@users.noreply.github.com>
2021-01-15 16:12:50 +0100

    Add the tabs plugin for a cleaner interface in the roadmap

--- packages/cms/schemas/maatregelen/measure-group.js
+++ packages/cms/schemas/maatregelen/measure-group.js
@@ -1,3 +1,5 @@
+import Tabs from "sanity-plugin-tabs";
+
 export default {
   title: "Maatregel groep",
   name: "measureGroup",
@@ -9,35 +11,82 @@ export default {
       name: "title",
       type: "localeString",
     },
+    // {
+    //   title: "Maatregelen",
+    //   description:
+    //     "Schrijf per risiconiveau op wat de geldende maatregelen zijn. Let op, maatregelen in een lockdown vallen niet onder deze routekaart!",
+    //   name: "measures",
+    //   type: "object",
+    //   fields: [
+    //     {
+    //       title: "Waakzaam",
+    //       name: "waakzaam",
+    //       type: "array",
+    //       of: [{ type: "measure" }],
+    //     },
+    //     {
+    //       title: "Zorgelijk",
+    //       name: "zorgelijk",
+    //       type: "array",
+    //       of: [{ type: "measure" }],
+    //     },
+    //     {
+    //       title: "Ernstig",
+    //       name: "ernstig",
+    //       type: "array",
+    //       of: [{ type: "measure" }],
+    //     },
+    //     {
+    //       title: "Zeer Ernstig",
+    //       name: "zeerErnstig",
+    //       type: "array",
+    //       of: [{ type: "measure" }],
+    //     },
+    //   ],
+    // },
+
     {
-      title: "Maatregelen",
-      description:
-        "Schrijf per risiconiveau op wat de geldende maatregelen zijn. Let op, maatregelen in een lockdown vallen niet onder deze routekaart!",
       name: "measures",
       type: "object",
+      inputComponent: Tabs,
+
+      fieldsets: [
+        { name: "waakzaam", title: "Waakzaam", options: { sortOrder: 10 } },
+        { name: "zorgelijk", title: "Zorgelijk", options: { sortOrder: 20 } },
+        { name: "ernstig", title: "Ernstig", options: { sortOrder: 30 } },
+        {
+          name: "zeerErnstig",
+          title: "zeer ernstig",
+          options: { sortOrder: 40 },
+        },
+      ],
       fields: [
         {
-          title: "Waakzaam",
+          title: "Maatregelen",
           name: "waakzaam",
           type: "array",
+          fieldset: "waakzaam",
           of: [{ type: "measure" }],
         },
         {
-          title: "Zorgelijk",
+          title: "Maatregelen",
           name: "zorgelijk",
           type: "array",
+          fieldset: "zorgelijk",
           of: [{ type: "measure" }],
         },
         {
-          title: "Ernstig",
+          title: "Maatregelen",
           name: "ernstig",
           type: "array",
+          fieldset: "ernstig",
           of: [{ type: "measure" }],
         },
         {
-          title: "Zeer Ernstig",
+          title: "Maatregelen",
           name: "zeerErnstig",
           type: "array",
+          fieldset: "zeerErnstig",
           of: [{ type: "measure" }],
         },
       ],
