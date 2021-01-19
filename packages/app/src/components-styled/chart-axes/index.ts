d362c975c3f98e83ea2135b2e81262b10b70dc74
VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2021-01-19 18:44:49 +0100

    Rename chart-axes to index

--- packages/app/src/components-styled/chart-axes/index.ts
+++
@@ -1,8 +0,0 @@
-export { ChartAxes, defaultPadding } from './chart-axes';
-export type {
-  ChartBounds,
-  ChartPadding,
-  ChartScales,
-  ComponentCallbackFunction,
-  ComponentCallbackInfo,
-} from './chart-axes';
