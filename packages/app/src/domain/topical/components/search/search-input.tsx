a005adfa9a5d3eeb5556f48c85d4523085d764f2
VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2021-01-21 14:19:19 +0100

    Prevent layout shift after activating navigation item

--- packages/app/src/domain/topical/components/search/search-input.tsx
+++ packages/app/src/domain/topical/components/search/search-input.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import CloseIcon from '~/assets/close.svg';
 import SearchIcon from '~/assets/search-icon.svg';
 import { Box } from '~/components-styled/base';
+import { Text } from '~/components-styled/typography';
 import { VisuallyHidden } from '~/components-styled/visually-hidden';
 import { default as siteText, default as text } from '~/locale';
 import { useSearchContext } from './context';
@@ -36,7 +37,9 @@ export function SearchInput() {
       )}
 
       <VisuallyHidden>
-        <label htmlFor={`${id}-input`}>{siteText.search.placeholder}</label>
+        <label htmlFor={`${id}-input`}>
+          <Text>{siteText.search.placeholder}</Text>
+        </label>
       </VisuallyHidden>
 
       <StyledSearchInput
