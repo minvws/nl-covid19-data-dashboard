cc35e999ea03e10033f5c4a38248097c12672b70
VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2021-01-25 11:56:03 +0100

    Fix polyfill order

--- packages/app/src/utils/formatDate.ts
+++ packages/app/src/utils/formatDate.ts
@@ -4,10 +4,10 @@
  * datetimeformat needs to be second
  * datetimeformat locale's last
  */
+import '@formatjs/intl-getcanonicallocales/polyfill';
+import '@formatjs/intl-datetimeformat/polyfill';
 import '@formatjs/intl-datetimeformat/locale-data/en';
 import '@formatjs/intl-datetimeformat/locale-data/nl';
-import '@formatjs/intl-datetimeformat/polyfill';
-import '@formatjs/intl-getcanonicallocales/polyfill';
 import { isSameDay, isToday, isYesterday, subDays } from 'date-fns';
 import siteText from '~/locale/index';
 import { getLocale } from '~/utils/getLocale';
