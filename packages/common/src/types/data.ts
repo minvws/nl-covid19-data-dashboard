54f9664bd87585aabc05b1db9ba750665ebeaa6d
T <71320230+VWSCoronaDashboard8@users.noreply.github.com>
2021-01-25 21:47:46 +0100

    Replace date with date span for vaccine availability

--- packages/common/src/types/data.ts
+++ packages/common/src/types/data.ts
@@ -446,7 +446,8 @@ export interface NlVaccineAvailabilityValue {
   janssen: number;
   sanofi: number;
   date_of_insertion_unix: number;
-  date_unix: number;
+  date_start_unix?: number;
+  date_end_unix?: number;
 }
 
 export interface Regionaal {
