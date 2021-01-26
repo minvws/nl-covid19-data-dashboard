eab39d01d8ce07598c5043c69f347d89e5d6bc90
VWSCoronaDashboard14 <76471292+VWSCoronaDashboard14@users.noreply.github.com>
2021-01-26 11:51:32 +0100

    Make font-size inherit from their parent so it has the same size (#1605)

--- packages/app/src/components-styled/content-header/metadata.tsx
+++ packages/app/src/components-styled/content-header/metadata.tsx
@@ -105,7 +105,7 @@ function MetadataItem(props: MetadataItemProps) {
               <ExternalLink href={item.href}>{item.text}</ExternalLink>
             )}
             {!item.href && (
-              <Text css={css({ display: 'inline-block', my: '0' })}>
+              <Text css={css({ display: 'inline-block', my: '0', fontSize: 'inherit' })}>
                 {item.text}
               </Text>
             )}
