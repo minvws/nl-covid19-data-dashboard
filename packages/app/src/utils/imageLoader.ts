f807ddb99143c5e7493bc1b8ae887db2b518c1e9
VWS Corona Dashboard <66416946+VWSCoronaDashboard@users.noreply.github.com>
2021-01-26 17:05:47 +0100

    array destructuring

--- packages/app/src/utils/imageLoader.ts
+++ packages/app/src/utils/imageLoader.ts
@@ -8,8 +8,8 @@ interface LoaderProps {
 
 export function imageLoader(props: LoaderProps) {
   const { src, width } = props;
-  const filename = src.split('.')[0];
-  const extension = src.split('.')[1];
+
+  const [filename, extension] = src.split('.');
   const url = `cms/${filename}-${findClosestSize(
     width,
     imageResizeTargets
