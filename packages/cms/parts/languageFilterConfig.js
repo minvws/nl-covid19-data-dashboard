395152bfa17ed7d0cdefdca43c9fe220a11d49ad
VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2021-01-26 14:07:22 +0100

    Remove unused file

--- packages/cms/parts/languageFilterConfig.js
+++
@@ -1,9 +0,0 @@
-export default {
-  supportedLanguages: [
-    { id: "en", title: "Engels" },
-    { id: "nl", title: "Nederlands" },
-  ],
-  filterField: (enclosingType, field, selectedLanguageIds) =>
-    !enclosingType.name.startsWith("locale") ||
-    selectedLanguageIds.includes(field.name),
-};
