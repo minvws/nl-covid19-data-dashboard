28392765856eb98046f421cb85e450f50245265f
VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2021-01-27 14:57:58 +0100

    Moving around some stuff

--- packages/app/src/domain/topical/editorial-teaser.tsx
+++ packages/app/src/domain/topical/editorial-teaser.tsx
@@ -124,8 +124,6 @@ function CoverImage({ image, children }: CoverImageProps) {
     ? `${hotspot.x * 100}% ${hotspot.y * 100}%`
     : undefined;
 
-  //linear-gradient(to bottom, rgba(245, 246, 252, 0.52), rgba(117, 19, 93, 0.73)),
-
   return (
     <BackgroundImage
       height="100%"
