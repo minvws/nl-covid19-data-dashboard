c0e6e79528311714f7271126d1ead42305dfddf3
J <66416946+VWSCoronaDashboard@users.noreply.github.com>
2021-01-27 16:59:34 +0100

    Fix images from articles missing on regional pages (#1680)

--- packages/app/src/pages/actueel/gemeente/[code].tsx
+++ packages/app/src/pages/actueel/gemeente/[code].tsx
@@ -55,10 +55,35 @@ export const getStaticProps = createGetStaticProps(
     highlight: { article: ArticleSummary };
   }>(
     `{
-    // Retrieve the latest 3 articles with the highlighted article filtered out:
-    'articles': *[_type == 'article' && !(_id == *[_type == 'topicalPage']{"i":highlightedArticle->{_id}}[0].i._id)] | order(publicationDate) {"title":title.${targetLanguage}, slug, "summary":summary.${targetLanguage}, cover}[0..2],
-    'editorial': *[_type == 'editorial'] | order(publicationDate) {"title":title.${targetLanguage}, slug, "summary":summary.${targetLanguage}, cover}[0],
-    'highlight': *[_type == 'topicalPage']{"article":highlightedArticle->{"title":title.${targetLanguage}, slug, "summary":summary.${targetLanguage}, cover}}[0],
+      // Retrieve the latest 3 articles with the highlighted article filtered out:
+      'articles': *[_type == 'article' && !(_id == *[_type == 'topicalPage']{"i":highlightedArticle->{_id}}[0].i._id)] | order(publicationDate) {
+        "title":title.${targetLanguage},
+        slug, "summary":summary.${targetLanguage},
+        "cover": {
+          ...cover,
+          "asset": cover.asset->
+        }
+      }[0..2],
+      'editorial': *[_type == 'editorial'] | order(publicationDate) {
+        "title":title.${targetLanguage},
+        slug,
+        "summary":summary.${targetLanguage},
+        "cover": {
+          ...cover,
+          "asset": cover.asset->
+        }
+      }[0],
+      'highlight': *[_type == 'topicalPage'] {
+        "article":highlightedArticle->{
+          "title":title.${targetLanguage},
+          slug,
+          "summary":summary.${targetLanguage},
+          "cover": {
+            ...cover,
+            "asset": cover.asset->
+          }
+        }
+      }[0],
     }`
   )
 );
