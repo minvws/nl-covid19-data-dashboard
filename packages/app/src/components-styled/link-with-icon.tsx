3930140f71836fdac643c4bd04968f75d0783b65
VWSCoronaDashboard14 <76471292+VWSCoronaDashboard14@users.noreply.github.com>
2021-01-28 16:48:05 +0100

    Update sitemap grid and arrow from quicklinks (#1698)
    * Link with icons improvement
    
    * Link in grid on medium screen and fixed arrow issues
    
    * 4 and 2 column layout and fixed arrow spacing

--- packages/app/src/components-styled/link-with-icon.tsx
+++ packages/app/src/components-styled/link-with-icon.tsx
@@ -2,6 +2,7 @@ import css from '@styled-system/css';
 import { ReactNode } from 'react';
 import { UrlObject } from 'url';
 import { Link } from '~/utils/link';
+import { Box } from './base';
 
 interface LinkWithIconProps {
   href: UrlObject | string;
@@ -10,6 +11,11 @@ interface LinkWithIconProps {
   iconPlacement?: 'left' | 'right';
   fontWeight?: 'bold' | 'normal';
   headingLink?: boolean | undefined;
+}
+
+interface IconProps {
+  icon: ReactNode;
+  singleWord?: boolean;
 }
 
 export function LinkWithIcon({
@@ -21,6 +27,8 @@ export function LinkWithIcon({
   headingLink,
 }: LinkWithIconProps) {
   const splittedString = children.split(' ');
+  const firstWords = `${splittedString.slice(0, -1).join(' ')} `;
+  const singleWord = splittedString.length === 1;
 
   return (
     <Link href={href} passHref>
@@ -37,39 +45,59 @@ export function LinkWithIcon({
           },
         })}
       >
-        {iconPlacement == 'right' && !headingLink && children}
-        {!headingLink && (
-          <span
-            css={css({
-              svg: {
-                height: '11px',
-                width: '13px',
-                mx: '3px',
-              },
-            })}
-          >
-            {icon}
-          </span>
-        )}
-        {iconPlacement == 'left' && !headingLink && children}
-        {headingLink && (
+        {iconPlacement == 'right' && !headingLink && (
           <>
-            {!splittedString.length
-              ? children
-              : `${splittedString.slice(0, -1).join(' ')} `}
+            {!splittedString.length ? children : firstWords}
+            <Box as="span" display="inline-block">
+              {splittedString[splittedString.length - 1]}
+              <IconSmall icon={icon} />
+            </Box>
+          </>
+        )}
+        {iconPlacement == 'left' && !headingLink && (
+          <Box as="span">
+            <IconSmall icon={icon} />
+            {children}
+          </Box>
+        )}
+        {headingLink && (
+          <Box paddingRight={singleWord ? `calc(0.5rem + 18px)` : ''}>
+            {!splittedString.length ? children : firstWords}
             <span css={css({ display: 'inline-block' })}>
               {splittedString[splittedString.length - 1]}
-              <span
-                css={css({
-                  svg: { height: '16px', width: '18px', marginLeft: 2 },
-                })}
-              >
-                {icon}
-              </span>
+              <IconLarge icon={icon} singleWord={singleWord} />
             </span>
-          </>
+          </Box>
         )}
       </a>
     </Link>
   );
+
+  function IconSmall({ icon }: IconProps) {
+    return (
+      <span css={css({ svg: { height: '11px', width: '13px', mx: '3px' } })}>
+        {icon}
+      </span>
+    );
+  }
+
+  function IconLarge({ icon, singleWord }: IconProps) {
+    return (
+      <span
+        css={css({
+          svg: {
+            height: '16px',
+            width: '18px',
+            marginLeft: 2,
+            position: singleWord ? 'absolute' : 'relative',
+            minHeight: '100%',
+            right: 0,
+            top: 0,
+          },
+        })}
+      >
+        {icon}
+      </span>
+    );
+  }
 }
