edb1605ba50987eb5d29cd7c918acd326870e8da
VWSCoronaDashboard14 <76471292+VWSCoronaDashboard14@users.noreply.github.com>
2021-02-04 16:11:28 +0100

    WIP

--- packages/cms/schemas/documents/pages/topical-page.js
+++ packages/cms/schemas/documents/pages/topical-page.js
@@ -4,11 +4,31 @@ export default {
   type: 'document',
   fields: [
     {
+      name: 'article_or_custom',
+      title: 'Laat uitgelicht artikel zien.',
+      type: 'boolean'
+    },
+    {
       title: 'Uitgelicht artikel',
       name: 'highlightedArticle',
       type: 'reference',
       to: [{ type: 'article' }],
       validation: (Rule) => Rule.required(),
     },
+    {
+      name: 'title',
+      type: 'localeString',
+      title: 'Titel'
+    },
+    {
+      name: 'summary',
+      type: 'localeText',
+      title: 'Descriptie',
+    },
+    {
+      name: 'href',
+      type: 'localeString',
+      title: 'Link',
+    }
   ],
 };
