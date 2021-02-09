9b84421d14d362c90b4783aeaf29e9314e100432
T <71320230+VWSCoronaDashboard8@users.noreply.github.com>
2021-02-09 21:25:54 +0100

    Add deceased_rivm schema for GM

--- packages/common/src/types/data.ts
+++ packages/common/src/types/data.ts
@@ -9,11 +9,22 @@ export interface Municipal {
   proto_name: string;
   name: string;
   code: string;
+  deceased_rivm: GmDeceasedRivm;
   difference: MunicipalDifference;
   hospital_nice: MunicipalHospitalNice;
   tested_overall: MunicipalTestedOverall;
   sewer?: MunicipalSewer;
   sewer_per_installation?: MunicipalSewerPerInstallation;
+}
+export interface GmDeceasedRivm {
+  values: GmDeceasedRivmValue[];
+  last_value: GmDeceasedRivmValue;
+}
+export interface GmDeceasedRivmValue {
+  covid_daily: number;
+  covid_total: number;
+  date_unix: number;
+  date_of_insertion_unix: number;
 }
 export interface MunicipalDifference {
   tested_overall__infected_per_100k: DifferenceDecimal;
