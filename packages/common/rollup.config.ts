9122b72be85314a1eb7f33aa1c60f12a716d467f
T <71320230+VWSCoronaDashboard8@users.noreply.github.com>
2021-02-11 23:07:13 +0100

    First attempt at bundling common with rollup

---
+++ packages/common/rollup.config.ts
@@ -0,0 +1,37 @@
+// rollup.config.js
+import typescript from 'rollup-plugin-typescript2';
+import commonjs from 'rollup-plugin-commonjs';
+import { typescriptPaths } from 'rollup-plugin-typescript-paths';
+import autoExternal from 'rollup-plugin-auto-external';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import pkg from './package.json';
+
+export default {
+  input: './src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    autoExternal(),
+    nodeResolve({ preferBuiltins: false }),
+    commonjs({
+      /**
+       * Rollup fails to understand some named imports when converting to
+       * commonjs. I don't quite understand why that is, but this seems to solve
+       * it.
+       */
+      namedExports: { 'ts-is-present': ['isFilled'] },
+    }),
+    typescript(),
+    typescriptPaths(),
+  ],
+};
