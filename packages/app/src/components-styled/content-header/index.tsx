a25667ef1e5244c3a6b8bb221a473c191c657e41
VWSCoronaDashboard14 <76471292+VWSCoronaDashboard14@users.noreply.github.com>
2021-02-11 16:12:22 +0100

    Add dynamic hidden accessibility text for metadata sources (#1884)
    * Add dynamic hidden accessibility text for metadata sources
    
    * Rename some props according to feedback
    
    * Changed name and removed test source
    
    * Title to accessibilitySubject
    
    * Title to accessibilitySubject

--- packages/app/src/components-styled/content-header/index.tsx
+++ packages/app/src/components-styled/content-header/index.tsx
@@ -167,7 +167,7 @@ export function ContentHeader(props: ContentHeaderProps) {
 
           {metadata && (
             <MetadataBox>
-              <Metadata {...metadata} />
+              <Metadata {...metadata} accessibilitySubject={title} />
             </MetadataBox>
           )}
         </Box>
