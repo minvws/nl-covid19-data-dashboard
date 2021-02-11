da5f0cf8128c5d9060ef08c81e4d16d0d2133d73
VWS Corona Dashboard <66416946+VWSCoronaDashboard@users.noreply.github.com>
2021-02-11 10:11:51 +0100

    Simplify import and export name

--- packages/app/src/locale/APP_TARGET.ts
+++ packages/app/src/locale/APP_TARGET.ts
@@ -1,7 +1,7 @@
 // This file is meant to trick TypeScript.
 // We're using a webpack plugin to rewrite some imports based on desired locale.
 // This file just exports the type from the NL file so all types still work.
-import localefile from './nl.json';
+import locale from './nl.json';
 
-export type TLocale = typeof localefile;
-export default localefile;
+export type TLocale = typeof locale;
+export default locale;
