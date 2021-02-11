cd0c9d949ba49d626ad84b9dbd0e25484b3cff89
VWS Corona Dashboard <66416946+VWSCoronaDashboard@users.noreply.github.com>
2021-02-11 10:26:10 +0100

    Documentation syntax

--- packages/app/src/locale/APP_TARGET.ts
+++ packages/app/src/locale/APP_TARGET.ts
@@ -1,6 +1,7 @@
-// This file is meant to trick TypeScript.
-// We're using a webpack plugin to rewrite some imports based on desired locale.
-// This file just exports the type from the NL file so all types still work.
+/** This file is meant to trick TypeScript. We're using a webpack plugin to
+ * rewrite some imports based on desired locale. This file just exports the
+ * type from the NL file so all types still work.
+ */
 import locale from './nl.json';
 
 export type TLocale = typeof locale;
