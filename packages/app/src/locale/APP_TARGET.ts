db1dbba0a382c9b3b05a5f5ec59361e41d2ccdc8
VWS Corona Dashboard <66416946+VWSCoronaDashboard@users.noreply.github.com>
2021-02-11 11:00:24 +0100

    Ensure TS throws errors when locales mismatch

--- packages/app/src/locale/APP_TARGET.ts
+++ packages/app/src/locale/APP_TARGET.ts
@@ -5,4 +5,25 @@
 import locale from './nl.json';
 
 export type TLocale = typeof locale;
-export default locale;
+
+import en from './en.json';
+import nl from './nl.json';
+
+export type TNLLocale = typeof nl;
+export type TENLocale = typeof en;
+
+export type TLanguages = {
+  nl: TNLLocale;
+  en: TENLocale;
+};
+
+export type TALLLanguages = TNLLocale | TENLocale;
+
+export type TLanguageKey = keyof TLanguages;
+const languages: TLanguages = { en, nl } as const;
+
+export const targetLanguage: TLanguageKey =
+  (process.env.NEXT_PUBLIC_LOCALE as TLanguageKey) || 'nl';
+const dictionary: TALLLanguages = languages[targetLanguage];
+
+export default dictionary;
