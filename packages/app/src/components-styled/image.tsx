cec3470167b2458ab6f952c4f48af3d2810cdd00
VWS Corona Dashboard <66416946+VWSCoronaDashboard@users.noreply.github.com>
2021-02-12 12:42:52 +0100

    Use an img instead of a Box as base for images

--- packages/app/src/components-styled/image.tsx
+++ packages/app/src/components-styled/image.tsx
@@ -1,16 +1,7 @@
 import css from '@styled-system/css';
 import styled from 'styled-components/';
-import { Box } from '~/components-styled/base';
 
-type ImageProps = {
-  src: string;
-};
-
-export const Image = styled(Box).attrs({
-  as: 'img',
-  lazy: true,
-  decoding: 'async',
-})<ImageProps>(
+export const Image = styled.img(
   css({
     display: 'block',
     maxWidth: '100%',
