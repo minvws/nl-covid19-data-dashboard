153e5a0061b29d617e6931ae5e331075eddd0626
VWSCoronaDashboard14 <76471292+VWSCoronaDashboard14@users.noreply.github.com>
2021-02-12 14:36:56 +0100

    Reduce padding of tiles on mobile by 1rem

--- packages/app/src/components-styled/tile.tsx
+++ packages/app/src/components-styled/tile.tsx
@@ -1,13 +1,14 @@
 import { Box } from './base';
 import { css } from '@styled-system/css';
 import styled from 'styled-components';
+import { asResponsiveArray } from '~/style/utils';
 
 export const Tile = styled(Box).attrs({ as: 'article' })(
   css({
     display: 'flex',
     flexDirection: 'column',
     bg: 'white',
-    p: 4,
+    p: asResponsiveArray({ _: 3, sm: 4 }),
     borderRadius: 1,
     boxShadow: 'tile',
   })
