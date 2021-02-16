a5e8c44ca5a04bae5d73c9a4d630de1862f55031
J <66416946+VWSCoronaDashboard@users.noreply.github.com>
2021-02-16 10:06:54 +0100

    Load IE11 polyfills in _document.js (#1912)
    *  Load polyfills from polyfill.io with the nomodule attribute

--- packages/app/src/pages/_document.tsx
+++ packages/app/src/pages/_document.tsx
@@ -45,13 +45,25 @@ class MyDocument extends Document {
     return (
       <Html lang={locale} className="has-no-js">
         <Head>
+          {/**
+           * Because we add the nomodule attribute polyfill.io is only called on IE11
+           * IE11 will never complain about CSP policies, and modern browsers don't
+           * need to load the polyfills.
+           *
+           * IMPORTANT:
+           * This polyfill only loads for IE11.
+           * Other polyfills should be imported in _app.ts!
+           * */}
+          <script
+            noModule
+            src="https://polyfill.io/v3/polyfill.js?features=Intl.~locale.en,Intl.~locale.nl%2CDate.now%2CDate.prototype.toISOString%2CIntl.DateTimeFormat%2CIntl.DateTimeFormat.prototype.formatToParts%2CIntl.getCanonicalLocales"
+          />
           <script src="/init.js" async />
         </Head>
         <body>
           <Main />
 
           <script src="/piwik.js"></script>
-
           <noscript>
             <img
               src="https://statistiek.rijksoverheid.nl/piwik/piwik.php?idsite=7939&rec=1"
