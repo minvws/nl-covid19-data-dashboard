0f37acafbeb354b6232001170be9fe0a3e5257bf
K <73584448+VWSCoronaDashboard10@users.noreply.github.com>
2021-02-18 10:18:10 +0100

    wip

--- packages/app/src/components-styled/background-image.tsx
+++ packages/app/src/components-styled/background-image.tsx
@@ -1,7 +1,6 @@
-import styled from 'styled-components';
+import { maybeTransformImageUrlToWebp } from '~/lib/sanity';
+import styled, { css } from 'styled-components';
 import {
-  backgroundImage,
-  BackgroundImageProps,
   backgroundPosition,
   BackgroundPositionProps,
   backgroundRepeat,
@@ -15,8 +14,11 @@ import {
 } from 'styled-system';
 import { styledShouldForwardProp } from '~/utils/styled-should-forward-prop';
 
-export type BackgroundImageLocalProps = BackgroundImageProps &
-  BackgroundPositionProps &
+export type BackgroundImageLocalProps = {
+  backgroundImageUrl: string;
+  backgroundImagePrefix?: string;
+  backgroundImageSuffix?: string;
+} & BackgroundPositionProps &
   BackgroundSizeProps &
   BackgroundRepeatProps &
   LayoutProps &
@@ -26,10 +28,41 @@ export const BackgroundImage = styled.div.withConfig({
   shouldForwardProp: styledShouldForwardProp,
 })<BackgroundImageLocalProps>(
   { boxSizing: 'border-box', minWidth: 0 },
-  backgroundImage,
   backgroundPosition,
   backgroundSize,
   backgroundRepeat,
   layout,
-  position
+  position,
+  (x) => {
+    const webpUrl = maybeTransformImageUrlToWebp(x.backgroundImageUrl);
+    const prefix = x.backgroundImagePrefix ? `${x.backgroundImagePrefix},` : '';
+    const suffix = x.backgroundImageSuffix ? `,${x.backgroundImageSuffix}` : '';
+
+    return css`
+      background-image: url('${x.backgroundImageUrl}');
+
+      @supports (
+        background-image: ${prefix} url('${x.backgroundImageUrl}') ${suffix}
+      ) {
+        background-image: ${prefix} url('${x.backgroundImageUrl}') ${suffix};
+      }
+
+      ${webpUrl &&
+      css`
+        /* Chrome 66+, Edge 79+, Opera 53+, Android Brower 80+ */
+        @media screen and (min-resolution: 0.001dpcm) and (-webkit-min-device-pixel-ratio: 0) {
+          @supports (
+            background-image: ${prefix} -webkit-image-set(url('${webpUrl}') 1x) ${suffix}
+          ) {
+            background-image: ${prefix} -webkit-image-set(url('${webpUrl}') 1x) ${suffix};
+          }
+        }
+
+        /* FF 66+ -- note the double parentheses are necessary (styled components bug?) */
+        @supports ((flex-basis: max-content)) and ((-moz-appearance: meterbar)) {
+          background-image: ${prefix} url('${webpUrl}') ${suffix};
+        }
+      `}
+    `;
+  }
 );
--- packages/app/src/components-styled/highlight-teaser.tsx
+++ packages/app/src/components-styled/highlight-teaser.tsx
@@ -101,7 +101,7 @@ function CoverImage({ height, image }: CoverImageProps) {
     <Box height={height} overflow="hidden">
       <BackgroundImage
         height={height}
-        backgroundImage={`url(${url})`}
+        backgroundImageUrl={url}
         backgroundPosition={bgPosition}
         backgroundRepeat="no-repeat"
         backgroundSize="cover"
