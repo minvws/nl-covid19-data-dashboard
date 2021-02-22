3e9163e941388912e9cb0a3eadb3505e286edb57
VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2021-02-22 11:02:46 +0100

    Fix null checks

--- packages/app/src/domain/vaccines/use-vaccine-names.ts
+++ packages/app/src/domain/vaccines/use-vaccine-names.ts
@@ -1,9 +1,12 @@
 import { NlVaccineAdministeredValue } from '@corona-dashboard/common';
+import { useMemo } from 'react';
 import { colors } from '~/style/theme';
 
 export function useVaccineNames(
   lastValue: NlVaccineAdministeredValue
 ): string[] {
-  const productNames = Object.keys(colors.data.vaccines);
-  return productNames.filter((x) => (lastValue as any).hasOwnProperty(x));
+  return useMemo(() => {
+    const productNames = Object.keys(colors.data.vaccines);
+    return productNames.filter((x) => (lastValue as any)[x] !== undefined);
+  }, [lastValue]);
 }
