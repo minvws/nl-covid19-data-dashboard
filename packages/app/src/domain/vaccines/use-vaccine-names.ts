4c7272c7a7e71db5a8c4c06912e91e55e0bfdc1f
VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2021-02-22 12:38:30 +0100

    Tweak

--- packages/app/src/domain/vaccines/use-vaccine-names.ts
+++ packages/app/src/domain/vaccines/use-vaccine-names.ts
@@ -1,12 +1,24 @@
-import { NlVaccineAdministeredValue } from '@corona-dashboard/common';
+import { assert, NlVaccineAdministeredValue } from '@corona-dashboard/common';
 import { useMemo } from 'react';
 import { colors } from '~/style/theme';
+
+const vaccines = [
+  'pfizer',
+  'moderna',
+  'astra_zeneca',
+  'cure_vac',
+  'janssen',
+  'sanofi',
+] as const;
+
+vaccines.forEach((x) =>
+  assert(colors.data.vaccines[x], `missing vaccine color for vaccine ${x}`)
+);
 
 export function useVaccineNames(
   lastValue: NlVaccineAdministeredValue
 ): string[] {
   return useMemo(() => {
-    const productNames = Object.keys(colors.data.vaccines);
-    return productNames.filter((x) => (lastValue as any)[x] !== undefined);
+    return vaccines.filter((x) => (lastValue as any)[x] !== undefined);
   }, [lastValue]);
 }
