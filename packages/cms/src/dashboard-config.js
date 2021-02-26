72c6e201cd8cf851f8740e1ecba63c93b02419db
VWS Corona Dashboard <66416946+VWSCoronaDashboard@users.noreply.github.com>
2021-02-26 12:09:29 +0100

    Add 2 columns to add new articles or editorials

--- packages/cms/src/dashboard-config.js
+++ packages/cms/src/dashboard-config.js
@@ -6,6 +6,34 @@ export default {
         title: 'Documents with unpublished changes',
         query: '*[_id in path("drafts.**")] | order(_updatedAt desc)',
       },
+      layout: {
+        width: 'small',
+        height: 'small',
+      },
+    },
+    {
+      name: 'document-list',
+      options: {
+        title: 'Last edited articles',
+        order: '_updatedAt desc',
+        types: ['article'],
+      },
+      layout: {
+        width: 'small',
+        height: 'small',
+      },
+    },
+    {
+      name: 'document-list',
+      options: {
+        title: 'Last edited editorials',
+        order: '_updatedAt desc',
+        types: ['editorial'],
+      },
+      layout: {
+        width: 'small',
+        height: 'small',
+      },
     },
   ],
 };
