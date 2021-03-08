490aad813612ca4d02b0708ab7dc0ca5bb367bbe
VWS Corona Dashboard <75375917+VWSCoronaDashboard13@users.noreply.github.com>
2021-03-08 19:00:13 +0100

    expand animation

--- packages/app/src/components-styled/collapsible-button.tsx
+++ packages/app/src/components-styled/collapsible-button.tsx
@@ -23,44 +23,61 @@ export function CollapsibleButton({ label, children }: CollapsibleButtonProps) {
   const openHeight =
     buttonHeight && contentHeight ? buttonHeight + contentHeight : 0;
   return (
-    <Container minHeight={open ? openHeight : 0}>
+    <Box as="section">
       <Disclosure open={open} onChange={() => setOpen(!open)}>
-        <ExpandButton ref={buttonRef}>
-          {label}
-          <Chevron open={open} />
-        </ExpandButton>
-
+        <Box display="flex" alignItems="center">
+          <Background
+            minWidth={open ? '100%' : 0}
+            css={css({
+              '&::after': {
+                content: '""',
+                position: 'absolute',
+                zIndex: 0,
+                top: 0,
+                bottom: 0,
+                left: 0,
+                right: 0,
+                bg: 'tileGray',
+                minHeight: open ? openHeight : 0,
+                transitionProperty: 'min-height',
+                transitionDuration: '0.5s',
+              },
+            })}
+          >
+            <ExpandButton ref={buttonRef}>
+              {label}
+              <Chevron open={open} />
+            </ExpandButton>
+          </Background>
+        </Box>
         <Content style={{ height: open ? contentHeight : 0 }}>
           <div ref={contentRef}>{children}</div>
         </Content>
       </Disclosure>
-    </Container>
+    </Box>
   );
 }
 
-const Container = styled(Box).attrs({ as: 'section' })(
+const Background = styled(Box)(
   css({
     position: 'relative',
-    display: 'inline-flex',
-    flexDirection: 'column',
     padding: 0,
     margin: '0 auto',
     transitionProperty: 'min-width, min-height',
     transitionDuration: '0.5s',
-    bg: 'tileGray',
-    width: '100%',
   })
 );
 
 const Content = styled(DisclosurePanel)(
   css({
-    transitionProperty: 'height, opacity',
-    transitionDuration: '0.5s',
-    width: '100%',
+    display: 'block',
     overflow: 'hidden',
+    transition: 'height .3s, opacity 0.5s',
+    width: '100%',
     opacity: 0,
     '&[data-state="open"]': {
       opacity: 1,
+      transition: 'height .5s, opacity 1s 0.2s',
     },
   })
 );
@@ -77,6 +94,7 @@ const ExpandButton = styled(DisclosureButton)(
     cursor: 'pointer',
     fontWeight: 'bold',
     zIndex: 1,
+    width: '100%',
 
     '&:focus': {
       outlineWidth: '1px',
