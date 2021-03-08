2a66650872e680e3a2a2c70c659a369469621894
VWSCoronaDashboard14 <76471292+VWSCoronaDashboard14@users.noreply.github.com>
2021-03-08 15:06:20 +0100

    Add schema for risk leveels for safety regions

--- packages/common/src/types/data.ts
+++ packages/common/src/types/data.ts
@@ -840,6 +840,9 @@ export interface RegionalElderlyAtHomeValue {
 export interface VrEscalationLevel {
   date_unix: number;
   level: number;
+  date_start_measurement: number;
+  date_end_measurement: number;
+  date_next_estimate: number;
   valid_from_unix: number;
   last_determined_unix: number;
   date_of_insertion_unix: number;
@@ -897,6 +900,9 @@ export interface EscalationLevels {
   date_unix: number;
   vrcode: string;
   level: number;
+  date_start_measurement: number;
+  date_end_measurement: number;
+  date_next_estimate: number;
   valid_from_unix: number;
   last_determined_unix: number;
   date_of_insertion_unix: number;
