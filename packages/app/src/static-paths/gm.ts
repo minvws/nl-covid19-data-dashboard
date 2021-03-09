10f33a1a128887c74746815a8122cae9d84bd7f6
VWS Corona Dashboard <66416946+VWSCoronaDashboard@users.noreply.github.com>
2021-03-09 17:24:22 +0100

    Test build times with all getStaticPaths emptied

--- packages/app/src/static-paths/gm.ts
+++ packages/app/src/static-paths/gm.ts
@@ -1,16 +1,17 @@
-import municipalities from '~/data/municipalSearchData';
+// import municipalities from '~/data/municipalSearchData';
 
 /**
  * getStaticPaths creates an array of all the allowed `/gemeente/[code]` routes.
  */
 export function getStaticPaths() {
-  const paths = municipalities.map((municipality) => ({
-    params: { code: municipality.gemcode },
-  }));
+  // const paths = municipalities.map((municipality) => ({
+  //   params: { code: municipality.gemcode },
+  // }));
+
+  const paths = [] as any[];
 
   return {
     paths,
-    // other routes should 404
-    fallback: false,
+    fallback: 'blocking',
   };
 }
