b55df98aa5c8554f15990a16c6bf4cc3883233f8
VWSCoronaDashboard14 <76471292+VWSCoronaDashboard14@users.noreply.github.com>
2021-03-10 08:41:27 +0100

    Revert "Add date range GGD positive tested (#2238)"
    This reverts commit afcd23469e75fbb2809cbbe7f277de7d90f5e325.

--- packages/app/src/pages/veiligheidsregio/[code]/positief-geteste-mensen.tsx
+++ packages/app/src/pages/veiligheidsregio/[code]/positief-geteste-mensen.tsx
@@ -63,6 +63,7 @@ const PositivelyTestedPeople: FCWithLayout<typeof getStaticProps> = (props) => {
   const router = useRouter();
 
   const lastValue = data.tested_overall.last_value;
+
   const ggdAverageLastValue = data.tested_ggd_average.last_value;
   const ggdDailyValues = data.tested_ggd_daily.values;
 
@@ -265,10 +266,7 @@ const PositivelyTestedPeople: FCWithLayout<typeof getStaticProps> = (props) => {
           <KpiTile
             title={ggdText.totaal_getest_week_titel}
             metadata={{
-              date: [
-                ggdAverageLastValue.date_start_unix,
-                ggdAverageLastValue.date_end_unix,
-              ],
+              date: ggdAverageLastValue.date_end_unix,
               source: ggdText.bronnen.rivm,
             }}
           >
