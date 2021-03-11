c3fbce7e994ad091c8e90edd532c21461adb9aad
VWS Corona Dashboard <66416946+VWSCoronaDashboard@users.noreply.github.com>
2021-03-11 11:27:48 +0100

    Add build script and update Docker file

---
+++ build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+start=`date +%s`
+set -e # Any subsequent(*) commands which fail will cause the shell script to exit immediately
+
+# These are the environment variables we need to build the application.
+# We also define defaults in case you're running this locally
+# SANITY_DATASET="${SANITY_DATASET:-development}"
+
+# Zip JSON files so the data is included in the build.
+cd packages/app/public/ && zip -qq latest-data.zip json/* && cd ../../../
+
+# Prepare the export folders
+mkdir -p exports/nl
+mkdir -p exports/en
+
+# Install dependencies
+yarn
+yarn workspace @corona-dashboard/common build
+
+# Validate data
+yarn workspace @corona-dashboard/cli validate-json
+
+# Prepare types and assets
+yarn workspace @corona-dashboard/cli generate-typescript
+yarn workspace @corona-dashboard/cms sync-assets
+
+# Build the Dutch application and move to export folder
+NEXT_PUBLIC_LOCALE="nl"
+yarn workspace @corona-dashboard/app build
+yarn workspace @corona-dashboard/app export
+mv packages/app/out exports/nl
+
+# Do the same thing again for EN build
+NEXT_PUBLIC_LOCALE="en"
+yarn workspace @corona-dashboard/app build
+yarn workspace @corona-dashboard/app export
+mv packages/app/out exports/en
+
+# Done
+end=`date +%s`
+runtime=$((end-start))
+echo "Build executed in $runtime seconds, proceed to build Dockerfile."
+
+# Now you need to copy .exports/ to the Nginx folder.
+# The Dockerfile will take care of this
