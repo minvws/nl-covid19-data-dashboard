363b6fdde22bb377d79edf725fbdeb0f6c452043
VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2021-03-16 18:42:59 +0100

    Add dotenv to download script

--- packages/app/infra/download.js
+++ packages/app/infra/download.js
@@ -1,7 +1,12 @@
+const path = require('path');
+const config = { path: path.resolve('.env.local') };
+require('dotenv').config(config);
 const download = require('download');
+
 const API_URL =
   process.env.API_URL ||
   'https://coronadashboard.rijksoverheid.nl/json/latest-data.zip';
+
 (async () => {
   await download(API_URL, './public/json', {
     extract: true,
