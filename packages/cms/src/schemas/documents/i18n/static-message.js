2a9f7607ab329011dfb508fc91b38dd94bc6e4dc
J <66416946+VWSCoronaDashboard@users.noreply.github.com>
2021-03-24 17:28:40 +0100

    Take code from static messages poc
    Restore package.json
    
    Restore .gitignore
    
    Add POC script for importing lokalise files into Sanity
    
    change schema slightly
    
    Add sanity client configuration
    
    Sanity client and documentation
    
    install promise queue
    
    Invalidate token and read from process.env from now on
    
    Add export script to create nl.json and en.json from Sanity messages

--- packages/cms/src/schemas/documents/i18n/static-message.js
+++ packages/cms/src/schemas/documents/i18n/static-message.js
@@ -1,49 +1,49 @@
-import { FaLanguage } from "react-icons/fa";
+import { FaLanguage } from 'react-icons/fa';
 
 export default {
-  name: "message",
-  title: "Translation Messages",
+  name: 'message',
+  title: 'Translation Messages',
   icon: FaLanguage,
-  type: "document",
+  type: 'document',
   fields: [
     {
-      name: "key",
-      title: "Message Key (extracted from source)",
-      type: "string",
+      name: 'key',
+      title: 'Message Key (extracted from source)',
+      type: 'string',
     },
     {
-      name: "description",
-      title: "Description (optional)",
-      description: "Describe the key, where is it used and for what purposes?",
-      type: "string",
+      name: 'description',
+      title: 'Description (optional)',
+      description: 'Describe the key, where is it used and for what purposes?',
+      type: 'string',
     },
     {
-      name: "translations",
-      title: "Value",
-      type: "object",
+      name: 'translations',
+      title: 'Value',
+      type: 'object',
       fields: [
         {
-          name: "original",
-          title: "Dutch: Original value",
+          name: 'default',
+          title: 'Original value in source code',
           description:
-            "This is extracted directly from the sourcecode and not editable.",
-          type: "string",
+            'This is extracted directly from the sourcecode and not editable.',
+          type: 'string',
           validation: (Rule) => Rule.required(),
-          // readOnly: true,
+          readOnly: true,
         },
         {
-          name: "nl",
-          title: "Dutch: Override default value (optional)",
+          name: 'nl',
+          title: 'Dutch: Override default value (optional)',
           description:
             "We will use this value if it's provided. If not filled in, we use the original language value in the front-end.",
-          type: "string",
+          type: 'string',
         },
         {
-          name: "en",
-          title: "English (required)",
+          name: 'en',
+          title: 'English (required)',
           description:
-            "This is the english translation of the original value. This is required.",
-          type: "string",
+            'This is the english translation of the original value. This is required.',
+          type: 'string',
           validation: (Rule) => Rule.required(),
         },
       ],
@@ -51,8 +51,8 @@ export default {
   ],
   preview: {
     select: {
-      title: "description",
-      subtitle: "value.nl",
+      title: 'key',
+      subtitle: 'translations.default',
     },
   },
 };
