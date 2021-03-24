6e246249fab15e6a274c7ee1f0efaadbded02232
VWS Corona Dashboard <66416946+VWSCoronaDashboard@users.noreply.github.com>
2021-03-24 16:44:44 +0100

    Update schema

--- packages/cms/schemas/documents/i18n/static-message.js
+++ packages/cms/schemas/documents/i18n/static-message.js
@@ -7,19 +7,46 @@ export default {
   type: "document",
   fields: [
     {
-      name: "description",
-      title: "Description",
-      type: "string",
-    },
-    {
       name: "key",
-      title: "System Key",
+      title: "Message Key (extracted from source)",
       type: "string",
     },
     {
-      name: "value",
+      name: "description",
+      title: "Description (optional)",
+      description: "Describe the key, where is it used and for what purposes?",
+      type: "string",
+    },
+    {
+      name: "translations",
       title: "Value",
-      type: "localeString",
+      type: "object",
+      fields: [
+        {
+          name: "original",
+          title: "Dutch: Original value",
+          description:
+            "This is extracted directly from the sourcecode and not editable.",
+          type: "string",
+          validation: (Rule) => Rule.required(),
+          // readOnly: true,
+        },
+        {
+          name: "nl",
+          title: "Dutch: Override default value (optional)",
+          description:
+            "We will use this value if it's provided. If not filled in, we use the original language value in the front-end.",
+          type: "string",
+        },
+        {
+          name: "en",
+          title: "English (required)",
+          description:
+            "This is the english translation of the original value. This is required.",
+          type: "string",
+          validation: (Rule) => Rule.required(),
+        },
+      ],
     },
   ],
   preview: {
