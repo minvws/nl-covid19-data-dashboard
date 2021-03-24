a3a554988c2595a51a907ba13cba150c6deee476
VWS Corona Dashboard <66416946+VWSCoronaDashboard@users.noreply.github.com>
2021-03-24 21:51:16 +0100

    Add sanity client configuration

--- packages/cms/src/import/static-messages.ts
+++ packages/cms/src/import/static-messages.ts
@@ -2,6 +2,28 @@ import flatten from 'flat';
 import fs from 'fs';
 import get from 'lodash/get';
 import path from 'path';
+import { assert } from '@corona-dashboard/common';
+
+import sanityConfig from '../../sanity.json';
+
+import sanityClient from '@sanity/client';
+// assert(
+//   process.env.NEXT_PUBLIC_SANITY_DATASET,
+//   'NEXT_PUBLIC_SANITY_DATASET is undefined'
+// );
+
+// assert(
+//   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+//   'NEXT_PUBLIC_SANITY_PROJECT_ID is undefined'
+// );
+
+const config = {
+  dataset: 'development',
+  projectId: sanityConfig.api.projectId,
+  useCdn: false,
+};
+
+export const client = sanityClient(config);
 
 const dutch = JSON.parse(
   fs.readFileSync(path.resolve(__dirname, '../../../app/src/locale/nl.json'), {
