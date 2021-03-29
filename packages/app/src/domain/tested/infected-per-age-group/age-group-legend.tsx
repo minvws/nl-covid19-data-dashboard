b95bcbe1572559b0c34469be5a025e98895a4cc4
A <66416946+VWSCoronaDashboard@users.noreply.github.com>
2021-03-29 13:32:03 +0200

    Infected per age group over time

--- packages/app/src/domain/tested/infected-per-age-group/age-group-legend.tsx
+++ packages/app/src/domain/tested/infected-per-age-group/age-group-legend.tsx
@@ -1,10 +1,13 @@
-import css from '@styled-system/css';
-import styled from 'styled-components';
 import { NlTestedPerAgeGroupValue } from '@corona-dashboard/common';
+import css, { SystemStyleObject } from '@styled-system/css';
+import styled from 'styled-components';
 import { LineSeriesDefinition } from '~/components-styled/time-series-chart/logic';
+import { useIntl } from '~/intl';
 
 interface AgeGroupLegendProps {
-  seriesConfig: LineSeriesDefinition<NlTestedPerAgeGroupValue>[];
+  seriesConfig: (LineSeriesDefinition<NlTestedPerAgeGroupValue> & {
+    isSelected: boolean;
+  })[];
   ageGroupSelection: string[];
   onToggleAgeGroup: (ageGroupRange: string) => void;
   onReset: () => void;
@@ -16,29 +19,62 @@ export function AgeGroupLegend({
   onToggleAgeGroup,
   onReset,
 }: AgeGroupLegendProps) {
+  const { siteText } = useIntl();
+  const text = siteText.infected_per_agroup;
+
+  const hasNoneSelected = ageGroupSelection.length === 0;
+  const hasAllSelected = ageGroupSelection.length === 0;
+
+  function toggleAgeGroup(metricProperty: string) {
+    onToggleAgeGroup(metricProperty);
+
+    /* Without setting focus, the focus is lost on re-render */
+    requestAnimationFrame(() => {
+      const item: HTMLElement | null = document.querySelector(
+        `button[data-metric-property=${metricProperty}]`
+      );
+      if (item) {
+        item.focus();
+      }
+    });
+  }
+
   return (
-    <>
+    <Legend>
       <List>
-        {seriesConfig.map((item, i) => {
-          return (
-            <Item key={i}>
-              <button onClick={() => onToggleAgeGroup(item.metricProperty)}>
-                {item.label}
-                {ageGroupSelection.includes(item.metricProperty)
-                  ? ' SELECTED'
-                  : '-'}
-                <Line color={item.color} />
-              </button>
-            </Item>
-          );
-        })}
+        {seriesConfig.map((item) => (
+          <Item key={item.label}>
+            <ItemButton
+              onClick={() => toggleAgeGroup(item.metricProperty)}
+              isSelected={!hasAllSelected && item.isSelected}
+              color={item.color}
+              data-metric-property={item.metricProperty}
+              data-text={item.label}
+            >
+              {item.label}
+              <Line color={item.color} />
+            </ItemButton>
+          </Item>
+        ))}
       </List>
-      <button disabled={ageGroupSelection.length === 0} onClick={onReset}>
-        RESET!
-      </button>
-    </>
+      <ResetButton
+        onClick={onReset}
+        css={css({
+          visibility: hasNoneSelected ? 'hidden' : 'visible',
+        })}
+      >
+        {text.reset_button_label}
+      </ResetButton>
+    </Legend>
   );
 }
+
+const Legend = styled.div(
+  css({
+    display: 'flex',
+    alignItems: 'start',
+  })
+);
 
 const List = styled.ul(
   css({
@@ -50,27 +86,88 @@ const List = styled.ul(
 
 const Item = styled.li(
   css({
-    my: 1,
+    mb: 2,
     mr: 3,
     position: 'relative',
     display: 'inline-block',
-    pl: '25px', // alignment with shape
   })
 );
 
-const Shape = styled.div<{ color: string }>((x) =>
+/**
+ * Styling for this button contains:
+ * :before to render the colored line on hover/focus
+ * :after to widen the button to avoid font-weight bold jumps
+ */
+const ItemButton = styled.button<{
+  isSelected: boolean;
+  color: string;
+  text?: string;
+}>(({ isSelected, color }) =>
   css({
-    content: '',
+    appearance: 'none',
+    background: 'tileGray',
+    cursor: 'pointer',
+    pr: 10,
+    pl: 30,
+    py: '3px',
+    border: '3px solid',
+    borderColor: isSelected ? color : 'transparent',
+    fontWeight: isSelected ? 'bold' : 'normal',
+    fontFamily: 'inherit',
+    position: 'relative',
+    outline: 'none',
+    display: 'inline-flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    '&:hover,&:focus': {
+      '&:before': {
+        content: '""',
+        background: color,
+        height: '3px',
+        position: 'absolute',
+        left: '-3px',
+        right: '-3px',
+        bottom: '-3px',
+      },
+    },
+    '&:focus': {
+      outline: '1px dashed black',
+    },
+    '&:after': {
+      content: 'attr(data-text)',
+      height: 0,
+      visibility: 'hidden',
+      overflow: 'hidden',
+      userSelect: 'none',
+      pointerEvents: 'none',
+      fontWeight: 'bold',
+      pr: '1px',
+    },
+  })
+);
+
+const ResetButton = styled.button<{ fontWeight?: SystemStyleObject }>(
+  ({ fontWeight = 'normal' }) =>
+    css({
+      backgroundColor: 'blue',
+      color: 'white',
+      p: 20,
+      py: '6px',
+      border: 'none',
+      fontWeight,
+      fontFamily: 'inherit',
+      ml: 40,
+    })
+);
+
+const Line = styled.div<{ color: string }>(({ color }) =>
+  css({
     display: 'block',
     position: 'absolute',
-    left: 0,
-    backgroundColor: x.color,
-  })
-);
-
-const Line = styled(Shape)(
-  css({
-    top: '10px',
+    left: 10,
+    backgroundColor: color,
+    top: '9px',
     width: '15px',
     height: '3px',
     borderRadius: '2px',
--- packages/app/src/domain/tested/infected-per-age-group/infected-per-age-group.tsx
+++ packages/app/src/domain/tested/infected-per-age-group/infected-per-age-group.tsx
@@ -1,63 +1,19 @@
 import { NlTestedPerAgeGroupValue } from '@corona-dashboard/common';
+import css from '@styled-system/css';
 import { useMemo } from 'react';
+import styled from 'styled-components';
 import { TimeSeriesChart } from '~/components-styled/time-series-chart';
+import { TooltipChildren } from '~/components-styled/time-series-chart/components';
 import { LineSeriesDefinition } from '~/components-styled/time-series-chart/logic';
 import { useIntl } from '~/intl';
-import { colors } from '~/style/theme';
 import { AgeGroupLegend } from './age-group-legend';
+import { SERIES_CONFIG } from './series-config';
 import { useAgeGroupSelection } from './use-age-group-selection';
 
 interface InfectedPerAgeGroup {
   values: NlTestedPerAgeGroupValue[];
   timeframe: 'all' | '5weeks' | 'week';
 }
-
-const BASE_AGE_GROUP_SERIES: {
-  metricProperty: keyof NlTestedPerAgeGroupValue;
-  defaultColor: string;
-}[] = [
-  {
-    metricProperty: 'infected_age_0_9_per_100k',
-    defaultColor: colors.data.multiseries.cyan,
-  },
-  {
-    metricProperty: 'infected_age_10_19_per_100k',
-    defaultColor: colors.data.multiseries.cyan_dark,
-  },
-  {
-    metricProperty: 'infected_age_20_29_per_100k',
-    defaultColor: colors.data.multiseries.yellow,
-  },
-  {
-    metricProperty: 'infected_age_30_39_per_100k',
-    defaultColor: colors.data.multiseries.yellow_dark,
-  },
-  {
-    metricProperty: 'infected_age_40_49_per_100k',
-    defaultColor: colors.data.multiseries.turquoise,
-  },
-  {
-    metricProperty: 'infected_age_50_59_per_100k',
-    defaultColor: colors.data.multiseries.turquoise_dark,
-  },
-  {
-    metricProperty: 'infected_age_60_69_per_100k',
-    defaultColor: colors.data.multiseries.orange,
-  },
-  {
-    metricProperty: 'infected_age_70_79_per_100k',
-    defaultColor: colors.data.multiseries.orange_dark,
-  },
-  {
-    metricProperty: 'infected_age_80_89_per_100k',
-    defaultColor: colors.data.multiseries.magenta,
-  },
-  {
-    metricProperty: 'infected_age_90_plus_per_100k',
-    defaultColor: colors.data.multiseries.magenta_dark,
-  },
-  { metricProperty: 'infected_overall_per_100k', defaultColor: colors.gray },
-];
 
 export function InfectedPerAgeGroup({
   values,
@@ -72,37 +28,54 @@ export function InfectedPerAgeGroup({
   const { siteText } = useIntl();
   const text = siteText.infected_per_agroup;
 
-  const ageGroupSeriesConfig: LineSeriesDefinition<NlTestedPerAgeGroupValue>[] = useMemo(() => {
-    return BASE_AGE_GROUP_SERIES.map((baseAgeGroup) => ({
-      metricProperty: baseAgeGroup.metricProperty,
-      type: 'line',
-      label: text.legend[baseAgeGroup.metricProperty],
-      color:
+  const ageGroupLegendConfig: (LineSeriesDefinition<NlTestedPerAgeGroupValue> & {
+    isSelected: boolean;
+  })[] = useMemo(() => {
+    return SERIES_CONFIG.map((baseAgeGroup) => {
+      const isSelected =
         ageGroupSelection.includes(baseAgeGroup.metricProperty) ||
-        ageGroupSelection.length === 0
-          ? baseAgeGroup.defaultColor
-          : 'rgba(160, 160, 160, 0.5)',
-    }));
+        ageGroupSelection.length === 0;
+      return {
+        ...baseAgeGroup,
+        type: 'line',
+        label:
+          baseAgeGroup.metricProperty in text.legend
+            ? text.legend[baseAgeGroup.metricProperty]
+            : baseAgeGroup.metricProperty,
+        isSelected,
+      };
+    });
   }, [ageGroupSelection, text.legend]);
 
+  const ageGroupChartConfig = useMemo(() => {
+    return ageGroupLegendConfig.filter((x) => x.isSelected);
+  }, [ageGroupLegendConfig]);
+
+  /* Conditionally wrap tooltip over two columns due to amount of items */
+  const tooltipColumns =
+    ageGroupSelection.length === 0 || ageGroupSelection.length > 4 ? 2 : 1;
+  const InfectedPerAgeGroupWrapper = styled.div(
+    css({
+      [`${TooltipChildren}`]: {
+        columns: tooltipColumns,
+      },
+    })
+  );
+
   return (
-    <>
+    <InfectedPerAgeGroupWrapper>
       <TimeSeriesChart
         values={values}
         timeframe={timeframe}
-        seriesConfig={[...ageGroupSeriesConfig]}
-        dataOptions={{
-          isPercentage: true,
-        }}
-        onSeriesClick={(config) => toggleAgeGroup(config.label)}
+        seriesConfig={[...ageGroupChartConfig]}
         disableLegend
       />
       <AgeGroupLegend
-        seriesConfig={[...ageGroupSeriesConfig]}
+        seriesConfig={[...ageGroupLegendConfig]}
         ageGroupSelection={ageGroupSelection}
         onToggleAgeGroup={toggleAgeGroup}
         onReset={resetAgeGroupSelection}
       />
-    </>
+    </InfectedPerAgeGroupWrapper>
   );
 }
