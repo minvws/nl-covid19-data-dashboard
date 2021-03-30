d9395335ec394ee93d71c427d33ba3222054f39f
VWS Corona Dashboard <75375917+VWSCoronaDashboard13@users.noreply.github.com>
2021-03-30 13:37:59 +0200

    cleanup

--- packages/app/src/domain/tested/g-number-bar-chart-tile.tsx
+++ packages/app/src/domain/tested/g-number-bar-chart-tile.tsx
@@ -1,4 +1,4 @@
-import { NlGNumber, NlGNumberValue } from '@corona-dashboard/common';
+import { NlGNumber, VrGNumber } from '@corona-dashboard/common';
 import { ChartTileWithTimeframe } from '~/components-styled/chart-tile';
 import { InlineText } from '~/components-styled/typography';
 import { VerticalBarChart } from '~/components-styled/vertical-bar-chart';
@@ -6,27 +6,8 @@ import { useIntl } from '~/intl';
 import { colors } from '~/style/theme';
 import { TimeframeOption } from '~/utils/timeframe';
 
-function generateDummyData() {
-  const data = [];
-  const currentDate = new Date();
-  currentDate.setHours(0, 0, 0, 0);
-
-  for (let i = 0; i < 5 * 7; ++i) {
-    data.push({
-      g_number: i % 3 === 0 ? -Math.random() * 40 : Math.random() * 40,
-      date_of_insertion_unix: 0,
-      date_start_unix: new Date(currentDate.getTime() - 7).getTime() / 1000,
-      date_end_unix: currentDate.getTime() / 1000,
-    });
-
-    currentDate.setDate(currentDate.getDate() - 1);
-  }
-
-  return data.reverse();
-}
-
 interface GNumberBarChartTileProps {
-  data: NlGNumber;
+  data: NlGNumber | VrGNumber;
   timeframeOptions?: TimeframeOption[];
 }
 
@@ -38,10 +19,8 @@ export function GNumberBarChartTile({
 
   const text = siteText.g_number.bar_chart;
 
-  // Dummy Data
-  const values: NlGNumberValue[] = __data?.values ?? generateDummyData();
-  const last_value: NlGNumberValue =
-    __data?.last_value ?? values[values.length - 1];
+  const values = __data.values;
+  const last_value = __data.last_value;
 
   return (
     <ChartTileWithTimeframe
