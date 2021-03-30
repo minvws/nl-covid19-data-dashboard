5a4277a90a2a60a6e2391e2df0b1cce122f4e958
VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2021-03-30 14:06:25 +0200

    first tweaks

--- packages/app/src/intl/index.ts
+++ packages/app/src/intl/index.ts
@@ -1,6 +1,1 @@
-import { createContext } from 'react';
-import { languages } from '~/locale';
-
-export const IntlContext = createContext(languages['nl']);
-
 export { useIntl } from './hooks/use-intl';
