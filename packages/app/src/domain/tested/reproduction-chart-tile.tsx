517f83212c9c06d3c61f4a18a9e5610cfc790989
VWS Corona Dashboard <75375917+VWSCoronaDashboard13@users.noreply.github.com>
2021-04-01 14:12:33 +0200

    r-chart conversion

---
+++ packages/app/src/domain/tested/reproduction-chart-tile.tsx
@@ -0,0 +1,66 @@
+import { useIntl } from '~/intl';
+import { colors } from '~/style/theme';
+import { NationalReproduction } from '@corona-dashboard/common';
+import { ChartTileWithTimeframe } from '~/components-styled/chart-tile';
+import { TimeSeriesChart } from '~/components-styled/time-series-chart';
+import { TimeframeOption } from '~/utils/timeframe';
+
+interface ReproductionChartTileProps {
+  data: NationalReproduction;
+  timeframeOptions?: TimeframeOption[];
+  timeframeInitialValue?: TimeframeOption;
+}
+
+export function ReproductionChartTile({
+  data: __data,
+  timeframeOptions = ['all', '5weeks'],
+  timeframeInitialValue = 'all',
+}: ReproductionChartTileProps) {
+  const { siteText } = useIntl();
+  const text = siteText.reproductiegetal;
+
+  /**
+   * There is no data for the last 2 weeks so we are getting a slice
+   * of all the values before the first datapoint with a null value to
+   * display in the chart
+   */
+  const values = __data.values.slice(
+    0,
+    __data.values.map((x) => x.index_average).indexOf(null)
+  );
+  const last_value = __data.last_value;
+
+  return (
+    <ChartTileWithTimeframe
+      title={text.linechart_titel}
+      description={text.legenda_r}
+      timeframeOptions={timeframeOptions}
+      timeframeInitialValue={timeframeInitialValue}
+      metadata={{
+        date: last_value.date_of_insertion_unix,
+        source: text.bronnen.rivm,
+      }}
+    >
+      {(timeframe) => (
+        <TimeSeriesChart
+          values={values}
+          timeframe={timeframe}
+          seriesConfig={[
+            {
+              type: 'line',
+              metricProperty: 'index_average',
+              label: 'R',
+              color: colors.data.primary,
+            },
+          ]}
+          dataOptions={{
+            benchmark: {
+              value: 1,
+              label: siteText.common.signaalwaarde,
+            },
+          }}
+        />
+      )}
+    </ChartTileWithTimeframe>
+  );
+}
