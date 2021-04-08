e0b6daa5a5f89380b236ed1fa128765e4cf29fff
A <69853534+VWSCoronaDashboard4@users.noreply.github.com>
2021-04-08 14:21:51 +0200

    Remove stock and delivery tiles and schema (#2551)

--- packages/common/src/types/data.ts
+++ packages/common/src/types/data.ts
@@ -172,7 +172,6 @@ export interface National {
   vaccine_delivery: NlVaccineDelivery;
   vaccine_delivery_estimate: NlVaccineDeliveryEstimate;
   vaccine_delivery_per_supplier: NlVaccineDeliveryPerSupplier;
-  vaccine_delivery_estimate_time_span: NlVaccineDeliveryEstimateTimeSpan;
   vaccine_administered: NlVaccineAdministered;
   vaccine_administered_estimate: NlVaccineAdministeredEstimate;
   vaccine_administered_care_institutions: NlVaccineAdministeredCareInstitutions;
@@ -592,18 +591,6 @@ export interface NlVaccineDeliveryPerSupplierValue {
   sanofi?: number;
   is_estimate: boolean;
   week_number: number;
-  date_of_insertion_unix: number;
-  date_start_unix: number;
-  date_end_unix: number;
-  date_of_report_unix: number;
-}
-export interface NlVaccineDeliveryEstimateTimeSpan {
-  values: NlVaccineDeliveryEstimateTimeSpanValue[];
-  last_value: NlVaccineDeliveryEstimateTimeSpanValue;
-}
-export interface NlVaccineDeliveryEstimateTimeSpanValue {
-  doses: number;
-  time_span_weeks: number;
   date_of_insertion_unix: number;
   date_start_unix: number;
   date_end_unix: number;
