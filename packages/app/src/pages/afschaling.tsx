95d9a18782c3e08b873833e781455ba8c1dda8a2
VWSCoronaDashboard14 <76471292+VWSCoronaDashboard14@users.noreply.github.com>
2021-04-08 10:57:22 +0200

    Add minitrend layout for downscaling

--- packages/app/src/pages/afschaling.tsx
+++ packages/app/src/pages/afschaling.tsx
@@ -5,13 +5,31 @@ import { MaxWidth } from '~/components-styled/max-width';
 import { WarningTile } from '~/components-styled/warning-tile';
 import { Layout } from '~/domain/layout/layout';
 import { useIntl } from '~/intl';
+import { ReactNode } from 'react';
 import {
   createGetStaticProps,
   StaticProps,
 } from '~/static-props/create-get-static-props';
 import { getLastGeneratedDate } from '~/static-props/get-data';
-
+import Repro from '~/assets/reproductiegetal-small.svg';
+import { Text } from '~/components-styled/typography';
+import IconDown from '~/assets/pijl-omlaag.svg';
+import css from '@styled-system/css';
+import { replaceVariablesInText } from '~/utils/replaceVariablesInText';
+import { asResponsiveArray } from '~/style/utils';
+import styled from 'styled-components';
+import Arts from '~/assets/arts-small.svg';
+import Ziekenhuis from '~/assets/ziekenhuis-small.svg';
 export const getStaticProps = createGetStaticProps(getLastGeneratedDate);
+
+const reproduction_is_below_threshold = false;
+const reproduction_threshold_day_span = 14;
+
+const intensive_care_nice_is_below_threshold = true;
+const intensive_care_nice_threshold_day_span = 14;
+
+const hospital_nice_is_below_threshold = false;
+const hospital_nice_threshold_day_span = 14;
 
 const Afschaling = (props: StaticProps<typeof getStaticProps>) => {
   const { siteText } = useIntl();
@@ -47,10 +65,129 @@ const Afschaling = (props: StaticProps<typeof getStaticProps>) => {
               />
             </Box>
           )}
+
+          <Box
+            display={'flex'}
+            justifyContent={{ _: undefined, md: 'space-between' }}
+            flexDirection={{ _: 'column', md: 'row' }}
+            mb={5}
+          >
+            <MiniTrendBox>
+              <MiniTrend
+                title="Reproductiegetal"
+                icon={Repro()}
+                is_below_threshold={reproduction_is_below_threshold}
+                threshold_day_span={reproduction_threshold_day_span}
+              >
+                <p>Hier komt de grafiek</p>
+              </MiniTrend>
+            </MiniTrendBox>
+            <MiniTrendBox>
+              <MiniTrend
+                title="IC-opnames"
+                icon={Arts()}
+                is_below_threshold={intensive_care_nice_is_below_threshold}
+                threshold_day_span={intensive_care_nice_threshold_day_span}
+              >
+                <p>Hier komt de grafiek</p>
+              </MiniTrend>
+            </MiniTrendBox>
+            <MiniTrendBox>
+              <MiniTrend
+                title="Ziekenhuisopnames (incl. IC)"
+                icon={Ziekenhuis()}
+                is_below_threshold={hospital_nice_is_below_threshold}
+                threshold_day_span={hospital_nice_threshold_day_span}
+              >
+                <p>Hier komt de grafiek</p>
+              </MiniTrend>
+            </MiniTrendBox>
+          </Box>
         </MaxWidth>
       </div>
     </Layout>
   );
 };
+
+export const MiniTrendBox = styled.div(
+  css({
+    width: asResponsiveArray({
+      _: '100%',
+      md: 'calc(1 / 3 * 100% - 32px)',
+    }),
+    mb: asResponsiveArray({
+      _: 3,
+      md: undefined,
+    }),
+  })
+);
+
+interface MiniTrendProps {
+  title: string;
+  icon: ReactNode;
+  is_below_threshold: boolean;
+  threshold_day_span: number;
+  children: ReactNode;
+}
+
+function MiniTrend({
+  title,
+  icon,
+  is_below_threshold,
+  threshold_day_span,
+  children,
+}: MiniTrendProps) {
+  const descriptionTrue =
+    'Waarde is minder dan {{days}} dagen onder de grenswaarde';
+  const descriptionFalse = 'Grenswaarde is overschreden';
+
+  return (
+    <Box mb={3}>
+      <Box display="flex" alignItems="center" mb={1}>
+        <Box width={25} display="flex" mt="-5px">
+          {icon}
+        </Box>
+        <Text fontWeight="bold" mb={0}>
+          {title}
+        </Text>
+      </Box>
+      <Box display="flex" mb={3}>
+        <Box
+          height={18}
+          width={18}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          backgroundColor={is_below_threshold ? '#0390D6' : 'red'}
+          css={css({
+            svg: {
+              padding: '2px',
+            },
+            path: {
+              fill: 'white',
+            },
+          })}
+          mr={1}
+          mt="2px"
+          borderRadius="50%"
+        >
+          {is_below_threshold ? (
+            <IconDown />
+          ) : (
+            <Box width={8} height={2} bg="white" />
+          )}
+        </Box>
+        <Text mb={0}>
+          {is_below_threshold
+            ? replaceVariablesInText(descriptionTrue, {
+                days: threshold_day_span,
+              })
+            : descriptionFalse}
+        </Text>
+      </Box>
+      {children}
+    </Box>
+  );
+}
 
 export default Afschaling;
