ddd3a4f6e4f00c44b77ac52ced6c34ad669ad497
K <73584448+VWSCoronaDashboard10@users.noreply.github.com>
2021-04-09 10:35:38 +0200

    Create mini time series chart (#2562)
    * Tweak the time series chart to be renderd as mini chart
    
    * remove unused variable
    
    * Clean up

--- packages/app/src/components-styled/time-series-chart/logic/dimensions.ts
+++ packages/app/src/components-styled/time-series-chart/logic/dimensions.ts
@@ -8,9 +8,11 @@ const defaultPadding: Padding = {
   bottom: 30,
   /**
    * The left padding gives room for the Y-axis labels.
-   * for server-side rendered charts we set it to 40px in order to support labels
-   * up to 4 digits (4,000).
-   * During run-time (client-side) the actual Y-axis width will be measured.
+   * for server-side rendered charts we set it to 40px in order to support
+   * labels up to 4 digits (4,000).
+   * During run-time (client-side) the actual Y-axis width will be measured,
+   * unless a user gives a static paddingLeft via props. (@TODO I think we can
+   * remove that prop from the time-series-chart api)
    */
   left: 40,
 };
@@ -23,10 +25,18 @@ export function useDimensions(
   const isMounted = useIsMounted();
 
   return useMemo(() => {
+    /**
+     * When there's a padding left set/measured we'll add 10px extra to give the
+     * y-axis labels a bit more space, otherwise they could be cut off.
+     */
+    const paddingLeftWithExtraSpace = paddingLeft
+      ? paddingLeft + 10
+      : paddingLeft;
+
     const padding: Padding = {
       ...defaultPadding,
       left: isMounted
-        ? paddingLeft ?? defaultPadding.left
+        ? paddingLeftWithExtraSpace ?? defaultPadding.left
         : defaultPadding.left,
     };
 
