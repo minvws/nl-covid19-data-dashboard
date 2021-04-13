b04c78619458a65c22e2d1a330a2aeb9baec0677
K <73584448+VWSCoronaDashboard10@users.noreply.github.com>
2021-04-13 16:36:57 +0200

    add comment

--- packages/app/src/utils/current-date-context.tsx
+++ packages/app/src/utils/current-date-context.tsx
@@ -8,7 +8,19 @@ import {
 } from 'react';
 
 /**
+ * Dates shouldn't be created during render because the server-side date can be
+ * different compared to the date created client-side (especially with a static
+ * site: build today, visit tomorrow). The difference between dates (or any
+ * variable actually) between server and client can result in different html
+ * output between server and client. These differences can in turn result in
+ * react rehydration errors such as missing DOM-elements or DOM-
+ * elements with wrong attributes.
  *
+ * The CurrentDateContext will now hold the "today" date. On the server this is
+ * populated with the "last generated" date and on the client the state will be
+ * mutated after mount. The explicit state mutation will ensure components are
+ * correctly re-rendered where necessary, which means rehydration issues won't
+ * pop up anymore.
  */
 
 const CurrentDateContext = createContext<Date | undefined>(undefined);
