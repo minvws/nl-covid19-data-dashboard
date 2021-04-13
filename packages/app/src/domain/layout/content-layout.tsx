251e5bdc737d6b0dc2c86293e85037fdbef21de5
VWSCoronaDashboard14 <76471292+VWSCoronaDashboard14@users.noreply.github.com>
2021-04-13 15:53:13 +0200

    Increase font size on the ContentLayout component same way as it was orginally

--- packages/app/src/domain/layout/content-layout.tsx
+++ packages/app/src/domain/layout/content-layout.tsx
@@ -1,16 +1,27 @@
 import { Box } from '~/components-styled/base';
 import { ReactNode } from 'react';
-
+import css from '@styled-system/css';
+import styled from 'styled-components';
 interface ContentLayoutProps {
   children: ReactNode;
 }
 
 export function ContentLayout({ children }: ContentLayoutProps) {
   return (
-    <Box bg="white">
+    <StyledBox>
       <Box pt={6} pb={5} px={{ _: 3, sm: 0 }} maxWidth="maxWidthText" mx="auto">
         {children}
       </Box>
-    </Box>
+    </StyledBox>
   );
 }
+
+const StyledBox = styled(Box)(
+  css({
+    bg: 'white',
+
+    p: {
+      fontSize: '1.1rem',
+    },
+  })
+);
