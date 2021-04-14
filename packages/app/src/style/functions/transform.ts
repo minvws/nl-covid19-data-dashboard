03fbe21b57b0a58512ee6a8754f5d55732794aca
VWSCoronaDashboard14 <76471292+VWSCoronaDashboard14@users.noreply.github.com>
2021-04-14 10:44:46 +0200

    Remove transformBox and transformStyle

--- packages/app/src/style/functions/transform.ts
+++ packages/app/src/style/functions/transform.ts
@@ -6,22 +6,16 @@ import { asResponsiveArray } from '../utils';
 export interface TransformProps {
   transform?: ResponsiveValue<string>;
   transformOrigin?: ResponsiveValue<string>;
-  transformBox?: ResponsiveValue<'content-box' | 'border-box' | 'fill-box' | 'stroke-box' | 'view-box'>;
-  transformStyle?: ResponsiveValue<'preserve-3d' | 'flat'>;
 }
 
 export const transform: styleFn = (x: TransformProps) => {
-  if (isDefined(x.transform) || isDefined(x.transformOrigin) || isDefined(x.transformBox) || isDefined(x.transformStyle)) {
+  if (isDefined(x.transform) || isDefined(x.transformOrigin)) {
     const transformValue = asResponsiveArray(x.transform);
     const transformOriginValue = asResponsiveArray(x.transformOrigin);
-    const transformBox = asResponsiveArray(x.transformBox);
-    const transformStyle = asResponsiveArray(x.transformStyle);
 
     return css({
       transform: transformValue,
-      transformOrigin:transformOriginValue,
-      transformBox: transformBox,
-      transformStyle: transformStyle
+      transformOrigin:transformOriginValue
     })
   }
 }
