2d49a50e51366d3235fe029e85ebfa9f7bda5e0f
K <73584448+VWSCoronaDashboard10@users.noreply.github.com>
2021-04-16 14:57:28 +0200

    Transform Sanity lokalize texts into documents (#2637)
    * references approach
    
    * wip
    
    * Add lokalize path to documents
    
    * Speed up the import script by 40 times
    
    * Improve readability
    
    * Clean up
    
    * Report issues
    
    * Trim values on export/import
    
    * Display all locales when editing lokalize texts (#2639)
    
    * Display all lokalize locales in sanity UI
    
    * List lokalize keys w/o EN on dashboard (#2642)
    
    * List lokalize keys w/o EN on dashboard
    
    * Simplify dashboard layout

---
+++ packages/cms/src/lokalize/structure-list-item.ts
@@ -0,0 +1,53 @@
+import { StructureBuilder as S } from '@sanity/structure';
+import uniq from 'lodash/uniq';
+import documentStore from 'part:@sanity/base/datastore/document';
+import { FaLanguage } from 'react-icons/fa';
+import { map } from 'rxjs/operators';
+
+export function lokalizeListItem() {
+  return S.listItem()
+    .title('Lokalize')
+    .icon(FaLanguage)
+    .child(() => {
+      const type = 'lokalizeText';
+
+      return documentStore
+        .listenQuery(`*[_type == $type]{ subject }`, { type })
+        .pipe(
+          map((_subjects: { subject: string }[], index) => {
+            const subjects = uniq(_subjects.map((x) => x.subject));
+
+            return S.list()
+              .title('Onderwerp')
+              .items(
+                subjects
+                  .sort((a, b) => a.localeCompare(b))
+                  .map((subject) =>
+                    S.listItem()
+                      .title(subject)
+                      .id(subject)
+                      .child(
+                        S.documentList()
+                          .id(`${subject}-child`)
+                          .title(subject)
+                          .schemaType(type)
+                          .defaultOrdering([
+                            { field: 'path', direction: 'asc' },
+                          ])
+                          .filter('subject == $subject')
+                          .params({ subject })
+                          .child((id) =>
+                            S.editor()
+                              .title(id.replace('jsonKey__', ''))
+                              .id(id)
+                              .schemaType(type)
+                              .documentId(id)
+                              .views([S.view.form()])
+                          )
+                      )
+                  )
+              );
+          })
+        );
+    });
+}
