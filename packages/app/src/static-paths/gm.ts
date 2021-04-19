e03d2d82098e0b673d23155256bd0c95fe09d8c5
VWS Corona Dashboard <66416946+VWSCoronaDashboard@users.noreply.github.com>
2021-04-19 10:58:41 +0200

    Do not pre-render all routes

--- packages/app/src/static-paths/gm.ts
+++ packages/app/src/static-paths/gm.ts
@@ -1,16 +1,11 @@
-import municipalities from '~/data/municipal-search-data';
-
 /**
  * getStaticPaths creates an array of all the allowed `/gemeente/[code]` routes.
  */
 export function getStaticPaths() {
-  const paths = municipalities.map((municipality) => ({
-    params: { code: municipality.gemcode },
-  }));
-
+  const paths = [] as any[];
   return {
     paths,
-    // other routes should 404
-    fallback: false,
+    // other routes should load on-demand
+    fallback: 'blocking',
   };
 }
--- packages/app/src/static-paths/vr.ts
+++ packages/app/src/static-paths/vr.ts
@@ -1,20 +1,13 @@
-import safetyRegions from '~/data/index';
-
 /**
  * getStaticPaths creates an array of all the allowed `/veiligheidsregio/[code]`
  * routes.
  */
 export function getStaticPaths() {
-  const filteredRegions = safetyRegions.filter(
-    (region) => region.code.indexOf('VR') === 0
-  );
-  const paths = filteredRegions.map((region) => ({
-    params: { code: region.code },
-  }));
+  const paths = [] as any[];
 
   return {
     paths,
     // other routes should 404
-    fallback: false,
+    fallback: 'blocking',
   };
 }
