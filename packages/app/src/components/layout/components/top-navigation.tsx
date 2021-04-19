bd4cf7da77379220db7d1817f62d8e5cac45e3f0
VWS Corona Dashboard <75375917+VWSCoronaDashboard13@users.noreply.github.com>
2021-04-19 14:09:58 +0200

    lint cleanup

--- packages/app/src/components/layout/components/top-navigation.tsx
+++ packages/app/src/components/layout/components/top-navigation.tsx
@@ -1,6 +1,6 @@
 import css from '@styled-system/css';
 import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Close from '~/assets/close.svg';
 import Menu from '~/assets/menu.svg';
