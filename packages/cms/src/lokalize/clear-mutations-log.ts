c35d0ac588bb229b3309d37a2ce57b8282a2d3ff
T <71320230+VWSCoronaDashboard8@users.noreply.github.com>
2021-04-29 18:16:08 +0200

    Simplify

--- packages/cms/src/lokalize/clear-mutations-log.ts
+++ packages/cms/src/lokalize/clear-mutations-log.ts
@@ -7,20 +7,18 @@ import { clearLogFile } from './logic';
 import prompts from 'prompts';
 
 (async function run() {
-  {
-    const response = await prompts([
-      {
-        type: 'confirm',
-        name: 'isConfirmed',
-        message:
-          'This script should only be executed at the end of a release cycle. Are you aware of this?',
-        initial: false,
-      },
-    ]);
+  const response = await prompts([
+    {
+      type: 'confirm',
+      name: 'isConfirmed',
+      message:
+        'This script should only be executed at the end of a release cycle. Are you aware of this?',
+      initial: false,
+    },
+  ]);
 
-    if (!response.isConfirmed) {
-      process.exit(0);
-    }
+  if (!response.isConfirmed) {
+    process.exit(0);
   }
 
   clearLogFile();
