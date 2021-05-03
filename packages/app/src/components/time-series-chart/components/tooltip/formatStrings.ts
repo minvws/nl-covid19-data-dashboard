97a2cb7958a74304a335c42c0bc2646111c5fe39
VWS Corona Dashboard <75375917+VWSCoronaDashboard13@users.noreply.github.com>
2021-05-03 09:34:58 +0200

    removing user callback

--- packages/app/src/components/time-series-chart/components/tooltip/formatStrings.ts
+++ packages/app/src/components/time-series-chart/components/tooltip/formatStrings.ts
@@ -5,57 +5,48 @@ import {
   isDateValue,
   TimestampedValue,
 } from '@corona-dashboard/common';
-import { useCallback } from 'react';
 
 export function useGetValueString() {
   const { formatPercentage, formatNumber } = useIntl();
 
-  return useCallback(
-    (value: number | null, isPercentage?: boolean) => {
-      return isPresent(value)
-        ? isPercentage
-          ? `${formatPercentage(value)}%`
-          : formatNumber(value)
-        : '-';
-    },
-    [formatPercentage, formatNumber]
-  );
+  return (value: number | null, isPercentage?: boolean) => {
+    return isPresent(value)
+      ? isPercentage
+        ? `${formatPercentage(value)}%`
+        : formatNumber(value)
+      : '-';
+  };
 }
 
 export function useGetRangeValueString() {
   const getValueString = useGetValueString();
 
-  return useCallback(
-    (valueA: number | null, valueB: number | null, isPercentage?: boolean) => {
-      return `${getValueString(valueA, isPercentage)} - ${getValueString(
-        valueB,
-        isPercentage
-      )}`;
-    },
-    [getValueString]
-  );
+  return (
+    valueA: number | null,
+    valueB: number | null,
+    isPercentage?: boolean
+  ) => {
+    return `${getValueString(valueA, isPercentage)} - ${getValueString(
+      valueB,
+      isPercentage
+    )}`;
+  };
 }
 
 export function useGetDateStringFromValue() {
   const { formatDateFromSeconds } = useIntl();
 
-  return useCallback(
-    (value: TimestampedValue) => {
-      if (isDateValue(value)) {
-        return formatDateFromSeconds(value.date_unix);
-      } else if (isDateSpanValue(value)) {
-        const dateStartString = formatDateFromSeconds(
-          value.date_start_unix,
-          'axis'
-        );
-        const dateEndString = formatDateFromSeconds(
-          value.date_end_unix,
-          'axis'
-        );
+  return (value: TimestampedValue) => {
+    if (isDateValue(value)) {
+      return formatDateFromSeconds(value.date_unix);
+    } else if (isDateSpanValue(value)) {
+      const dateStartString = formatDateFromSeconds(
+        value.date_start_unix,
+        'axis'
+      );
+      const dateEndString = formatDateFromSeconds(value.date_end_unix, 'axis');
 
-        return `${dateStartString} - ${dateEndString}`;
-      }
-    },
-    [formatDateFromSeconds]
-  );
+      return `${dateStartString} - ${dateEndString}`;
+    }
+  };
 }
