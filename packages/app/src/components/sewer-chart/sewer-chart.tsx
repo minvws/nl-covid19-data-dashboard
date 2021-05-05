c80bcb85acfd8edbd5f17932f7de1c393e67093a
VWSCoronaDashboard14 <76471292+VWSCoronaDashboard14@users.noreply.github.com>
2021-05-05 13:23:57 +0200

    Spelling

--- packages/app/src/components/sewer-chart/sewer-chart.tsx
+++ packages/app/src/components/sewer-chart/sewer-chart.tsx
@@ -171,9 +171,9 @@ export function SewerChart(props: SewerChartProps) {
 
   /**
    * Manually set Y axis values, as per design.
-   * The middle calculation finds first the difference between the higehst
-   * and the lowest, then divide it by 2 and add the lowest value
-   * to find value exact value in between.
+   * The middle calculation finds first the difference between the highest
+   * and the lowest. Then divide it by 2 and add the lowest value to find value
+   * exact value in between.
    */
   const tickValuesY = useMemo(
     () => [
