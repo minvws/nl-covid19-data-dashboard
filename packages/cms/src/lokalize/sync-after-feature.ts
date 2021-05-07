58a68abe99328c8bd9131716b4267eacdcd1d4b4
T <71320230+VWSCoronaDashboard8@users.noreply.github.com>
2021-05-07 14:01:12 +0200

    Fix sync-after-feature early return

--- packages/cms/src/lokalize/sync-after-feature.ts
+++ packages/cms/src/lokalize/sync-after-feature.ts
@@ -40,7 +40,7 @@ import { LokalizeText } from './types';
 async function syncAdditionsToProduction(additions: TextMutation[]) {
   if (additions.length === 0) {
     console.log('There are no mutations that result in keys to add');
-    process.exit(0);
+    return;
   }
 
   /**
