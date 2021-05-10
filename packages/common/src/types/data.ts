5f182087a7725b713614d4d1fabb12003b9e476f
VWSCoronaDashboard14 <76471292+VWSCoronaDashboard14@users.noreply.github.com>
2021-05-10 16:32:33 +0200

    Fix wrong name required key disability care

--- packages/common/src/types/data.ts
+++ packages/common/src/types/data.ts
@@ -848,7 +848,7 @@ export interface RegionalDifference {
   nursing_home__infected_locations_total: DifferenceInteger;
   nursing_home__deceased_daily: DifferenceInteger;
   disability_care__newly_infected_people: DifferenceInteger;
-  disability_care__infected_locations_total?: DifferenceInteger;
+  disability_care__infected_locations_total: DifferenceInteger;
   elderly_at_home__positive_tested_daily: DifferenceInteger;
   deceased_rivm__covid_daily: DifferenceInteger;
 }
