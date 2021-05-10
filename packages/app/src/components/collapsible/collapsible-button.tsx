6c3cc5483f863f389afa7e0b2566ec9f8618f562
VWSCoronaDashboard14 <76471292+VWSCoronaDashboard14@users.noreply.github.com>
2021-05-10 11:11:55 +0200

    WIP

--- packages/app/src/components/collapsible/collapsible-button.tsx
+++ packages/app/src/components/collapsible/collapsible-button.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import css from '@styled-system/css';
 import styled from 'styled-components';
 import { asResponsiveArray } from '~/style/utils';
@@ -7,22 +7,27 @@ import {
   DisclosureButton,
   DisclosurePanel,
 } from '@reach/disclosure';
-
 import useResizeObserver from 'use-resize-observer';
 import { Box } from '~/components/base';
 import { useSetLinkTabbability } from './use-set-link-tabbability';
-
 interface CollapsibleButtonProps {
   children: React.ReactNode;
   label: string;
+  icon?: React.ReactNode;
 }
 
 export const CollapsibleButton = ({
   label,
   children,
+  icon,
 }: CollapsibleButtonProps) => {
   const { ref: contentRef, height: contentHeight = 0 } = useResizeObserver();
   const { ref: buttonRef, height: buttonHeight = 0 } = useResizeObserver();
+  const {
+    ref: buttonContainerRef,
+    width: buttonWidth = 0,
+  } = useResizeObserver();
+
   const [isOpen, setIsOpen] = useState(false);
   const { wrapperRef } = useSetLinkTabbability(isOpen);
 
@@ -33,17 +38,17 @@ export const CollapsibleButton = ({
   const height = buttonHeight + (isOpen ? contentHeight : 0) || undefined;
 
   return (
-    <Container style={{ height }}>
+    <Container style={{ height }} isOpen={isOpen} buttonWidth={buttonWidth}>
       <Disclosure open={isOpen} onChange={() => setIsOpen(!isOpen)}>
-        <div
-          ref={buttonRef}
-          css={css({ display: 'flex', justifyContent: 'center' })}
-        >
-          <DisclosureButton>
-            {label}
-            <Chevron open={isOpen} />
-          </DisclosureButton>
-        </div>
+        <ButtonContainer ref={buttonRef}>
+          <div ref={buttonContainerRef} css={css({ display: 'flex' })}>
+            <DisclosureButton>
+              {icon && <IconContainer>{icon}</IconContainer>}
+              {label}
+              <Chevron open={isOpen} />
+            </DisclosureButton>
+          </div>
+        </ButtonContainer>
 
         <DisclosurePanel>
           <div ref={contentRef}>
@@ -55,38 +60,68 @@ export const CollapsibleButton = ({
   );
 };
 
-const Container = styled(Box).attrs({ as: 'section' })(
+const ButtonContainer = styled.div(
   css({
-    borderRadius: 1,
+    display: 'flex',
+    justifyContent: 'center',
+    mx: 'auto',
+  })
+);
+
+const Container = styled(Box).attrs({ as: 'section' })<{
+  isOpen: boolean;
+  buttonWidth: number;
+}>((x) =>
+  css({
     position: 'relative',
-    display: 'inline-flex',
-    flexDirection: 'column',
     padding: 0,
-    margin: '0 auto',
-    transitionProperty: 'height',
-    transitionDuration: '0.5s',
-    bg: 'tileGray',
-    width: '100%',
 
     //button
     '[data-reach-disclosure-button]': {
-      position: 'relative',
-      px: asResponsiveArray({ _: 2, sm: 4 }),
+      display: 'flex',
+      justifyContent: 'center',
+      width: x.isOpen ? '100%' : 'fit-content',
+      px: asResponsiveArray({ _: 1, sm: 3 }),
       py: 3,
-      border: 'none',
       background: 'none',
       font: 'inherit',
-      color: 'blue',
+      color: 'black',
       cursor: 'pointer',
-      fontWeight: 'bold',
       zIndex: 1,
-      width: '100%',
+      alignItems: 'center',
+      transition: 'color 0.2s ease-out, border-color 0.2s ease-out',
+      border: '1px solid',
+      borderColor: x.isOpen ? 'rgba(0, 0, 0, 0)' : 'lightGray',
+      fontWeight: 'bold',
+      borderRadius: 1,
+
+      '&:hover': {
+        color: 'blue',
+        borderColor: x.isOpen ? 'rgba(0, 0, 0, 0)' : 'data.primary',
+
+        svg: {
+          fill: 'blue',
+        },
+      },
+
+      '&:before': {
+        transition: 'transform 0.3s ease-in, border-color 0.5s, width 0.5s',
+        position: 'absolute',
+        top: 0,
+        width: x.isOpen ? '100%' : x.buttonWidth,
+        height: '100%',
+        border: '1px solid',
+        borderColor: x.isOpen ? 'lightGray' : 'rgba(0, 0, 0, 0)',
+        content: '""',
+      },
     },
+
     '[data-reach-disclosure-button]:focus': {
       outlineWidth: '1px',
       outlineStyle: 'dashed',
-      outlineColor: 'blue',
+      outlineColor: x.isOpen ? 'rgba(0, 0, 0, 0)' : 'blue',
     },
+
     '[data-reach-disclosure-button][data-state="open"]:after': {
       content: '""',
       position: 'absolute',
@@ -96,21 +131,53 @@ const Container = styled(Box).attrs({ as: 'section' })(
       bg: 'lightGray',
       height: '1px',
     },
+
     //panel
     '[data-reach-disclosure-panel]': {
-      transitionProperty: 'height, opacity',
-      transitionDuration: '0.5s',
+      position: 'relative',
+      transition: 'height 0.5s, border-color 1.5s',
       width: '100%',
       overflow: 'hidden',
       opacity: 0,
       display: 'block',
+
+      '&:before': {
+        content: '""',
+        position: 'absolute',
+        top: 0,
+        left: x.isOpen ? '5%' : '0%',
+        width: x.isOpen ? '90%' : '0%',
+        height: '1px',
+        backgroundColor: 'lightGray',
+        transition: 'transform 0.5s',
+        transform: x.isOpen ? 'scaleX(1)' : 'scale(0)',
+      },
+
+      '> div': {
+        transition: 'opacity 0.6s',
+        transitionDelay: '0.4s',
+        opacity: x.isOpen ? 1 : 0,
+      },
     },
+
     '[data-reach-disclosure-panel][data-state="open"]': {
       opacity: 1,
     },
+
     '[data-reach-disclosure-panel][data-state="collapsed"]': {
       height: 0,
       opacity: 0,
+    },
+  })
+);
+
+const IconContainer = styled.div(
+  css({
+    mr: 2,
+
+    svg: {
+      transition: 'fill 0.2s ease-out',
+      fill: 'black',
     },
   })
 );
