82febc831e3305ff7ecadd352be2effaa00afa81
VWSCoronaDashboard14 <76471292+VWSCoronaDashboard14@users.noreply.github.com>
2021-05-11 14:50:12 +0200

    Clean up

--- packages/app/src/components/collapsible/collapsible-button.tsx
+++ packages/app/src/components/collapsible/collapsible-button.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from 'react';
+import { useState } from 'react';
 import css from '@styled-system/css';
 import styled from 'styled-components';
 import { asResponsiveArray } from '~/style/utils';
@@ -38,11 +38,7 @@ export const CollapsibleButton = ({
   const height = buttonHeight + (isOpen ? contentHeight : 0) || undefined;
 
   return (
-    <Container
-      style={{ maxHeight: height }}
-      isOpen={isOpen}
-      buttonWidth={buttonWidth}
-    >
+    <Container style={{ height }} isOpen={isOpen} buttonWidth={buttonWidth}>
       <Disclosure open={isOpen} onChange={() => setIsOpen(!isOpen)}>
         <ButtonContainer ref={buttonRef}>
           <div ref={buttonContainerRef} css={css({ display: 'flex' })}>
@@ -79,25 +75,27 @@ const Container = styled(Box).attrs({ as: 'section' })<{
   css({
     position: 'relative',
     padding: 0,
+    transitionProperty: 'height',
+    transitionDuration: '0.5s',
 
     //button
     '[data-reach-disclosure-button]': {
       display: 'flex',
       justifyContent: 'center',
+      alignItems: 'center',
+      zIndex: 1,
       width: x.isOpen ? '100%' : 'fit-content',
+      border: '1px solid',
+      borderRadius: 1,
+      borderColor: x.isOpen ? 'rgba(0, 0, 0, 0)' : 'lightGray',
       px: asResponsiveArray({ _: 1, sm: 3 }),
       py: 3,
       background: 'none',
-      font: 'inherit',
       color: 'black',
-      cursor: 'pointer',
-      zIndex: 1,
-      alignItems: 'center',
-      transition: 'color 0.2s ease-out, border-color 0.2s ease-out',
-      border: '1px solid',
-      borderColor: x.isOpen ? 'rgba(0, 0, 0, 0)' : 'lightGray',
+      font: 'inherit',
       fontWeight: 'bold',
-      borderRadius: 1,
+      cursor: 'pointer',
+      transition: 'color 0.2s ease-out, border-color 0.2s ease-out',
 
       '&:hover': {
         color: 'blue',
@@ -109,7 +107,7 @@ const Container = styled(Box).attrs({ as: 'section' })<{
       },
 
       '&:before': {
-        transition: 'transform 0.3s ease-in, border-color 0.5s, width 0.5s',
+        transition: 'transform 0.3s ease-in, width 0.5s',
         position: 'absolute',
         top: 0,
         width: x.isOpen ? '100%' : x.buttonWidth,
@@ -126,26 +124,14 @@ const Container = styled(Box).attrs({ as: 'section' })<{
       outlineColor: x.isOpen ? 'rgba(0, 0, 0, 0)' : 'blue',
     },
 
-    '[data-reach-disclosure-button][data-state="open"]:after': {
-      content: '""',
-      position: 'absolute',
-      bottom: 0,
-      left: 0,
-      right: 0,
-      bg: 'lightGray',
-      height: '1px',
-    },
-
     //panel
     '[data-reach-disclosure-panel]': {
       position: 'relative',
-      transition: 'max-height 0.5s, border-color 1.5s',
-      // transitionProperty: 'max-height, opacity',
-      // transitionDuration: '0.5s',
       width: '100%',
       overflow: 'hidden',
-      opacity: 0,
       display: 'block',
+      transition: 'opacity 1.0s',
+      opacity: x.isOpen ? 1 : 0,
 
       '&:before': {
         content: '""',
@@ -155,24 +141,13 @@ const Container = styled(Box).attrs({ as: 'section' })<{
         width: x.isOpen ? '90%' : '0%',
         height: '1px',
         backgroundColor: 'lightGray',
-        transition: 'transform 0.5s',
         transform: x.isOpen ? 'scaleX(1)' : 'scale(0)',
+        transition: 'transform 0.5s',
       },
-
-      '> div': {
-        transition: 'opacity 0.6s',
-        transitionDelay: '0.4s',
-        // opacity: x.isOpen ? 1 : 0,
-      },
-    },
-
-    '[data-reach-disclosure-panel][data-state="open"]': {
-      opacity: 1,
     },
 
     '[data-reach-disclosure-panel][data-state="collapsed"]': {
-      maxHeight: 0,
-      opacity: 0,
+      height: 0,
     },
   })
 );
