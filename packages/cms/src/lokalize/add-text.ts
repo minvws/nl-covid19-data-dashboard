b383ae4658ce941309da9bf23a6ea40465ca4c09
T <71320230+VWSCoronaDashboard8@users.noreply.github.com>
2021-05-13 14:54:46 +0200

    Simplify and improve text-add

--- packages/cms/src/lokalize/add-text.ts
+++ packages/cms/src/lokalize/add-text.ts
@@ -1,13 +1,34 @@
 /**
  * Add one or multiple texts to the Sanity "Lokalize" dataset.
  */
-import { set, uniq } from 'lodash';
+import meow from 'meow';
 import prompts from 'prompts';
 import { getClient } from '../client';
-import { appendTextMutation } from './logic';
+import { appendTextMutation, exportLokalizeTexts } from './logic';
 import { LokalizeText } from './types';
 
 (async function run() {
+  const cli = meow(
+    `
+      Usage
+        $ add-text
+
+      Options
+        --key, -k The new text key to add in dot-notation
+
+      Examples
+        $ add-text -k some.unique.path
+    `,
+    {
+      flags: {
+        key: {
+          type: 'string',
+          alias: 'k',
+        },
+      },
+    }
+  );
+
   const client = getClient();
 
   /**
@@ -21,71 +42,58 @@ import { LokalizeText } from './types';
       throw new Error(`Failed to fetch texts: ${err.message}`);
     })) as LokalizeText[];
 
-  const allSubjects = uniq(allTexts.map((x) => x.subject));
+  const existingKeys = allTexts.map((x) => x.key);
 
-  const subjectChoices = allSubjects.map((x) => ({ title: x, value: x }));
+  let additionsCounter = 0;
 
-  const response = await prompts([
+  while (true) {
+    const textDocument = await createTextDocument(existingKeys, cli.flags.key);
+
+    console.log({
+      key: textDocument.key,
+      nl: textDocument.text.nl,
+      en: textDocument.text.en,
+    });
+
     {
-      type: 'confirm',
-      name: 'listSubjects',
-      message: 'List existing subjects?',
-      initial: false,
-    },
-    {
-      type: (_, values) => (values.listSubjects ? 'select' : null),
-      name: 'subject',
-      message: `Select a subject`,
-      choices: subjectChoices,
-    },
-    {
-      type: (_, values) => (values.listSubjects ? null : 'text'),
-      name: 'subject',
-      message: `What is the subject?`,
-      format: (x: string) => x.toLowerCase(),
-    },
-  ]);
-
-  const { subject } = response;
-
-  let continueCreating = true;
-
-  while (continueCreating) {
-    const textDocument = await createTextDocumentForSubject(subject, allTexts);
-
-    console.table(textDocument);
-
-    const response = await prompts(
-      [
+      const response = await prompts([
         {
           type: 'confirm',
           name: 'confirmed',
-          message: 'Is this what you want to add?',
+          message: 'Is this correct?',
+          onState,
         },
+      ]);
+
+      if (response.confirmed) {
+        await client.create(textDocument);
+
+        await appendTextMutation('add', textDocument.key);
+
+        console.log(`Successfully created ${textDocument.key}`);
+
+        existingKeys.push(textDocument.key);
+
+        additionsCounter++;
+      }
+    }
+    {
+      const response = await prompts([
         {
           type: 'confirm',
           name: 'continue',
-          message: 'Create another text in the same subject?',
+          message: 'Add another text?',
+          onState,
         },
-      ],
-      {
-        onCancel: () => {
-          continueCreating = false;
-        },
+      ]);
+
+      if (!response.continue) {
+        if (additionsCounter > 0) {
+          console.log('Updating text export...');
+          await exportLokalizeTexts();
+        }
+        process.exit();
       }
-    );
-
-    if (response.confirmed) {
-      await client.create(textDocument);
-
-      const key = `${textDocument.subject}.${textDocument.path}`;
-      await appendTextMutation('add', key);
-
-      console.log(`Successfully created ${key}`);
-    }
-
-    if (!response.continue) {
-      continueCreating = false;
     }
   }
 })().catch((err) => {
@@ -93,21 +101,37 @@ import { LokalizeText } from './types';
   process.exit(1);
 });
 
-async function createTextDocumentForSubject(
-  subject: string,
-  allTexts: LokalizeText[]
-) {
+/**
+ * There is currently no native way to exit prompts on ctrl-c. This is a
+ * workaround that needs to be added to every prompts instance. For more info
+ * see: https://github.com/terkelg/prompts/issues/252#issuecomment-778683666
+ */
+function onState(state: { aborted: boolean }) {
+  if (state.aborted) {
+    process.nextTick(() => {
+      process.exit(0);
+    });
+  }
+}
+
+async function createTextDocument(existingKeys: string[], initialKey?: string) {
   const response = await prompts([
     {
       type: 'text',
-      name: 'path',
-      message: 'What is the path in dot notation?',
-      format: (x: string) => x.toLowerCase(),
-      /**
-       * Do not allow creating a text with a key that already exists
-       */
-      validate: (x: string) =>
-        allTexts.find((text) => text.key === `${subject}.${x}`) === undefined,
+      name: 'key',
+      message: 'What is the key? (Use snake_cased.dot.notation)',
+      initial: initialKey,
+      validate: (x: string) => {
+        /**
+         * Validation requires the key to be new and also to only contain
+         * lower-snake-case paths in dot notation
+         */
+        return (
+          existingKeys.find((key) => key === x) === undefined &&
+          /^([a-z_]+\.?)+$/.test(x)
+        );
+      },
+      onState,
     },
     {
       type: 'text',
@@ -115,6 +139,7 @@ async function createTextDocumentForSubject(
       message: 'What is the Dutch text?',
       format: (x: string) => x.trim(),
       validate: (x: string) => x.length > 1,
+      onState,
     },
     {
       type: 'text',
@@ -124,14 +149,25 @@ async function createTextDocumentForSubject(
         const value = x.trim();
         return value.length > 0 ? value : undefined;
       },
+      onState,
     },
   ]);
 
+  /**
+   * Here we split the key into a subject and (remaining) path. This is required
+   * for the way LokalizeText documents are queried in Sanity. But possibly we
+   * can work with just the key and omit subject+path from the object.
+   *
+   * @TODO see if key is enough to build the UI in Sanity.
+   */
+  const [subject, ...pathElements] = response.key.split('.');
+  const path = pathElements.join('.');
+
   const text: Omit<LokalizeText, '_id'> = {
     _type: 'lokalizeText',
-    key: `${subject}.${response.path}`,
+    key: response.key,
     subject,
-    path: response.path,
+    path,
     is_newly_added: true,
     publish_count: 0,
     should_display_empty: false,
---
+++ packages/cms/src/lokalize/logic/export.ts
@@ -0,0 +1,67 @@
+/**
+ * This script exports LokalizeText documents from Sanity as a locale JSON file,
+ * and strips any keys that have been marked by the key-mutations.csv file as a
+ * result of text changes via the CLI.
+ */
+import { LokalizeText } from '@corona-dashboard/app/src/types/cms';
+import { createFlatTexts } from '@corona-dashboard/common';
+import { unflatten } from 'flat';
+import fs from 'fs';
+import meow from 'meow';
+import path from 'path';
+import prettier from 'prettier';
+import { getClient } from '../../client';
+import { collapseTextMutations, readTextMutations } from '.';
+
+const localeDirectory = path.resolve(
+  __dirname,
+  '..', // lokalize
+  '..', // src
+  '..', // cms
+  '..', // packages
+  'app/src/locale'
+);
+
+async function writePrettyJson(data: Record<string, unknown>, path: string) {
+  const json = prettier.format(JSON.stringify(data), { parser: 'json' });
+  return new Promise<void>((resolve, reject) =>
+    fs.writeFile(path, json, { encoding: 'utf8' }, (err) =>
+      err ? reject(err) : resolve()
+    )
+  );
+}
+
+export async function exportLokalizeTexts(
+  dataset?: string,
+  includeDrafts?: boolean
+) {
+  const client = getClient(dataset);
+  /**
+   * The client will load drafts by default because it is authenticated with a
+   * token. If the `drafts` flag is not set to true, we will manually
+   * exclude draft-documents on query-level.
+   */
+  const draftsQueryPart = includeDrafts ? '' : '&& !(_id in path("drafts.**"))';
+
+  const documents: LokalizeText[] = await client.fetch(
+    `*[_type == 'lokalizeText' ${draftsQueryPart}] | order(key asc)`
+  );
+
+  const mutations = await readTextMutations();
+
+  const deletedKeys = collapseTextMutations(mutations)
+    .filter((x) => x.action === 'delete')
+    .map((x) => x.key);
+
+  let flatTexts = createFlatTexts(documents, deletedKeys);
+
+  await writePrettyJson(
+    unflatten(flatTexts.nl, { object: true }),
+    path.join(localeDirectory, 'nl_export.json')
+  );
+
+  await writePrettyJson(
+    unflatten(flatTexts.en, { object: true }),
+    path.join(localeDirectory, 'en_export.json')
+  );
+}
