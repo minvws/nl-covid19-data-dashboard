c5e1b7cbebabc91ec6bc8d0fb0a413ffe9e7491f
VWSCoronaDashboard14 <76471292+VWSCoronaDashboard14@users.noreply.github.com>
2021-05-17 11:34:02 +0200

    Put temporary behaviour component that serves as a page behind a feature flag

---
+++ packages/app/src/domain/behavior/behavior-page-national.tsx
@@ -0,0 +1,13 @@
+import { TileList } from '~/components/tile-list';
+
+interface behaviourPageNationalProps {
+  data: any;
+}
+
+export function BehaviorPageNational({ data }: behaviourPageNationalProps) {
+  return (
+    <TileList>
+      <h1>New behaviour national page</h1>
+    </TileList>
+  );
+}
---
+++ packages/app/src/domain/behavior/behavior-page-safety-region.tsx
@@ -0,0 +1,15 @@
+import { TileList } from '~/components/tile-list';
+
+interface behaviorPageSafetyRegionProps {
+  data: any;
+}
+
+export function BehaviorPageSafetyRegion({
+  data,
+}: behaviorPageSafetyRegionProps) {
+  return (
+    <TileList>
+      <h1>New behaviour safety region page</h1>
+    </TileList>
+  );
+}
