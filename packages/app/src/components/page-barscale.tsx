17b10d64f5281fabda1ac0a825a7c184c398cd22
TK <71320230+VWSCoronaDashboard8@users.noreply.github.com>
2021-05-17 18:27:52 +0200

    Topic/refactor difference indicators (#2842)
    * Split code into separate files
    
    * Move data driven text indicator
    
    * Rename difference text property
    
    * Break it up into flat components

--- packages/app/src/components/page-barscale.tsx
+++ packages/app/src/components/page-barscale.tsx
@@ -13,10 +13,7 @@ import {
   metricContainsPartialData,
 } from '../metric-config';
 import { Box } from './base';
-import {
-  DifferenceIndicator,
-  MovingAverageDifferenceIndicator,
-} from './difference-indicator';
+import { TileAverageDifference, TileDifference } from './difference-indicator';
 import { useIntl } from '~/intl';
 import { SiteText } from '~/locale';
 
@@ -139,9 +136,9 @@ export function PageBarScale<T>({
 
       {isDefined(differenceKey) &&
         (isMovingAverageDifference ? (
-          <MovingAverageDifferenceIndicator differenceValue={differenceValue} />
+          <TileAverageDifference value={differenceValue} />
         ) : (
-          <DifferenceIndicator
+          <TileDifference
             value={differenceValue}
             isDecimal={config.isDecimal}
             staticTimespan={differenceStaticTimespan}
