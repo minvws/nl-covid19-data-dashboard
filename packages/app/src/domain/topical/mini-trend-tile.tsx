5f1d46e9ed434fdf99302c2211ff8215d8160324
K <73584448+VWSCoronaDashboard10@users.noreply.github.com>
2021-05-20 16:52:53 +0200

    Clean up

--- packages/app/src/domain/topical/mini-trend-tile.tsx
+++ packages/app/src/domain/topical/mini-trend-tile.tsx
@@ -22,7 +22,7 @@ type MiniTrendTileProps<T extends TimestampedValue> = {
 export function MiniTrendTile<T extends TimestampedValue>(
   props: MiniTrendTileProps<T>
 ) {
-  const intl = useIntl();
+  const { formatNumber } = useIntl();
 
   const { icon, title, text, trendData, metricProperty, href } = props;
 
@@ -62,7 +62,7 @@ export function MiniTrendTile<T extends TimestampedValue>(
         mb={2}
         data-cy={metricProperty}
       >
-        {intl.formatNumber(value as unknown as number)}
+        {formatNumber(value as unknown as number)}
       </Text>
 
       <Box>{text}</Box>
@@ -77,31 +77,9 @@ export function MiniTrendTile<T extends TimestampedValue>(
         seriesConfig={[
           {
             metricProperty,
-            type: 'split-bar',
+            type: 'area',
             label: title,
-            fillOpacity: 1,
-            splitPoints: [
-              {
-                color: colors.data.scale.blue[0],
-                label: intl.siteText.escalatie_niveau.types['1'].titel,
-                value: 1100,
-              },
-              {
-                color: colors.data.scale.blue[1],
-                label: intl.siteText.escalatie_niveau.types['2'].titel,
-                value: 2600,
-              },
-              {
-                color: colors.data.scale.blue[2],
-                label: intl.siteText.escalatie_niveau.types['3'].titel,
-                value: 4800,
-              },
-              {
-                color: colors.data.scale.blue[4],
-                label: intl.siteText.escalatie_niveau.types['4'].titel,
-                value: Infinity,
-              },
-            ],
+            color: colors.data.primary,
           },
         ]}
       />
