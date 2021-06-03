16e6b658efa6aea2457ced63cab641908a54351d
T <71320230+VWSCoronaDashboard8@users.noreply.github.com>
2021-06-03 11:24:42 +0200

    Remove mark nearest point only

--- packages/app/src/components/sewer-chart/new-sewer-chart.tsx
+++ packages/app/src/components/sewer-chart/new-sewer-chart.tsx
@@ -86,7 +86,6 @@ export function NewSewerChart({
                   selectedInstallation
                 )}
                 timeframe={timeframe}
-                markNearestPointOnly
                 seriesConfig={[
                   {
                     type: 'line',
