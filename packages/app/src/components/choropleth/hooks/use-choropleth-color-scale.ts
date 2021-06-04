b4c39d7b5d6da7bde373b8e2bb1d21139172fd2f
K <73584448+VWSCoronaDashboard10@users.noreply.github.com>
2021-06-04 11:04:32 +0200

    Fix lint warning

--- packages/app/src/components/choropleth/hooks/use-choropleth-color-scale.ts
+++ packages/app/src/components/choropleth/hooks/use-choropleth-color-scale.ts
@@ -1,11 +1,10 @@
+import { ChoroplethThresholdsValue } from '@corona-dashboard/common';
 import { scaleThreshold } from 'd3-scale';
 import { useCallback, useMemo } from 'react';
-import { isDefined, isPresent } from 'ts-is-present';
+import { isPresent } from 'ts-is-present';
 import { assert } from '~/utils/assert';
-import { ChoroplethThresholdsValue } from '@corona-dashboard/common';
-import { GetRegionDataFunctionType } from './use-safety-region-data';
 import { GetMunicipalityDataFunctionType } from './use-municipality-data';
-import { useEscalationColor } from '~/utils/use-escalation-color';
+import { GetRegionDataFunctionType } from './use-safety-region-data';
 
 /**
  * This hook return a color scale for the given domain and gradient.
