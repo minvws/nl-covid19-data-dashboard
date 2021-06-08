5a686fca2cd445314bbae355df33890603e78993
K <73584448+VWSCoronaDashboard10@users.noreply.github.com>
2021-06-08 11:26:13 +0200

    Remove singleton tooltip because this doesn't work nicely on touch devices (moving focus doesn't work)

--- packages/app/src/lib/tooltip.tsx
+++ packages/app/src/lib/tooltip.tsx
@@ -1,64 +1,43 @@
-import Tippy, { TippyProps, useSingleton } from '@tippyjs/react';
+import Tippy, { TippyProps } from '@tippyjs/react';
 import { Instance } from 'tippy.js';
-import { createContext, ReactNode, useContext } from 'react';
 import 'tippy.js/dist/tippy.css';
 import 'tippy.js/themes/light.css';
-import { assert } from '~/utils/assert';
-import { isPresent } from 'ts-is-present';
 
-const tooltipContext = createContext<TippyProps['singleton']>(undefined);
-
-export function TooltipContext({ children }: { children: ReactNode }) {
-  const [source, target] = useSingleton();
-
-  return (
-    <tooltipContext.Provider value={target}>
-      {children}
-      <Tippy
-        singleton={source}
-        theme="light"
-        onMount={
-          process.env.NODE_ENV !== 'production' ? reportIsFocusable : undefined
-        }
-      />
-    </tooltipContext.Provider>
-  );
-}
+let handleMount: undefined | ((tippyInstance: Instance) => void);
 
 /**
  * Currently we'll only support the `content` prop of the tippy tooltip, but a
  * lot more should be possible if necessary.
+ *
+ * Usage:
+ *
+ *     <WithTooltip content={<p>message</p>}><button>foo</button></WithTooltip>
  */
 type TooltipProps = Pick<TippyProps, 'children' | 'content'>;
 
 export function WithTooltip({ children, content }: TooltipProps) {
-  const target = useContext(tooltipContext);
-  assert(target, 'missing TooltipContext in component tree');
-
   return (
-    <Tippy singleton={target} content={content}>
+    <Tippy theme="light" content={content} onMount={handleMount}>
       {children}
     </Tippy>
   );
 }
 
-const isFocusableSelector =
-  'button:not([disabled]), [href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), [tabindex]:not([tabindex="-1"]):not([disabled]), details:not([disabled]), summary:not(:disabled)';
+if (process.env.NODE_ENV !== 'production') {
+  /**
+   * This entire block won't be part of a production build
+   */
+  const isFocusableSelector =
+    'button:not([disabled]), [href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), [tabindex]:not([tabindex="-1"]):not([disabled]), details:not([disabled]), summary:not(:disabled)';
 
-async function reportIsFocusable(tippyInstance: Instance) {
-  const { triggerTarget } = tippyInstance.props;
-  const triggerTargets = Array.isArray(triggerTarget)
-    ? triggerTarget
-    : [triggerTarget].filter(isPresent);
+  handleMount = function reportIsFocusable(tippyInstance: Instance) {
+    const { reference } = tippyInstance;
 
-  const unfocusableTriggerTargets = triggerTargets.filter(
-    (x) => !x.matches(isFocusableSelector)
-  );
-
-  if (triggerTargets.some((x) => !x.matches(isFocusableSelector))) {
-    console.error({ tippyInstance, unfocusableTriggerTargets });
-    throw new Error(
-      `A tooltip has been wrapped around an element which cannot receive focus state. Please make it focusable for a11y by setting tabIndex={0}`
-    );
-  }
+    if (!reference.matches(isFocusableSelector)) {
+      console.error({ tippyInstance, reference });
+      throw new Error(
+        `A tooltip has been wrapped around an element which cannot receive focus state. Please make it focusable for a11y by setting tabIndex={0}`
+      );
+    }
+  };
 }
