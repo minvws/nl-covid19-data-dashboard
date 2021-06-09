b9c0741019a88017447a0d409028bf7bcb0bb42f
A <66416946+VWSCoronaDashboard@users.noreply.github.com>
2021-06-09 14:17:35 +0200

    Clear build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,1 @@
 #!/bin/bash
-start=`date +%s`
-set -e # Any subsequent(*) commands which fail will cause the shell script to exit immediately
-
-# Zip JSON files so the data is included in the build.
-cd packages/app/public/ && zip -qq latest-data.zip json/* && mv latest-data.zip json/ && cd -
-
-# Build the application in Docker 
-end=`date +%s`
-runtime=$((end-start))
-echo "Build executed in $runtime seconds, proceed to build Dockerfile."
