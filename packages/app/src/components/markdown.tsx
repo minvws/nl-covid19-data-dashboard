ee3a05666c708ff8c8986a360d5e4054c595b0d8
VWSCoronaDashboard14 <76471292+VWSCoronaDashboard14@users.noreply.github.com>
2021-06-10 16:21:39 +0200

    Add next link to markdown also

--- packages/app/src/components/markdown.tsx
+++ packages/app/src/components/markdown.tsx
@@ -1,25 +1,25 @@
 import { ReactNode } from 'react';
 import ReactMarkdown from 'react-markdown';
+import { ExternalLink } from '~/components/external-link';
+import { Link } from '~/utils/link';
 import { isAbsoluteUrl } from '~/utils/is-absolute-url';
 interface MarkdownProps {
   content: string;
 }
-
 interface LinkProps {
   children: ReactNode;
   href: string;
 }
 
 const renderers = {
-  link: (props: LinkProps) => (
-    <a
-      href={props.href}
-      rel="noreferrer noopener"
-      target={isAbsoluteUrl(props.href) ? '_blank' : undefined}
-    >
-      {props.children}
-    </a>
-  ),
+  link: (props: LinkProps) =>
+    isAbsoluteUrl(props.href) ? (
+      <ExternalLink href={props.href}>{props.children}</ExternalLink>
+    ) : (
+      <Link href={props.href} passHref>
+        <a>{props.children}</a>
+      </Link>
+    ),
 };
 
 export function Markdown({ content }: MarkdownProps) {
