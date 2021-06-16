e51c547817860221f297ddc910db863d6f2ba233
TK <71320230+VWSCoronaDashboard8@users.noreply.github.com>
2021-06-16 11:12:19 +0200

    Upgrade dependencies (#2999)
    * Upgrade a lot of deps
    
    * Revert react-markdown to ^5
    
    * Fix compiler errors
    
    * Enable webpack 5 and upgrade visx
    
    * Update svg config to avoid warnings
    
    * Remove unused dependency
    
    * Fix lint errors
    
    * Last bit of upgrades
    
    * Fix another lint error
    
    * Apply review feedback

--- packages/app/src/domain/topical/components/search/search-input.tsx
+++ packages/app/src/domain/topical/components/search/search-input.tsx
@@ -26,7 +26,7 @@ export function SearchInput() {
         <IconContainer
           as="button"
           align="right"
-          onClick={(evt: MouseEvent<HTMLButtonElement>) => {
+          onClick={(evt: MouseEvent) => {
             evt.stopPropagation();
             inputRef.current?.focus();
             setTerm('');
