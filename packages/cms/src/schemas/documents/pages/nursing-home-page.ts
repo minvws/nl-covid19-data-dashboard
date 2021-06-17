e38513c0e5b00347839fed2cbbacbd5585c0df7b
VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2021-06-17 14:54:45 +0200

    Add article strips to nursing home, infectious people, elderly at home and disability care pages. Add extra article strip to GGD section of positive tests page.

---
+++ packages/cms/src/schemas/documents/pages/nursing-home-page.ts
@@ -0,0 +1,8 @@
+import { HIGHLIGHTED_ARTICLES } from '../../fields';
+
+export const nursinghomePage = {
+  name: 'nursinghomePage',
+  type: 'document',
+  title: 'Verpleeghuiszorg pagina',
+  fields: [HIGHLIGHTED_ARTICLES],
+};
--- packages/cms/src/schemas/documents/pages/positive-tests-page.ts
+++ packages/cms/src/schemas/documents/pages/positive-tests-page.ts
@@ -4,5 +4,12 @@ export const positiveTestsPage = {
   title: 'Positieve testen',
   name: 'positiveTestsPage',
   type: 'document',
-  fields: [HIGHLIGHTED_ARTICLES],
+  fields: [
+    HIGHLIGHTED_ARTICLES,
+    {
+      ...HIGHLIGHTED_ARTICLES,
+      title: 'Uitgelichte GGD artikelen',
+      name: 'ggdArticles',
+    },
+  ],
 };
