67632c3332d39211fa76914182712b796f05580d
VWSCoronaDashboard14 <76471292+VWSCoronaDashboard14@users.noreply.github.com>
2021-06-23 14:53:39 +0200

    newline

--- packages/app/src/pages/landelijk/covid-varianten.tsx
+++ packages/app/src/pages/landelijk/covid-varianten.tsx
@@ -24,12 +24,12 @@ import { createPageArticlesQuery } from '~/queries/create-page-articles-query';
 import { getVaccinePageQuery } from '~/queries/variants-page-query';
 import {
   createGetStaticProps,
-  StaticProps
+  StaticProps,
 } from '~/static-props/create-get-static-props';
 import {
   createGetContent,
   getLastGeneratedDate,
-  selectNlPageMetricData
+  selectNlPageMetricData,
 } from '~/static-props/get-data';
 import { VariantsPageQuery } from '~/types/cms';
 import { assert } from '~/utils/assert';
