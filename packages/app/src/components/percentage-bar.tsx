f982b0be771e7339ab6363727fb2cc5e7e8ccf5f
R <69849293+VWSCoronaDashboard1@users.noreply.github.com>
2021-06-23 15:23:21 +0200

    Add minwidth (#3048)
    Co-authored-by: VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>

--- packages/app/src/components/percentage-bar.tsx
+++ packages/app/src/components/percentage-bar.tsx
@@ -13,18 +13,23 @@ const Wrapper = styled.div(
   })
 );
 
-const Bar = styled.div<{ height?: number | string }>((x) =>
+const Bar = styled.div<{ height?: number | string; minWidth?: string }>((x) =>
   css({
     backgroundColor: 'currentcolor',
     height: x.height ?? '0.8em',
-    minWidth: '2px',
+    minWidth: x.minWidth,
   })
 );
 
 export function PercentageBar({ percentage, height }: PercentageProps) {
+  const minWidth = percentage > 0 ? '2px' : undefined;
   return (
     <Wrapper>
-      <Bar style={{ width: `${percentage}%` }} height={height} />
+      <Bar
+        style={{ width: `${percentage}%` }}
+        height={height}
+        minWidth={minWidth}
+      />
     </Wrapper>
   );
 }
