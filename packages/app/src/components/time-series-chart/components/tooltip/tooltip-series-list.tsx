5d5ae9a0005bd53aeb0574d9f5efbd5c8ca49fc1
VWSCoronaDashboard14 <76471292+VWSCoronaDashboard14@users.noreply.github.com>
2021-06-23 09:50:36 +0200

    Remove unnecessary box in tooltip

--- packages/app/src/components/time-series-chart/components/tooltip/tooltip-series-list.tsx
+++ packages/app/src/components/time-series-chart/components/tooltip/tooltip-series-list.tsx
@@ -190,12 +190,10 @@ function TooltipListItem({
         </Box>
       ) : (
         <>
-          {icon ? (
+          {icon && (
             <Box flexShrink={0} display="flex" alignItems="baseline" mt={1}>
               {icon}
             </Box>
-          ) : (
-            <Box width={0} mt={1} />
           )}
           <Box flexGrow={1}>
             <TooltipEntryContainer>
