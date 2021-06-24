a2faa22b25cfa4ab191157203393b106c97bf303
VWSCoronaDashboard14 <76471292+VWSCoronaDashboard14@users.noreply.github.com>
2021-06-24 16:09:57 +0200

    Change color of button and darker color for the modal

--- packages/app/src/components/modal.tsx
+++ packages/app/src/components/modal.tsx
@@ -85,7 +85,7 @@ const StyledFullscreenModal = styled.div(
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    background: 'rgba(0, 0, 0, 0.2)',
+    background: 'rgba(0, 0, 0, 0.6)',
     width: '100%',
     height: '100%',
     display: 'grid',
