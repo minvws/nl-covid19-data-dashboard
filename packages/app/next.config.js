cf9c2523bc451c78f0829b176c5d36f2882cd753
TK <71320230+VWSCoronaDashboard8@users.noreply.github.com>
2021-06-28 18:53:19 +0200

    Upgrade to nextjs 11 (#3027)
    * Upgrade next to 11 plus some minor deps
    
    * Use router in common way
    
    * Remove the future webpack flag
    
    * Update config for v 11
    
    * Use canary version of nextjs to get around svg issue
    
    * Fix compile error
    
    * Add eslint-config-next
    
    * Add guard for content.highlight is null
    
    * Add guard for content pageLinks is null
    
    * Clean up
    
    * Upgrade to next 11.0.1
    
    * Upgrade some more deps

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -5,61 +5,11 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 });
 
 const nextConfig = {
-  future: {
-    webpack5: true,
-  },
   /**
-   * Enables react strict mode https://nextjs.org/docs/api-reference/next.config.js/react-strict-mode
+   * Enables react strict mode
+   * https://nextjs.org/docs/api-reference/next.config.js/react-strict-mode
    */
   reactStrictMode: true,
-
-  /*i18n: {
-    // These are all the locales you want to support in
-    // your application
-    locales: ['nl', 'en'],
-    // This is the default locale you want to be used when visiting
-    // a non-locale prefixed path e.g. `/hello` or a (sub)domain that's not mapped to a locale.
-    defaultLocale: 'nl',
-    // When localeDetection is set to false Next.js will no longer automatically
-    // redirect based on the user's preferred locale and will only provide locale information
-    // detected from either the locale based domain or locale path as described above.
-    localeDetection: false,
-    // This is a list of locale domains and the default locale they
-    // should handle (these are only required when setting up domain routing)
-    // Note: subdomains must be included in the domain value to be matched e.g. "fr.example.com".
-    domains: [
-      //Production environments
-      // {
-      //   domain: 'coronadashboard.nl',
-      //   defaultLocale: 'nl',
-      // },
-      // {
-      //   domain: 'coronadashboard.rijksoverheid.nl',
-      //   defaultLocale: 'nl',
-      // },
-      // {
-      //   domain: 'coronadashboard.government.com',
-      //   defaultLocale: 'en',
-      // },
-      // For convenience, the deploy previews are configured as well
-      {
-        domain: 'develop-en-covid19-data-dashboard.vercel.app',
-        defaultLocale: 'en',
-      },
-      {
-        domain: 'master-en-covid19-data-dashboard.vercel.app',
-        defaultLocale: 'en',
-      },
-      {
-        domain: 'develop-nl-covid19-data-dashboard.vercel.app',
-        defaultLocale: 'nl',
-      },
-      {
-        domain: 'master-nl-covid19-data-dashboard.vercel.app',
-        defaultLocale: 'nl',
-      },
-    ],
-  },*/
 
   webpack(config) {
     config.module.rules.push({
@@ -89,17 +39,6 @@ const nextConfig = {
 
     config.resolve.alias = {
       ...config.resolve.alias,
-
-      /**
-       * react-spring is a dependency of @visx/xychart and the default import
-       * is not ie11-compatible. We'll use an alias to point the import to a
-       * common js version of that library.
-       *
-       * @TODO currently disabled because we don't depend on @visx/xychart
-       * yet, but I'll leave it here for future reference.
-       */
-      // 'react-spring$': 'react-spring/web.cjs',
-      // 'react-spring/renderprops$': 'react-spring/renderprops.cjs',
     };
 
     config.plugins.push(
