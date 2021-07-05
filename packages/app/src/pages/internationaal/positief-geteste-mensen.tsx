696d700d2f05757c8cf3f460053e7d3dd8089747
VWSCoronaDashboard14 <76471292+VWSCoronaDashboard14@users.noreply.github.com>
2021-07-05 10:13:08 +0200

    Add new information header element international page (#3072)
    * Add new information header element
    
    * Delete old files from other branch
    
    * Replace content header with new information block
    
    * Change metadata add sources keys
    
    * Github feedback
    
    * Github feedback
    
    * Remove comment code
    
    * Last hidden feedback

--- packages/app/src/pages/internationaal/positief-geteste-mensen.tsx
+++ packages/app/src/pages/internationaal/positief-geteste-mensen.tsx
@@ -1,12 +1,11 @@
 import { assert } from '@corona-dashboard/common';
 import { isDefined } from 'ts-is-present';
 import Getest from '~/assets/test.svg';
-import { ArticleStrip } from '~/components/article-strip';
 import { ArticleSummary } from '~/components/article-teaser';
 import { EuropeChoropleth } from '~/components/choropleth/europe-choropleth';
 import { internationalThresholds } from '~/components/choropleth/international-thresholds';
 import { PositiveTestedPeopleInternationalTooltip } from '~/components/choropleth/tooltips/international/positive-tested-people-international-tooltip';
-import { ContentHeader } from '~/components/content-header';
+import { PageInformationBlock } from '~/components/page-information-block';
 import { TileList } from '~/components/tile-list';
 import { EuropeChoroplethTile } from '~/domain/internationaal/europe-choropleth-tile';
 import { choroplethMockData } from '~/domain/internationaal/logic/choropleth-mock-data';
@@ -14,6 +13,7 @@ import { InternationalLayout } from '~/domain/layout/international-layout';
 import { Layout } from '~/domain/layout/layout';
 import { useIntl } from '~/intl';
 import { createPageArticlesQuery } from '~/queries/create-page-articles-query';
+import { getInPositiveTestsQuery } from '~/queries/in-positive-tests-query';
 import {
   createGetStaticProps,
   StaticProps,
@@ -24,14 +24,21 @@ import {
   getLastGeneratedDate,
 } from '~/static-props/get-data';
 import { getCountryNames } from '~/static-props/utils/get-country-names';
+import { InPositiveTestsQuery } from '~/types/cms';
 
 export const getStaticProps = createGetStaticProps(
   getLastGeneratedDate,
   createGetContent<{
-    articles?: ArticleSummary[];
+    page: InPositiveTestsQuery;
+    highlight: {
+      articles?: ArticleSummary[];
+    };
   }>(() => {
     const locale = process.env.NEXT_PUBLIC_LOCALE || 'nl';
-    return createPageArticlesQuery('in_positiveTestsPage', locale);
+    return `{
+      "page": ${getInPositiveTestsQuery()},
+      "highlight": ${createPageArticlesQuery('in_positiveTestsPage', locale)}
+    }`;
   }),
   createGetChoroplethData({
     in: ({ tested_overall }) => tested_overall || choroplethMockData(),
@@ -72,20 +79,24 @@ export default function PositiefGetesteMensenPage(
     <Layout {...metadata} lastGenerated={lastGenerated}>
       <InternationalLayout lastGenerated={lastGenerated}>
         <TileList>
-          <ContentHeader
+          <PageInformationBlock
             title={text.titel}
             icon={<Getest />}
-            subtitle={text.pagina_toelichting}
+            description={text.pagina_toelichting}
             metadata={{
               datumsText: text.datums,
               dateOrRange: 0, // @TODO date
               dateOfInsertionUnix: 0, // @TODO date
-              dataSources: [text.bronnen.rivm],
+              dataSources: [
+                text.bronnen.rivm,
+                text.bronnen.our_world_in_data,
+                text.bronnen.ecdc,
+              ],
             }}
-            reference={text.reference}
+            referenceLink={text.reference.href}
+            articles={content.highlight.articles}
+            usefulLinks={content.page.usefulLinks}
           />
-
-          {content.articles && <ArticleStrip articles={content.articles} />}
 
           <EuropeChoroplethTile
             title={text.choropleth.titel}
