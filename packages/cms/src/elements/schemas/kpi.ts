bdb9560ff3178e9d557ece7bd0a7f8669aaf72e9
TK <71320230+VWSCoronaDashboard8@users.noreply.github.com>
2021-07-06 17:42:09 +0200

    Clean up

--- packages/cms/src/elements/schemas/kpi.ts
+++ packages/cms/src/elements/schemas/kpi.ts
@@ -1,6 +1,4 @@
-import { snakeCase } from 'change-case';
-import { isDefined } from 'ts-is-present';
-import { commonFields, commonPreview, scopes } from './shared';
+import { commonFields, commonPreview } from './shared';
 
 export const kpi = {
   name: 'kpi',
