5d4fdf421d2bce89548475039b72ac576d7f8e32
VWSCoronaDashboard14 <76471292+VWSCoronaDashboard14@users.noreply.github.com>
2021-07-09 11:20:49 +0200

    Remove mock data

--- packages/app/src/pages/internationaal/positief-geteste-mensen.tsx
+++ packages/app/src/pages/internationaal/positief-geteste-mensen.tsx
@@ -14,7 +14,6 @@ import { TimeSeriesChart } from '~/components/time-series-chart';
 import { LineSeriesDefinition } from '~/components/time-series-chart/logic';
 import { EuropeChoroplethTile } from '~/domain/international/europe-choropleth-tile';
 import { InfectedTableTile } from '~/domain/international/infected-table-tile';
-import { choroplethMockData } from '~/domain/international/logic/choropleth-mock-data';
 import { CountryOption } from '~/domain/international/select-countries/context';
 import {
   CountryCode,
@@ -63,7 +62,7 @@ export const getStaticProps = withFeatureNotFoundPage(
     }`;
     }),
     createGetChoroplethData({
-      in: ({ tested_overall }) => tested_overall || choroplethMockData(),
+      in: ({ tested_overall }) => tested_overall,
     }),
     () => {
       const { internationalData } = getInData([...countryCodes])();
