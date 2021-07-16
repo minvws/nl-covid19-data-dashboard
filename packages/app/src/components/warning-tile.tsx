8ad213b2c10d2f357ffe6fb6198c10ec39ab3d07
A <66416946+VWSCoronaDashboard@users.noreply.github.com>
2021-07-16 15:16:31 +0200

    Fix lint boobo

--- packages/app/src/components/warning-tile.tsx
+++ packages/app/src/components/warning-tile.tsx
@@ -2,7 +2,6 @@ import css from '@styled-system/css';
 import { ComponentType, ReactNode } from 'react';
 import styled from 'styled-components';
 import WarningIcon from '~/assets/warning.svg';
-import { useBreakpoints } from '~/utils/use-breakpoints';
 import { Box } from './base';
 import { Markdown } from './markdown';
 
