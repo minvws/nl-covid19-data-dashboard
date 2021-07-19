90d919644d1fa34f973cba85cb5af10a50fdef48
TK <71320230+VWSCoronaDashboard8@users.noreply.github.com>
2021-07-19 17:48:56 +0200

    Apply some visual tweaks to vaccine coverage chart (#3207)
    * Apply some visual tweaks to vaccine coverage chart
    
    * Add two more tooltips
    
    * Use date_unix for footer
    
    * Move click handler to button

--- packages/app/src/pages/landelijk/vaccinaties.tsx
+++ packages/app/src/pages/landelijk/vaccinaties.tsx
@@ -200,8 +200,7 @@ const VaccinationPage = (props: StaticProps<typeof getStaticProps>) => {
               }
               metadata={{
                 datumsText: text.datums,
-                date: vaccine_coverage_per_age_group.values[0]
-                  .date_of_report_unix,
+                date: vaccine_coverage_per_age_group.values[0].date_unix,
                 source: siteText.vaccinaties.vaccination_coverage.bronnen.rivm,
               }}
             >
