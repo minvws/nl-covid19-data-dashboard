80cfa44490a1c648c97c75559b8f8308d345b142
R <69849293+VWSCoronaDashboard1@users.noreply.github.com>
2021-07-19 17:21:18 +0200

    Add information tiles to international pages (#3202)
    * Add information blocks to international pages
    
    * Add focusable="false"
    
    * Remove comment
    
    Co-authored-by: VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>

---
+++ packages/app/src/components/information-tile.tsx
@@ -0,0 +1,24 @@
+import css from '@styled-system/css';
+import { ComponentType, ReactNode } from 'react';
+import InformationIcon from '~/assets/information.svg';
+import { Box } from './base';
+import { WarningTile } from './warning-tile';
+
+interface InformationTileProps {
+  message: ReactNode;
+  icon?: ComponentType;
+  isFullWidth?: boolean;
+}
+
+export function InformationTile({
+  message,
+  icon = InformationIcon,
+  isFullWidth = true,
+}: InformationTileProps) {
+  const Icon = icon;
+  return (
+    <Box css={css({ boxShadow: 'tile' })}>
+      <WarningTile message={message} isFullWidth={isFullWidth} icon={Icon} />
+    </Box>
+  );
+}
