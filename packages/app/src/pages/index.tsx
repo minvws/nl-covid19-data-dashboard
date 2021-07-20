c022f71579c0aba04317d19b5d278223f1afa421
VWSCoronaDashboard14 <76471292+VWSCoronaDashboard14@users.noreply.github.com>
2021-07-20 12:18:06 +0200

    Add functionality to toggle the weekly message on the homepage (#3111)
    * Work in progress adding sanity query
    
    * Write custom validation
    
    * Add warning and safeguard to never map more items than needed
    
    * Remove log

--- packages/app/src/pages/index.tsx
+++ packages/app/src/pages/index.tsx
@@ -80,6 +80,7 @@ export const getStaticProps = createGetStaticProps(
     gm: ({ tested_overall }) => ({ tested_overall }),
   }),
   createGetContent<{
+    showWeeklyHighlight: boolean;
     articles?: ArticleSummary[];
     weeklyHighlight?: WeeklyHighlightProps;
     highlights?: HighlightTeaserProps[];
@@ -227,6 +228,7 @@ const Home = (props: StaticProps<typeof getStaticProps>) => {
                 <HighlightsTile
                   weeklyHighlight={content.weeklyHighlight}
                   highlights={content.highlights}
+                  showWeeklyHighlight={content.showWeeklyHighlight}
                 />
               </Box>
             )}
