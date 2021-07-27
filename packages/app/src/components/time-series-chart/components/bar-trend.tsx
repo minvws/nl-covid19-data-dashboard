6eb842e923a65c0910deb87a7a511983952770f2
VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2021-07-27 13:34:35 +0200

    Render bar chart as a stepped area chart when bar width is 1 pixel Fix centering of the marker dot on the tooltip marker line

--- packages/app/src/components/time-series-chart/components/bar-trend.tsx
+++ packages/app/src/components/time-series-chart/components/bar-trend.tsx
@@ -1,9 +1,11 @@
 import { scaleBand } from '@visx/scale';
+import { PositionScale } from '@visx/shape/lib/types';
 import { transparentize } from 'polished';
 import { useMemo } from 'react';
 import { isPresent } from 'ts-is-present';
 import { useUniqueId } from '~/utils/use-unique-id';
 import { Bounds, GetX, GetY, SeriesSingleValue } from '../logic';
+import { AreaTrend } from './area-trend';
 
 const DEFAULT_FILL_OPACITY = 0.2;
 
@@ -16,6 +18,7 @@ type BarTrendProps = {
   bounds: Bounds;
   bandPadding?: number;
   id: string;
+  yScale: PositionScale;
 };
 
 export function BarTrend({
@@ -27,6 +30,7 @@ export function BarTrend({
   bounds,
   bandPadding = 0.2,
   id,
+  yScale,
 }: BarTrendProps) {
   const nonNullSeries = useMemo(
     () => series.filter((x) => isPresent(x.__value)),
@@ -51,27 +55,49 @@ export function BarTrend({
   const barWidth = Math.max(xScale.bandwidth(), 1);
   const zeroPosition = getY({ __value: 0, __date_unix: 0 });
 
-  return (
-    <>
-      {nonNullSeries.map((item, index) => {
-        const x = getX(item) - barWidth / 2;
-        const y = Math.min(zeroPosition, getY(item));
-        const barHeight = Math.abs(zeroPosition - getY(item));
+  /**
+   * Only render bars when the bar width is greater than one pixel, otherwise render
+   * it as a 'stepped' area instead. A stepped area will consistently look like a
+   * bar chart with 1 pixel bar widths, where an actual bar chart with these sizes
+   * will sometimes render a gap and sometimes not. Resulting in weird looking charts.
+   */
+  if (barWidth > 1) {
+    return (
+      <>
+        {nonNullSeries.map((item, index) => {
+          const x = getX(item) - barWidth / 2;
+          const y = Math.min(zeroPosition, getY(item));
+          const barHeight = Math.abs(zeroPosition - getY(item));
 
-        return (
-          <rect
-            key={index}
-            x={x}
-            y={y}
-            height={barHeight}
-            width={barWidth}
-            fill={transparentize(1 - fillOpacity, color)}
-            id={id}
-          />
-        );
-      })}
-    </>
-  );
+          return (
+            <rect
+              key={index}
+              x={x}
+              y={y}
+              height={barHeight}
+              width={barWidth}
+              fill={transparentize(1 - fillOpacity, color)}
+              id={id}
+            />
+          );
+        })}
+      </>
+    );
+  } else {
+    return (
+      <AreaTrend
+        series={series}
+        color={color}
+        fillOpacity={fillOpacity}
+        strokeWidth={0}
+        curve="step"
+        getX={getX}
+        getY={getY}
+        yScale={yScale}
+        id={id}
+      />
+    );
+  }
 }
 
 interface BarTrendIconProps {
