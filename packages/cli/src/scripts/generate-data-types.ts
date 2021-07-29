9753443170af83b7445ad94de4fd175154ef05e7
TK <71320230+VWSCoronaDashboard8@users.noreply.github.com>
2021-07-29 15:13:24 +0200

    Make it compile again

--- packages/cli/src/scripts/generate-data-types.ts
+++ packages/cli/src/scripts/generate-data-types.ts
@@ -64,7 +64,7 @@ async function generateTypeScriptFromSchema(schemaName: string) {
 }
 
 function writeDefinitionsToFile(typeDefinitions: string[]) {
-  const outputFile = path.join(outputDirectory, 'data.d.ts');
+  const outputFile = path.join(outputDirectory, 'data.ts');
 
   fs.writeFileSync(
     outputFile,
