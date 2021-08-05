64ce539ccc5bb0725f80af262d46b44fa504312d
A <69853534+VWSCoronaDashboard4@users.noreply.github.com>
2021-08-05 15:45:13 +0200

    Allow larger y axis for vaccine chart (#3312)

--- packages/app/src/domain/vaccine/vaccine-delivery-and-administrations-area-chart.tsx
+++ packages/app/src/domain/vaccine/vaccine-delivery-and-administrations-area-chart.tsx
@@ -60,6 +60,7 @@ export function VaccineDeliveryAndAdministrationsAreaChart({
                 siteText.vaccinaties.data.vaccination_chart.legend.expected,
             },
           ],
+          forcedMaximumValue: (seriesMax) => seriesMax * 1.1,
           timeAnnotations: [
             {
               type: 'divider',
