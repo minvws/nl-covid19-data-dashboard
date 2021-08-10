6eadb81adda8ba2d4e7f24ded2ac62eb8d8790df
VWS Corona Dashboard <75375917+VWSCoronaDashboard13@users.noreply.github.com>
2021-08-10 11:40:59 +0200

    update legend

--- packages/app/src/domain/international/variants-stacked-area-tile/variants-stacked-area-tile.tsx
+++ packages/app/src/domain/international/variants-stacked-area-tile/variants-stacked-area-tile.tsx
@@ -86,8 +86,8 @@ function VariantStackedAreaTileWithData({
   /* Static legend contains only the inaccurate item */
   const staticLegendItems: LegendItem[] = [
     {
-      shape: 'square',
-      color: colors.data.underReported,
+      shape: 'outlined-square',
+      color: colors.white,
       label: text.legend_niet_compleet_label,
     },
   ];
