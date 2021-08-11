ffb20b534473c1f98dc6e548454e8e6f82bd98ae
VWS Corona Dashboard <75375917+VWSCoronaDashboard13@users.noreply.github.com>
2021-08-11 13:08:41 +0200

    fixing lint error

--- packages/app/src/domain/vaccine/vaccine-page-introduction.tsx
+++ packages/app/src/domain/vaccine/vaccine-page-introduction.tsx
@@ -2,7 +2,6 @@ import { Nl } from '@corona-dashboard/common';
 import css from '@styled-system/css';
 import { ReactComponent as VaccinatieIcon } from '~/assets/vaccinaties.svg';
 import { Box } from '~/components/base';
-import { HeadingWithIcon } from '~/components/heading-with-icon';
 import { KpiValue } from '~/components/kpi-value';
 import { Tile } from '~/components/tile';
 import { TwoKpiSection } from '~/components/two-kpi-section';
