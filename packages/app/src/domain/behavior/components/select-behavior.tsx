56a83c9b263ea22a8eae6ca4beb71295c3851b83
VWSCoronaDashboard13 <75375917+VWSCoronaDashboard13@users.noreply.github.com>
2021-08-12 10:19:10 +0200

    Filter Behavior Chart Select Options (#3326)

--- packages/app/src/domain/behavior/components/select-behavior.tsx
+++ packages/app/src/domain/behavior/components/select-behavior.tsx
@@ -9,21 +9,28 @@ import { BehaviorIcon } from './behavior-icon';
 interface SelectBehaviorProps {
   value: BehaviorIdentifier;
   onChange: (value: BehaviorIdentifier) => void;
+  options?: BehaviorIdentifier[];
 }
 
-export function SelectBehavior({ value, onChange }: SelectBehaviorProps) {
+export function SelectBehavior({
+  value,
+  onChange,
+  options = behaviorIdentifiers as unknown as BehaviorIdentifier[],
+}: SelectBehaviorProps) {
   const intl = useIntl();
-  const options = behaviorIdentifiers.map((id) => ({
-    value: id,
-    label: intl.siteText.gedrag_onderwerpen[id],
-  }));
+  const selectOptions = options
+    .map((id) => ({
+      value: id,
+      label: intl.siteText.gedrag_onderwerpen[id],
+    }))
+    .sort((a, b) => a.label.localeCompare(b.label));
 
   return (
     <Select
       value={value}
       onChange={onChange}
       icon={<BehaviorIcon name={value} size={25} />}
-      options={options}
+      options={selectOptions}
     />
   );
 }
