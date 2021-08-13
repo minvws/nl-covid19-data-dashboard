c1ea53b87652f3dbc5c2485e516587bf9da23a85
VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2021-08-13 11:11:42 +0200

    Enhance comment

--- packages/app/src/components/choropleth/logic/utils.ts
+++ packages/app/src/components/choropleth/logic/utils.ts
@@ -41,7 +41,8 @@ export function featureHasPath(
 }
 
 /**
- *  Take the feature path and round each coordinate to create a smaller number, hence decreasing the file size immensely
+ * Take the feature path and round each coordinate to create a smaller number, hence decreasing the file size immensely.
+ * The extra feature detail isn't needed, since the difference between coordinate 30.4323424323423424 or 30.43 isn't visible.
  */
 export function truncatePathCoordinates(feature: ParsedFeatureWithPath) {
   return {
