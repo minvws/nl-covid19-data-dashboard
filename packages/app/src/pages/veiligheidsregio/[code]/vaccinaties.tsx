dca933e87a3c658964c00cf65b67b1991e1ec1d3
VWSCoronaDashboard14 <76471292+VWSCoronaDashboard14@users.noreply.github.com>
2021-08-24 12:13:13 +0200

    Add pages gm and vr

--- packages/app/src/pages/veiligheidsregio/[code]/vaccinaties.tsx
+++ packages/app/src/pages/veiligheidsregio/[code]/vaccinaties.tsx
@@ -1,11 +1,15 @@
+import { PageInformationBlock } from '~/components/page-information-block';
 import { TileList } from '~/components/tile-list';
 import { Layout } from '~/domain/layout/layout';
 import { VrLayout } from '~/domain/layout/vr-layout';
+import { VaccinePageIntroductionVrGm } from '~/domain/vaccine/vaccine-page-introduction-vr-gm';
 import { useIntl } from '~/intl';
+import { withFeatureNotFoundPage } from '~/lib/features';
 import {
   createPageArticlesQuery,
   PageArticlesQueryResult,
 } from '~/queries/create-page-articles-query';
+import { getVaccinePageQuery } from '~/queries/vaccine-page-query';
 import {
   createGetStaticProps,
   StaticProps,
@@ -15,22 +19,32 @@ import {
   getLastGeneratedDate,
   selectVrPageMetricData,
 } from '~/static-props/get-data';
+import { VaccinationPageQuery } from '~/types/cms';
 import { replaceVariablesInText } from '~/utils/replace-variables-in-text';
 export { getStaticPaths } from '~/static-paths/vr';
 
-export const getStaticProps = createGetStaticProps(
-  getLastGeneratedDate,
-  selectVrPageMetricData(),
-  createGetContent<PageArticlesQueryResult>((context) => {
-    const { locale } = context;
-    return createPageArticlesQuery('elderlyAtHomePage', locale);
-  })
+export const getStaticProps = withFeatureNotFoundPage(
+  'vrVaccinationPage',
+  createGetStaticProps(
+    getLastGeneratedDate,
+    selectVrPageMetricData(),
+    createGetContent<{
+      page: VaccinationPageQuery;
+      highlight: PageArticlesQueryResult;
+    }>((context) => {
+      const { locale } = context;
+      return `{
+      "page": ${getVaccinePageQuery(locale)},
+      "highlight": ${createPageArticlesQuery('vaccinationsPage', locale)}
+    }`;
+    })
+  )
 );
 
-const VaccinationsVrPage = (props: StaticProps<typeof getStaticProps>) => {
-  const { vrName, selectedVrData: data, lastGenerated } = props;
-
-  // const { difference } = data;
+export const VaccinationsVrPage = (
+  props: StaticProps<typeof getStaticProps>
+) => {
+  const { vrName, selectedVrData: data, lastGenerated, content } = props;
   const { siteText } = useIntl();
 
   const text = siteText.veiligheidsregio_vaccinaties;
@@ -38,10 +52,10 @@ const VaccinationsVrPage = (props: StaticProps<typeof getStaticProps>) => {
   const metadata = {
     ...siteText.veiligheidsregio_vaccinaties.metadata,
     title: replaceVariablesInText(text.metadata.title, {
-      safetyRegion: vrName,
+      veiligheidsRegioNaam: vrName,
     }),
     description: replaceVariablesInText(text.metadata.description, {
-      safetyRegion: vrName,
+      veiligheidsRegioNaam: vrName,
     }),
   };
 
@@ -49,7 +63,27 @@ const VaccinationsVrPage = (props: StaticProps<typeof getStaticProps>) => {
     <Layout {...metadata} lastGenerated={lastGenerated}>
       <VrLayout data={data} vrName={vrName} lastGenerated={lastGenerated}>
         <TileList>
-          <h1>Hoi</h1>
+          <VaccinePageIntroductionVrGm
+            title={replaceVariablesInText(text.introductie_sectie.titel, {
+              veiligheidsRegioNaam: vrName,
+            })}
+            description={text.introductie_sectie.beschrijving}
+            kpiTitle={text.introductie_sectie.kpi_titel}
+            kpiValue={9999999}
+          />
+
+          <PageInformationBlock
+            description={text.informatie_blok.beschrijving}
+            metadata={{
+              datumsText: text.informatie_blok.datums,
+              dateOrRange: 1629798465,
+              dateOfInsertionUnix: 1629798465,
+              dataSources: [],
+            }}
+            usefulLinks={content.page.usefulLinks}
+            referenceLink={text.informatie_blok.reference.href}
+            articles={content.highlight.articles}
+          />
         </TileList>
       </VrLayout>
     </Layout>
