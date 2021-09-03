82bb6dc33aad0719db0ed35076040696e7d96893
A <66416946+VWSCoronaDashboard@users.noreply.github.com>
2021-09-03 14:13:30 +0200

    Message for future CMS developer

--- packages/cms/src/schemas/locale/locale-block.ts
+++ packages/cms/src/schemas/locale/locale-block.ts
@@ -5,6 +5,10 @@ export const localeBlock = {
   name: 'localeBlock',
   type: 'object',
   title: 'Locale Block Content',
+  /**
+   * The structure of localeBlock and localeRichContentBlock need to stay equal.
+   * When extending the fields, ensure to do that in blockFields and/or sync your changes.
+   */
   fields: supportedLanguages.map((lang) => ({
     title: lang.title,
     name: lang.id,
--- packages/cms/src/schemas/locale/locale-rich-content-block.ts
+++ packages/cms/src/schemas/locale/locale-rich-content-block.ts
@@ -5,6 +5,11 @@ export const localeRichContentBlock = {
   name: 'localeRichContentBlock',
   type: 'object',
   title: 'Content',
+  /**
+   * The structure of localeBlock and localeRichContentBlock need to stay equal.
+   * When extending the fields, ensure to do that in blockFields and/or sync your changes.
+   * @TODO refactor and migrate content into one type.
+   */
   fields: supportedLanguages.map((lang) => ({
     title: lang.title,
     name: lang.id,
