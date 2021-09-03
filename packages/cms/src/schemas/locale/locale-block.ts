9d481fdca99128884124079cde2e65be1b546bdc
A <69853534+VWSCoronaDashboard4@users.noreply.github.com>
2021-09-03 14:27:14 +0200

    Message for future CMS developer (#3471)

--- packages/cms/src/schemas/locale/locale-block.ts
+++ packages/cms/src/schemas/locale/locale-block.ts
@@ -5,6 +5,11 @@ export const localeBlock = {
   name: 'localeBlock',
   type: 'object',
   title: 'Locale Block Content',
+  /**
+   * The structure of localeBlock and localeRichContentBlock need to stay equal.
+   * When extending the fields, ensure to do that in blockFields and/or sync your changes.
+   * @TODO refactor and migrate content into one type.
+   */
   fields: supportedLanguages.map((lang) => ({
     title: lang.title,
     name: lang.id,
