aea5001b669f5d9fc87e9ca857062f0afce01eb8
R <69849293+VWSCoronaDashboard1@users.noreply.github.com>
2021-09-06 13:24:40 +0200

    Don't redirect to image, just load it and push to the browser (#3483)
    Co-authored-by: VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>

--- packages/app/src/utils/api/hash.ts
+++ packages/app/src/utils/api/hash.ts
@@ -3,6 +3,6 @@ import crypto from 'crypto';
 /**
  * Creates an md5 ash for the given string
  */
-export function hash(input: string) {
+export function hash(input: string | Buffer) {
   return crypto.createHash('md5').update(input).digest('hex');
 }
