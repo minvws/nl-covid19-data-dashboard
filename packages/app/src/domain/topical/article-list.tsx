e2a2e5425d022f9fa0b88a60781f5b9430b90738
denniswork <76471292+VWSCoronaDashboard14@users.noreply.github.com>
2021-09-22 15:47:13 +0200

    Remove double article

--- packages/app/src/domain/topical/article-list.tsx
+++ packages/app/src/domain/topical/article-list.tsx
@@ -8,27 +8,25 @@ interface ArticleListProps {
 
 export function ArticleList({ articles }: ArticleListProps) {
   return (
-    <article>
-      <Box
-        display="grid"
-        gridTemplateColumns={{ sm: 'repeat(1, 1fr)', md: 'repeat(3, 1fr)' }}
-        css={css({
-          // Same value to be aligned with the footer grid
-          columnGap: asResponsiveArray({ md: '48px' }),
-        })}
-        spacing={{ _: 3, md: 0 }}
-      >
-        {articles.map((item) => (
-          <ContentTeaser
-            key={item.slug}
-            title={item.title}
-            slug={item.slug}
-            cover={item.cover}
-            category={item.category}
-            variant="small"
-          />
-        ))}
-      </Box>
-    </article>
+    <Box
+      display="grid"
+      gridTemplateColumns={{ sm: 'repeat(1, 1fr)', md: 'repeat(3, 1fr)' }}
+      css={css({
+        // Same value to be aligned with the footer grid
+        columnGap: asResponsiveArray({ md: '48px' }),
+      })}
+      spacing={{ _: 3, md: 0 }}
+    >
+      {articles.map((item) => (
+        <ContentTeaser
+          key={item.slug}
+          title={item.title}
+          slug={item.slug}
+          cover={item.cover}
+          category={item.category}
+          variant="small"
+        />
+      ))}
+    </Box>
   );
 }
