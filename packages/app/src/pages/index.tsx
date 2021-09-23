5e26e1435992d175bef2793765a5ad2ebd2d5c76
VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>
2021-09-23 12:17:26 +0200

    Show correct hospital value on actueel Fix municipality name placeholder

--- packages/app/src/pages/index.tsx
+++ packages/app/src/pages/index.tsx
@@ -184,7 +184,7 @@ const Home = (props: StaticProps<typeof getStaticProps>) => {
                   <DataDrivenText
                     data={data}
                     metricName="hospital_nice"
-                    metricProperty="admissions_on_date_of_reporting"
+                    metricProperty="admissions_on_date_of_admission_moving_average"
                     differenceKey="hospital_nice__admissions_on_date_of_reporting_moving_average"
                     valueTexts={
                       text.data_driven_texts.intake_hospital_ma_nieuw.value
