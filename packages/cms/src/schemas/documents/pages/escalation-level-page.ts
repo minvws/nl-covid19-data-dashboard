5188c6890c8ae68a79802cf2f5d969613e0f09b1
denniswork <76471292+VWSCoronaDashboard14@users.noreply.github.com>
2021-09-29 12:46:18 +0200

    Update Sanity schema

--- packages/cms/src/schemas/documents/pages/escalation-level-page.ts
+++ packages/cms/src/schemas/documents/pages/escalation-level-page.ts
@@ -1,8 +1,37 @@
-import { HIGHLIGHTED_ARTICLES } from '../../fields/highlighted-articles';
+import { Radio } from '@sanity/ui';
+import { Rule } from '~/sanity';
 
 export const escalationLevelPage = {
   title: 'Inschaling risiconiveau',
   name: 'escalationLevelPage',
   type: 'document',
-  fields: [HIGHLIGHTED_ARTICLES],
+  fields: [
+    {
+      title: 'Risiconiveau kiezen',
+      name: 'riskLevel',
+      type: 'array',
+      of: [{ type: 'number' }],
+      validation: (rule: Rule) => rule.required().length(1),
+      options: {
+        list: [
+          { title: 'Waakzaam', value: 1 },
+          { title: 'Zorgelijk', value: 2 },
+          { title: 'Ernstig', value: 3 },
+        ],
+      },
+    },
+    {
+      title: 'Inschaling vanaf',
+      name: 'date',
+      type: 'date',
+      options: {
+        dateFormat: 'YYYY-MM-DD',
+      },
+      validation: (rule: Rule) => rule.required(),
+    },
+  ],
 };
+
+// 'riskLevel': *[_type == 'escalationLevelPage']{
+//   riskLevel,
+// }[0]
