5117d8f9dc1ed425adad53d5756f10fc4c0ee6a8
denniswork <76471292+VWSCoronaDashboard14@users.noreply.github.com>
2021-10-04 10:25:04 +0200

    Switch metrics around

--- packages/app/src/pages/actueel/veiligheidsregio/[code].tsx
+++ packages/app/src/pages/actueel/veiligheidsregio/[code].tsx
@@ -184,9 +184,8 @@ const TopicalVr = (props: StaticProps<typeof getStaticProps>) => {
                       siteText.veiligheidsregio_actueel.mini_trend_tiles
                         .vaccinatiegraad.menu_item_label,
                     data: data.vaccine_coverage_per_age_group.values,
-                    dataProperty: 'fully_vaccinated_percentage',
-                    value:
-                      renderedAgeGroup18Pluslabels.fully_vaccinated_percentage,
+                    dataProperty: 'has_one_shot_percentage',
+                    value: renderedAgeGroup18Pluslabels.has_one_shot_percentage,
                     valueIsPercentage: true,
                     warning: getWarning(
                       content.elements.warning,
