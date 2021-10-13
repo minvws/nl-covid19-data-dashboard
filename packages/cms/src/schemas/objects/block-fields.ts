2aff01fa90a3698b6f657b6d2200570f5b96edde
VWSCoronaDashboard16 <85888969+VWSCoronaDashboard16@users.noreply.github.com>
2021-10-13 15:27:33 +0200

    Support relative URLs in rich text

--- packages/cms/src/schemas/objects/block-fields.ts
+++ packages/cms/src/schemas/objects/block-fields.ts
@@ -27,13 +27,14 @@ export const blockFields = [
         {
           name: 'link',
           type: 'object',
-          title: 'External link',
+          title: 'Link',
           icon: MdLink,
           fields: [
             {
               name: 'href',
               type: 'url',
               title: 'URL',
+              validation: (rule: Rule) => rule.uri({ allowRelative: true }),
             },
           ],
         },
