f25e910e0c8c459ff49b4c113357c7f8921c0b7e
VWSCoronaDashboard16 <85888969+VWSCoronaDashboard16@users.noreply.github.com>
2021-10-19 17:49:10 +0200

    Fix 7-day-average period being inclusive (#3683)
    # Conflicts:
    #	packages/app/src/pages/landelijk/intensive-care-opnames.tsx
    #	packages/app/src/pages/landelijk/ziekenhuis-opnames.tsx

--- packages/app/src/pages/gemeente/[code]/ziekenhuis-opnames.tsx
+++ packages/app/src/pages/gemeente/[code]/ziekenhuis-opnames.tsx
@@ -93,7 +93,7 @@ const IntakeHospital = (props: StaticProps<typeof getStaticProps>) => {
   );
 
   const sevenDayAverageDates: [number, number] = [
-    underReportedRange - WEEK_IN_SECONDS - DAY_IN_SECONDS,
+    underReportedRange - WEEK_IN_SECONDS,
     underReportedRange - DAY_IN_SECONDS,
   ];
 
