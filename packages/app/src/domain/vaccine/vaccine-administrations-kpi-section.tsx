6e4838e7548c1ce1fa08209d9b3fb75d7dde01e4
denniswork <76471292+VWSCoronaDashboard14@users.noreply.github.com>
2021-10-26 09:58:54 +0200

    Remove comments

--- packages/app/src/domain/vaccine/vaccine-administrations-kpi-section.tsx
+++ packages/app/src/domain/vaccine/vaccine-administrations-kpi-section.tsx
@@ -16,11 +16,10 @@ export function VaccineAdministrationsKpiSection({
 }: {
   data: Pick<
     Nl,
-    | 'vaccine_administered_total' // keep
-    | 'vaccine_administered_ggd' // not used other then this
-    | 'vaccine_administered_hospitals_and_care_institutions' // not used other then this
-    | 'vaccine_administered_doctors' // not used other then this
-    | 'vaccine_administered_ggd_ghor' // not used other then this
+    | 'vaccine_administered_total'
+    | 'vaccine_administered_ggd'
+    | 'vaccine_administered_hospitals_and_care_institutions'
+    | 'vaccine_administered_doctors'
   >;
 }) {
   const { siteText } = useIntl();
