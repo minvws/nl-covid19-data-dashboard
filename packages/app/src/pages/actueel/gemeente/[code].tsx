7151a23d39da4f1ce367e13b2ab41d5e0625df01
VWSCoronaDashboard14 <76471292+VWSCoronaDashboard14@users.noreply.github.com>
2021-10-26 09:13:26 +0200

    Swap buttons topical page (#3704)
    * Move buttons
    
    * Also move on GM/VR

--- packages/app/src/pages/actueel/gemeente/[code].tsx
+++ packages/app/src/pages/actueel/gemeente/[code].tsx
@@ -353,6 +353,25 @@ const TopicalMunicipality = (props: StaticProps<typeof getStaticProps>) => {
               </MiniTileSelectorLayout>
             </Box>
 
+            <Box pt={4} pb={5}>
+              <Search title={siteText.common_actueel.secties.search.title.gm} />
+            </Box>
+
+            <VaccinationCoverageChoropleth
+              title={replaceVariablesInText(
+                siteText.common_actueel.secties.vaccination_coverage_choropleth
+                  .title.gm,
+                { municipalityName: municipalityName }
+              )}
+              content={replaceVariablesInText(
+                siteText.common_actueel.secties.vaccination_coverage_choropleth
+                  .content.gm,
+                { municipalityName: municipalityName }
+              )}
+              gmCode={gmCode}
+              data={{ gm: choropleth.gm.vaccine_coverage_per_age_group }}
+            />
+
             <CollapsibleButton
               label={siteText.common_actueel.overview_links_header}
             >
@@ -391,25 +410,6 @@ const TopicalMunicipality = (props: StaticProps<typeof getStaticProps>) => {
                 dataSitemap={dataSitemap}
               />
             </CollapsibleButton>
-
-            <VaccinationCoverageChoropleth
-              title={replaceVariablesInText(
-                siteText.common_actueel.secties.vaccination_coverage_choropleth
-                  .title.gm,
-                { municipalityName: municipalityName }
-              )}
-              content={replaceVariablesInText(
-                siteText.common_actueel.secties.vaccination_coverage_choropleth
-                  .content.gm,
-                { municipalityName: municipalityName }
-              )}
-              gmCode={gmCode}
-              data={{ gm: choropleth.gm.vaccine_coverage_per_age_group }}
-            />
-
-            <Box pt={4} pb={5}>
-              <Search title={siteText.common_actueel.secties.search.title.gm} />
-            </Box>
           </TileList>
         </MaxWidth>
 
