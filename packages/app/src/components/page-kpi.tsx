d5ff7edfd188113aff2e89d941fad3e3e453d03f
VWSCoronaDashboard17 <89384854+VWSCoronaDashboard17@users.noreply.github.com>
2021-10-28 09:55:09 +0200

    Grab PageKpi from develop

--- packages/app/src/components/page-kpi.tsx
+++ packages/app/src/components/page-kpi.tsx
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-import { DifferenceDecimal, DifferenceInteger } from '@corona-dashboard/common';
-import hash from 'hash-sum';
-import { isDefined } from 'ts-is-present';
-<<<<<<<< HEAD:packages/app/src/components/page-kpi.tsx
-=======
 import {
   getLastFilledValue,
   Metric,
@@ -11,56 +5,23 @@ import {
 } from '@corona-dashboard/common';
 import { get } from 'lodash';
 import { isDefined } from 'ts-is-present';
->>>>>>> develop
 import { KpiValue } from '~/components/kpi-value';
 import { assert } from '~/utils/assert';
 import { Box } from './base';
 import { TileAverageDifference, TileDifference } from './difference-indicator';
 
-<<<<<<< HEAD
-interface PageKpieBaseProps<T> {
-=======
 interface PageKpiBaseProps<T> {
->>>>>>> develop
   data: T;
   metricName: MetricKeys<T>;
   metricProperty: string;
   differenceFractionDigits?: number;
   currentValue?: number;
-<<<<<<< HEAD
-========
-import { BarScale } from '~/components/bar-scale';
-import { BarScaleConfig } from '~/metric-config/common';
-import { Box } from './base';
-import { TileAverageDifference, TileDifference } from './difference-indicator';
-
-interface PageBarScaleBaseProps {
-  value: number;
-  config: BarScaleConfig;
-  screenReaderText: string;
->>>>>>>> develop:packages/app/src/components/page-barscale.tsx
-=======
->>>>>>> develop
   isMovingAverageDifference?: boolean;
   showOldDateUnix?: boolean;
 }
 
 type DifferenceProps =
   | {
-<<<<<<< HEAD
-      difference?: never;
-      isAmount?: boolean;
-    }
-  | {
-      difference: DifferenceInteger | DifferenceDecimal;
-      isAmount: boolean;
-    };
-
-<<<<<<<< HEAD:packages/app/src/components/page-kpi.tsx
-type PageKpiProps<T> = PageKpieBaseProps<T> & DifferenceProps;
-
-const metricNamesHoldingPartialData = ['infectious_people', 'reproduction'];
-=======
       differenceKey?: never;
       isAmount?: boolean;
     }
@@ -75,7 +36,6 @@ export const metricNamesHoldingPartialData = [
   'infectious_people',
   'reproduction',
 ];
->>>>>>> develop
 
 export function PageKpi<T>({
   data,
@@ -123,56 +83,6 @@ export function PageKpi<T>({
     );
   }
 
-<<<<<<< HEAD
-  return (
-    <Box spacing={0} mb={3}>
-      <KpiValue absolute={propertyValue} />
-========
-type PageBarScaleProps = PageBarScaleBaseProps & DifferenceProps;
-
-export function PageBarScale(props: PageBarScaleProps) {
-  const {
-    value,
-    config,
-    screenReaderText,
-    difference,
-    isMovingAverageDifference,
-    showOldDateUnix,
-    isAmount,
-  } = props;
-  /**
-   * A unique id is required for path rendering and should be constant between
-   * server and client side rendering
-   */
-  const uniqueId = hash(props);
-
-  return (
-    <Box spacing={2} mb={3}>
-      <BarScale
-        min={config.min}
-        max={config.max}
-        limit={config.limit}
-        screenReaderText={screenReaderText}
-        value={value}
-        id={uniqueId}
-        gradient={config.gradient}
-        showValue
-        showAxis
-      />
->>>>>>>> develop:packages/app/src/components/page-barscale.tsx
-
-      {isDefined(difference) &&
-        isDefined(isAmount) &&
-        (isMovingAverageDifference ? (
-          <TileAverageDifference value={difference} isAmount={isAmount} />
-        ) : (
-          <TileDifference
-            value={difference}
-            showOldDateUnix={showOldDateUnix}
-            isAmount={isAmount}
-          />
-        ))}
-=======
   const hasDifference = isDefined(differenceKey) || isDefined(differenceValue);
 
   return (
@@ -199,7 +109,6 @@ export function PageBarScale(props: PageBarScaleProps) {
             />
           </Box>
         ) : null)}
->>>>>>> develop
     </Box>
   );
 }
