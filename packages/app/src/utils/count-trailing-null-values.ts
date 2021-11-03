f7f70445cdd151904c617bd3a90bbdc4d9c8ab98
VWSCoronaDashboard17 <89384854+VWSCoronaDashboard17@users.noreply.github.com>
2021-11-03 17:05:29 +0100

    Add countTrailingNullValues unit test

--- packages/app/src/utils/count-trailing-null-values.ts
+++ packages/app/src/utils/count-trailing-null-values.ts
@@ -3,6 +3,6 @@ import { isPresent } from 'ts-is-present';
 
 export function countTrailingNullValues<T>(values: T[], property?: keyof T) {
   return isPresent(property)
-    ? values.length - findLastIndex(values, (d) => d[property] !== null) - 1
+    ? values.length - findLastIndex(values, (d) => d[property] !== null)
     : values.length - findLastIndex(values, (d) => d !== null);
 }
