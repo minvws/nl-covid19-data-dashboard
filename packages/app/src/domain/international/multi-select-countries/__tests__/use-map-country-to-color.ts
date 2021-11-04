bf830043ead662583e7d221c1939bf155025b6b0
VWSCoronaDashboard17 <89384854+VWSCoronaDashboard17@users.noreply.github.com>
2021-11-04 09:10:25 +0100

    Improve useMapCountryToColor test coverage

--- packages/app/src/domain/international/multi-select-countries/__tests__/use-map-country-to-color.ts
+++ packages/app/src/domain/international/multi-select-countries/__tests__/use-map-country-to-color.ts
@@ -21,6 +21,27 @@ UseMapCountryToColor('should initialize in order', () => {
   );
 });
 
+UseMapCountryToColor('should assign a unique color to a new country', () => {
+  const selectedCountries: CountryCode[] = ['nld', 'bel', 'deu'];
+
+  const { result } = renderHook(() => useMapCountryToColor(selectedCountries));
+
+  act(() => {
+    result.current.toggleColor('fra');
+  });
+
+  const uniqueAssignedColors = [
+    ...new Set(
+      (['nld', 'bel', 'deu', 'fra'] as CountryCode[]).map(
+        result.current.getColor
+      )
+    ),
+  ];
+
+  assert.is(result.current.getColor('fra'), ORDERED_COLORS[3]);
+  assert.is(['nld', 'bel', 'deu', 'fra'].length, uniqueAssignedColors.length);
+});
+
 UseMapCountryToColor(
   'should return the same color for the same country after updates',
   () => {
