cb19b96ab36597826dfcf6118570acb7d5bbca40
VWSCoronaDashboard17 <89384854+VWSCoronaDashboard17@users.noreply.github.com>
2021-11-08 13:01:23 +0100

    Add tests for getBoundaryDateStartUnix

---
+++ packages/app/src/utils/__tests__/get-boundary-date-start-unix.spec.ts
@@ -0,0 +1,64 @@
+import { suite } from 'uvu';
+import * as assert from 'uvu/assert';
+import { getBoundaryDateStartUnix } from '../get-boundary-date-start-unix';
+
+const GetBoundaryDateStartUnix = suite('getBoundaryDateStartUnix');
+
+GetBoundaryDateStartUnix(
+  'should return the start date for a timespan numberOfItems from the end of a range of date values',
+  () => {
+    const values = [
+      { date_unix: Date.now() / 1000 },
+      { date_unix: Date.now() / 1000 },
+      { date_unix: Date.now() / 1000 },
+      { date_unix: Date.now() / 1000 },
+      { date_unix: Date.now() / 1000 },
+    ];
+
+    const numberOfItems = 3;
+
+    const result = getBoundaryDateStartUnix(values, numberOfItems);
+
+    assert.is(result, values[values.length - numberOfItems].date_unix);
+  }
+);
+
+GetBoundaryDateStartUnix(
+  'should return the start date for a timespan numberOfItems from the end of a range of date span values',
+  () => {
+    const values = [
+      { date_start_unix: Date.now() / 1000, date_end_unix: Date.now() / 1000 },
+      { date_start_unix: Date.now() / 1000, date_end_unix: Date.now() / 1000 },
+      { date_start_unix: Date.now() / 1000, date_end_unix: Date.now() / 1000 },
+      { date_start_unix: Date.now() / 1000, date_end_unix: Date.now() / 1000 },
+      { date_start_unix: Date.now() / 1000, date_end_unix: Date.now() / 1000 },
+    ];
+
+    const numberOfItems = 3;
+
+    const result = getBoundaryDateStartUnix(values, numberOfItems);
+
+    assert.is(result, values[values.length - numberOfItems].date_start_unix);
+  }
+);
+
+GetBoundaryDateStartUnix(
+  'should return Infinity when numberOfItems is larger then values.length',
+  () => {
+    const values = [
+      { date_unix: Date.now() / 1000 },
+      { date_unix: Date.now() / 1000 },
+      { date_unix: Date.now() / 1000 },
+      { date_unix: Date.now() / 1000 },
+      { date_unix: Date.now() / 1000 },
+    ];
+
+    const numberOfItems = values.length + 1;
+
+    const result = getBoundaryDateStartUnix(values, numberOfItems);
+
+    assert.is(result, Infinity);
+  }
+);
+
+GetBoundaryDateStartUnix.run();
