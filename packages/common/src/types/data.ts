787dcf28a19e05918a847730602a3b0d93658224
VWSCoronaDashboard16 <85888969+VWSCoronaDashboard16@users.noreply.github.com>
2021-11-16 11:14:07 +0100

    Add infected moving averages for VR/GM

--- packages/common/src/types/data.ts
+++ packages/common/src/types/data.ts
@@ -94,6 +94,7 @@ export interface GmTestedOverall {
 export interface GmTestedOverallValue {
   date_unix: number;
   infected: number;
+  infected_moving_average: number | null;
   infected_per_100k: number;
   infected_per_100k_moving_average: number | null;
   date_of_insertion_unix: number;
@@ -1074,6 +1075,7 @@ export interface VrTestedOverall {
 export interface VrTestedOverallValue {
   date_unix: number;
   infected: number;
+  infected_moving_average: number | null;
   infected_per_100k: number;
   infected_per_100k_moving_average: number | null;
   date_of_insertion_unix: number;
