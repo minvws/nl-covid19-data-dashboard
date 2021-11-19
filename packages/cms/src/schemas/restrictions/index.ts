9464d2cbd467c8cdd653c127ed8a855adb2d16fc
Jeroen Drenth <jeroen.drenth@ordina.nl>
2021-11-19 08:52:16 +0100

    Removed roadmap page

--- packages/cms/src/schemas/restrictions/index.ts
+++ packages/cms/src/schemas/restrictions/index.ts
@@ -1,6 +1,4 @@
 export * from './lockdown';
 export * from './restriction';
-export * from './restriction-category';
 export * from './restriction-group';
 export * from './restriction-group-lockdown';
-export * from './roadmap';
