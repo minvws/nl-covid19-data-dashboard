b1a6064ec7fa543ccd74670921f88dba887c2c19
VWSCoronaDashboard16 <85888969+VWSCoronaDashboard16@users.noreply.github.com>
2021-11-19 10:24:37 +0100

    Add timeline events to legend when relevant

--- packages/app/src/domain/tested/infected-per-age-group/series-config.ts
+++ packages/app/src/domain/tested/infected-per-age-group/series-config.ts
@@ -4,42 +4,52 @@ export const BASE_SERIES_CONFIG = [
   {
     metricProperty: 'infected_age_0_9_per_100k',
     color: colors.data.multiseries.cyan,
+    noLegend: true,
   },
   {
     metricProperty: 'infected_age_10_19_per_100k',
     color: colors.data.multiseries.cyan_dark,
+    noLegend: true,
   },
   {
     metricProperty: 'infected_age_20_29_per_100k',
     color: colors.data.multiseries.turquoise,
+    noLegend: true,
   },
   {
     metricProperty: 'infected_age_30_39_per_100k',
     color: colors.data.multiseries.turquoise_dark,
+    noLegend: true,
   },
   {
     metricProperty: 'infected_age_40_49_per_100k',
     color: colors.data.multiseries.yellow,
+    noLegend: true,
   },
   {
     metricProperty: 'infected_age_50_59_per_100k',
     color: colors.data.multiseries.yellow_dark,
+    noLegend: true,
   },
   {
     metricProperty: 'infected_age_60_69_per_100k',
     color: colors.data.multiseries.orange,
+    noLegend: true,
   },
   {
     metricProperty: 'infected_age_70_79_per_100k',
     color: colors.data.multiseries.orange_dark,
+    noLegend: true,
   },
   {
     metricProperty: 'infected_age_80_89_per_100k',
     color: colors.data.multiseries.magenta,
+    noLegend: true,
   },
   {
     metricProperty: 'infected_age_90_plus_per_100k',
     color: colors.data.multiseries.magenta_dark,
+    noLegend: true,
   },
   {
     metricProperty: 'infected_overall_per_100k',
