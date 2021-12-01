2d2bdeaa73b213810c27d586f25e06d87b142b78
VWSCoronaDashboard18 <jorrik.klijnsma@ordina.nl>
2021-12-01 12:50:04 +0100

    Added loader component

---
+++ packages/app/src/components/loader/loader.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { colors } from '@corona-dashboard/common';
+import { Box, MotionBox } from '~/components/base';
+import { Text } from '~/components/typography';
+
+type MotionBoxProps = { 
+  height: string,
+  width: string,
+  bg: string,
+  transformOrigin: string,
+  animate: object,
+};
+
+type TransitionProps = { 
+  duration: number,
+  repeat: number,
+  ease: string,
+};
+
+
+export function Loader() {
+
+  const duration = 1.5;
+
+  const attributes:MotionBoxProps = {
+    height: '3rem',
+    width: '0.75rem',
+    bg: colors.data.scale.blue[0],
+    transformOrigin: 'bottom center',
+    animate: { scaleY: [1, .2, .7, .4, 1] },
+  }
+
+  const transitionProps:TransitionProps = {
+    duration: duration,
+    repeat: Infinity,
+    ease: "easeInOut"
+  }
+
+
+  return (
+    <Box spacing={3} m={3} display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+      <Box spacingHorizontal={1} display={'flex'} alignItems="end">
+        <MotionBox {...attributes} transition={{...transitionProps}} />
+        <MotionBox {...attributes} transition={{...transitionProps, delay: .3 }} />
+        <MotionBox {...attributes} transition={{...transitionProps, delay: .5 }} />
+        <MotionBox {...attributes} transition={{...transitionProps, delay: .2 }} />
+        <MotionBox {...attributes} transition={{...transitionProps, delay: .4 }} />
+      </Box>
+      <Text lineHeight="1.0">Loading</Text>
+    </Box>
+  );
+}
