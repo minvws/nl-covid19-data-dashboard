76f6f8353f15835548f1343b0164865ca36c9114
R <69849293+VWSCoronaDashboard1@users.noreply.github.com>
2021-12-03 13:12:23 +0100

    Add booster KPIs and schemas (#3876)
    * Create separate schema's for the different booster metrics
    
    * Add third shot schema
    
    * Add feature flags and enable the right schemas
    
    * Generate typescript
    
    * Work in progress booster shots
    
    * Connect with data and add articles and links
    
    * Update data to come from Lokalise
    
    Co-authored-by: VWSCoronaDashboard1 <66416946+VWSCoronaDashboard1@users.noreply.github.com>
    Co-authored-by: denniswork <76471292+VWSCoronaDashboard14@users.noreply.github.com>

--- packages/common/src/feature-flags/features.ts
+++ packages/common/src/feature-flags/features.ts
@@ -118,7 +118,11 @@ export const features: Feature[] = [
   },
   {
     name: 'nlVaccinationsBoosterInformationBlock',
-    isEnabled: false,
+    isEnabled: true,
+  },
+  {
+    name: 'nlVaccinationsBoosterThirdShotInformationBlock',
+    isEnabled: true,
   },
   {
     name: 'nlVaccinationBoosterShotsPerAgeGroup',
@@ -127,10 +131,28 @@ export const features: Feature[] = [
     metricName: 'booster_shot_per_age_group',
   },
   {
-    name: 'nlVaccinationsBoosterShotsKpi',
-    isEnabled: false,
+    name: 'nlBoosterShotAdministeredKpiTile', // aan met data
+    isEnabled: true,
     dataScopes: ['nl'],
-    metricName: 'booster_shot',
+    metricName: 'booster_shot_administered',
+  },
+  {
+    name: 'nlBoosterShotDeliveredKpiTile',
+    isEnabled: true,
+    dataScopes: ['nl'],
+    metricName: 'booster_shot_delivered',
+  },
+  {
+    name: 'nlBoosterShotPlannedKpiTile',
+    isEnabled: true,
+    dataScopes: ['nl'],
+    metricName: 'booster_shot_planned',
+  },
+  {
+    name: 'nlThirdShotAdministeredKpiTile', // aan met data
+    isEnabled: true,
+    dataScopes: ['nl'],
+    metricName: 'third_shot_administered',
   },
 
   /**
@@ -144,9 +166,27 @@ export const features: Feature[] = [
     metricName: 'booster_shot_per_age_group',
   },
   {
-    name: 'nlVaccinationsBoosterShotsKpiSchemaDisable',
+    name: 'nlBoosterShotAdministeredSchemaDisable',
+    isEnabled: true,
+    dataScopes: ['nl'],
+    metricName: 'booster_shot_administered',
+  },
+  {
+    name: 'nlBoosterShotDeliveredSchemaDisable',
     isEnabled: false,
     dataScopes: ['nl'],
-    metricName: 'booster_shot',
+    metricName: 'booster_shot_delivered',
+  },
+  {
+    name: 'nlBoosterShotPlannedSchemaDisable',
+    isEnabled: false,
+    dataScopes: ['nl'],
+    metricName: 'booster_shot_planned',
+  },
+  {
+    name: 'nlThirdShotAdministeredDisable',
+    isEnabled: true,
+    dataScopes: ['nl'],
+    metricName: 'third_shot_administered',
   },
 ];
