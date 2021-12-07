0cd0e6cc4f8430bd28dd6bb12cc8d2f1cd0a7293
VWSCoronaDashboard22 <jeroen.drenth@ordina.nl>
2021-12-07 12:54:46 +0100

    Progress mobbing sessie

--- packages/app/src/components/loader/loading-wrapper.tsx
+++ packages/app/src/components/loader/loading-wrapper.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useRouter } from 'next/router';
 import { Loader } from '~/components/loader/loader';
 
@@ -10,29 +10,37 @@ export function LoadingWrapper(props: LoadingRouterProps) {
   const { previousUrl } = props;
   const router = useRouter();
 
-  const [routerLoadState, setRouterLoadState] = useState<'idle'|'loading'|'complete'>('loading');
+  const [routerLoadState, setRouterLoadState] = useState<
+    'idle' | 'loading' | 'complete'
+  >('loading');
   const [currentRoute, setCurrentRoute] = useState<string>('');
+  const handleLoadingTimeout = () => setRouterLoadState('loading');
+  const loaderTimeout = useRef(() => setTimeout(handleLoadingTimeout, 1000));
 
   useEffect(() => {
-    const handleRouteChangeStart  = (url) => {
-      setRouterLoadState('idle');
-      window.scrollTo(0, 0);
-      console.log(routerLoadState);
-      if (url.startsWith('/' + previousUrl) && routerLoadState === 'idle' && currentRoute !== url) {
-        setTimeout(() => {
-          setRouterLoadState('loading');
-        }, 700);
+    const handleRouteChangeStart = async (url: string) => {
+      await setRouterLoadState('idle');
+
+      if (
+        url.startsWith('/' + previousUrl) &&
+        // routerLoadState === 'idle' &&
+        currentRoute !== url
+      ) {
+        console.log('check');
+        loaderTimeout.current();
       }
-    }
+    };
 
-    const handleRouteChangeComplete  = (url) => {
+    const handleRouteChangeComplete = (url: string) => {
+      clearTimeout(loaderTimeout.current());
       setRouterLoadState('complete');
-      setCurrentRoute(url)
-    }
+      setCurrentRoute(url);
+    };
 
-    const handleRouteChangeError  = () => {
+    const handleRouteChangeError = () => {
+      clearTimeout(loaderTimeout.current());
       setRouterLoadState('complete');
-    }
+    };
 
     router.events.on('routeChangeStart', handleRouteChangeStart);
     router.events.on('routeChangeComplete', handleRouteChangeComplete);
@@ -42,12 +50,14 @@ export function LoadingWrapper(props: LoadingRouterProps) {
       router.events.off('routeChangeStart', handleRouteChangeStart);
       router.events.off('routeChangeComplete', handleRouteChangeComplete);
       router.events.off('routeChangeError', handleRouteChangeError);
-    }
+    };
   }, [currentRoute, previousUrl, router.events, routerLoadState]);
 
   return (
     <>
-      {(routerLoadState === 'loading' || routerLoadState === 'idle') && <Loader showLoader={routerLoadState === 'loading'} />}
+      {(routerLoadState === 'loading' || routerLoadState === 'idle') && (
+        <Loader showLoader={routerLoadState === 'loading'} />
+      )}
     </>
   );
 }
