b8d28d0a3a628ae653ebfc4c3461f81704060793
VWSCoronaDashboard22 <jeroen.drenth@ordina.nl>
2021-12-14 15:55:14 +0100

    Added lokalize texts to page part list

--- packages/cms/src/page/page-part-list-item.ts
+++ packages/cms/src/page/page-part-list-item.ts
@@ -3,6 +3,8 @@ import documentStore from 'part:@sanity/base/datastore/document';
 import { BsBook, BsBookHalf, BsFillPuzzleFill } from 'react-icons/bs';
 import { map } from 'rxjs/operators';
 
+const scopes = ['nl', 'gm', 'vr'];
+
 export function pagePartListItem() {
   return S.listItem()
     .title("Dashboard Pagina's")
@@ -13,7 +15,7 @@ export function pagePartListItem() {
 function pageIdentifierListemItem() {
   return documentStore
     .listenQuery(
-      `*[_type == 'pageIdentifier' && !(_id in path("drafts.**"))]{ title,_id }`
+      `*[_type == 'pageIdentifier' && !(_id in path("drafts.**"))]{ title,_id,identifier }`
     )
     .pipe(
       map((pages: { title: string; _id: string }[]) => {
@@ -29,6 +31,7 @@ function pageIdentifierListemItem() {
 }
 
 function pageDataListItem(page: any) {
+  console.log(page);
   return S.listItem()
     .title(page.title)
     .id(page._id)
@@ -36,9 +39,10 @@ function pageDataListItem(page: any) {
     .child(
       documentStore
         .listenQuery(
-          `*[pageIdentifier._ref == $id && !(_id in path("drafts.**"))]`,
+          `*[pageIdentifier._ref == $id && !(_id in path("drafts.**")) || _type == "lokalizeText" && subject match $name]`,
           {
             id: page._id,
+            name: `${page.identifier}*`,
           }
         )
         .pipe(
@@ -49,6 +53,58 @@ function pageDataListItem(page: any) {
                 childPages
                   .sort((a: any, b: any) => a.title.localeCompare(b.title))
                   .map(pageDataItem)
+                  .concat(
+                    scopes.map((scope) =>
+                      S.listItem()
+                        .title(scope)
+                        .icon(BsBookHalf)
+                        .child(
+                          documentStore
+                            .listenQuery(
+                              `*[_type == "lokalizeText" && subject == $name]`,
+                              {
+                                name: `deceasedPage_${scope}`,
+                              }
+                            )
+                            .pipe(
+                              map((subjects: []) => {
+                                console.log(subjects);
+                                const type = 'lokalizeText';
+                                return S.list()
+                                  .title('Onderwerp')
+                                  .items(
+                                    subjects.map((subject: any) => {
+                                      return S.listItem()
+                                        .title(subject.key)
+                                        .id(subject.key)
+                                        .child(
+                                          S.documentList()
+                                            .id(`${subject}-child`)
+                                            .title(subject.subject)
+                                            .schemaType(type)
+                                            .defaultOrdering([
+                                              {
+                                                field: 'key',
+                                                direction: 'asc',
+                                              },
+                                            ])
+                                            .filter('subject == $subject')
+                                            .params({ subject })
+                                            .child((id) =>
+                                              S.editor()
+                                                .id(id)
+                                                .schemaType(type)
+                                                .documentId(id)
+                                                .views([S.view.form()])
+                                            )
+                                        );
+                                    })
+                                  );
+                              })
+                            )
+                        )
+                    )
+                  )
               )
           )
         )
