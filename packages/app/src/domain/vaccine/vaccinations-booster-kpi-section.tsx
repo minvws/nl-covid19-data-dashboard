4e6cdb0e614c4ad418386efc17ba40de3bcb7ab2
VWSCoronaDashboard21 <VWSCoronaDashboard21@users.noreply.github.com>
2022-01-17 16:40:09 +0100

    correct kpi tile font size implementation

--- packages/app/src/domain/vaccine/vaccinations-booster-kpi-section.tsx
+++ packages/app/src/domain/vaccine/vaccinations-booster-kpi-section.tsx
@@ -1,9 +1,7 @@
-import css from '@styled-system/css';
-import { Box } from '~/components/base';
 import { KpiTile } from '~/components/kpi-tile';
+import { KpiValue } from '~/components/kpi-value';
 import { Markdown } from '~/components/markdown';
 import { TwoKpiSection } from '~/components/two-kpi-section';
-import { Text } from '~/components/typography';
 import { useIntl } from '~/intl';
 import { Metadata, MetadataProps } from '~/components/metadata';
 
@@ -25,41 +23,17 @@ export function VaccinationsBoosterKpiSection({
   const text = siteText.pages.vaccinations.nl.booster_ggd_kpi_section;
 
   return (
-    <Box spacing={4}>
-      <TwoKpiSection>
-        <KpiTile title={text.booster_last_7_days.title}>
-          <Text
-            as="div"
-            css={css({
-              color: 'data.primary',
-              fontSize: 9,
-              fontWeight: 600,
-              fontVariantNumeric: 'tabular-nums',
-              lineHeight: 1,
-            })}
-          >
-            {formatNumber(dataBoosterShotAdministered)}
-          </Text>
-          <Markdown content={text.booster_last_7_days.description} />
-          <Metadata {...metadataBoosterShotAdministered} />
-        </KpiTile>
-        <KpiTile title={text.booster_planned_7_days.title}>
-          <Text
-            as="div"
-            css={css({
-              color: 'data.primary',
-              fontSize: 9,
-              fontWeight: 600,
-              fontVariantNumeric: 'tabular-nums',
-              lineHeight: 1,
-            })}
-          >
-            {formatNumber(dataBoosterShotPlanned)}
-          </Text>
-          <Markdown content={text.booster_planned_7_days.description} />
-          <Metadata {...metadataBoosterShotPlanned} isTileFooter />
-        </KpiTile>
-      </TwoKpiSection>
-    </Box>
+    <TwoKpiSection>
+      <KpiTile title={text.booster_last_7_days.title}>
+        <KpiValue text={formatNumber(dataBoosterShotAdministered)} />
+        <Markdown content={text.booster_last_7_days.description} />
+        <Metadata {...metadataBoosterShotAdministered} isTileFooter />
+      </KpiTile>
+      <KpiTile title={text.booster_planned_7_days.title}>
+        <KpiValue text={formatNumber(dataBoosterShotPlanned)} />
+        <Markdown content={text.booster_planned_7_days.description} />
+        <Metadata {...metadataBoosterShotPlanned} isTileFooter />
+      </KpiTile>
+    </TwoKpiSection>
   );
 }
