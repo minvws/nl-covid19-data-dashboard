ec8eda682a6c942f87c3852012226213d8fa0241
VWSCoronaDashboard22 <jeroen.drenth@ordina.nl>
2022-02-17 09:12:23 +0100

    Revert "Reordered tested people chart legend (#4041)"
    This reverts commit bc7083f85afe1caf00b15528b21463f633791c25.

--- packages/app/src/pages/landelijk/positief-geteste-mensen.tsx
+++ packages/app/src/pages/landelijk/positief-geteste-mensen.tsx
@@ -475,6 +475,16 @@ const PositivelyTestedPeople = (props: StaticProps<typeof getStaticProps>) => {
                   seriesConfig={[
                     {
                       type: 'line',
+                      metricProperty: 'tested_total_moving_average',
+                      color: colors.data.secondary,
+                      label:
+                        ggdText.linechart_totaltests_legend_label_moving_average,
+                      shortLabel:
+                        siteText.positief_geteste_personen.tooltip_labels
+                          .ggd_tested_total_moving_average,
+                    },
+                    {
+                      type: 'line',
                       metricProperty: 'infected_moving_average',
                       color: colors.data.primary,
                       label:
@@ -492,16 +502,6 @@ const PositivelyTestedPeople = (props: StaticProps<typeof getStaticProps>) => {
                       shortLabel:
                         siteText.positief_geteste_personen.tooltip_labels
                           .infected,
-                    },
-                    {
-                      type: 'line',
-                      metricProperty: 'tested_total_moving_average',
-                      color: colors.data.secondary,
-                      label:
-                        ggdText.linechart_totaltests_legend_label_moving_average,
-                      shortLabel:
-                        siteText.positief_geteste_personen.tooltip_labels
-                          .ggd_tested_total_moving_average,
                     },
                     {
                       type: 'bar',
