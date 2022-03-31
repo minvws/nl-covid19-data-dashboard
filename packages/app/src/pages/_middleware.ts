0a8aca76327b851ada39ed0c76fa916546bd239d
VWSCoronaDashboard24 <VWSCoronaDashboard24@users.noreply.github.com>
2022-03-31 11:29:17 +0200

    feat: removed helmet, custom next-server.js

--- packages/app/src/pages/_middleware.ts
+++ packages/app/src/pages/_middleware.ts
@@ -11,36 +11,15 @@ export function middleware(req: NextRequest) {
     });
   }
 
-  const response = NextResponse.next();
-
   /**
    * Ensure the correct language by resetting the original hostname
    * Next.js will use the hostname to detect the language it should serve.
    */
+  const response = NextResponse.next();
   response.headers.set(
     'host',
     req.headers.get('x-original-host') || req.headers.get('host') || ''
   );
 
   return response;
-
-  //   return NextResponse.next();
-
-  //   const basicAuth = req.headers.get('authorization');
-
-  //   if (basicAuth) {
-  //     const auth = basicAuth.split(' ')[1];
-  //     const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':');
-
-  //     if (user === '4dmin' && pwd === 'testpwd123') {
-  //       return NextResponse.next();
-  //     }
-  //   }
-
-  //   return new Response('Auth required', {
-  //     status: 401,
-  //     headers: {
-  //       'WWW-Authenticate': 'Basic realm="Secure Area"',
-  //     },
-  //   });
 }
