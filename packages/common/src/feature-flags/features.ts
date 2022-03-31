d6b49c24bcd043ea312afddb92e61bfd2d8dc489
A <93994194+VWSCoronaDashboard21@users.noreply.github.com>
2022-03-31 14:34:40 +0200

    feature flag added (#4143)
    Co-authored-by: VWSCoronaDashboard21 <VWSCoronaDashboard21@users.noreply.github.com>

--- packages/common/src/feature-flags/features.ts
+++ packages/common/src/feature-flags/features.ts
@@ -224,4 +224,11 @@ export const features: Feature[] = [
     dataScopes: ['nl'],
     metricName: 'behavior_annotations',
   },
+  {
+    name: 'nlVaccineAdministered',
+    isEnabled: false,
+    dataScopes: ['nl'],
+    metricName: 'vaccine_administered',
+    metricProperties: ['novavax'],
+  }, 
 ];
