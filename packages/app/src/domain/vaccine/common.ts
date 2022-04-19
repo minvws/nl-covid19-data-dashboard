872d5d33c899cb9725a6125f9fc6a653b6dd7ff6
VWSCoronaDashboard18 <93984341+VWSCoronaDashboard18@users.noreply.github.com>
2022-04-19 16:07:05 +0200

    Connecting the right metric to the right field

--- packages/app/src/domain/vaccine/common.ts
+++ packages/app/src/domain/vaccine/common.ts
@@ -1,8 +1,7 @@
 import { colors } from '@corona-dashboard/common';
 
-export const COLOR_HAS_ONE_SHOT = colors.data.scale.blue[0];
-export const COLOR_FULLY_VACCINATED = colors.data.primary;
-export const COLOR_FULLY_BOOSTERED = colors.data.scale.blue[5];
+export const COLOR_FIRST = colors.data.scale.blue[0];
+export const COLOR_LAST = colors.data.primary
 
 export interface vaccinceMetrics {
     first: {
