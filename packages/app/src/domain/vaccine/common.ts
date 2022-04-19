2bd9a1e8f23e2dc5afe38e5e2a837943feac2586
VWSCoronaDashboard18 <93984341+VWSCoronaDashboard18@users.noreply.github.com>
2022-04-19 15:13:01 +0200

    WiP3

--- packages/app/src/domain/vaccine/common.ts
+++ packages/app/src/domain/vaccine/common.ts
@@ -3,3 +3,14 @@ import { colors } from '@corona-dashboard/common';
 export const COLOR_HAS_ONE_SHOT = colors.data.scale.blue[0];
 export const COLOR_FULLY_VACCINATED = colors.data.primary;
 export const COLOR_FULLY_BOOSTERED = colors.data.scale.blue[5];
+
+export interface vaccinceMetrics {
+    first: {
+      label: string;
+      percentage: string;
+    };
+    last: {
+      label: string;
+      percentage: string;
+    };
+  }
