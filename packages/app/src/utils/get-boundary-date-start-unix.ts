935f8c3aa6d19e9e35be12ab70c4d269ddcdbd71
VWSCoronaDashboard22 <jeroen.drenth@ordina.nl>
2022-04-25 13:27:22 +0200

    Subtract half a day from boundary start date

--- packages/app/src/utils/get-boundary-date-start-unix.ts
+++ packages/app/src/utils/get-boundary-date-start-unix.ts
@@ -1,4 +1,8 @@
-import { isDateValue, TimestampedValue } from '@corona-dashboard/common';
+import {
+  DAY_IN_SECONDS,
+  isDateValue,
+  TimestampedValue,
+} from '@corona-dashboard/common';
 
 /**
  * Return the start date for a time span calculated x items from the end of a
@@ -7,14 +11,17 @@ import { isDateValue, TimestampedValue } from '@corona-dashboard/common';
 export function getBoundaryDateStartUnix(
   values: TimestampedValue[],
   numberOfItems: number
-): number {
+) {
   if (numberOfItems >= values.length) {
     return Infinity;
   }
 
   const boundaryValue = values[values.length - numberOfItems];
 
-  return isDateValue(boundaryValue)
-    ? boundaryValue.date_unix
-    : boundaryValue.date_start_unix;
+  return (
+    (isDateValue(boundaryValue)
+      ? boundaryValue.date_unix
+      : boundaryValue.date_start_unix) -
+    DAY_IN_SECONDS / 2
+  );
 }
