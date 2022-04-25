17c6f99be3b91472871922a0568f3bd1ab51fe65
VWSCoronaDashboard22 <jeroen.drenth@ordina.nl>
2022-04-25 13:39:31 +0200

    Fixed unit test

--- packages/app/src/utils/__tests__/get-boundary-date-start-unix.spec.ts
+++ packages/app/src/utils/__tests__/get-boundary-date-start-unix.spec.ts
@@ -1,3 +1,4 @@
+import { DAY_IN_SECONDS } from '@corona-dashboard/common';
 import { suite } from 'uvu';
 import * as assert from 'uvu/assert';
 import { getBoundaryDateStartUnix } from '../get-boundary-date-start-unix';
@@ -19,7 +20,10 @@ GetBoundaryDateStartUnix(
 
     const result = getBoundaryDateStartUnix(values, numberOfItems);
 
-    assert.is(result, values[values.length - numberOfItems].date_unix);
+    assert.is(
+      result,
+      values[values.length - numberOfItems].date_unix - DAY_IN_SECONDS / 2
+    );
   }
 );
 
@@ -38,7 +42,10 @@ GetBoundaryDateStartUnix(
 
     const result = getBoundaryDateStartUnix(values, numberOfItems);
 
-    assert.is(result, values[values.length - numberOfItems].date_start_unix);
+    assert.is(
+      result,
+      values[values.length - numberOfItems].date_start_unix - DAY_IN_SECONDS / 2
+    );
   }
 );
 
