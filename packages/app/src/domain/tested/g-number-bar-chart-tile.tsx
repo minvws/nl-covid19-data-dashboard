56a4b381c3300f395eb8e607777b8c90bc8974f2
VWSCoronaDashboard25 <98813868+VWSCoronaDashboard25@users.noreply.github.com>
2022-04-26 09:41:53 +0200

    G-number chart incomplete data with extra options (#4050)
    * feature: highlight incomplete data at the beginning of the growth number chart
    
    Also, allow growth number to switch between 5-weeks and all instead of always showing 5-weeks
    
    * feat: updated mutations with new key
    
    * feat: updated chart with legend, options and keys
    
    * feat: adjusted date type and used constant for calculation
    
    Co-authored-by: VWSCoronaDashboard25 <VWSCoronaDashboard25@users.noreply.github.com>
    Co-authored-by: VWSCoronaDashboard19 <hasan.ozaynaci@ordina.nl>

--- packages/app/src/domain/tested/g-number-bar-chart-tile.tsx
+++ packages/app/src/domain/tested/g-number-bar-chart-tile.tsx
@@ -6,6 +6,7 @@ import {
 } from '@corona-dashboard/common';
 import { ChartTile } from '~/components/chart-tile';
 import { TimeSeriesChart } from '~/components/time-series-chart';
+import { TooltipSeriesListContainer } from '~/components/time-series-chart/components/tooltip/tooltip-series-list-container';
 import { BoldText } from '~/components/typography';
 import { useIntl } from '~/intl';
 
@@ -16,7 +17,7 @@ interface GNumberBarChartTileProps {
 
 export function GNumberBarChartTile({
   data: __data,
-  timeframeInitialValue = TimeframeOption.FIVE_WEEKS,
+  timeframeInitialValue = TimeframeOption.FIRST_OF_SEPTEMBER,
 }: GNumberBarChartTileProps) {
   const { formatPercentage, commonTexts } = useIntl();
 
@@ -30,69 +31,78 @@ export function GNumberBarChartTile({
       title={text.title}
       description={text.description}
       timeframeInitialValue={timeframeInitialValue}
+      timeframeOptions={[
+        TimeframeOption.FIRST_OF_SEPTEMBER,
+        TimeframeOption.ONE_WEEK,
+        TimeframeOption.THIRTY_DAYS,
+        TimeframeOption.THREE_MONTHS,
+        TimeframeOption.SIX_MONTHS,
+        TimeframeOption.LAST_YEAR,
+      ]}
       metadata={{
         date: last_value.date_of_insertion_unix,
         source: text.bronnen,
       }}
     >
-      <TimeSeriesChart
-        accessibility={{
-          key: 'g_number',
-          features: ['keyboard_bar_chart'],
-        }}
-        values={
-          /**
-           * @TODO the two `g_number`-schema's needs to be updated from
-           * date-span-value to date-value:
-           *
-           *     - "date_start_unix": { "type": "integer" },
-           *     - "date_end_unix": { "type": "integer" },
-           *     + "date_unix": { "type": "integer" },
-           *
-           * After this schema change we can also get rid of the following map:
-           */
-          values.map((x) => ({
-            date_of_insertion_unix: x.date_of_insertion_unix,
-            g_number: x.g_number,
-            date_unix: x.date_end_unix,
-          }))
-        }
-        timeframe={timeframeInitialValue}
-        dataOptions={{
-          isPercentage: true,
-        }}
-        disableLegend
-        seriesConfig={[
-          {
-            type: 'split-bar',
-            metricProperty: 'g_number',
-            label: 'G number',
-            fillOpacity: 1,
-            splitPoints: [
-              {
-                color: colors.data.primary,
-                value: 0,
-                label: '', // legend is hidden, we can leave this empty
-              },
-              {
-                color: colors.red,
-                value: Infinity,
-                label: '', // legend is hidden, we can leave this empty
-              },
-            ],
-          },
-        ]}
-        formatTooltip={({ value }) => (
-          <>
-            <BoldText>
-              {`${formatPercentage(Math.abs(value.g_number))}% `}
-            </BoldText>
-            {value.g_number > 0
-              ? text.positive_descriptor
-              : text.negative_descriptor}
-          </>
-        )}
-      />
+      {(timeframe) => (
+        <TimeSeriesChart
+          accessibility={{
+            key: 'g_number',
+            features: ['keyboard_bar_chart'],
+          }}
+          values={
+            /**
+             * @TODO the two `g_number`-schema's needs to be updated from
+             * date-span-value to date-value:
+             *
+             *     - "date_start_unix": { "type": "integer" },
+             *     - "date_end_unix": { "type": "integer" },
+             *     + "date_unix": { "type": "integer" },
+             *
+             * After this schema change we can also get rid of the following map:
+             */
+            values.map((x) => ({
+              date_of_insertion_unix: x.date_of_insertion_unix,
+              g_number: x.g_number,
+              date_unix: x.date_end_unix,
+            }))
+          }
+          timeframe={timeframe}
+          dataOptions={{
+            isPercentage: true,
+          }}
+          seriesConfig={[
+            {
+              type: 'split-bar',
+              metricProperty: 'g_number',
+              label: '',
+              fillOpacity: 1,
+              splitPoints: [
+                {
+                  color: colors.data.primary,
+                  value: 0,
+                  label: text.legend.negative_label,
+                },
+                {
+                  color: colors.red,
+                  value: Infinity,
+                  label: text.legend.positive_label,
+                },
+              ],
+            },
+          ]}
+          formatTooltip={(data) => (
+            <TooltipSeriesListContainer {...data}>
+              <BoldText>
+                {`${formatPercentage(Math.abs(data.value.g_number))}% `}
+              </BoldText>
+              {data.value.g_number > 0
+                ? text.positive_descriptor
+                : text.negative_descriptor}
+            </TooltipSeriesListContainer>
+          )}
+        />
+      )}
     </ChartTile>
   );
 }
