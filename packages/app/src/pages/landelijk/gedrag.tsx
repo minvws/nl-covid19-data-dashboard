ca7e8dc0f023b39ce84475605f01b57978ffdd15
VWSCoronaDashboard22 <93981265+VWSCoronaDashboard22@users.noreply.github.com>
2022-05-05 09:44:22 +0200

    Update packages/app/src/pages/landelijk/gedrag.tsx
    Co-authored-by: VWSCoronaDashboard25 <98813868+VWSCoronaDashboard25@users.noreply.github.com>

--- packages/app/src/pages/landelijk/gedrag.tsx
+++ packages/app/src/pages/landelijk/gedrag.tsx
@@ -235,7 +235,7 @@ export default function BehaviorPage(
             {...timelineProp}
             currentId={currentId}
             setCurrentId={setCurrentId}
-            useDatesAsRange={true}
+            useDatesAsRange
             text={text}
           />
 
