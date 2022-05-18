494c6a90ecf42d7a0e182018ce70968c06e1e8a2
VWSCoronaDashboard18 <93984341+VWSCoronaDashboard18@users.noreply.github.com>
2022-05-18 23:51:57 +0200

    Fix margins and paddings

--- packages/app/src/components/two-kpi-section.tsx
+++ packages/app/src/components/two-kpi-section.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Children } from 'react';
 import { asResponsiveArray } from '~/style/utils';
 import { Box } from './base';
 
@@ -8,11 +8,17 @@ interface TwoKpiSectionProps {
 }
 
 export function TwoKpiSection({ children, spacing }: TwoKpiSectionProps) {
+  const hasOnlyOneChild = Children.toArray(children).length === 1;
+
   return (
     <Box
       display="grid"
-      grid-template-columns={{ _: '1fr', lg: '1fr 1fr' }}
-      column-gap={asResponsiveArray({ _: 0, lg: spacing ?? 10 })}
+      gridTemplateColumns={asResponsiveArray({ _: '1fr', lg: '1fr 1fr' })}
+      gridColumnGap={asResponsiveArray({ _: 0, lg: spacing ?? '10rem' })}
+      gridRowGap={asResponsiveArray({ _: '3rem', lg: spacing ?? '8rem' })}
+      borderTop={hasOnlyOneChild ? 'solid 2px lightGray' : undefined}
+      pt={hasOnlyOneChild ? 4 : undefined}
+      pb={hasOnlyOneChild ? asResponsiveArray({ _: 3, sm: 4 }) : undefined}
     >
       {children}
     </Box>
