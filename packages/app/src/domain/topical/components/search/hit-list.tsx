4f4abc9e9045f8a13de18678966c8e92c8072cb2
VWSCoronaDashboard22 <93981265+VWSCoronaDashboard22@users.noreply.github.com>
2022-05-25 13:27:53 +0200

    Implemented redesign combobox and select list (#4264)
    * Implemented redesign combobox and select list
    
    * Removed unused imports
    
    * Removed sorting by score
    
    * Added padding to no results message
    
    * Always keep the same width for search form
    
    * Removed unused import
    
    * Fixed padding search results

--- packages/app/src/domain/topical/components/search/hit-list.tsx
+++ packages/app/src/domain/topical/components/search/hit-list.tsx
@@ -50,7 +50,9 @@ export function HitList({ scope }: HitListProps) {
           ))}
         </StyledHitList>
       ) : (
-        <Text color="gray">{noHitsMessage}</Text>
+        <NoResultMessage>
+          <Text color="gray">{noHitsMessage}</Text>
+        </NoResultMessage>
       )}
     </Box>
   );
@@ -60,13 +62,18 @@ interface HitLinkProps {
   href: string;
   children: ReactNode;
   hasFocus: boolean;
+  onClick: () => void;
   onHover: () => void;
   onFocus: () => void;
   id: string;
+  isActiveResult: boolean;
 }
 
 const HitLink = forwardRef<HTMLAnchorElement, HitLinkProps>(
-  ({ href, children, hasFocus, onHover, onFocus, id }, ref) => {
+  (
+    { href, children, hasFocus, onClick, onHover, onFocus, id, isActiveResult },
+    ref
+  ) => {
     return (
       <Link passHref href={href}>
         <StyledHitLink
@@ -77,6 +84,8 @@ const HitLink = forwardRef<HTMLAnchorElement, HitLinkProps>(
           role="option"
           id={id}
           aria-selected={hasFocus ? 'true' : 'false'}
+          aria-active={isActiveResult ? 'true' : 'false'}
+          onClick={onClick}
         >
           {children}
         </StyledHitLink>
@@ -85,16 +94,42 @@ const HitLink = forwardRef<HTMLAnchorElement, HitLinkProps>(
   }
 );
 
-const StyledHitLink = styled(Anchor)<{ hasFocus: boolean }>((x) =>
+const StyledHitLink = styled(Anchor)<{
+  hasFocus: boolean;
+}>((x) =>
   css({
-    p: 2,
+    padding: '0.5em 1.75em',
     display: 'block',
     textDecoration: 'none',
     color: 'black',
     width: '100%',
-    bg: x.hasFocus ? 'contextualContent' : 'transparant',
     transitionProperty: 'background',
     transitionDuration: x.hasFocus ? '0ms' : '120ms',
+    position: 'relative',
+    '&:before': {
+      content: 'attr(data-text)',
+      position: 'absolute',
+      left: 0,
+      top: 0,
+      height: '100%',
+      width: '5px',
+      backgroundColor: 'blue',
+      transform: 'scaleX(0)',
+      transformOrigin: 'left',
+      transition: '0.2s transform',
+    },
+    '&[aria-active="true"]': {
+      color: 'blue',
+      fontWeight: 'bold',
+      '&:before': {
+        transform: 'scaleX(1)',
+      },
+    },
+    '&:hover, &[aria-selected="true"]': {
+      bg: 'blue',
+      color: 'white',
+      fontWeight: 'normal',
+    },
   })
 );
 
@@ -104,7 +139,7 @@ const HitListHeader = styled.span(
     textTransform: 'uppercase',
     fontSize: 1,
     fontWeight: 'bold',
-    px: 2,
+    px: '2em',
   })
 );
 
@@ -114,5 +149,11 @@ const StyledHitList = styled.ol(
     p: 0,
     m: 0,
     width: ['100%', null],
+  })
+);
+
+const NoResultMessage = styled.div(
+  css({
+    px: '1.75em',
   })
 );
--- packages/app/src/domain/topical/components/search/search.tsx
+++ packages/app/src/domain/topical/components/search/search.tsx
@@ -12,9 +12,11 @@ import { SearchResults } from './search-results';
 export function Search({
   initialValue,
   title,
+  activeResult,
 }: {
   initialValue?: string;
   title: string;
+  activeResult?: string;
 }) {
   const [heightRef, { height }] = useResizeObserver<HTMLDivElement>();
   const containerRef = useRef<HTMLFormElement>(null);
@@ -23,6 +25,7 @@ export function Search({
     <SearchContextProvider
       containerRef={containerRef}
       initialValue={initialValue}
+      activeResult={activeResult}
     >
       {(context) => (
         <Box spacing={3}>
@@ -48,7 +51,7 @@ export function Search({
               width={{
                 _: '100%',
                 xs: '20rem',
-                sm: context.showResults ? '42rem' : '26rem',
+                sm: '42rem',
               }}
               px={{ sm: 4 }}
               bg={colors.white}
