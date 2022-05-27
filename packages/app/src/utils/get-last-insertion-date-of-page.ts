5b83bb4f9a8d068e13daa5722d8217145b213da7
VWSCoronaDashboard18 <93984341+VWSCoronaDashboard18@users.noreply.github.com>
2022-05-27 12:44:55 +0200

    updated get last date logic
    (cherry picked from commit f731f7a02064026eb4fa9352a65bbd164e80d9ca)

--- packages/app/src/utils/get-last-insertion-date-of-page.ts
+++ packages/app/src/utils/get-last-insertion-date-of-page.ts
@@ -11,10 +11,12 @@ export function getLastInsertionDateOfPage(
   return pageMetrics.reduce((lastDate, metricProperty) => {
     const metricOrUnixDate = get(data, metricProperty);
 
+    // TODO: extract all cases in seperate function. 1. date, 2. last_values, 3. values
+
     const metricDate =
       typeof metricOrUnixDate === 'number'
         ? metricOrUnixDate
-        : metricOrUnixDate?.last_value?.date_of_insertion_unix || 0;
+        : metricOrUnixDate?.last_value?.date_of_insertion_unix || metricOrUnixDate?.values.reduce((max: number, value: any) => value?.date_of_insertion_unix > max ? value.date_of_insertion_unix : max, 0) || 0;
 
     return metricDate > lastDate ? metricDate : lastDate;
   }, 0);
