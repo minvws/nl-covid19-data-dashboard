64ac5fd7bf735feb0d8337ff3140166f7576da29
VWSCoronaDashboard18 <93984341+VWSCoronaDashboard18@users.noreply.github.com>
2022-06-07 11:54:34 +0200

    Remove old hideInLegend configs

--- packages/app/src/domain/hospital/admissions-per-age-group/series-config.ts
+++ packages/app/src/domain/hospital/admissions-per-age-group/series-config.ts
@@ -4,47 +4,38 @@ export const BASE_SERIES_CONFIG = [
   {
     metricProperty: 'admissions_age_0_19_per_million',
     color: colors.data.multiseries.cyan,
-    hideInLegend: true,
   },
   {
     metricProperty: 'admissions_age_20_29_per_million',
     color: colors.data.multiseries.turquoise,
-    hideInLegend: true,
   },
   {
     metricProperty: 'admissions_age_30_39_per_million',
     color: colors.data.multiseries.turquoise_dark,
-    hideInLegend: true,
   },
   {
     metricProperty: 'admissions_age_40_49_per_million',
     color: colors.data.multiseries.yellow,
-    hideInLegend: true,
   },
   {
     metricProperty: 'admissions_age_50_59_per_million',
     color: colors.data.multiseries.yellow_dark,
-    hideInLegend: true,
   },
   {
     metricProperty: 'admissions_age_60_69_per_million',
     color: colors.data.multiseries.orange,
-    hideInLegend: true,
   },
   {
     metricProperty: 'admissions_age_70_79_per_million',
     color: colors.data.multiseries.orange_dark,
-    hideInLegend: true,
   },
   {
     metricProperty: 'admissions_age_80_89_per_million',
     color: colors.data.multiseries.magenta,
-    hideInLegend: true,
   },
   {
     metricProperty: 'admissions_age_90_plus_per_million',
     color: colors.data.multiseries.magenta_dark,
-    hideInLegend: true,
   },
   {
     metricProperty: 'admissions_overall_per_million',
