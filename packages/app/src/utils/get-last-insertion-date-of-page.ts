ebf15687f88f2a9d41a51cd8fa959e7c074ed22c
VWSCoronaDashboard18 <93984341+VWSCoronaDashboard18@users.noreply.github.com>
2022-06-30 12:31:22 +0200

    Added the extraction logic to each metric property

--- packages/app/src/utils/get-last-insertion-date-of-page.ts
+++ packages/app/src/utils/get-last-insertion-date-of-page.ts
@@ -1,10 +1,9 @@
-import { get, last } from 'lodash';
+import { get } from 'lodash';
 /**
  * This method gets the most recent insertion date from the metrics used in a page
  *
  */
- const getLastInsertionDatePerMetric = (data, metricProperty: String) => {
-  let metricDate: number;
+ const getLastInsertionDatePerMetric = (data: any, metricProperty: String): number => {
    switch (metricProperty) {
     case "booster_shot_administered":
     case "behavior":  
@@ -47,32 +46,33 @@ import { get, last } from 'lodash';
     case "vaccine_delivery_per_supplier":
     case "vaccine_stock":
     case "vaccine_vaccinated_or_support":
+    case 'one_variant':
+    case 'one_sewer_installation':
+    case 'one_variant':
       // Has Last Value
-      metricDate = data.last_value.date_of_insertion_unix
-      break;
-
+      return data.last_value.date_of_insertion_unix;
     case "behavior_annotations":
     case "booster_coverage":
     case "vaccine_coverage_per_age_group":
     case "vaccine_coverage_per_age_group_archived":
     case "deceased_rivm_per_age_group":
       // Has Values
-      metricDate = last(data.values as array) .date_of_insertion_unix
-      break;
-
+      return data.values[data.values.length - 1].date_of_insertion_unix
     case 'behavior_per_age_group':
       // Has direct date_of_insertion_unix
-      metricDate = data.date_of_insertion_unix
-      break;
-      
+      return data.date_of_insertion_unix
     case 'variants':
       // Has unique format
-      break;
-
+      return data.values.reduce((lastDate :number, variant: any) => {
+        const metricDate = getLastInsertionDatePerMetric(variant, 'one_variant');
+        return Math.max(metricDate, lastDate);
+      }, 0);
     case 'sewer_per_installation':
       // Has unique format
-      break;
-      
+      return data.values.reduce((lastDate :number, sewerInstallation: any) => {
+        const metricDate = getLastInsertionDatePerMetric(sewerInstallation, 'one_sewer_installation');
+        return Math.max(metricDate, lastDate);
+      }, 0);
     case "named_difference":
     case "difference":
       // Has no date_of_insertion_unix
@@ -80,26 +80,17 @@ import { get, last } from 'lodash';
     default:
       console.log(`error in metric ${metricProperty}`)
    }
+   return 0;
+}
 
 export function getLastInsertionDateOfPage(
-  data: unknown,
+  data: any,
   pageMetrics: string[]
 ) {
   return pageMetrics.reduce((lastDate, metricProperty) => {
-    const metricOrUnixDate = get(data, metricProperty);
-
-    let metricDate: number;
-
-    if (typeof metricOrUnixDate === 'number') {
-      metricDate = metricOrUnixDate;
-    } else if (typeof metricOrUnixDate?.last_value?.date_of_insertion_unix !== 'undefined') {
-      metricDate = metricOrUnixDate?.last_value?.date_of_insertion_unix;
-    } else if (typeof metricOrUnixDate?.values !== 'undefined') {
-      metricDate = metricOrUnixDate?.values.reduce((max: number, value: any) => value.date_of_insertion_unix > max ? value.date_of_insertion_unix : max, 0);
-    } else {
-      metricDate = 0;
-    }
+    const metricInstance = get(data, metricProperty);
+    const metricDate = getLastInsertionDatePerMetric(metricInstance, metricProperty);
     
-    return metricDate > lastDate ? metricDate : lastDate;
+    return Math.max(metricDate, lastDate);
   }, 0);
 }
