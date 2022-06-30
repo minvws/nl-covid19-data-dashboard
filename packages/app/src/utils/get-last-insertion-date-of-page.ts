a3f218eefdb31226fff75bc1c43cea35508d2a9e
VWSCoronaDashboard18 <93984341+VWSCoronaDashboard18@users.noreply.github.com>
2022-06-30 14:25:22 +0200

    replaced switch with PR feedback

--- packages/app/src/utils/get-last-insertion-date-of-page.ts
+++ packages/app/src/utils/get-last-insertion-date-of-page.ts
@@ -3,94 +3,72 @@ import { get } from 'lodash';
  * This method gets the most recent insertion date from the metrics used in a page
  *
  */
- const getLastInsertionDatePerMetric = (data: any, metricProperty: string): number => {
-   switch (metricProperty) {
-    case "booster_shot_administered":
-    case "behavior":  
-    case "corona_melder_app_download":
-    case "corona_melder_app_warning":
-    case "deceased_cbs":
-    case "deceased_rivm":
-    case "disability_care":
-    case "doctor":
-    case "elderly_at_home":
-    case "g_number":
-    case "hospital_lcps":
-    case "hospital_nice":
-    case "hospital_nice_sum":
-    case "hospital_nice_per_age_group":
-    case "infectious_people":
-    case "intensive_care_lcps":
-    case "intensive_care_nice":
-    case "intensive_care_nice_per_age_group":
-    case "nursing_home":
-    case "repeating_shot_administered":
-    case "reproduction":
-    case "sewer":
-    case "situations":
-    case "tested_ggd":
-    case "tested_ggd_archived":
-    case "tested_overall":
-    case "tested_overall_sum":
-    case "tested_per_age_group":
-    case "vaccine_administered":
-    case "vaccine_administered_doctors":
-    case "vaccine_administered_ggd":
-    case "vaccine_administered_ggd_ghor":
-    case "vaccine_administered_hospitals_and_care_institutions":
-    case "vaccine_administered_planned":
-    case "vaccine_administered_total":
-    case "vaccine_coverage":
-    case "vaccine_coverage_per_age_group_estimated":
-    case "vaccine_delivery_estimate":
-    case "vaccine_delivery_per_supplier":
-    case "vaccine_stock":
-    case "vaccine_vaccinated_or_support":
-    case 'one_variant':
-    case 'one_sewer_installation':
-      // Has Last Value
-      return data.last_value.date_of_insertion_unix;
-    case "behavior_annotations":
-    case "booster_coverage":
-    case "vaccine_coverage_per_age_group":
-    case "vaccine_coverage_per_age_group_archived":
-    case "deceased_rivm_per_age_group":
-      // Has Values
-      return data.values[data.values.length - 1].date_of_insertion_unix
-    case 'behavior_per_age_group':
-      // Has direct date_of_insertion_unix
-      return data.date_of_insertion_unix
-    case 'variants':
-      // Has unique format
-      return data.values.reduce((lastDate :number, variant: any) => {
-        const metricDate = getLastInsertionDatePerMetric(variant, 'one_variant');
-        return Math.max(metricDate, lastDate);
-      }, 0);
-    case 'sewer_per_installation':
-      // Has unique format
-      return data.values.reduce((lastDate :number, sewerInstallation: any) => {
-        const metricDate = getLastInsertionDatePerMetric(sewerInstallation, 'one_sewer_installation');
-        return Math.max(metricDate, lastDate);
-      }, 0);
-    case "named_difference":
-    case "difference":
-    case "code":
-    case "static_values":
-      // Has no date_of_insertion_unix or is usable for this purpose.
-      break;
-    default:
-      console.error(`Found unmatched metric: ${metricProperty}`)
-   }
-   return 0;
+
+//functions for checking type of metric
+function hasLastValue(metric: any): boolean {
+  return typeof metric?.last_value?.date_of_insertion_unix !== 'undefined';
+}
+
+function hasValues(metric: any): boolean {
+  return Array.isArray(metric?.values) ?
+    typeof metric?.values[metric?.values.length - 1]?.date_of_insertion_unix !== 'undefined'
+    : false;
+}
+
+function hasInsertionDate(metric: any): boolean {
+  return typeof metric?.date_of_insertion_unix !== 'undefined'
+}
+
+function hasNestedLastValue(metric: any): boolean {
+  return Array.isArray(metric?.values) ?
+    typeof metric?.values[0]?.last_value?.date_of_insertion_unix !== 'undefined'
+    : false;
+}
+
+// functions for getting values
+function getLastValue(metric: any): number {
+  return metric?.last_value?.date_of_insertion_unix;
+}
+
+function getValues(metric: any): number {
+  return metric?.values[metric.values.length - 1]?.date_of_insertion_unix
+}
+
+function getInsertionDate(metric: any): number {
+  return metric?.date_of_insertion_unix;
+}
+
+function getNestedLastValue(metric: any): number {
+  return metric?.values.reduce((lastDate :number, innerValue: any) => {
+    const metricDate = getMetricDate(innerValue);
+    return Math.max(metricDate, lastDate);
+  }, 0);
+}
+
+function getMetricDate(metricOrUnixDate: any): number {
+  if (hasLastValue(metricOrUnixDate)) {
+    return getLastValue(metricOrUnixDate);
+  }
+  if (hasValues(metricOrUnixDate)) {
+    return getValues(metricOrUnixDate);
+  }
+  if (hasInsertionDate(metricOrUnixDate)) {
+    return getInsertionDate(metricOrUnixDate);
+  }
+  if (hasNestedLastValue(metricOrUnixDate)) {
+    return getNestedLastValue(metricOrUnixDate);
+  }
+  console.error(`Found unmatched metric: ${metricOrUnixDate}`)
+  return 0;
 }
 
 export function getLastInsertionDateOfPage(
-  data: any,
+  data: unknown,
   pageMetrics: string[]
 ) {
   return pageMetrics.reduce((lastDate, metricProperty) => {
-    const metricInstance = get(data, metricProperty);
-    const metricDate = getLastInsertionDatePerMetric(metricInstance, metricProperty);
+    const metricOrUnixDate: any = get(data, metricProperty);
+    const metricDate = getMetricDate(metricOrUnixDate);
     
     return Math.max(metricDate, lastDate);
   }, 0);
