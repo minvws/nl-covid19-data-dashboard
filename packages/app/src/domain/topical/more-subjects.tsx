d93fe35b08412e75065f7c150e1e243e7ae2867e
VWSCoronaDashboard21 <VWSCoronaDashboard21@users.noreply.github.com>
2022-08-11 11:04:23 +0200

    add mobile styling

--- packages/app/src/domain/topical/more-subjects.tsx
+++ packages/app/src/domain/topical/more-subjects.tsx
@@ -3,6 +3,8 @@ import { Box } from '~/components/base';
 import { ReactNode } from 'react';
 import styled from 'styled-components';
 import { asResponsiveArray } from '~/style/utils';
+import { useBreakpointsAsync } from '~/utils/use-breakpoints';
+import { LinkWithIcon } from '~/components/link-with-icon';
 
 interface SubjectsTitleProps {
   title: string;
@@ -31,25 +33,58 @@ export function SubjectsTitle({ title }: SubjectsTitleProps) {
 }
 
 export function SubjectList({ subjects, icon }: SubjectListProps) {
+  const breakpoints = useBreakpointsAsync();
+
+  // Prevents flickering; don't show anything until breakpoints are loaded
+  if (!breakpoints) {
+    return null;
+  }
+
   return (
-    <Box
-      display="flex-start"
-      gridTemplateColumns={{ sm: 'repeat(1, 1fr)', md: 'repeat(3, 1fr)' }}
-      css={css({
-        // Same value to be aligned with the footer grid
-        columnGap: asResponsiveArray({ md: '16px' }),
-      })}
-      spacing={{ _: 3, md: 0 }}
-    >
-      {subjects.map((item) => (
-        <Button type="button" key={item.text} as="a" href={item.url}>
-          {item.text}
-          <IconWrapper>
-            <IconSmall icon={icon} width={11} height={10} />
-          </IconWrapper>
-        </Button>
-      ))}
-    </Box>
+    <>
+      {breakpoints.sm ? (
+        <>
+          <SubjectsTitle title="Meer onderwerpen:" />
+          <Box
+            display="flex-start"
+            flex-wrap="wrap"
+            gridTemplateColumns={{ sm: 'repeat(1, 1fr)', md: 'repeat(3, 1fr)' }}
+            css={css({
+              // Same value to be aligned with the footer grid
+              columnGap: asResponsiveArray({ md: '16px' }),
+            })}
+            spacing={{ _: 3, md: 0 }}
+          >
+            {subjects.map((item) => (
+              <Button type="button" key={item.text} as="a" href={item.url}>
+                {item.text}
+                <IconWrapper>
+                  <IconSmall icon={icon} width={11} height={10} />
+                </IconWrapper>
+              </Button>
+            ))}
+          </Box>
+        </>
+      ) : (
+        <>
+          <Box flex-direction="row" display="grid" spacing={{ _: 1, sm: 0 }}>
+            <SubjectsTitle title="Meer onderwerpen:" />
+            <Box display="grid" spacing={{ _: 1, sm: 0 }}>
+              {subjects.map((item) => (
+                <LinkWithIcon
+                  key={item.text}
+                  href={item.url}
+                  icon={icon}
+                  iconPlacement="right"
+                >
+                  {item.text}
+                </LinkWithIcon>
+              ))}
+            </Box>
+          </Box>
+        </>
+      )}
+    </>
   );
 }
 
@@ -62,8 +97,7 @@ const Button = styled.button<{ isActive?: boolean }>(({ isActive }) =>
     px: !isActive ? 3 : 0,
     py: !isActive ? 12 : 0,
     cursor: 'pointer',
-    margin: '1em',
-    padding: '1em',
+    margin: '16px',
 
     '&:hover': {
       bg: 'blue',
