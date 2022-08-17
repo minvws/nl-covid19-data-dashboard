52989e083cd13ac2a950fcbff4e336d4d306959e
VWSCoronaDashboard18 <93984341+VWSCoronaDashboard18@users.noreply.github.com>
2022-08-17 16:13:21 +0200

    New measurement tile because of rebase issues

---
+++ packages/app/src/domain/topical/components/measurement-tile/measurement-tile.tsx
@@ -0,0 +1,51 @@
+import { Box } from '~/components/base';
+import css from '@styled-system/css';
+import styled from 'styled-components';
+import { asResponsiveArray } from '~/style/utils';
+import { colors } from '@corona-dashboard/common';
+import DynamicIcon from '~/components/get-icon-by-name';
+import { Markdown } from '~/components/markdown';
+
+export function MeasurementTile() {
+  return (
+    <Box
+      spacing={3}
+      borderColor={colors.gray}
+      borderWidth="1px"
+      borderStyle="solid"
+      position="relative"
+      display="flex"
+      flexDirection={{ _: 'column', xs: 'row' }}
+      justifyContent={'space-between'}
+    >
+      <Box
+        display="flex"
+        justifyContent={'flex-start'}
+        textAlign={'left'}
+        p={'1.5rem'}
+      >
+        <KpiIcon>
+          <DynamicIcon name="Doorstroomevenementen" />
+        </KpiIcon>
+
+        <Box
+          display="flex"
+          justifyContent={'flex-start'}
+          textAlign={'left'}
+          pr={{ _: 0, xs: 4 }}
+        >
+          <Markdown content={'Het aantal positief geteste mensen.'} />
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+const KpiIcon = styled.div(
+  css({
+    color: colors.blue,
+    display: 'flex',
+    minWidth: asResponsiveArray(40),
+    marginRight: 3,
+  })
+);
