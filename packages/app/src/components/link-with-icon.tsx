27bb0a1f77897824c68ecfb4408353a1d90e54e4
VWSCoronaDashboard18 <93984341+VWSCoronaDashboard18@users.noreply.github.com>
2022-08-18 08:58:31 +0200

    Nicer link-with-icon

--- packages/app/src/components/link-with-icon.tsx
+++ packages/app/src/components/link-with-icon.tsx
@@ -1,4 +1,5 @@
 import { colors } from '@corona-dashboard/common';
+import { space } from '~/style/theme';
 import css from '@styled-system/css';
 import { ReactNode } from 'react';
 import styled from 'styled-components';
@@ -6,6 +7,7 @@ import { UrlObject } from 'url';
 import { Link } from '~/utils/link';
 import { Box } from './base';
 import { Anchor } from './typography';
+import { isDefined } from 'ts-is-present';
 
 interface LinkWithIconProps {
   href: UrlObject | string;
@@ -25,6 +27,10 @@ interface IconProps {
   mr?: number | string;
 }
 
+interface buttonBoxProps {
+  showAsButton: boolean;
+}
+
 export function LinkWithIcon({
   href,
   icon,
@@ -36,25 +42,8 @@ export function LinkWithIcon({
   const words = children.split(' ');
   const firstWords = `${words.slice(0, -1).join(' ')} `;
 
-  const Container = ({
-    hasButtonStyling,
-    children,
-  }: {
-    hasButtonStyling: boolean;
-    children: ReactNode;
-  }) =>
-    hasButtonStyling ? (
-      <ButtonBox as="span" display="inline-block" position="relative">
-        {children}
-      </ButtonBox>
-    ) : (
-      <Box as="span" display="inline-block" position="relative">
-        {children}
-      </Box>
-    );
-
   return (
-    <Container hasButtonStyling={hasButtonStyling}>
+    <ButtonBox showAsButton={hasButtonStyling}>
       <Link href={href} passHref locale={false}>
         <Anchor
           underline="hover"
@@ -79,7 +68,7 @@ export function LinkWithIcon({
           )}
         </Anchor>
       </Link>
-    </Container>
+    </ButtonBox>
   );
 }
 
@@ -157,24 +146,30 @@ const IconWrapper = styled.span(
   })
 );
 
-const ButtonBox = styled(Box)`
-  a {
-    background-color: ${colors.lightBlue};
-    color: ${colors.blue};
-    padding: 12px ${({ theme }) => theme.space[3]};
-
-    &:hover {
-      background-color: ${colors.blue};
-      color: ${colors.offWhite};
-    }
-
-    &:focus {
-      outline: #000000 dotted 2px;
-    }
-
-    &:hover,
-    &:focus {
-      text-decoration: none;
-    }
-  }
-`;
+const ButtonBox = styled.span((x: buttonBoxProps) =>
+  css({
+    display: 'inline-block',
+    position: 'relative',
+    a:
+      isDefined(x.showAsButton) && x.showAsButton
+        ? {
+            backgroundColor: colors.lightBlue,
+            color: colors.blue,
+            px: 12,
+            py: space[3],
+            '&:hover': {
+              backgroundColor: colors.blue,
+              color: colors.offWhite,
+            },
+            '&:focus': {
+              outline: '#000000 dotted 2px',
+            },
+            '&:hover, &:focus': {
+              textDecoration: 'none',
+            },
+          }
+        : {
+            color: 'inherit',
+          },
+  })
+);
