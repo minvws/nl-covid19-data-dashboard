d5b8d60de475d15eaa11288cd1e27b689a364b52
J <93984341+VWSCoronaDashboard18@users.noreply.github.com>
2022-08-18 12:20:39 +0200

    COR-993 measurement tile (#4364)
    * New measurement tile because of rebase issues
    
    * Fixed data input instead of static mokup

---
+++ packages/app/src/domain/topical/components/measurement-tile/measurement-tile.tsx
@@ -0,0 +1,57 @@
+import { Box } from '~/components/base';
+import css from '@styled-system/css';
+import styled from 'styled-components';
+import { asResponsiveArray } from '~/style/utils';
+import { colors } from '@corona-dashboard/common';
+import DynamicIcon from '~/components/get-icon-by-name';
+import { Markdown } from '~/components/markdown';
+import { TopicalIcon } from '@corona-dashboard/common/src/types';
+
+interface MeasurementTileProps {
+  icon: TopicalIcon;
+  title: string;
+}
+
+export function MeasurementTile({ icon, title }: MeasurementTileProps) {
+  return (
+    <Box
+      spacing={3}
+      borderColor={colors.gray}
+      borderWidth="1px"
+      borderStyle="solid"
+      position="relative"
+      display="flex"
+      flexDirection={{ _: 'column', xs: 'row' }}
+      justifyContent={'space-between'}
+    >
+      <Box
+        display="flex"
+        justifyContent={'flex-start'}
+        textAlign={'left'}
+        p={'1.5rem'}
+      >
+        <KpiIcon>
+          <DynamicIcon name={icon} />
+        </KpiIcon>
+
+        <Box
+          display="flex"
+          justifyContent={'flex-start'}
+          textAlign={'left'}
+          pr={{ _: 0, xs: 4 }}
+        >
+          <Markdown content={title} />
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+const KpiIcon = styled.div(
+  css({
+    color: colors.blue,
+    display: 'flex',
+    minWidth: asResponsiveArray(40),
+    marginRight: 3,
+  })
+);
