354cd3e8199818cc195a38c1f74835945388d809
HO <93981322+VWSCoronaDashboard19@users.noreply.github.com>
2022-09-20 08:46:01 +0200

    Vaccinations campaign for autumn and fully vaccin… (#4415)
    * feat(vaccinations): Vaccinations campaign for autumn and fully vaccinated
    
    - Archive and duplicate Campaigns component
    - Add dummy data
    - Add feature flag
    - Added new locale keys for autumn and archived description
    
    * fix: merge conflicts
    
    Co-authored-by: VWSCoronaDashboard24 <VWSCoronaDashboard24@users.noreply.github.com>

--- packages/app/src/pages/landelijk/vaccinaties.tsx
+++ packages/app/src/pages/landelijk/vaccinaties.tsx
@@ -66,6 +66,7 @@ import { useDynamicLokalizeTexts } from '~/utils/cms/use-dynamic-lokalize-texts'
 import { getLastInsertionDateOfPage } from '~/utils/get-last-insertion-date-of-page';
 import { DUMMY_DATA_VACCINE_COVERAGE } from '~/domain/vaccine/vaccine-coverage-tile/vaccine-coverage-dummy-data';
 import { useFeature } from '~/lib/features';
+import { DUMMY_DATA_VACCINE_CAMPAIGNS_PLANNED } from '~/domain/vaccine/vaccine-campaigns-tile/vaccine-campaigns-dummy-data';
 
 const pageMetrics = [
   'vaccine_administered_doctors',
@@ -204,6 +205,9 @@ function VaccinationPage(props: StaticProps<typeof getStaticProps>) {
 
   const vaccinationsCoverageFeature = useFeature('vaccinationsCoverage');
   const coverageDummyData = DUMMY_DATA_VACCINE_COVERAGE.last_value;
+
+  const vaccinationsCampaignsFeature = useFeature('vaccinationCampaigns');
+  const campaignsDummyData = DUMMY_DATA_VACCINE_CAMPAIGNS_PLANNED;
 
   const vaccineCoverageEstimatedLastValue =
     data.vaccine_coverage_per_age_group_estimated.last_value;
@@ -359,24 +363,28 @@ function VaccinationPage(props: StaticProps<typeof getStaticProps>) {
             </>
           )}
 
-          <VaccineCampaignsTile
-            title={textNl.vaccine_campaigns.title}
-            description={replaceVariablesInText(
-              textNl.vaccine_campaigns.description,
-              {
-                vaccinePlanned: formatNumber(data.vaccine_planned.doses),
-              }
-            )}
-            descriptionFooter={textNl.vaccine_campaigns.description_footer}
-            headers={textNl.vaccine_campaigns.headers}
-            campaigns={data.vaccine_campaigns.vaccine_campaigns}
-            campaignDescriptions={textNl.vaccine_campaigns.campaigns}
-            metadata={{
-              datumsText: textNl.datums,
-              date: data.vaccine_campaigns.date_unix,
-              source: textNl.vaccine_campaigns.bronnen.rivm,
-            }}
-          />
+          {vaccinationsCampaignsFeature && (
+            <VaccineCampaignsTile
+              title={textNl.vaccine_campaigns.title}
+              description={replaceVariablesInText(
+                textNl.vaccine_campaigns.description,
+                {
+                  vaccinePlanned: formatNumber(
+                    campaignsDummyData.vaccine_planned.doses
+                  ),
+                }
+              )}
+              descriptionFooter={textNl.vaccine_campaigns.description_footer}
+              headers={textNl.vaccine_campaigns.headers}
+              campaigns={campaignsDummyData.vaccine_campaigns.vaccine_campaigns}
+              campaignDescriptions={textNl.vaccine_campaigns.campaigns}
+              metadata={{
+                datumsText: textNl.datums,
+                date: campaignsDummyData.vaccine_campaigns.date_unix,
+                source: textNl.vaccine_campaigns.bronnen.rivm,
+              }}
+            />
+          )}
 
           <VaccinationsPerSupplierOverLastWeekTile
             title={textNl.vaccinations_per_supplier_over_last_week.title}
@@ -468,6 +476,24 @@ function VaccinationPage(props: StaticProps<typeof getStaticProps>) {
                 age18PlusToggleText={
                   textNl.vaccination_grade_toggle_tile.age_18_plus
                 }
+              />
+              <VaccineCampaignsTile
+                title={textNl.vaccine_campaigns.title}
+                description={replaceVariablesInText(
+                  textNl.vaccine_campaigns.description_archived,
+                  {
+                    vaccinePlanned: formatNumber(data.vaccine_planned.doses),
+                  }
+                )}
+                descriptionFooter={textNl.vaccine_campaigns.description_footer}
+                headers={textNl.vaccine_campaigns.headers}
+                campaigns={data.vaccine_campaigns.vaccine_campaigns}
+                campaignDescriptions={textNl.vaccine_campaigns.campaigns}
+                metadata={{
+                  datumsText: textNl.datums,
+                  date: data.vaccine_campaigns.date_unix,
+                  source: textNl.vaccine_campaigns.bronnen.rivm,
+                }}
               />
               <VaccinationsKpiHeader
                 text={textNl.repeating_shot_information_block}
