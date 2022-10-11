7914e5e5ca1ab59a98fc39b916a0d8d1c6a55d68
HO <93981322+VWSCoronaDashboard19@users.noreply.github.com>
2022-10-11 16:06:03 +0200

    Add thermometer component to the topical page (#4449)
    * feat(thermometer): Add thermometer component to the topical page
    
    - Adjust sizes of the component
    - Generate new Sanity keys and pass them to the component
    - Create logic to handle labels and only show if levels are 1 to 4 to avoid exceptions
    
    * feat: moved constants to a more central location
    
    * feat: removed complexity and added labels

--- packages/app/src/components/severity-indicator-tile/severity-indicator-tile.tsx
+++ packages/app/src/components/severity-indicator-tile/severity-indicator-tile.tsx
@@ -3,48 +3,37 @@ import css from '@styled-system/css';
 import { space } from '~/style/theme';
 import { Box } from '~/components/base';
 import { Markdown } from '~/components/markdown';
-import { Metadata, MetadataProps } from '~/components/metadata';
 import { SeverityIndicatorLabel } from './components/severity-indicator-label';
 import { SeverityIndicator } from './components/severity-indicator';
 import { getSeverityColor } from './logic/get-severity-color';
-import { SeverityLevel, SeverityLevels } from './types';
+import { SeverityLevels } from './types';
+import { InlineText } from '../typography';
+import { SEVERITY_INDICATOR_TILE_COLUMN_MIN_WIDTH } from '~/components/severity-indicator-tile/constants';
 
 interface SeverityIndicatorTileProps {
   description: string;
   label: string;
-  level: SeverityLevel;
-  metadata: MetadataProps;
+  level: SeverityLevels;
+  footerText: string;
   title: string;
 }
 
-export const SeverityIndicatorTile = ({
-  description,
-  label,
-  level,
-  metadata,
-  title,
-}: SeverityIndicatorTileProps) => {
-  const SEVERITY_INDICATOR_TILE_COLUMN_MIN_WIDTH = 250;
-
+export const SeverityIndicatorTile = ({ description, label, level, title, footerText }: SeverityIndicatorTileProps) => {
   return (
     <Box
       alignItems="flex-start"
       border={`1px solid ${colors.gray3}`}
-      borderLeft={`${space[2]} solid ${getSeverityColor(
-        level as SeverityLevels
-      )}`}
-      css={css({ gap: `0 ${space[4]}` })}
+      borderLeft={`${space[2]} solid ${getSeverityColor(level)}`}
+      css={css({ gap: `0 ${space[5]}` })}
       display="flex"
       flexDirection="row"
       flexWrap="wrap"
       justifyContent="space-between"
       p={space[4]}
+      mt={4}
       as="figure"
     >
-      <Box
-        flexGrow={1}
-        width={`min(${SEVERITY_INDICATOR_TILE_COLUMN_MIN_WIDTH}px, 50%)`}
-      >
+      <Box flexGrow={1} width={`min(${SEVERITY_INDICATOR_TILE_COLUMN_MIN_WIDTH}px, 50%)`}>
         <Markdown content={title} />
 
         <SeverityIndicatorLabel label={label} level={level} />
@@ -52,14 +41,11 @@ export const SeverityIndicatorTile = ({
         <SeverityIndicator level={level} />
       </Box>
 
-      <Box
-        flexGrow={1}
-        width={`min(${SEVERITY_INDICATOR_TILE_COLUMN_MIN_WIDTH}px, 50%)`}
-        as="figcaption"
-      >
+      <Box flexGrow={1} width={`min(${SEVERITY_INDICATOR_TILE_COLUMN_MIN_WIDTH}px, 50%)`} as="figcaption">
         <Markdown content={description} />
-
-        <Metadata {...metadata} isTileFooter />
+        <Box my={3}>
+          <InlineText color="gray7">{footerText}</InlineText>
+        </Box>
       </Box>
     </Box>
   );
--- packages/app/src/pages/index.tsx
+++ packages/app/src/pages/index.tsx
@@ -1,30 +1,19 @@
 import { Box, Spacer } from '~/components/base';
 import { MaxWidth } from '~/components';
 import { Layout } from '~/domain/layout';
-import {
-  Search,
-  TopicalArticlesList,
-  TopicalHeader,
-  TopicalLinksList,
-  TopicalMeasureTile,
-  TopicalSectionHeader,
-  TopicalThemeHeader,
-  TopicalTile,
-} from '~/domain/topical';
+import { Search, TopicalArticlesList, TopicalHeader, TopicalLinksList, TopicalMeasureTile, TopicalSectionHeader, TopicalThemeHeader, TopicalTile } from '~/domain/topical';
 import { isPresent } from 'ts-is-present';
 import { Languages, SiteText } from '~/locale';
-import {
-  createGetStaticProps,
-  StaticProps,
-} from '~/static-props/create-get-static-props';
+import { createGetStaticProps, StaticProps } from '~/static-props/create-get-static-props';
 import { getTopicalPageData } from '~/queries/get-topical-page-data';
-import {
-  getLastGeneratedDate,
-  getLokalizeTexts,
-  selectTopicalData,
-} from '~/static-props/get-data';
+import { getLastGeneratedDate, getLokalizeTexts, selectTopicalData } from '~/static-props/get-data';
 import { useDynamicLokalizeTexts } from '~/utils/cms/use-dynamic-lokalize-texts';
 import { colors } from '@corona-dashboard/common';
+import { SeverityIndicatorTile } from '~/components/severity-indicator-tile/severity-indicator-tile';
+import { replaceVariablesInText } from '~/utils';
+import { SeverityLevels } from '~/components/severity-indicator-tile/types';
+import { useBreakpoints } from '~/utils/use-breakpoints';
+import { THERMOMETER_ICON_NAME, TOPICAL_SEVERITY_INDICATOR_TILE_MAX_WIDTH, SEVERITY_LEVELS_LIST } from '~/components/severity-indicator-tile/constants';
 
 const selectLokalizeTexts = (siteText: SiteText) => ({
   hospitalText: siteText.pages.hospital_page.nl,
@@ -38,8 +27,7 @@ const selectLokalizeTexts = (siteText: SiteText) => ({
 type LokalizeTexts = ReturnType<typeof selectLokalizeTexts>;
 
 export const getStaticProps = createGetStaticProps(
-  ({ locale }: { locale: keyof Languages }) =>
-    getLokalizeTexts(selectLokalizeTexts, locale),
+  ({ locale }: { locale: keyof Languages }) => getLokalizeTexts(selectLokalizeTexts, locale),
   getLastGeneratedDate,
   getTopicalPageData('nl', []),
   ({ locale }: { locale: keyof Languages }) => ({
@@ -50,10 +38,9 @@ export const getStaticProps = createGetStaticProps(
 const Home = (props: StaticProps<typeof getStaticProps>) => {
   const { pageText, content, lastGenerated, selectedTopicalData } = props;
 
-  const { textNl, textShared } = useDynamicLokalizeTexts<LokalizeTexts>(
-    pageText,
-    selectLokalizeTexts
-  );
+  const { textNl, textShared } = useDynamicLokalizeTexts<LokalizeTexts>(pageText, selectLokalizeTexts);
+
+  const breakpoints = useBreakpoints();
 
   const metadata = {
     ...textNl.nationaal_metadata,
@@ -67,20 +54,36 @@ const Home = (props: StaticProps<typeof getStaticProps>) => {
     md: `repeat(3, 1fr)`,
   };
 
+  const currentSeverityLevel = textNl.thermometer.indicator.level as SeverityLevels;
+
   return (
     <Layout {...metadata} lastGenerated={lastGenerated}>
       <Box bg={colors.white}>
         <MaxWidth id="content">
-          <Box
-            marginBottom={{ _: 4, md: 5 }}
-            pt={{ _: 3, md: 5 }}
-            px={{ _: 3, sm: 4 }}
-          >
-            <TopicalHeader
-              title={selectedTopicalData.title}
-              dynamicDescriptions={selectedTopicalData.dynamicDescription}
-            />
+          <Box marginBottom={{ _: 4, md: 5 }} pt={{ _: 3, md: 5 }} px={{ _: 3, sm: 4 }} maxWidth={TOPICAL_SEVERITY_INDICATOR_TILE_MAX_WIDTH}>
+            <TopicalHeader title={selectedTopicalData.title} dynamicDescriptions={selectedTopicalData.dynamicDescription} />
           </Box>
+          {SEVERITY_LEVELS_LIST.includes(currentSeverityLevel) && (
+            <Box my={5} px={breakpoints.sm ? 4 : 3} maxWidth={TOPICAL_SEVERITY_INDICATOR_TILE_MAX_WIDTH}>
+              <TopicalThemeHeader
+                title={textNl.thermometer.title}
+                dynamicSubtitle={replaceVariablesInText(textNl.thermometer.description, {
+                  level: currentSeverityLevel,
+                  label: textNl.thermometer.indicator.label,
+                })}
+                icon={THERMOMETER_ICON_NAME}
+              />
+              <SeverityIndicatorTile
+                level={currentSeverityLevel}
+                description={replaceVariablesInText(textNl.thermometer.indicator.description, {
+                  label: textNl.thermometer.indicator.label.toLowerCase(),
+                })}
+                title={textNl.thermometer.indicator.title}
+                label={textNl.thermometer.indicator.label}
+                footerText={textNl.thermometer.indicator.footerText}
+              />
+            </Box>
+          )}
           <Box spacing={{ _: 5, md: 6 }} px={{ _: 3, sm: 4 }}>
             {selectedTopicalData.themes
               .sort((a, b) => a.index - b.index)
@@ -88,19 +91,9 @@ const Home = (props: StaticProps<typeof getStaticProps>) => {
                 return (
                   <Box key={theme.index}>
                     <Box marginBottom={4}>
-                      <TopicalThemeHeader
-                        title={theme.title}
-                        dynamicSubtitle={theme.dynamicSubtitle}
-                        icon={theme.icon}
-                      />
+                      <TopicalThemeHeader title={theme.title} dynamicSubtitle={theme.dynamicSubtitle} icon={theme.icon} />
                     </Box>
-                    <Box
-                      display="grid"
-                      gridTemplateColumns={tileGridTemplate}
-                      gridColumnGap={{ _: 4, md: 5 }}
-                      gridRowGap={{ _: 4, md: 5 }}
-                      marginBottom={{ _: 4, sm: 5 }}
-                    >
+                    <Box display="grid" gridTemplateColumns={tileGridTemplate} gridColumnGap={{ _: 4, md: 5 }} gridRowGap={{ _: 4, md: 5 }} marginBottom={{ _: 4, sm: 5 }}>
                       {theme.themeTiles
                         .sort((a, b) => a.index - b.index)
                         .map((themeTile) => {
@@ -135,23 +128,11 @@ const Home = (props: StaticProps<typeof getStaticProps>) => {
                   icon={selectedTopicalData.measures.icon}
                 />
               </Box>
-              <Box
-                display="grid"
-                gridTemplateColumns={tileGridTemplate}
-                gridColumnGap={{ _: 4, md: 5 }}
-                gridRowGap={{ _: 4, md: 5 }}
-                marginBottom={5}
-              >
+              <Box display="grid" gridTemplateColumns={tileGridTemplate} gridColumnGap={{ _: 4, md: 5 }} gridRowGap={{ _: 4, md: 5 }} marginBottom={5}>
                 {selectedTopicalData.measures.measureTiles
                   .sort((a, b) => a.index - b.index)
                   .map((measureTile) => {
-                    return (
-                      <TopicalMeasureTile
-                        icon={measureTile.icon}
-                        title={measureTile.title}
-                        key={measureTile.index}
-                      />
-                    );
+                    return <TopicalMeasureTile icon={measureTile.icon} title={measureTile.title} key={measureTile.index} />;
                   })}
               </Box>
             </Box>
@@ -169,11 +150,7 @@ const Home = (props: StaticProps<typeof getStaticProps>) => {
         <Spacer mb={5} />
 
         <Box width="100%" pb={5}>
-          <MaxWidth
-            spacing={4}
-            pt={{ _: 3, md: 5 }}
-            px={{ _: 3, sm: 4, md: 3, lg: 4 }}
-          >
+          <MaxWidth spacing={4} pt={{ _: 3, md: 5 }} px={{ _: 3, sm: 4, md: 3, lg: 4 }}>
             <TopicalSectionHeader
               title={textShared.secties.meer_lezen.titel}
               description={textShared.secties.meer_lezen.omschrijving}
@@ -182,12 +159,7 @@ const Home = (props: StaticProps<typeof getStaticProps>) => {
               text={textShared}
             />
 
-            {isPresent(content.articles) && (
-              <TopicalArticlesList
-                articles={content.articles}
-                text={textShared}
-              />
-            )}
+            {isPresent(content.articles) && <TopicalArticlesList articles={content.articles} text={textShared} />}
           </MaxWidth>
         </Box>
       </Box>
