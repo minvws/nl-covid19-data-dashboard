fe530cf6b4bd5cb178b507bed5d70e20d502c0f5
VWSCoronaDashboard18 <93984341+VWSCoronaDashboard18@users.noreply.github.com>
2022-10-13 11:52:58 +0200

    Revert sanity button

--- packages/app/src/locale/use-lokalize-text.tsx
+++ packages/app/src/locale/use-lokalize-text.tsx
@@ -21,9 +21,7 @@ export type Dataset = typeof datasets[number];
 const query = `*[_type == 'lokalizeText']`;
 const enableHotReload = process.env.NEXT_PUBLIC_PHASE === 'develop';
 
-export const IS_STAGING_ENV =
-  typeof window !== 'undefined' &&
-  window.location.host === 'staging.coronadashboard.rijksoverheid.nl';
+export const IS_STAGING_ENV = typeof window !== 'undefined' && window.location.host === 'staging.coronadashboard.rijksoverheid.nl';
 
 /**
  * This hook will return an object which contains all lokalize translations.
@@ -41,19 +39,12 @@ export function useLokalizeText(initialLocale: LanguageKey) {
   const lokalizeTextsRef = useRef<SanityDocument<LokalizeText>[]>([]);
   const showSanityDebugToggle = enableHotReload || IS_STAGING_ENV;
 
-  const [dataset, setDataset] = useState<Dataset>(
-    (process.env.NEXT_PUBLIC_SANITY_DATASET as Dataset | undefined) ??
-      'development'
-  );
+  const [dataset, setDataset] = useState<Dataset>((process.env.NEXT_PUBLIC_SANITY_DATASET as Dataset | undefined) ?? 'development');
 
-  const toggleHotReloadButton = showSanityDebugToggle ? (
+  const toggleButton = showSanityDebugToggle ? (
     <ToggleButton isActive={isActive} onClick={() => setIsActive((x) => !x)}>
       <Toggle values={[...datasets]} onToggle={setDataset} value={dataset} />
-      <Toggle
-        values={Object.keys(languages) as LanguageKey[]}
-        onToggle={setLocale}
-        value={locale}
-      />
+      <Toggle values={Object.keys(languages) as LanguageKey[]} onToggle={setLocale} value={locale} />
     </ToggleButton>
   ) : null;
 
@@ -86,44 +77,38 @@ export function useLokalizeText(initialLocale: LanguageKey) {
 
         updateSiteText();
 
-        subscription = client
-          .listen(query)
-          .subscribe((update: MutationEvent<LokalizeText>) => {
-            /**
-             * `appear`-transition is emitted for newly created documents/drafts
-             */
-            if (update.transition === 'appear' && update.result) {
-              lokalizeTextsRef.current.push(update.result);
+        subscription = client.listen(query).subscribe((update: MutationEvent<LokalizeText>) => {
+          /**
+           * `appear`-transition is emitted for newly created documents/drafts
+           */
+          if (update.transition === 'appear' && update.result) {
+            lokalizeTextsRef.current.push(update.result);
+            updateSiteTextDebounced();
+          }
+
+          /**
+           * `update`-transition is emitted for updated documents/drafts
+           */
+          if (update.transition === 'update' && update.result) {
+            const index = lokalizeTextsRef.current.findIndex((x) => x._id === update.documentId);
+
+            if (index > -1) {
+              lokalizeTextsRef.current[index] = update.result;
               updateSiteTextDebounced();
             }
+          }
 
-            /**
-             * `update`-transition is emitted for updated documents/drafts
-             */
-            if (update.transition === 'update' && update.result) {
-              const index = lokalizeTextsRef.current.findIndex(
-                (x) => x._id === update.documentId
-              );
-
-              if (index > -1) {
-                lokalizeTextsRef.current[index] = update.result;
-                updateSiteTextDebounced();
-              }
+          /**
+           * `disappear`-transition is emitted for deleted documents/drafts
+           */
+          if (update.transition === 'disappear') {
+            const index = lokalizeTextsRef.current.findIndex((x) => x._id === update.documentId);
+            if (index > -1) {
+              lokalizeTextsRef.current.splice(index, 1);
+              updateSiteTextDebounced();
             }
-
-            /**
-             * `disappear`-transition is emitted for deleted documents/drafts
-             */
-            if (update.transition === 'disappear') {
-              const index = lokalizeTextsRef.current.findIndex(
-                (x) => x._id === update.documentId
-              );
-              if (index > -1) {
-                lokalizeTextsRef.current.splice(index, 1);
-                updateSiteTextDebounced();
-              }
-            }
-          });
+          }
+        });
       });
 
       return () => {
@@ -133,7 +118,7 @@ export function useLokalizeText(initialLocale: LanguageKey) {
     }
   }, [initialLocale, dataset, isActive, locale]);
 
-  return { text, toggleHotReloadButton, dataset, locale } as const;
+  return [text, toggleButton, dataset] as const;
 }
 
 /**
@@ -148,10 +133,7 @@ export function useLokalizeText(initialLocale: LanguageKey) {
 export function mapSiteTextValuesToKeys(siteText: SiteText) {
   const keys = Object.keys(flatten(siteText));
 
-  const obj = keys.reduce(
-    (result, key) => set(result, key, key),
-    {} as Record<string, string>
-  );
+  const obj = keys.reduce((result, key) => set(result, key, key), {} as Record<string, string>);
 
   return unflatten(obj, { object: true }) as SiteText;
 }
@@ -164,13 +146,7 @@ interface ToggleProps<T extends string> {
 
 function Toggle<T extends string>({ values, value, onToggle }: ToggleProps<T>) {
   return (
-    <Box
-      border="1px solid"
-      borderColor="gray3"
-      mx={2}
-      borderRadius={1}
-      overflow="hidden"
-    >
+    <Box border="1px solid" borderColor="gray3" mx={2} borderRadius={1} overflow="hidden">
       {values.map((x, i) => (
         <label
           key={x}
@@ -187,11 +163,7 @@ function Toggle<T extends string>({ values, value, onToggle }: ToggleProps<T>) {
           })}
         >
           <VisuallyHidden>
-            <input
-              type="radio"
-              checked={x === value}
-              onChange={() => onToggle(x)}
-            />
+            <input type="radio" checked={x === value} onChange={() => onToggle(x)} />
           </VisuallyHidden>
           {x}
         </label>
@@ -200,17 +172,7 @@ function Toggle<T extends string>({ values, value, onToggle }: ToggleProps<T>) {
   );
 }
 
-function ToggleButton({
-  isActive,
-  onClick,
-  color,
-  children,
-}: {
-  isActive: boolean;
-  onClick: () => void;
-  children: ReactNode;
-  color?: 'green1' | 'blue8';
-}) {
+function ToggleButton({ isActive, onClick, color, children }: { isActive: boolean; onClick: () => void; children: ReactNode; color?: 'green1' | 'blue8' }) {
   return (
     <Container isActive={isActive}>
       <DisplayOnHover>{isActive && children}</DisplayOnHover>
@@ -255,15 +217,14 @@ const Container = styled.div<{ isActive: boolean }>((x) =>
     zIndex: 9999,
   })
 );
-const StyledToggleButton = styled.div<{ isActive: boolean; color?: string }>(
-  (x) =>
-    css({
-      cursor: 'pointer',
-      borderRadius: 1,
-      color: x.isActive ? 'white' : 'black',
-      bg: x.isActive ? 'blue8' : 'transparent',
-      transition: 'all 100ms linear',
-      p: 1,
-      display: 'inline-block',
-    })
+const StyledToggleButton = styled.div<{ isActive: boolean; color?: string }>((x) =>
+  css({
+    cursor: 'pointer',
+    borderRadius: 1,
+    color: x.isActive ? 'white' : 'black',
+    bg: x.isActive ? 'blue8' : 'transparent',
+    transition: 'all 100ms linear',
+    p: 1,
+    display: 'inline-block',
+  })
 );
--- packages/app/src/pages/_app.tsx
+++ packages/app/src/pages/_app.tsx
@@ -4,7 +4,7 @@ import { LazyMotion } from 'framer-motion';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { IntlContext } from '~/intl';
 import { useIntlHelperContext } from '~/intl/hooks/use-intl';
@@ -16,37 +16,20 @@ import theme from '~/style/theme';
 import { BreakpointContextProvider } from '~/utils/use-breakpoints';
 import { IsTouchDeviceContextProvider } from '~/utils/use-is-touch-device';
 
-const pagesWithSmoothScroll = [
-  'landelijk',
-  'veiligheidsregio',
-  'gemeente',
-] as const;
+const pagesWithSmoothScroll = ['landelijk', 'veiligheidsregio', 'gemeente'] as const;
 
-const loadAnimationFeatures = () =>
-  import('~/style/animations').then((mod) => mod.default);
+const loadAnimationFeatures = () => import('~/style/animations').then((mod) => mod.default);
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
   const router = useRouter();
-  const [locale, setLocale] = useState((router.locale as LanguageKey) || 'nl');
+  const { locale = 'nl' } = router;
 
-  const {
-    text,
-    toggleHotReloadButton,
-    dataset,
-    locale: debugToggleLocale,
-  } = useLokalizeText(locale);
+  const [text, toggleHotReloadButton, dataset] = useLokalizeText(locale as LanguageKey);
 
-  useEffect(() => {
-    setLocale(debugToggleLocale);
-  }, [debugToggleLocale]);
+  assert(text, `[${loadAnimationFeatures.name}] Encountered unknown language: ${locale}`);
 
-  assert(
-    text,
-    `[${loadAnimationFeatures.name}] Encountered unknown language: ${locale}`
-  );
-
-  const intlContext = useIntlHelperContext(locale, text.common, dataset);
+  const intlContext = useIntlHelperContext(locale as LanguageKey, text.common, dataset);
 
   useEffect(() => {
     const handleRouteChange = (pathname: string) => {
@@ -58,9 +41,7 @@ export default function App(props: AppProps) {
 
       // For any page that should not smooth scroll after load, we should
       // disable smooth scroll during the page transition
-      if (
-        !pagesWithSmoothScroll.some((fragment) => pathname.includes(fragment))
-      ) {
+      if (!pagesWithSmoothScroll.some((fragment) => pathname.includes(fragment))) {
         document.documentElement.style.scrollBehavior = 'auto';
       }
 
@@ -78,11 +59,7 @@ export default function App(props: AppProps) {
   return (
     <>
       <Head>
-        <meta
-          key="viewport"
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=5"
-        />
+        <meta key="viewport" name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5" />
       </Head>
       <ThemeProvider theme={theme}>
         <IntlContext.Provider value={intlContext}>
@@ -102,9 +79,7 @@ export default function App(props: AppProps) {
 }
 
 function scrollToTop() {
-  const navigationBar = document.querySelector(
-    '#main-navigation'
-  ) as HTMLElement | null;
+  const navigationBar = document.querySelector('#main-navigation') as HTMLElement | null;
   const offset = navigationBar?.offsetTop ?? 0;
 
   window.scrollTo(0, window.scrollY >= offset ? offset : 0);
