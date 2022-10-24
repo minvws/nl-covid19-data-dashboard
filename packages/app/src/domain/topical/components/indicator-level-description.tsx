8036ea697f3ce96c5900522954f2a6a159789fa6
VWSCoronaDashboard18 <93984341+VWSCoronaDashboard18@users.noreply.github.com>
2022-10-24 09:05:34 +0200

    Responsive and sizes

--- packages/app/src/domain/topical/components/indicator-level-description.tsx
+++ packages/app/src/domain/topical/components/indicator-level-description.tsx
@@ -1,9 +1,11 @@
 import { SeverityIndicatorLevel } from '~/components/severity-indicator-tile/components/severity-indicator-label';
 import { SeverityLevel } from '~/components/severity-indicator-tile/types';
 import { replaceVariablesInText } from '~/utils';
-import { Text, Heading } from '~/components/typography';
+import { asResponsiveArray } from '~/style/utils';
+import { Text } from '~/components/typography';
 import { Box } from '~/components/base';
 import css from '@styled-system/css';
+import { space } from '~/style/theme';
 
 interface indicatorLevelDescriptionProps {
   level: SeverityLevel;
@@ -13,12 +15,17 @@ interface indicatorLevelDescriptionProps {
 
 export const IndicatorLevelDescription = ({ level, label, description }: indicatorLevelDescriptionProps) => {
   return (
-    <Box display={'grid'} gridTemplateColumns="60px auto" css={css({ gap: 1 })} mb={4}>
+    <Box
+      display="grid"
+      gridTemplateRows="auto"
+      gridTemplateColumns={`${space[4]} auto`}
+      alignItems="center"
+      mb={4}
+      css={css({ columnGap: 3, rowGap: asResponsiveArray({ _: 3, sm: 1 }) })}
+    >
       <SeverityIndicatorLevel level={level}>{level}</SeverityIndicatorLevel>
-      <Heading level={4} fontWeight="bold">
-        {label}
-      </Heading>
-      <Text css={css({ gridColumnStart: 2 })}>
+      <Text fontWeight="bold">{label}</Text>
+      <Text css={css({ gridColumnStart: asResponsiveArray({ _: 1, sm: 2 }), gridColumnEnd: 3 })}>
         {replaceVariablesInText(description.split('**').join(''), {
           label: label.toLowerCase(),
         })}
