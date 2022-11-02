17e686b61a47efdb702c5b0fc07c79f4e11937e0
VWSCoronaDashboard19 <hasan.ozaynaci@ordina.nl>
2022-11-02 11:51:49 +0100

    feat: added feature flags for new keys

--- packages/common/src/feature-flags/features.ts
+++ packages/common/src/feature-flags/features.ts
@@ -6,10 +6,21 @@ export const features: Feature[] = [
     isEnabled: true,
     metricName: 'vaccine_coverage_per_age_group',
   },
-
   {
     name: 'vaccinationCampaigns',
     isEnabled: true,
     metricName: 'vaccine_campaigns',
   },
+  {
+    name: 'nlVaccinationCoveragePerAgeGroupAutumn2022',
+    isEnabled: false,
+    metricName: 'vaccine_coverage_per_age_group_estimated_autumn_2022',
+    dataScopes: ['nl']
+  },
+  {
+    name: 'nlVaccinationCoveragePerAgeGroupFullyVaccinated',
+    isEnabled: false,
+    metricName: 'vaccine_coverage_per_age_group_estimated_fully_vaccinated',
+    dataScopes: ['nl']
+  }
 ];
