1e99f759583fbbe0c7edd641c0a61aab0eea72f5
VWSCoronaDashboard27 <VWSCoronaDashboard27@users.noreply.github.com>
2022-11-10 22:31:26 +0100

    fix: fixed styling and component structure

--- packages/app/src/components/weekly-summary/TopicalWeeklySummaryTile.tsx
+++ packages/app/src/components/weekly-summary/TopicalWeeklySummaryTile.tsx
@@ -1,92 +1,68 @@
 import css from '@styled-system/css';
-import { colors } from '@corona-dashboard/common';
-import { Box } from '~/components/base';
-import DynamicIcon from '~/components/get-icon-by-name';
-import { Markdown } from '~/components/markdown';
-import { space } from '~/style/theme';
-import { BoldText, InlineText } from '~/components/typography';
-import { SeverityIndicatorLabel } from '../severity-indicator-tile/components/severity-indicator-label';
-import { IconName } from '@corona-dashboard/icons/src/icon-name2filename';
-import { SeverityLevels } from '../severity-indicator-tile/types';
-import theme from '~/style/theme';
 import styled from 'styled-components';
+import { colors } from '@corona-dashboard/common';
+import { fontWeights, space } from '~/style/theme';
+import { Markdown } from '~/components/markdown';
+import { Box } from '~/components/base';
+import { BoldText, InlineText } from '~/components/typography';
+import { IconName } from '@corona-dashboard/icons/src/icon-name2filename';
+import DynamicIcon from '~/components/get-icon-by-name';
+import { SeverityLevel, SeverityLevels } from '../severity-indicator-tile/types';
+import { getSeverityColor } from '../severity-indicator-tile/logic/get-severity-color';
+import { asResponsiveArray } from '~/style/utils';
+import { WEEKLY_SUMMARY_HEAD_TEXT, WEEKLY_SUMMARY_CONTENT } from '../severity-indicator-tile/constants';
+
 interface TopicalWeeklySummaryProps {
   label: string | undefined;
   level: SeverityLevels;
-  text: string;
-  icon: IconName;
-  iconWithText: any;
-  name: IconName;
 }
 
 export const TopicalWeeklySummaryTile = ({ label, level }: TopicalWeeklySummaryProps) => {
-  const HeadText = 'Weeksamenvatting 10 t/m 16 oktober';
-  const WeeklySummaryDescription = [
-    {
-      icon: 'Eye',
-      text: 'De verspreiding van het virus zet zich verder door. We zitten in de najaarsgolf.',
-    },
-    {
-      icon: 'MedischeScreening',
-      text: 'Het aantal COVID-patiënten dat in het ziekenhuis wordt opgenomen blijft stijgen. Het aantal bezette bedden op de verpleegafdeling is op dit moment zo goed als gelijk aan het niveau van de piek van de zomergolf.',
-    },
-    {
-      icon: 'Coronathermometer',
-      text: 'Op basis van bovenstaande ontwikkelingen staat de Coronathermometer op stand',
-    },
-  ];
-  // console.log('RRR', WeeklySummaryDescription);
-  // const ICONS_LIST = ['Eye', 'MedischeScreening', 'Coronathermometer'];
-  // const SummaryText = ['De verspreiding van het virus zet zich verder door. We zitten in de najaarsgolf.', 'gj;sjg;skgl;skgl;sd', 'efljalfjldf'];
-  // const TopicalWeeklySummaryText = SummaryText.map((text, index) => <InlineText key={index}>{text}</InlineText>);
-  // console.log(TopicalWeeklySummaryText, 'DDD');
-  // const DynamicIcon = icons[name];
-  // const element = WeeklySummaryDescription.map((iconWithText, index) => {
-
   return (
     <Box px={3} maxWidth={930}>
-      <Box border={`1px solid ${colors.gray3}`} p={4} m={3}>
-        <Box>
-          <BoldText css={css({ fontSize: [3] })}>
-            <Markdown content={HeadText} />
-          </BoldText>
+      <Box border={`1px solid ${colors.gray3}`} p={4} m={asResponsiveArray({ _: undefined, sm: 3 })}>
+        <BoldText css={css({ fontSize: [3] })}>
+          <Markdown content={WEEKLY_SUMMARY_HEAD_TEXT.text} />
+        </BoldText>
 
-          <Box pt={2} display="flex" flexWrap="wrap" css={css({ gap: `0 ${space[2]}` })}>
-            {WeeklySummaryDescription.map((iconWithText, index) => {
-              return (
-                <>
-                  <Box display="flex" css={css({ gap: `0 ${space[3]}` })} alignItems="center">
-                    {/* <Box key={index} width={25} height={25} maxWidth="none"> */}
-                    <TopicalThemeHeaderIcon key={index}>
-                      <DynamicIcon width={25} name={iconWithText.icon} />
-                    </TopicalThemeHeaderIcon>
-                    {/* </Box> */}
-                    <Box>
-                      <InlineText>{iconWithText.text}</InlineText>
-                    </Box>
+        <Box as="ul">
+          {WEEKLY_SUMMARY_CONTENT.map((iconText, index) => {
+            return (
+              <>
+                <Box key={index} display="flex" pt={2} css={css({ gap: `0 ${space[3]}` })} alignItems="center" as="li">
+                  <Box minWidth={25} height={25}>
+                    <DynamicIcon width={25} name={iconText.icon as IconName} />
                   </Box>
-                </>
-              );
-            })}
-            <Box>
-              <SeverityIndicatorLabel label={label} level={level} />
-            </Box>
-          </Box>
+                  <Box>
+                    <InlineText>{iconText.text}</InlineText>
+                    {iconText.isThermometerMetric ? (
+                      <InlineText css={css({ whiteSpace: 'nowrap' })}>
+                        <SeverityIndicatorLevel level={level}>{level}</SeverityIndicatorLevel>
+                        <BoldText>{label}</BoldText>
+                      </InlineText>
+                    ) : (
+                      ''
+                    )}
+                  </Box>
+                </Box>
+              </>
+            );
+          })}
         </Box>
       </Box>
     </Box>
   );
 };
 
-const TopicalThemeHeaderIcon = styled.span`
-  display: block;
-  height: 25px;
-  // margin-right: 10px;
-  width: 25px;
-
-  @media ${theme.mediaQueries.sm} {
-    height: 30px;
-    margin-right: 15px;
-    width: 30px;
-  }
+const SeverityIndicatorLevel = styled.span`
+  margin: 0 ${space[1]};
+  background-color: ${({ level }: { level: SeverityLevel }) => getSeverityColor(level as SeverityLevels)};
+  border-radius: 50%;
+  color: ${colors.white};
+  display: inline-block;
+  text-align: center;
+  font-weight: ${fontWeights.heavy};
+  height: 24px;
+  width: 24px;
+  white-space: nowrap;
 `;
