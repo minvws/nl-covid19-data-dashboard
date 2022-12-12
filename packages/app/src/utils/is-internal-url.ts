ef17c77fa769a2ff32bbe91719cda068963dc95f
VWSCoronaDashboard18 <93984341+VWSCoronaDashboard18@users.noreply.github.com>
2022-12-12 21:41:19 +0100

    update description comment

--- packages/app/src/utils/is-internal-url.ts
+++ packages/app/src/utils/is-internal-url.ts
@@ -1,7 +1,9 @@
 /**
- * Return true if the given url starts with http://coronadashboard. or https://coronadashboard.
- * Return false if the given url starts with http or https
+ * Return true if the given url is an internal link to one of the dashboard pages.
+ * This counts for international links as well.
+ * Return false if the given link is an external link.
  */
+
 export function isInternalUrl(url: string) {
   const totalRegEx = new RegExp('(^(#))|(^(/)[^/]|^(/)$)|(^(((https?://(www.)?)?|www.)coronadashboard(.rijskoverheid|.government)?.nl))');
 
