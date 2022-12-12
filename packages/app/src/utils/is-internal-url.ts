50f497a7c8a4249de867dd57a39a21f6a1151c8a
VWSCoronaDashboard18 <93984341+VWSCoronaDashboard18@users.noreply.github.com>
2022-12-12 21:39:03 +0100

    Combine all regexes in to one regex

--- packages/app/src/utils/is-internal-url.ts
+++ packages/app/src/utils/is-internal-url.ts
@@ -3,11 +3,7 @@
  * Return false if the given url starts with http or https
  */
 export function isInternalUrl(url: string) {
-  const regExAnchor = new RegExp('^(#)');
-  const regExInternal = new RegExp('^(/)[^/]|^(/)$');
-  const regExInternalBaseDomain = new RegExp('^((https?://)?coronadashboard.rijskoverheid.nl)');
-  const regExInternalEnglishDomain = new RegExp('^((https?://)?coronadashboard.government.nl)');
-  const regExInternalShortDomain = new RegExp('^(((https?://(www.)?)?|www.)coronadashboard.nl)');
+  const totalRegEx = new RegExp('(^(#))|(^(/)[^/]|^(/)$)|(^(((https?://(www.)?)?|www.)coronadashboard(.rijskoverheid|.government)?.nl))');
 
-  return regExAnchor.test(url) || regExInternal.test(url) || regExInternalBaseDomain.test(url) || regExInternalEnglishDomain.test(url) || regExInternalShortDomain.test(url);
+  return totalRegEx.test(url);
 }
