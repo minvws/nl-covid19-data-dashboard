cdd36bda7c7d5d896a4b0d15592402fd87074a95
J <93984341+VWSCoronaDashboard18@users.noreply.github.com>
2022-12-20 14:26:42 +0100

    Update packages/app/src/components/choropleth-tile.tsx
    Nice, didn't catch this one. No magic numbers!!
    
    Co-authored-by: LR <107395524+VWSCoronaDashboard26@users.noreply.github.com>

--- packages/app/src/components/choropleth-tile.tsx
+++ packages/app/src/components/choropleth-tile.tsx
@@ -63,7 +63,7 @@ export function ChoroplethTile({
           {typeof description === 'string' ? <Text>{description}</Text> : description}
 
           {onChartRegionChange && chartRegion && (
-            <Box display="flex" justifyContent={'flex-start'} pt={4}>
+<Box display="flex" justifyContent="flex-start" paddingTop={space[4]}>
               <ChartRegionControls value={chartRegion} onChange={onChartRegionChange} />
             </Box>
           )}
