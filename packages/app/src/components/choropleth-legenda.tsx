d3780c40c12650f92e6571bc692dcf8425420f9c
VWSCoronaDashboard18 <93984341+VWSCoronaDashboard18@users.noreply.github.com>
2022-12-21 16:57:04 +0100

    Fix PR feedback

--- packages/app/src/components/choropleth-legenda.tsx
+++ packages/app/src/components/choropleth-legenda.tsx
@@ -1,13 +1,11 @@
 import { ChoroplethThresholdsValue, colors } from '@corona-dashboard/common';
-import styled from 'styled-components';
 import { ValueAnnotation } from '~/components/value-annotation';
 import { useIntl } from '~/intl';
-import { space, fontSizes } from '~/style/theme';
 import { replaceVariablesInText } from '~/utils';
 import { useBreakpoints } from '~/utils/use-breakpoints';
 import { Box } from './base';
 import { Legend, LegendItem } from './legend';
-import { InlineText, Text } from './typography';
+import { Text } from './typography';
 
 interface ChoroplethLegendaProps {
   title: string;
@@ -37,38 +35,21 @@ export function ChoroplethLegenda({ title, thresholds, valueAnnotation, pageType
       } as LegendItem)
   );
 
+  if (pageType === 'sewer') {
+    legendItems.push({
+      label: outdatedDataLabel,
+      shape: 'square',
+      color: colors.yellow1,
+    } as LegendItem);
+  }
+
   return (
     <Box width="100%" spacing={2} aria-hidden="true">
       {title && <Text variant="subtitle1">{title}</Text>}
 
       <Legend items={legendItems} columns={breakpoints.lg ? 1 : 2} />
 
-      {pageType === 'sewer' && (
-        <Box display="flex" alignItems="center" paddingBottom={space[2]}>
-          <LegendaColor first={true} width={`${100 / thresholds.length}%`} color={colors.yellow1} />
-          <Box paddingLeft={space[3]} fontSize={fontSizes[1]}>
-            <InlineText>{outdatedDataLabel}</InlineText>
-          </Box>
-        </Box>
-      )}
       <ValueAnnotation>{valueAnnotation}</ValueAnnotation>
     </Box>
   );
 }
-
-type LegendaColorProps = {
-  first?: boolean;
-  last?: boolean;
-  width?: string;
-  color: string;
-};
-
-const LegendaColor = styled.div<LegendaColorProps>`
-  width: 100%;
-  height: 10px;
-  flex-grow: 0;
-  flex-shrink: 0;
-  flex-basis: ${({ width }) => (width ? width : 'inherit')};
-  border-radius: ${({ first, last }) => (first ? '2px 0 0 2px' : last ? '0 2px 2px 0' : 0)};
-  background-color: ${({ color }) => (color ? color : 'inherit')};
-`;
--- packages/app/src/components/choropleth-tile.tsx
+++ packages/app/src/components/choropleth-tile.tsx
@@ -1,4 +1,5 @@
 import { ChoroplethThresholdsValue } from '@corona-dashboard/common';
+import { space } from '~/style/theme';
 import { ChoroplethLegenda } from '~/components/choropleth-legenda';
 import { DataProps } from '~/types/attributes';
 import { useBreakpoints } from '~/utils/use-breakpoints';
@@ -16,7 +17,6 @@ type ChoroplethTileProps = DataProps & {
   legend?: {
     title: string;
     thresholds: ChoroplethThresholdsValue[];
-    type?: 'default' | 'bar';
     outdatedDataLabel?: string;
   };
   metadata?: MetadataProps;
@@ -56,14 +56,14 @@ export function ChoroplethTile({
 
   return (
     <FullscreenChartTile metadata={metadata}>
-      <Box display="flex" flexDirection={{ _: 'column', lg: 'row' }} m={0} as="figure" {...dataProps} height="100%" spacing={3}>
+      <Box display="flex" flexDirection={{ _: 'column', lg: 'row' }} margin={space[0]} as="figure" {...dataProps} height="100%" spacing={3}>
         <Box flex={{ lg: 1 }} as="figcaption" spacing={3}>
           <Heading level={3}>{title}</Heading>
 
           {typeof description === 'string' ? <Text>{description}</Text> : description}
 
           {onChartRegionChange && chartRegion && (
-<Box display="flex" justifyContent="flex-start" paddingTop={space[4]}>
+            <Box display="flex" justifyContent="flex-start" paddingTop={space[4]}>
               <ChartRegionControls value={chartRegion} onChange={onChartRegionChange} />
             </Box>
           )}
@@ -76,7 +76,7 @@ export function ChoroplethTile({
         </Box>
 
         <Box flex={{ lg: 1 }} display="flex" flexDirection="column" height="100%" spacing={3}>
-          <Box height="100%" mt={4} pl={hasPadding && breakpoints.lg ? 4 : undefined}>
+          <Box height="100%" marginTop={space[4]} pl={hasPadding && breakpoints.lg ? 4 : undefined}>
             <ErrorBoundary>{children}</ErrorBoundary>
           </Box>
 
