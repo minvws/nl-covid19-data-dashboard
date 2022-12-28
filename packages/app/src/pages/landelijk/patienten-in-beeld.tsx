518c23201139fe39e93560c93be4e9a607a54af7
VWSCoronaDashboard19 <hasan.ozaynaci@ordina.nl>
2022-12-28 13:33:43 +0100

    feat: added restructured page parts

--- packages/app/src/pages/landelijk/patienten-in-beeld.tsx
+++ packages/app/src/pages/landelijk/patienten-in-beeld.tsx
@@ -1,8 +1,8 @@
-import { GmCollectionHospitalNice, TimeframeOption, TimeframeOptionsList, VrCollectionHospitalNice } from '@corona-dashboard/common';
+import { colors, GmCollectionHospitalNice, TimeframeOption, TimeframeOptionsList, VrCollectionHospitalNice } from '@corona-dashboard/common';
 import { Ziekenhuis } from '@corona-dashboard/icons';
 import { GetStaticPropsContext } from 'next';
 import { useState } from 'react';
-import { TileList, SEOHead, ChartTile, DynamicChoropleth, ChoroplethTile, PageInformationBlock } from '~/components';
+import { TileList, SEOHead, ChartTile, DynamicChoropleth, ChoroplethTile, PageInformationBlock, TimeSeriesChart } from '~/components';
 import { RegionControlOption } from '~/components/chart-region-controls';
 import { thresholds } from '~/components/choropleth/logic/thresholds';
 import { AdmissionsPerAgeGroup } from '~/domain/hospital';
@@ -14,7 +14,7 @@ import { getArticleParts, getLinkParts, getPagePartsQuery } from '~/queries/get-
 import { createGetStaticProps, StaticProps } from '~/static-props/create-get-static-props';
 import { createGetChoroplethData, createGetContent, getLastGeneratedDate, getLokalizeTexts, selectNlData } from '~/static-props/get-data';
 import { ArticleParts, LinkParts, PagePartQueryResult } from '~/types/cms';
-import { useReverseRouter } from '~/utils';
+import { countTrailingNullValues, getBoundaryDateStartUnix, useReverseRouter } from '~/utils';
 import { getLastInsertionDateOfPage } from '~/utils/get-last-insertion-date-of-page';
 import { last } from 'lodash';
 import { useDynamicLokalizeTexts } from '~/utils/cms/use-dynamic-lokalize-texts';
@@ -31,7 +31,7 @@ type LokalizeTexts = ReturnType<typeof selectLokalizeTexts>;
 export const getStaticProps = createGetStaticProps(
   ({ locale }: { locale: keyof Languages }) => getLokalizeTexts(selectLokalizeTexts, locale),
   getLastGeneratedDate,
-  selectNlData('hospital_nice_per_age_group', 'hospital_nice'),
+  selectNlData('hospital_nice_per_age_group', 'intensive_care_nice_per_age_group', 'hospital_nice', 'intensive_care_nice'),
   createGetChoroplethData({
     vr: ({ hospital_nice_choropleth }) => ({ hospital_nice_choropleth }),
     gm: ({ hospital_nice_choropleth }) => ({ hospital_nice_choropleth }),
@@ -43,7 +43,7 @@ export const getStaticProps = createGetStaticProps(
     }>((context) => {
       return `{
             "parts": ${getPagePartsQuery('patients_page')},
-            "elements": ${getElementsQuery('nl', ['hospital_nice', 'hospital_nice_per_age_group'], context.locale)}
+            "elements": ${getElementsQuery('nl', ['hospital_nice', 'intensive_care_nice_per_age_group', 'hospital_nice_per_age_group', 'intensive_care_nice'], context.locale)}
           }`;
     })(context);
 
@@ -61,12 +61,24 @@ const PatientsPage = (props: StaticProps<typeof getStaticProps>) => {
   const { pageText, selectedNlData: data, choropleth, content, lastGenerated } = props;
 
   const [hospitalAdmissionsPerAgeTimeframe, setHospitalAdmissionsPerAgeTimeframe] = useState<TimeframeOption>(TimeframeOption.ALL);
+  const [IcuAdmissionsPerAgeTimeframe, setIcuAdmissionsPerAgeTimeframe] = useState<TimeframeOption>(TimeframeOption.ALL);
+  const [hospitalAdmissionsOverTimeTimeframe, setHospitalAdmissionsOverTimeTimeframe] = useState<TimeframeOption>(TimeframeOption.ALL);
+  const [intensiveCareAdmissionsTimeframe, setIntensiveCareAdmissionsTimeframe] = useState<TimeframeOption>(TimeframeOption.ALL);
 
   const reverseRouter = useReverseRouter();
   const [selectedMap, setSelectedMap] = useState<RegionControlOption>('vr');
 
   const lastValueNice = data.hospital_nice.last_value;
   const lastValueNiceChoropleth = (selectedMap === 'gm' ? last(choropleth.gm.hospital_nice_choropleth) : last(choropleth.vr.hospital_nice_choropleth)) || lastValueNice;
+  const underReportedRange = getBoundaryDateStartUnix(
+    data.hospital_nice.values,
+    countTrailingNullValues(data.hospital_nice.values, 'admissions_on_date_of_admission_moving_average')
+  );
+
+  const intakeUnderReportedRange = getBoundaryDateStartUnix(
+    data.intensive_care_nice.values,
+    countTrailingNullValues(data.intensive_care_nice.values, 'admissions_on_date_of_admission_moving_average')
+  );
 
   const { commonTexts } = useIntl();
   const { metadataTexts, textNl } = useDynamicLokalizeTexts<LokalizeTexts>(pageText, selectLokalizeTexts);
@@ -154,8 +166,26 @@ const PatientsPage = (props: StaticProps<typeof getStaticProps>) => {
           </ChoroplethTile>
 
           <ChartTile
-            title={commonTexts.hospital_admissions_per_age_group.chart_title}
-            description={commonTexts.hospital_admissions_per_age_group.chart_description}
+            title={textNl.icu.age_group_chart.title}
+            description={textNl.icu.age_group_chart.description}
+            timeframeOptions={TimeframeOptionsList}
+            timeframeInitialValue={TimeframeOption.THIRTY_DAYS}
+            metadata={{ source: textNl.bronnen.nice }}
+            onSelectTimeframe={setIcuAdmissionsPerAgeTimeframe}
+          >
+            <AdmissionsPerAgeGroup
+              accessibility={{
+                key: 'intensive_care_admissions_per_age_group_over_time_chart',
+              }}
+              values={data.intensive_care_nice_per_age_group.values}
+              timeframe={IcuAdmissionsPerAgeTimeframe}
+              timelineEvents={getTimelineEvents(content.elements.timeSeries, 'intensive_care_nice_per_age_group')}
+            />
+          </ChartTile>
+
+          <ChartTile
+            title={textNl.hospital.age_group_chart.title}
+            description={textNl.hospital.age_group_chart.description}
             timeframeOptions={TimeframeOptionsList}
             timeframeInitialValue={TimeframeOption.THIRTY_DAYS}
             metadata={{ source: textNl.bronnen.nice }}
@@ -168,6 +198,92 @@ const PatientsPage = (props: StaticProps<typeof getStaticProps>) => {
               values={data.hospital_nice_per_age_group.values}
               timeframe={hospitalAdmissionsPerAgeTimeframe}
               timelineEvents={getTimelineEvents(content.elements.timeSeries, 'hospital_nice_per_age_group')}
+            />
+          </ChartTile>
+          <ChartTile
+            title={textNl.hospital.line_chart.title}
+            description={textNl.hospital.line_chart.description}
+            metadata={{
+              source: textNl.bronnen.nice,
+            }}
+            timeframeOptions={TimeframeOptionsList}
+            timeframeInitialValue={TimeframeOption.THIRTY_DAYS}
+            onSelectTimeframe={setHospitalAdmissionsOverTimeTimeframe}
+          >
+            <TimeSeriesChart
+              accessibility={{
+                key: 'hospital_admissions_over_time_chart',
+              }}
+              values={data.hospital_nice.values}
+              timeframe={hospitalAdmissionsOverTimeTimeframe}
+              seriesConfig={[
+                {
+                  type: 'line',
+                  metricProperty: 'admissions_on_date_of_admission_moving_average',
+                  label: textNl.hospital.line_chart.legend_title_moving_average,
+                  color: colors.primary,
+                },
+                {
+                  type: 'bar',
+                  metricProperty: 'admissions_on_date_of_admission',
+                  label: textNl.hospital.line_chart.legend_title,
+                  color: colors.primary,
+                },
+              ]}
+              dataOptions={{
+                timespanAnnotations: [
+                  {
+                    start: underReportedRange,
+                    end: Infinity,
+                    label: textNl.hospital.line_chart.legend_underreported_title,
+                    shortLabel: commonTexts.common.incomplete,
+                    cutValuesForMetricProperties: ['admissions_on_date_of_admission_moving_average'],
+                  },
+                ],
+                timelineEvents: getTimelineEvents(content.elements.timeSeries, 'hospital_nice'),
+              }}
+            />
+          </ChartTile>
+          <ChartTile
+            title={textNl.icu.line_chart.title}
+            description={textNl.icu.line_chart.description}
+            metadata={{ source: textNl.bronnen.nice }}
+            timeframeOptions={TimeframeOptionsList}
+            timeframeInitialValue={TimeframeOption.THIRTY_DAYS}
+            onSelectTimeframe={setIntensiveCareAdmissionsTimeframe}
+          >
+            <TimeSeriesChart
+              accessibility={{
+                key: 'intensive_care_admissions_over_time_chart',
+              }}
+              values={data.intensive_care_nice.values}
+              timeframe={intensiveCareAdmissionsTimeframe}
+              dataOptions={{
+                timespanAnnotations: [
+                  {
+                    start: intakeUnderReportedRange,
+                    end: Infinity,
+                    label: textNl.icu.line_chart.legend_underreported_title,
+                    shortLabel: commonTexts.common.incomplete,
+                    cutValuesForMetricProperties: ['admissions_on_date_of_admission_moving_average'],
+                  },
+                ],
+                timelineEvents: getTimelineEvents(content.elements.timeSeries, 'intensive_care_nice'),
+              }}
+              seriesConfig={[
+                {
+                  type: 'line',
+                  metricProperty: 'admissions_on_date_of_admission_moving_average',
+                  label: textNl.icu.line_chart.legend_title_moving_average,
+                  color: colors.primary,
+                },
+                {
+                  type: 'bar',
+                  metricProperty: 'admissions_on_date_of_admission',
+                  label: textNl.icu.line_chart.legend_title_trend_label,
+                  color: colors.primary,
+                },
+              ]}
             />
           </ChartTile>
         </TileList>
--- packages/app/src/pages/landelijk/ziekenhuizen-en-zorg.tsx
+++ packages/app/src/pages/landelijk/ziekenhuizen-en-zorg.tsx
@@ -1,34 +1,21 @@
-import {
-  colors,
-  DAY_IN_SECONDS,
-  getLastFilledValue,
-  GmCollectionHospitalNice,
-  TimeframeOption,
-  TimeframeOptionsList,
-  VrCollectionHospitalNice,
-  WEEK_IN_SECONDS,
-} from '@corona-dashboard/common';
+import { colors, DAY_IN_SECONDS, getLastFilledValue, TimeframeOption, TimeframeOptionsList, WEEK_IN_SECONDS } from '@corona-dashboard/common';
 import { Ziekenhuis } from '@corona-dashboard/icons';
 import { GetStaticPropsContext } from 'next';
 import { useState } from 'react';
-import { TwoKpiSection, TimeSeriesChart, TileList, SEOHead, ChartTile, DynamicChoropleth, ChoroplethTile, KpiTile, KpiValue, PageInformationBlock, PageKpi } from '~/components';
-import { RegionControlOption } from '~/components/chart-region-controls';
-import { thresholds } from '~/components/choropleth/logic/thresholds';
-import { AdmissionsPerAgeGroup } from '~/domain/hospital';
+import { TwoKpiSection, TimeSeriesChart, TileList, SEOHead, ChartTile, KpiTile, KpiValue, PageInformationBlock, PageKpi } from '~/components';
 import { Layout, NlLayout } from '~/domain/layout';
 import { useIntl } from '~/intl';
 import { Languages, SiteText } from '~/locale';
-import { ElementsQueryResult, getElementsQuery, getTimelineEvents } from '~/queries/get-elements-query';
+import { ElementsQueryResult, getElementsQuery } from '~/queries/get-elements-query';
 import { getArticleParts, getLinkParts, getPagePartsQuery } from '~/queries/get-page-parts-query';
 import { createGetStaticProps, StaticProps } from '~/static-props/create-get-static-props';
 import { createGetChoroplethData, createGetContent, getLastGeneratedDate, getLokalizeTexts, selectNlData } from '~/static-props/get-data';
 import { ArticleParts, LinkParts, PagePartQueryResult } from '~/types/cms';
-import { countTrailingNullValues, getBoundaryDateStartUnix, replaceVariablesInText, useReverseRouter } from '~/utils';
+import { countTrailingNullValues, getBoundaryDateStartUnix, replaceVariablesInText } from '~/utils';
 import { getLastInsertionDateOfPage } from '~/utils/get-last-insertion-date-of-page';
-import { last } from 'lodash';
 import { useDynamicLokalizeTexts } from '~/utils/cms/use-dynamic-lokalize-texts';
 
-const pageMetrics = ['difference.hospital_lcps__beds_occupied_covid.new_date_unix', 'hospital_lcps', 'hospital_nice_per_age_group', 'hospital_nice'];
+const pageMetrics = ['difference.hospital_lcps__beds_occupied_covid.new_date_unix', 'hospital_lcps', 'hospital_nice_per_age_group'];
 
 const selectLokalizeTexts = (siteText: SiteText) => ({
   metadataTexts: siteText.pages.topical_page.nl.nationaal_metadata,
@@ -41,7 +28,7 @@ type LokalizeTexts = ReturnType<typeof selectLokalizeTexts>;
 export const getStaticProps = createGetStaticProps(
   ({ locale }: { locale: keyof Languages }) => getLokalizeTexts(selectLokalizeTexts, locale),
   getLastGeneratedDate,
-  selectNlData('difference.hospital_lcps__beds_occupied_covid', 'hospital_lcps', 'hospital_nice_per_age_group', 'hospital_nice'),
+  selectNlData('difference.hospital_lcps__beds_occupied_covid', 'hospital_lcps', 'hospital_nice_per_age_group'),
   createGetChoroplethData({
     vr: ({ hospital_nice_choropleth }) => ({ hospital_nice_choropleth }),
     gm: ({ hospital_nice_choropleth }) => ({ hospital_nice_choropleth }),
@@ -59,8 +46,8 @@ export const getStaticProps = createGetStaticProps(
 
     return {
       content: {
-        articles: getArticleParts(content.parts.pageParts, 'hospitalPageArticles'),
-        links: getLinkParts(content.parts.pageParts, 'hospitalPageLinks'),
+        articles: getArticleParts(content.parts.pageParts, 'hospitalAndCarePageArticles'),
+        links: getLinkParts(content.parts.pageParts, 'hospitalAndCarePageLinks'),
         elements: content.elements,
       },
     };
@@ -68,37 +55,24 @@ export const getStaticProps = createGetStaticProps(
 );
 
 const HospitalsAndCarePage = (props: StaticProps<typeof getStaticProps>) => {
-  const { pageText, selectedNlData: data, choropleth, content, lastGenerated } = props;
-
-  const [hospitalAdmissionsOverTimeTimeframe, setHospitalAdmissionsOverTimeTimeframe] = useState<TimeframeOption>(TimeframeOption.ALL);
+  const { pageText, selectedNlData: data, content, lastGenerated } = props;
 
   const [hospitalBedsOccupiedOverTimeTimeframe, setHospitalBedsOccupiedOverTimeTimeframe] = useState<TimeframeOption>(TimeframeOption.ALL);
 
-  const [hospitalAdmissionsPerAgeTimeframe, setHospitalAdmissionsPerAgeTimeframe] = useState<TimeframeOption>(TimeframeOption.ALL);
-
-  const reverseRouter = useReverseRouter();
-  const [selectedMap, setSelectedMap] = useState<RegionControlOption>('vr');
-
-  const dataHospitalNice = data.hospital_nice;
   const dataHospitalLcps = data.hospital_lcps;
-  const lastValueLcps = data.hospital_lcps.last_value;
-
-  const lastValueNice = data.hospital_nice.last_value;
-  const lastValueNiceChoropleth = (selectedMap === 'gm' ? last(choropleth.gm.hospital_nice_choropleth) : last(choropleth.vr.hospital_nice_choropleth)) || lastValueNice;
-
-  const underReportedRange = getBoundaryDateStartUnix(dataHospitalNice.values, countTrailingNullValues(dataHospitalNice.values, 'admissions_on_date_of_admission_moving_average'));
-
+  /**
+   * Can be replaced with corrent property once data / schema changes are available
+   * Temporary workaround by adding `beds_occupied_covid` to the following line
+   * */
+  const underReportedRange = getBoundaryDateStartUnix(data.hospital_lcps.values, countTrailingNullValues(data.hospital_lcps.values, 'beds_occupied_covid'));
   const sevenDayAverageDates: [number, number] = [underReportedRange - WEEK_IN_SECONDS, underReportedRange - DAY_IN_SECONDS];
 
-  const bedsLastValue = getLastFilledValue(data.hospital_lcps);
+  const bedsLastValue = getLastFilledValue(dataHospitalLcps);
 
   const { commonTexts, formatDateFromSeconds } = useIntl();
-  const { metadataTexts, textNl, textShared } = useDynamicLokalizeTexts<LokalizeTexts>(pageText, selectLokalizeTexts);
+  const { metadataTexts, textNl } = useDynamicLokalizeTexts<LokalizeTexts>(pageText, selectLokalizeTexts);
 
   const lastInsertionDateOfPage = getLastInsertionDateOfPage(data, pageMetrics);
-
-  const choroplethDataGm: GmCollectionHospitalNice[] = choropleth.gm.hospital_nice_choropleth;
-  const choroplethDataVr: VrCollectionHospitalNice[] = choropleth.vr.hospital_nice_choropleth;
 
   return (
     <Layout {...metadataTexts} lastGenerated={lastGenerated}>
@@ -113,7 +87,7 @@ const HospitalsAndCarePage = (props: StaticProps<typeof getStaticProps>) => {
             description={textNl.pagina_toelichting}
             metadata={{
               datumsText: textNl.datums,
-              dateOrRange: lastValueNice.date_unix,
+              dateOrRange: dataHospitalLcps.last_value.date_unix,
               dateOfInsertionUnix: lastInsertionDateOfPage,
               dataSources: [textNl.bronnen.nice, textNl.bronnen.lnaz],
             }}
@@ -133,14 +107,14 @@ const HospitalsAndCarePage = (props: StaticProps<typeof getStaticProps>) => {
                 source: textNl.bronnen.nice,
               }}
             >
-              <PageKpi data={data} metricName="hospital_nice" metricProperty="admissions_on_date_of_admission_moving_average_rounded" isMovingAverageDifference isAmount />
+              <PageKpi data={data} metricName="hospital_lcps" metricProperty="beds_occupied_covid" isMovingAverageDifference isAmount />
             </KpiTile>
 
             <KpiTile
               title={textNl.kpi_bedbezetting.title}
               description={textNl.kpi_bedbezetting.description}
               metadata={{
-                date: lastValueLcps.date_unix,
+                date: dataHospitalLcps.last_value.date_unix,
                 source: textNl.bronnen.lnaz,
               }}
             >
@@ -149,50 +123,6 @@ const HospitalsAndCarePage = (props: StaticProps<typeof getStaticProps>) => {
               )}
             </KpiTile>
           </TwoKpiSection>
-          <ChartTile
-            title={textNl.linechart_titel}
-            description={textNl.linechart_description}
-            metadata={{
-              source: textNl.bronnen.nice,
-            }}
-            timeframeOptions={TimeframeOptionsList}
-            timeframeInitialValue={TimeframeOption.THIRTY_DAYS}
-            onSelectTimeframe={setHospitalAdmissionsOverTimeTimeframe}
-          >
-            <TimeSeriesChart
-              accessibility={{
-                key: 'hospital_admissions_over_time_chart',
-              }}
-              values={dataHospitalNice.values}
-              timeframe={hospitalAdmissionsOverTimeTimeframe}
-              seriesConfig={[
-                {
-                  type: 'line',
-                  metricProperty: 'admissions_on_date_of_admission_moving_average',
-                  label: textNl.linechart_legend_titel_moving_average,
-                  color: colors.primary,
-                },
-                {
-                  type: 'bar',
-                  metricProperty: 'admissions_on_date_of_admission',
-                  label: textNl.linechart_legend_titel,
-                  color: colors.primary,
-                },
-              ]}
-              dataOptions={{
-                timespanAnnotations: [
-                  {
-                    start: underReportedRange,
-                    end: Infinity,
-                    label: textNl.linechart_legend_underreported_titel,
-                    shortLabel: commonTexts.common.incomplete,
-                    cutValuesForMetricProperties: ['admissions_on_date_of_admission_moving_average'],
-                  },
-                ],
-                timelineEvents: getTimelineEvents(content.elements.timeSeries, 'hospital_nice'),
-              }}
-            />
-          </ChartTile>
           <ChartTile
             title={textNl.chart_bedbezetting.title}
             description={textNl.chart_bedbezetting.description}
@@ -236,79 +166,6 @@ const HospitalsAndCarePage = (props: StaticProps<typeof getStaticProps>) => {
                   },
                 ],
               }}
-            />
-          </ChartTile>
-
-          <ChoroplethTile
-            title={textNl.map_titel}
-            description={textNl.map_toelichting}
-            onChartRegionChange={setSelectedMap}
-            chartRegion={selectedMap}
-            legend={{
-              thresholds: selectedMap === 'gm' ? thresholds.gm.admissions_on_date_of_admission_per_100000 : thresholds.gm.admissions_on_date_of_admission_per_100000,
-              title: textShared.chloropleth_legenda.titel,
-            }}
-            metadata={{
-              date: lastValueNiceChoropleth.date_unix,
-              source: textNl.bronnen.nice,
-            }}
-          >
-            {selectedMap === 'gm' && (
-              <DynamicChoropleth
-                accessibility={{
-                  key: 'hospital_admissions_municipal_choropleth',
-                }}
-                map="gm"
-                data={choroplethDataGm}
-                dataConfig={{
-                  metricName: 'hospital_nice_choropleth',
-                  metricProperty: 'admissions_on_date_of_admission_per_100000',
-                }}
-                dataOptions={{
-                  getLink: reverseRouter.gm.ziekenhuisopnames,
-                  tooltipVariables: {
-                    patients: commonTexts.choropleth_tooltip.patients,
-                  },
-                }}
-              />
-            )}
-            {selectedMap === 'vr' && (
-              <DynamicChoropleth
-                accessibility={{
-                  key: 'hospital_admissions_region_choropleth',
-                }}
-                map="vr"
-                thresholdMap="gm"
-                data={choroplethDataVr}
-                dataConfig={{
-                  metricName: 'hospital_nice_choropleth',
-                  metricProperty: 'admissions_on_date_of_admission_per_100000',
-                }}
-                dataOptions={{
-                  getLink: reverseRouter.vr.ziekenhuisopnames,
-                  tooltipVariables: {
-                    patients: commonTexts.choropleth_tooltip.patients,
-                  },
-                }}
-              />
-            )}
-          </ChoroplethTile>
-
-          <ChartTile
-            title={commonTexts.hospital_admissions_per_age_group.chart_title}
-            description={commonTexts.hospital_admissions_per_age_group.chart_description}
-            timeframeOptions={TimeframeOptionsList}
-            timeframeInitialValue={TimeframeOption.THIRTY_DAYS}
-            metadata={{ source: textNl.bronnen.nice }}
-            onSelectTimeframe={setHospitalAdmissionsPerAgeTimeframe}
-          >
-            <AdmissionsPerAgeGroup
-              accessibility={{
-                key: 'hospital_admissions_per_age_group_over_time_chart',
-              }}
-              values={data.hospital_nice_per_age_group.values}
-              timeframe={hospitalAdmissionsPerAgeTimeframe}
-              timelineEvents={getTimelineEvents(content.elements.timeSeries, 'hospital_nice_per_age_group')}
             />
           </ChartTile>
         </TileList>
