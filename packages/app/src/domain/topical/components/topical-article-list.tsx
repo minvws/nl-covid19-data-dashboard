bbb69dd1929ad624f4413512c98945dc81cea8d8
VWSCoronaDashboard26 <VWSCoronaDashboard26@users.noreply.github.com>
2023-01-04 10:36:46 +0100

    bugfix(summary-page): removed outdated ContentTeaser and HighlightsTile components; added new TopicalArticleTeaser component (which is a stripped down version of ContentTeaser); updated TopicalArticleList component to refer to new TopicalArticleTeaser component;

--- packages/app/src/domain/topical/components/topical-article-list.tsx
+++ packages/app/src/domain/topical/components/topical-article-list.tsx
@@ -1,35 +1,18 @@
 import { ArticleSummary } from '~/components/article-teaser';
 import { Box } from '~/components/base';
-import { ContentTeaser } from '~/components/content-teaser';
 import { SiteText } from '~/locale';
+import { TopicalArticleTeaser } from './topical-article-teaser';
 
 interface TopicalArticlesListProps {
   articles: ArticleSummary[];
   text: SiteText['pages']['topical_page']['shared'];
 }
 
-export const TopicalArticlesList = ({
-  articles,
-  text,
-}: TopicalArticlesListProps) => {
+export const TopicalArticlesList = ({ articles }: TopicalArticlesListProps) => {
   return (
-    <Box
-      display="grid"
-      gridColumnGap={{ md: '48px' }}
-      gridTemplateColumns={{ sm: 'repeat(1, 1fr)', md: 'repeat(3, 1fr)' }}
-      spacing={{ _: 3, md: 0 }}
-    >
+    <Box display="grid" gridColumnGap={{ md: '48px' }} gridTemplateColumns={{ sm: 'repeat(1, 1fr)', md: 'repeat(3, 1fr)' }} spacing={{ _: 3, md: 0 }}>
       {articles.map((item) => (
-        <ContentTeaser
-          key={item.slug.current}
-          title={item.title}
-          slug={item.slug.current}
-          cover={item.cover}
-          category={item.category}
-          variant="small"
-          isArticle
-          text={text}
-        />
+        <TopicalArticleTeaser key={item.slug.current} title={item.title} slug={item.slug.current} cover={item.cover} category={item.category} />
       ))}
     </Box>
   );
---
+++ packages/app/src/domain/topical/components/topical-article-teaser.tsx
@@ -0,0 +1,36 @@
+import { ArrowIconRight } from '~/components/arrow-icon';
+import { BackgroundImage } from '~/components/background-image';
+import { Box } from '~/components/base';
+import { HeadingLinkWithIcon } from '~/components/link-with-icon';
+import { Text } from '~/components/typography';
+import { space } from '~/style/theme';
+import { ImageBlock } from '~/types/cms';
+
+export interface TopicalArticleTeaserProps {
+  title: string;
+  slug: string;
+  cover: ImageBlock;
+  category: string;
+}
+
+export const TopicalArticleTeaser = ({ title, slug, cover, category }: TopicalArticleTeaserProps) => {
+  const imageWidth = 90;
+
+  return (
+    <Box display="flex" spacingHorizontal={3} width="100%" alignItems="center" paddingRight={space[3]}>
+      <Box maxWidth={imageWidth} width="100%">
+        <BackgroundImage image={cover} height={66} sizes={[[imageWidth]]} />
+      </Box>
+
+      <Box maxWidth="25rem" spacing={2}>
+        <Text variant="overline2" color="gray7">
+          {category}
+        </Text>
+
+        <HeadingLinkWithIcon href={`/artikelen/${slug}`} icon={<ArrowIconRight />} iconPlacement="right" underline>
+          {title}
+        </HeadingLinkWithIcon>
+      </Box>
+    </Box>
+  );
+};
