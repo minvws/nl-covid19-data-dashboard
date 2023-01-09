e034fc94628ca4abc292dbf55b358df7d158ce0c
VWSCoronaDashboard28 <VWSCoronaDashboard28@users.noreply.github.com>
2023-01-09 15:33:47 +0100

    feat(consistency-improvements): Implement changes for vaccinations per agegroup.

--- packages/app/src/domain/vaccine/autumn-2022-shot-coverage-per-age-group/components/wide-coverage-table.tsx
+++ packages/app/src/domain/vaccine/autumn-2022-shot-coverage-per-age-group/components/wide-coverage-table.tsx
@@ -1,20 +1,16 @@
-import { NlVaccineCoveragePerAgeGroupValue } from '@corona-dashboard/common';
-import css from '@styled-system/css';
-import styled from 'styled-components';
+import { AgeGroup } from '~/domain/vaccine/components/age-group';
+import { Bar } from '~/domain/vaccine/components/bar';
 import { Box } from '~/components/base';
-import { InlineText } from '~/components/typography';
-import { useIntl } from '~/intl';
-import { asResponsiveArray } from '~/style/utils';
+import { COLOR_FULLY_VACCINATED, COLOR_AUTUMN_2022_SHOT } from '~/domain/vaccine/common';
+import { fontWeights, space, fontSizes } from '~/style/theme';
 import { formatAgeGroupString } from '~/utils/format-age-group-string';
 import { formatBirthyearRangeString } from '~/utils/format-birthyear-range-string';
-import {
-  COLOR_FULLY_VACCINATED,
-  COLOR_AUTUMN_2022_SHOT,
-} from '~/domain/vaccine/common';
-import { Bar } from '~/domain/vaccine/components/bar';
-import { WidePercentage } from '~/domain/vaccine/components/wide-percentage';
-import { AgeGroup } from '~/domain/vaccine/components/age-group';
+import { InlineText } from '~/components/typography';
+import { NlVaccineCoveragePerAgeGroupValue } from '@corona-dashboard/common';
 import { SiteText } from '~/locale';
+import { useIntl } from '~/intl';
+import { WidePercentage } from '~/domain/vaccine/components/wide-percentage';
+import styled from 'styled-components';
 interface WideCoverageTableProps {
   text: SiteText['pages']['vaccinations_page']['nl']['vaccination_coverage'];
   values: NlVaccineCoveragePerAgeGroupValue[];
@@ -26,117 +22,48 @@ export const WideCoverageTable = ({ values, text }: WideCoverageTableProps) => {
   return (
     <Box overflow="auto">
       <StyledTable>
-        <thead
-          css={css({
-            borderBottom: '1px solid',
-            borderColor: 'silver',
-          })}
-        >
+        <TableHead>
           <Row>
-            <HeaderCell
-              css={css({
-                textAlign: 'left',
-                width: asResponsiveArray({
-                  _: '30%',
-                  lg: '30%',
-                }),
-              })}
-            >
+            <HeaderCell width="30%">
               <InlineText variant="label1">{text.headers.agegroup}</InlineText>
             </HeaderCell>
-            <HeaderCell
-              css={css({
-                textAlign: 'right',
-                pr: asResponsiveArray({ _: 3, xl: 4 }),
-                width: asResponsiveArray({
-                  _: '25%',
-                  lg: '20%',
-                }),
-              })}
-            >
-              <InlineText variant="label1">
-                {text.headers.autumn_2022_shot}
-              </InlineText>
+            <HeaderCell hasPaddingRight width="20%">
+              <InlineText variant="label1">{text.headers.autumn_2022_shot}</InlineText>
             </HeaderCell>
-            <HeaderCell
-              css={css({
-                textAlign: 'right',
-                pr: asResponsiveArray({ _: 3, xl: 4 }),
-                width: asResponsiveArray({
-                  _: '25%',
-                  lg: '20%',
-                }),
-              })}
-            >
-              <InlineText variant="label1">
-                {text.headers.fully_vaccinated}
-              </InlineText>
+            <HeaderCell hasPaddingRight width="20%">
+              <InlineText variant="label1">{text.headers.fully_vaccinated}</InlineText>
             </HeaderCell>
-            <HeaderCell
-              css={css({
-                width: asResponsiveArray({
-                  _: '20%',
-                  lg: '30%',
-                }),
-              })}
-            >
-              <InlineText variant="label1">
-                {text.headers.difference_autumn_2022_shot_and_fully_vaccinated}
-              </InlineText>
+            <HeaderCell width="30%">
+              <InlineText variant="label1">{text.headers.difference_autumn_2022_shot_and_fully_vaccinated}</InlineText>
             </HeaderCell>
           </Row>
-        </thead>
+        </TableHead>
+
         <tbody>
           {values.map((item, index) => (
             <Row key={index}>
               <HeaderCell isColumn>
                 <AgeGroup
-                  range={formatAgeGroupString(
-                    item.age_group_range,
-                    commonTexts.common.agegroup
-                  )}
-                  ageGroupTotal={
-                    'age_group_total' in item ? item.age_group_total : undefined
-                  }
-                  birthyear_range={formatBirthyearRangeString(
-                    item.birthyear_range,
-                    commonTexts.common.birthyears
-                  )}
+                  ageGroupTotal={'age_group_total' in item ? item.age_group_total : undefined}
                   text={commonTexts.common.agegroup.total_people}
+                  range={formatAgeGroupString(item.age_group_range, commonTexts.common.agegroup)}
+                  birthyear_range={formatBirthyearRangeString(item.birthyear_range, commonTexts.common.birthyears)}
                 />
               </HeaderCell>
               <Cell>
                 <WidePercentage
-                  value={
-                    item.autumn_2022_vaccinated_percentage !== null
-                      ? `${formatPercentage(
-                          item.autumn_2022_vaccinated_percentage
-                        )}%`
-                      : text.no_data
-                  }
+                  value={item.autumn_2022_vaccinated_percentage !== null ? `${formatPercentage(item.autumn_2022_vaccinated_percentage)}%` : text.no_data}
                   color={COLOR_AUTUMN_2022_SHOT}
-                  justifyContent="flex-end"
+                  justifyContent="flex-start"
                 />
               </Cell>
               <Cell>
-                <WidePercentage
-                  value={`${formatPercentage(
-                    item.fully_vaccinated_percentage
-                  )}%`}
-                  color={COLOR_FULLY_VACCINATED}
-                  justifyContent="flex-end"
-                />
+                <WidePercentage value={`${formatPercentage(item.fully_vaccinated_percentage)}%`} color={COLOR_FULLY_VACCINATED} justifyContent="flex-start" />
               </Cell>
               <Cell>
                 <Box spacing={1}>
-                  <Bar
-                    value={item.autumn_2022_vaccinated_percentage}
-                    color={COLOR_AUTUMN_2022_SHOT}
-                  />
-                  <Bar
-                    value={item.fully_vaccinated_percentage}
-                    color={COLOR_FULLY_VACCINATED}
-                  />
+                  <Bar value={item.autumn_2022_vaccinated_percentage} color={COLOR_AUTUMN_2022_SHOT} />
+                  <Bar value={item.fully_vaccinated_percentage} color={COLOR_FULLY_VACCINATED} />
                 </Box>
               </Cell>
             </Row>
@@ -147,33 +74,39 @@ export const WideCoverageTable = ({ values, text }: WideCoverageTableProps) => {
   );
 };
 
-const StyledTable = styled.table(
-  css({
-    borderCollapse: 'collapse',
-    width: '100%',
-  })
-);
+const StyledTable = styled.table`
+  border-collapse: collapse;
+  width: 100%;
+`;
 
-const Row = styled.tr(
-  css({
-    borderBottom: '1px solid',
-    borderColor: 'silver',
-  })
-);
+const Row = styled.tr`
+  border-bottom: 1px solid silver;
+`;
+interface HeaderCellProps {
+  isColumn?: boolean;
+  hasPaddingRight?: boolean;
+  width?: string;
+}
 
-const HeaderCell = styled.th<{ isColumn?: boolean }>((headerCellProps) =>
-  css({
-    textAlign: 'left',
-    fontWeight: headerCellProps.isColumn ? 'normal' : 'bold',
-    verticalAlign: 'middle',
-    pb: headerCellProps.isColumn ? undefined : 2,
-    py: headerCellProps.isColumn ? 3 : undefined,
-  })
-);
+const HeaderCell = styled.th<HeaderCellProps>`
+  font-weight: ${({ isColumn }) => (isColumn ? fontWeights.normal : fontWeights.bold)};
+  padding-block: ${({ isColumn }) => (isColumn ? space[3] : undefined)};
+  padding-bottom: ${({ isColumn }) => (isColumn ? undefined : space[2])};
+  padding-right: ${({ hasPaddingRight }) => (hasPaddingRight ? space[3] : undefined)};
+  text-align: left;
+  vertical-align: middle;
+  width: ${({ width }) => (width ? width : undefined)};
 
-const Cell = styled.td(
-  css({
-    py: 3,
-    verticalAlign: 'middle',
-  })
-);
+  > span {
+    font-size: ${fontSizes[2]};
+  }
+`;
+
+const Cell = styled.td`
+  padding-block: ${space[3]};
+  vertical-align: middle;
+`;
+
+const TableHead = styled.thead`
+  border-bottom: 1px solid silver;
+`;
