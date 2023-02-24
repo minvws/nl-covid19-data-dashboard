49a617db258a47b2fd5c9d0d1644ffcec7d3eeff
VWSCoronaDashboard18 <93984341+VWSCoronaDashboard18@users.noreply.github.com>
2023-02-24 14:22:50 +0100

    fix: create files if non existing

--- packages/cli/src/scripts/generate-data-types.ts
+++ packages/cli/src/scripts/generate-data-types.ts
@@ -57,6 +57,7 @@ function writeDefinitionsToFile(typeDefinitions: string[]) {
 
   fs.writeFileSync(outputFile, `${bannerComment}${typeDefinitions.join('\n')}`.replaceAll('"', "'"), {
     encoding: 'utf8',
+    flag: 'w',
   });
 
   console.info(`Written typescript definitions output to file '${outputFile}'`);
