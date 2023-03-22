c5e7dfa7f8b28104666115cd14dbb22dfc3f9674
VWSCoronaDashboard18 <93984341+VWSCoronaDashboard18@users.noreply.github.com>
2023-03-22 15:06:13 +0100

    fix: deleted a file to much

--- packages/app/src/utils/use-scoped-warning.ts
+++ packages/app/src/utils/use-scoped-warning.ts
@@ -1,4 +1,4 @@
-import { gmData } from '@corona-dashboard/common';
+import { gmData, vrData } from '@corona-dashboard/common';
 import { useIntl } from '~/intl';
 import { replaceVariablesInText } from '~/utils/replace-variables-in-text';
 /**
@@ -9,9 +9,16 @@ export function useScopedWarning(current: string, warning: string) {
   const { commonTexts } = useIntl();
   const scopedGmName = commonTexts.gemeente_index.municipality_warning;
   const scopedGm = gmData.find((gm) => gm.name === scopedGmName || (gm.searchTerms && gm.searchTerms.includes(scopedGmName)));
+  const scopedVr = vrData.find((vr) => vr.code === scopedGm?.vrCode);
 
   // Check if defined GM is the same as current
   if (scopedGm?.name === current) {
+    return replaceVariablesInText(warning, {
+      municipality: scopedGm?.name,
+    });
+  }
+  // Check if extracted VR from defined GM is the same as current VR
+  if (scopedVr?.name === current) {
     return replaceVariablesInText(warning, {
       municipality: scopedGm?.name,
     });
