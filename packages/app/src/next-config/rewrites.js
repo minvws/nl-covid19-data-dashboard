9ad28a191704db851377992b7779571497021cb1
VWSCoronaDashboard27 <VWSCoronaDashboard27@users.noreply.github.com>
2023-03-24 10:01:16 +0100

    feat: removed vr references

--- packages/app/src/next-config/rewrites.js
+++ packages/app/src/next-config/rewrites.js
@@ -5,10 +5,6 @@ async function rewrites() {
         source: '/gemeente/(g|G)(m|M):nr(\\d{4})/:page*',
         destination: '/gemeente/GM:nr/:page*',
       },
-      {
-        source: '/veiligheidsregio/(v|V)(r|R):nr(\\d{2})/:page*',
-        destination: '/veiligheidsregio/VR:nr/:page*',
-      },
     ],
   };
 }
