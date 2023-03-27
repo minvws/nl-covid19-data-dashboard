c1ba59f9b478fa794685131e0749990c0ceeec1e
LR <107395524+VWSCoronaDashboard26@users.noreply.github.com>
2023-03-27 10:10:37 +0200

    feat(e2e-package): removed entire e2e package; removed any 'e2e', 'cypress' or 'data-cy' references; kept data-testid references for testing-library package; cleaned up modules; (#4710)
    Co-authored-by: VWSCoronaDashboard26 <VWSCoronaDashboard26@users.noreply.github.com>

--- packages/e2e/src/integration/veelgestelde-vragen.spec.ts
+++
@@ -1,9 +0,0 @@
-context('Veelgestelde vragen', () => {
-  before(() => {
-    cy.beforeGeneralTests('veelgestelde-vragen');
-  });
-
-  it('dummy to trigger the before() tests', () => {
-    expect(true);
-  });
-});
