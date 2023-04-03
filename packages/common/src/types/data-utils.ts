e589b288a2931a7f37051b29e3f104efec8eb2ee
VWSCoronaDashboard18 <93984341+VWSCoronaDashboard18@users.noreply.github.com>
2023-04-03 16:58:48 +0200

    fix: PR feedback

--- packages/common/src/types/data-utils.ts
+++ packages/common/src/types/data-utils.ts
@@ -14,7 +14,10 @@ export type DataScopeKey = keyof ScopedData;
 
 export type DataScope = ScopedData[DataScopeKey];
 
-export type MetricKeys<T> = keyof Omit<T, 'last_generated' | 'proto_name' | 'name' | 'code'>;
+export type MetricKeys<T> = keyof Omit<
+  T,
+  'last_generated' | 'proto_name' | 'name' | 'code'
+>;
 
 export type MetricName = MetricKeys<Nl & Vr & Gm>;
 
@@ -32,4 +35,7 @@ type ValueKeys<T> = T extends ValuesMetric<infer V> ? ValueKeys<V> : keyof T;
 /**
  * The metric properties of metric M in data scope S (scope being In/Nl/Vr/Gm)
  */
-export type MetricProperty<S extends DataScope, M extends MetricKeys<S>> = ValueKeys<S[M]>;
+export type MetricProperty<
+  S extends DataScope,
+  M extends MetricKeys<S>
+> = ValueKeys<S[M]>;
