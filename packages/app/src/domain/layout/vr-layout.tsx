2997f4c4752a17df5bdc87dcd5c86baeff2cf84a
VWSCoronaDashboard27 <VWSCoronaDashboard27@users.noreply.github.com>
2023-04-04 16:09:55 +0200

    Revert "fix: merged conflicts"
    This reverts commit 50052d521e141f3ddbfbcceac711033edcef18f5, reversing
    changes made to 305184bc775aa8ca70d86422b02135a77bde289e.

---
+++ packages/app/src/domain/layout/vr-layout.tsx
@@ -0,0 +1,47 @@
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { ErrorBoundary } from '~/components/error-boundary';
+import { AppContent } from '~/components/layout/app-content';
+
+type VrLayoutProps = {
+  children?: React.ReactNode;
+  vrName?: string;
+  isLandingPage?: boolean;
+};
+
+/**
+ * VrLayout is a composition of persistent layouts.
+ *
+ * ## States
+ *
+ * ### Mobile
+ * - /veiligheidsregio -> only show aside
+ * - /veiligheidsregio/[metric] -> only show content (children)
+ *
+ * ### Desktop
+ * - /veiligheidsregio -> shows aside and content (children)
+ * - /veiligheidsregio/[metric] -> shows aside and content (children)
+ *
+ * More info on persistent layouts:
+ * https://adamwathan.me/2019/10/17/persistent-layout-patterns-in-nextjs/
+ */
+export function VrLayout(props: VrLayoutProps) {
+  const { children } = props;
+
+  const router = useRouter();
+
+  const isMainRoute = router.route === '/veiligheidsregio';
+
+  return (
+    <>
+      <Head>
+        <link key="dc-spatial" rel="dcterms:spatial" href="https://standaarden.overheid.nl/owms/terms/Nederland" />
+        <link key="dc-spatial-title" rel="dcterms:spatial" href="https://standaarden.overheid.nl/owms/terms/Nederland" title="Nederland" />
+      </Head>
+
+      <AppContent hideBackButton={isMainRoute} sidebarComponent={<></>}>
+        <ErrorBoundary>{children}</ErrorBoundary>
+      </AppContent>
+    </>
+  );
+}
