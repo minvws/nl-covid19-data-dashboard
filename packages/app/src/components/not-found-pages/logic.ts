6f84a6d5765f44da697ecfd724c432b4f0933dae
VWSCoronaDashboard28 <VWSCoronaDashboard28@users.noreply.github.com>
2023-04-17 11:58:40 +0200

    feat(404-pages): PR feedback - round 2.

--- packages/app/src/components/not-found-pages/logic.ts
+++ packages/app/src/components/not-found-pages/logic.ts
@@ -2,11 +2,7 @@ import { getClient } from '~/lib/sanity';
 import { getNotFoundPageQuery } from '~/queries/get-not-found-page-query';
 import { NotFoundPageConfiguration } from './types';
 
-/**
- * @function determinePageType
- * @description Returns the page type based on the request URL.
- */
-const determinePageType = (url: string) => {
+const determinePageTypeFromUrl = (url: string) => {
   const levelsToPageTypeMapping: { [key: string]: string } = { landelijk: 'nl', gemeente: 'gm', artikelen: 'article', general: 'general' };
   const pageType = levelsToPageTypeMapping[Object.keys(levelsToPageTypeMapping).find((key) => url.includes(`/${key}`)) ?? 'general'];
 
@@ -15,10 +11,13 @@ const determinePageType = (url: string) => {
 
 /**
  * @function getNotFoundPageData
- * @description Fetches the data for a given page type from Sanity.
+ * @description Fetches the data for a given page type from Sanity and adds additional properties to the page configuration.
+ * @param url - The current request URL.
+ * @param locale - The selected locale.
+ * @returns {Promise<NotFoundPageConfiguration> | null} - Returns the page configuration if found, otherwise null.
  */
 export const getNotFoundPageData = async (url: string, locale: string) => {
-  const pageType = url ? determinePageType(url) : 'general';
+  const pageType = url ? determinePageTypeFromUrl(url) : 'general';
   const query = getNotFoundPageQuery(locale, pageType);
   const client = await getClient();
   const notFoundPageConfiguration: NotFoundPageConfiguration = await client.fetch(query);
