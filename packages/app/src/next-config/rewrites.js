8552c9d7ae4d67a01c07d8b954f7cfe43fc2e576
AP <116002914+VWSCoronaDashboard28@users.noreply.github.com>
2023-04-17 16:36:21 +0200

    feature/COR-1516-404-page-improvements-multiple-pages (#4736)
    * feat(404-pages): Remove hardcoded article redirects
    
    * feat(404-pages): Enable configuration of 404 pages in Sanity.
    
    * feat(404-pages): Adjust schemas.
    
    * feat(404-pages): Wrap breadcrumbs with a guard.
    
    * feat(404-pages): Middleware redirect logic with parameters for each 404 page.
    
    * feat(404-pages): Add new icons. Adjust KpiIconInput component so that it closes upon selecting an icon.
    
    * feat(404-pages): Extract query to its own file and adjust not found schema.
    
    * feat(404-pages): Update SVG fill
    
    * feat(404-pages): Adjust query and schema. Delete old 404 page
    
    * feat(404-pages): WIP new 404 pages.
    
    * feat(404-pages): Finalize 404 pages.
    
    * feat(404-pages): Fine tune implementation.
    
    * feat(404-pages): PR Feedback, first round.
    
    * feat(404-pages): PR Feedback, second round.
    
    * feat(404-pages): Non middleware solution.
    
    * feat(404-pages): Refactor.
    
    * feat(404-pages): PR feedback and a few tweaks.
    
    * feat(404 pages): Refactor not found page for general, landelijk, and articles.
    
    * feat(404 pages): Change fallback back to blocking so that EN locale pages are built at runtime.
    
    * feat(404 pages): Create dedicated GM 404 page with new rewrites.
    
    * feat(404 pages): Formatting.
    
    * feat(404 pages): Finalize rewrites for all Gemeente scenarios.
    
    * feat(404 pages): Refactor and update utils typing.
    
    * feat(404-pages): PR feedback - round 1
    
    * feat(404-pages): PR feedback - round 2.
    
    ---------
    
    Co-authored-by: VWSCoronaDashboard28 <VWSCoronaDashboard28@users.noreply.github.com>

--- packages/app/src/next-config/rewrites.js
+++ packages/app/src/next-config/rewrites.js
@@ -5,6 +5,44 @@ async function rewrites() {
         source: '/gemeente/(g|G)(m|M):nr(\\d{4})/:page*',
         destination: '/gemeente/GM:nr/:page*',
       },
+      /**
+       * The rewrite below will match everything after /gemeente/ except for g/m or G/M or gm/GM.
+       * When matched, the destination is rewritten to /gemeente/code/404 so that it lands in the
+       * [...404].tsx catch-all route within packages/app/src/pages/gemeente/[code]/ The same applies
+       * to the two other gemeente rewrites below. This is a workaround for making sure all gemeente routes
+       * go to the correct 404 page ([...404].tsx).
+       *
+       * The regex states that if /gemeente/ is not followed by gm/GM/gM/Gm, then it should go to the 404 page.
+       * For example:
+       * 1. /gemeente/somethingWrong
+       * 2. /gemeente/blahblah
+       * 3. /gemeente/gblah
+       */
+      {
+        source: '/gemeente/((?!gm|GM|gM|Gm).*):slug*',
+        destination: '/gemeente/code/404',
+      },
+      /**
+       * The regex below states that if the URL contains GM after /gemeente/, it must be followed by 4 digits.
+       * This will catch:
+       * 1. /gemeente/GM123
+       * 2. /gemeente/GM
+       * 3. /gemeente/GM123/rioolwater
+       */
+      {
+        source: '/gemeente/(g|G)(m|M)((?!\\d{4}).*):slug*',
+        destination: '/gemeente/code/404',
+      },
+      /**
+       * The regex below matches URLs which contain g|G m|M followed by more than 4 digits, optionally
+       * followed by a forward slash, optionally followed by a string. This will catch:
+       * 1. /gemeente/GM12345
+       * 2. /gemeente/GM123456/rioolwater
+       */
+      {
+        source: '/gemeente/(g|G)(m|M)(\\d{5,})(\\/?)(\\S*):slug*',
+        destination: '/gemeente/code/404',
+      },
     ],
   };
 }
