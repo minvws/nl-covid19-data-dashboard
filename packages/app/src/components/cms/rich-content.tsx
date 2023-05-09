0f0d4bc77996f3091a0e80fdd3b79d2adef501e5
VWSCoronaDashboard28 <VWSCoronaDashboard28@users.noreply.github.com>
2023-05-09 16:28:00 +0200

    feat(sanity-migration): WIP on i18n. Removal of redundant files and schema types.
    Co-authored-by: VWSCoronaDashboard26

--- packages/app/src/components/cms/rich-content.tsx
+++ packages/app/src/components/cms/rich-content.tsx
@@ -1,13 +1,3 @@
-import {
-  AgeDemographicConfiguration,
-  ChartConfiguration,
-  ChoroplethConfiguration,
-  DataScope,
-  DataScopeKey,
-  DonutChartConfiguration,
-  KpiConfiguration,
-  MetricKeys,
-} from '@corona-dashboard/common';
 import { PortableTextEntry } from '@sanity/block-content-to-react';
 import css from '@styled-system/css';
 import { Fragment, FunctionComponent, ReactNode } from 'react';
@@ -19,19 +9,11 @@ import { ExternalLink } from '~/components/external-link';
 import { useIntl } from '~/intl';
 import { getFileSrc, PortableText } from '~/lib/sanity';
 import { nestedHtml } from '~/style/preset';
-import { asResponsiveArray } from '~/style/utils';
 import { ImageBlock, InlineAttachment, InlineCollapsibleList, InlineLink, RichContentImageBlock } from '~/types/cms';
 import { assert } from '~/utils/assert';
 import { isInternalUrl } from '~/utils/is-internal-url';
 import { Link } from '~/utils/link';
-import { AccessibilityDefinition } from '~/utils/use-accessibility-annotations';
-import { Heading } from '../typography';
 import { ContentImage } from './content-image';
-import { InlineAgeDemographic } from './inline-age-demographic';
-import { InlineChoropleth } from './inline-choropleth';
-import { InlineDonutChart } from './inline-donut-chart';
-import { InlineKpi } from './inline-kpi';
-import { InlineTimeSeriesCharts } from './inline-time-series-charts';
 import { ChevronRight, Download, External as ExternalLinkIcon } from '@corona-dashboard/icons';
 import { space } from '~/style/theme';
 import { replaceVariablesInText } from '~/utils';
@@ -47,55 +29,27 @@ interface RichContentProps {
   variableValue?: string | false;
 }
 
-interface ChartConfigNode {
-  title: string;
-  startDate?: string;
-  endDate?: string;
-  config: ChartConfiguration<DataScopeKey, MetricKeys<DataScope>>;
-}
-
-interface AgeDemographicConfigNode {
-  title: string;
-  startDate?: string;
-  endDate?: string;
-  config: AgeDemographicConfiguration<DataScopeKey, MetricKeys<DataScope>, AccessibilityDefinition['key']>;
-}
-
-interface ChoroplethConfigNode {
-  title: string;
-  config: ChoroplethConfiguration<DataScopeKey, MetricKeys<DataScope>>;
-}
-
-interface DonutConfigNode {
-  title: string;
-  startDate?: string;
-  endDate?: string;
-  config: DonutChartConfiguration<DataScopeKey, MetricKeys<DataScope>>;
-}
-
-interface KpiConfigNode {
-  endDate?: string;
-  config: KpiConfiguration;
-}
-
-interface KpisConfigNode {
-  _type: string;
-  kpis: KpiConfigNode[];
-}
-
 export function RichContent({ contentWrapper, blocks, imageSizes, elementAlignment, variableValue }: RichContentProps) {
   const ContentWrapper = contentWrapper ?? Fragment;
   const serializers = {
     types: {
       inlineBlock: (props: unknown) => {
-        assert(PortableText.defaultSerializers.types?.inlineBlock, `[${RichContent.name}] PortableText needs to provide a serializer for inlineBlock content`);
+        assert(
+          PortableText.defaultSerializers.types?.inlineBlock,
+          `[${RichContent.name}] PortableText needs to provide a serializer for inlineBlock content`
+        );
         return <ContentWrapper>{PortableText.defaultSerializers.types.inlineBlock(props)}</ContentWrapper>;
       },
       block: (props: unknown) => {
-        assert(PortableText.defaultSerializers.types?.block, `[${RichContent.name}] PortableText needs to provide a serializer for block content`);
+        assert(
+          PortableText.defaultSerializers.types?.block,
+          `[${RichContent.name}] PortableText needs to provide a serializer for block content`
+        );
         return <ContentWrapper>{PortableText.defaultSerializers.types.block(props)}</ContentWrapper>;
       },
-      image: (props: { node: ImageBlock | RichContentImageBlock }) => <ContentImage contentWrapper={contentWrapper} sizes={imageSizes} {...props} />,
+      image: (props: { node: ImageBlock | RichContentImageBlock }) => (
+        <ContentImage contentWrapper={contentWrapper} sizes={imageSizes} {...props} />
+      ),
       inlineCollapsible: (props: { node: InlineCollapsibleList }) => {
         if (!props.node.content.inlineBlockContent) return null;
 
@@ -123,106 +77,6 @@ export function RichContent({ contentWrapper, blocks, imageSizes, elementAlignme
           </ContentWrapper>
         );
       },
-      dashboardChart: (props: { node: ChartConfigNode }) => {
-        const node = props.node;
-
-        return (
-          <Box
-            css={css({
-              maxWidth: 'infoWidth',
-              width: '100%',
-              paddingX: asResponsiveArray({ _: space[4], md: undefined }),
-              paddingBottom: space[4],
-            })}
-          >
-            <Box paddingBottom={space[4]}>
-              <Heading level={3} as="h4">
-                {node.title}
-              </Heading>
-            </Box>
-            <InlineTimeSeriesCharts configuration={node.config} startDate={node.startDate} endDate={node.endDate} />
-          </Box>
-        );
-      },
-      dashboardAgeDemographicChart: (props: { node: AgeDemographicConfigNode }) => {
-        const node = props.node;
-
-        return (
-          <Box
-            css={css({
-              maxWidth: 'infoWidth',
-              width: '100%',
-              paddingX: asResponsiveArray({ _: space[4], md: undefined }),
-              paddingBottom: space[4],
-            })}
-          >
-            <Box paddingBottom={space[4]}>
-              <Box paddingBottom={space[4]}>
-                <Heading level={3} as="h4">
-                  {node.title}
-                </Heading>
-              </Box>
-              <InlineAgeDemographic configuration={node.config} startDate={node.startDate} endDate={node.endDate} />
-            </Box>
-          </Box>
-        );
-      },
-      dashboardChoropleth: (props: { node: ChoroplethConfigNode }) => {
-        const node = props.node;
-
-        return (
-          <ContentWrapper>
-            <Box paddingBottom={space[4]}>
-              <Box paddingBottom={space[4]}>
-                <Heading level={3} as="h4">
-                  {node.title}
-                </Heading>
-              </Box>
-
-              <InlineChoropleth configuration={node.config} title={node.title} />
-            </Box>
-          </ContentWrapper>
-        );
-      },
-      dashboardDonut: (props: { node: DonutConfigNode }) => {
-        const node = props.node;
-
-        return (
-          <ContentWrapper>
-            <Box paddingBottom={space[4]}>
-              <Box paddingBottom={space[4]}>
-                <Heading level={3} as="h4">
-                  {node.title}
-                </Heading>
-              </Box>
-
-              <InlineDonutChart configuration={node.config} startDate={node.startDate} endDate={node.endDate} />
-            </Box>
-          </ContentWrapper>
-        );
-      },
-      dashboardKpi: (props: { node: KpiConfigNode }) => {
-        const node = props.node;
-
-        return (
-          <ContentWrapper>
-            <InlineKpi configuration={node.config} date={node.endDate} />
-          </ContentWrapper>
-        );
-      },
-      dashboardKpis: (props: { node: KpisConfigNode }) => {
-        const kpiLeft = props.node.kpis[0];
-        const kpiRight = props.node.kpis[1];
-
-        return (
-          <ContentWrapper>
-            <Box spacing={{ _: 4, md: 2 }} display="flex" paddingY={space[3]} flexDirection={{ _: 'column', md: 'row' }}>
-              <InlineKpi configuration={kpiLeft.config} date={kpiLeft.endDate} />
-              <InlineKpi configuration={kpiRight.config} date={kpiRight.endDate} />
-            </Box>
-          </ContentWrapper>
-        );
-      },
     },
     marks: {
       inlineAttachment: InlineAttachmentMark,
@@ -232,7 +86,9 @@ export function RichContent({ contentWrapper, blocks, imageSizes, elementAlignme
         return (
           <>
             {children.map((child, index) => (
-              <React.Fragment key={index}>{child.includes('{{kpiValue}}') && variableValue ? replaceVariablesInText(child, { kpiValue: variableValue }) : child}</React.Fragment>
+              <React.Fragment key={index}>
+                {child.includes('{{kpiValue}}') && variableValue ? replaceVariablesInText(child, { kpiValue: variableValue }) : child}
+              </React.Fragment>
             ))}
           </>
         );
