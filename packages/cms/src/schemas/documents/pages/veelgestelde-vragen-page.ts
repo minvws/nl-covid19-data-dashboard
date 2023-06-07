a15d62ea19eeba2fdf1ed6010313256ffcc62f21
VWSCoronaDashboard18 <93984341+VWSCoronaDashboard18@users.noreply.github.com>
2023-06-07 10:25:49 +0200

    fix: PR review update

--- packages/cms/src/schemas/documents/pages/veelgestelde-vragen-page.ts
+++ packages/cms/src/schemas/documents/pages/veelgestelde-vragen-page.ts
@@ -1,3 +1,6 @@
+import React from 'react';
+import { FrequentlyAskedQuestionsList } from '../../../components/frequently-asked-questions-list';
+
 export const veelgesteldeVragen = {
   name: 'veelgesteldeVragen',
   type: 'document',
@@ -17,8 +20,7 @@ export const veelgesteldeVragen = {
       name: 'questions',
       type: 'array',
       title: 'Vragen',
-      description:
-        'Je kan veelgestelde vragen toevoegen, de volgorde veranderen, de tekst bijwerken of verwijderen. Om de groepen en vragen opnieuw te ordenen, hoeft je slechts één van de vragen in een groep te verplaatsen. Waar de eerste vraag van een bepaalde groep staat, is waar de groep zal verschijnen op de Veelgestelde vragen pagina. Heb je bijvoorbeeld één vraag uit de groep Algemeen bovenaan en de andere vragen onderaan de lijst, dan wordt de groep Algemeen als eerste groep getoond.',
+      description: React.createElement(FrequentlyAskedQuestionsList),
       of: [{ type: 'reference', to: { type: 'faqQuestion' } }],
     },
   ],
