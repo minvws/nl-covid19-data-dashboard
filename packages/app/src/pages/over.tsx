001a0d87fc4ba2d748a06dbc259a074e35434926
VWSCoronaDashboard18 <93984341+VWSCoronaDashboard18@users.noreply.github.com>
2023-06-26 15:58:34 +0200

    WiP: still net to get the qruey right

--- packages/app/src/pages/over.tsx
+++ packages/app/src/pages/over.tsx
@@ -3,22 +3,19 @@ import Head from 'next/head';
 import styled from 'styled-components';
 import { Box } from '~/components/base';
 import { RichContent } from '~/components/cms/rich-content';
+import { MaxWidth } from '~/components/max-width';
 import { Heading } from '~/components/typography';
-import { Content } from '~/domain/layout/content';
 import { Layout } from '~/domain/layout/layout';
 import { useIntl } from '~/intl';
-import {
-  createGetStaticProps,
-  StaticProps,
-} from '~/static-props/create-get-static-props';
-import {
-  createGetContent,
-  getLastGeneratedDate,
-} from '~/static-props/get-data';
-import { RichContentBlock } from '~/types/cms';
+import { createGetStaticProps, StaticProps } from '~/static-props/create-get-static-props';
+import { createGetContent, getLastGeneratedDate } from '~/static-props/get-data';
+import { space } from '~/style/theme';
+import { ImageBlock, RichContentBlock } from '~/types/cms';
 interface OverData {
   title: string | null;
+  intro: RichContentBlock[] | null;
   description: RichContentBlock[] | null;
+  timelineImage: ImageBlock | null;
 }
 
 export const getStaticProps = createGetStaticProps(
@@ -26,19 +23,41 @@ export const getStaticProps = createGetStaticProps(
   createGetContent<OverData>((context) => {
     const { locale = 'nl' } = context;
     return `
-    *[_type == 'overDitDashboard']{
-      ...,
-      "description": {
-        "_type": description._type,
-        "${locale}": [
-          ...description.${locale}[]
-          {
-            ...,
-            "asset": asset->
-           },
-        ]
-      }
-    }[0]
+      *[
+          _type == 'overDitDashboard' && !(_id in path('drafts.**'))
+      ][0]{...,
+        "description": {
+          "_type": description._type,
+          "${locale}": [
+            ...description.${locale}[]
+            {
+              ...,
+              "asset": asset->
+             },
+          ]
+        },
+        "intro": {
+          "_type": intro._type && !(_id in path('drafts.**')),
+          "${locale}": [
+            ...intro.${locale}[]
+            {
+              ...,
+              "asset": asset->
+             },
+          ]
+        },
+        "timelineImage": {
+          "_type": timelineImage._type && !(_id in path('drafts.**')),
+          "${locale}": [
+            ...intro.${locale}[]
+            {
+              "alt": alt,
+              "caption": caption,
+              "asset": asset->
+            },
+          ]
+        }
+      },
     `;
   })
 );
@@ -50,30 +69,19 @@ const Over = (props: StaticProps<typeof getStaticProps>) => {
   return (
     <Layout {...commonTexts.over_metadata} lastGenerated={lastGenerated}>
       <Head>
-        <link
-          key="dc-type"
-          rel="dcterms:type"
-          href="https://standaarden.overheid.nl/owms/terms/webpagina"
-        />
-        <link
-          key="dc-type-title"
-          rel="dcterms:type"
-          href="https://standaarden.overheid.nl/owms/terms/webpagina"
-          title="webpagina"
-        />
+        <link key="dc-type" rel="dcterms:type" href="https://standaarden.overheid.nl/owms/terms/webpagina" />
+        <link key="dc-type-title" rel="dcterms:type" href="https://standaarden.overheid.nl/owms/terms/webpagina" title="webpagina" />
       </Head>
 
-      <Content>
-        <Box spacing={4}>
-          {content.title && <Heading level={1}>{content.title}</Heading>}
-          {content.description && (
-            <RichContent
-              blocks={content.description}
-              contentWrapper={RichContentWrapper}
-            />
-          )}
-        </Box>
-      </Content>
+      <Box textVariant="body1" bg="white">
+        <MaxWidth paddingTop={space[6]} paddingBottom={space[5]} paddingX={{ _: space[3], sm: '0' }}>
+          <Box spacing={4}>{content.title && <Heading level={1}>{content.title}</Heading>}</Box>
+          <Box spacing={4} display="grid" gridTemplateColumns={{ _: '1fr', xl: '1fr 1fr' }} gridGap={space[6]}>
+            <div>{content.intro && <RichContent blocks={content.intro} contentWrapper={RichContentWrapper} />}</div>
+            <div>{content.description && <RichContent blocks={content.description} contentWrapper={RichContentWrapper} />}</div>
+          </Box>
+        </MaxWidth>
+      </Box>
     </Layout>
   );
 };
