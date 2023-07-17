341274bc14fbfa567ae6dfd4566d7cffda1e240c
VWSCoronaDashboard18 <93984341+VWSCoronaDashboard18@users.noreply.github.com>
2023-07-17 15:20:59 +0200

    fix: solve non showing data point

---
+++ packages/app/src/components/illustration-tile.tsx
@@ -0,0 +1,22 @@
+import { Tile } from '~/components/tile';
+import { css } from '@styled-system/css';
+import { space } from '~/style/theme';
+interface IllustrationProps {
+  image: string;
+  alt: string;
+  description: string;
+  hasNoBorder?: boolean;
+  hasNoPaddingBottom?: boolean;
+}
+
+/**
+ * A image KPI tile which composes an image into the tile itself.
+ */
+export function IllustrationTile({ image, alt, description, hasNoBorder, hasNoPaddingBottom }: IllustrationProps) {
+  return (
+    <Tile hasNoBorder={hasNoBorder} hasNoPaddingBottom={hasNoPaddingBottom}>
+      <img width="315px" height="100px" loading="lazy" src={image} alt={alt} css={css({ marginBottom: space[3] })} />
+      <p>{description}</p>
+    </Tile>
+  );
+}
