5606ac707563e374f1f4698fb0f7a3924497b879
VWSCoronaDashboard30 <mji@netcompany.com>
2023-08-23 12:31:33 +0200

    feat(COR-1705): PR comment resolution

--- packages/app/src/domain/layout/logic/use-sidebar.tsx
+++ packages/app/src/domain/layout/logic/use-sidebar.tsx
@@ -31,7 +31,7 @@ const mapKeysToReverseRouter = {
   positive_tests: 'positieveTesten',
   reproduction_number: 'reproductiegetal',
   sewage_measurement: 'rioolwater',
-  infection_radar: 'infectieRadar',
+  infection_radar: 'infectieradar',
   vaccinations: 'vaccinaties',
   variants: 'varianten',
 } as const;
