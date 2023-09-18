ad96e9f81406a369d3c5574472180c294aba39ea
VWSCoronaDashboard28 <arjun.puri.ordina@gmail.com>
2023-09-18 14:38:58 +0200

    feat(contact-page-redesign): Remove page title from schema.

--- packages/cms/src/schemas/documents/pages/contact/index.ts
+++ packages/cms/src/schemas/documents/pages/contact/index.ts
@@ -3,16 +3,9 @@ import { defineArrayMember, defineField, defineType } from 'sanity';
 export const contact = defineType({
   name: 'contact',
   title: 'Contact Page',
-  description:
-    'Dit is het startpunt voor het configureren van de contactpagina. Stel een optionele titel voor de pagina in en voeg er secties aan toe met behulp van de onderstaande lijst.',
+  description: 'Dit is het startpunt voor het configureren van de contactpagina. Voeg er secties aan toe met behulp van de onderstaande lijst.',
   type: 'document',
   fields: [
-    defineField({
-      name: 'title',
-      title: 'Pagina Titel',
-      description: 'Configureer de titel voor de contactpagina. Het is niet verplicht dit veld in te vullen.',
-      type: 'localeString',
-    }),
     defineField({
       name: 'contactPageGroups',
       title: 'Pagina Groepen',
