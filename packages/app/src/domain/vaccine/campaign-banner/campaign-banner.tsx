b1dd0b35d5d53912ce4dc01f789f30eff0d3cf6d
VWSCoronaDashboard29 <B>
2023-09-22 16:32:01 +0200

    feat(COR-1784): Update vaccine colours

--- packages/app/src/domain/vaccine/campaign-banner/campaign-banner.tsx
+++ packages/app/src/domain/vaccine/campaign-banner/campaign-banner.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import { MaxWidth } from '~/components/max-width';
 import { colors } from '@corona-dashboard/common';
 import { Heading } from '~/components/typography';
-import { Markdown } from '~/components';
+import { Markdown, Tile } from '~/components';
 import { Image } from '~/components/image';
 
 interface CampaignBannerProps {
@@ -13,34 +13,36 @@ interface CampaignBannerProps {
 }
 
 export const CampaignBanner = ({ title, description }: CampaignBannerProps) => (
-  <Box backgroundColor={colors.blue1} marginY={space[4]}>
-    <BannerContainer
-      alignItems={{ _: 'flex-start', sm: 'center' }}
-      display="flex"
-      flexDirection={{ _: 'column', sm: 'row' }}
-      justifyContent="center"
-      paddingX={{ _: space[3], sm: space[4] }}
-      paddingY={space[4]}
-    >
-      <CampaignDescription
-        maxWidth={sizes.maxWidthText}
+  <Tile>
+    <Box backgroundColor={colors.blue1} marginY={space[4]}>
+      <BannerContainer
+        alignItems={{ _: 'flex-start', sm: 'center' }}
         display="flex"
-        flexDirection="column"
-        justifyContent="space-between"
-        paddingX={{ sm: space[3], md: space[5] }}
+        flexDirection={{ _: 'column', sm: 'row' }}
+        justifyContent="center"
+        paddingX={{ _: space[3], sm: space[4] }}
         paddingY={space[4]}
       >
-        <Heading level={2} variant="h2">
-          {title}
-        </Heading>
+        <CampaignDescription
+          maxWidth={sizes.maxWidthText}
+          display="flex"
+          flexDirection="column"
+          justifyContent="space-between"
+          paddingX={{ sm: space[3], md: space[5] }}
+          paddingY={space[4]}
+        >
+          <Heading level={2} variant="h2">
+            {title}
+          </Heading>
 
-        <Markdown content={description} />
-      </CampaignDescription>
-      <Box paddingX={{ sm: space[5], md: space[2] }} alignSelf={{ _: 'center', sm: 'left' }}>
-        <CampaignIllustration src={'/images/Najaarsronde-coronaprik-hero.svg'} extension="svg" />
-      </Box>
-    </BannerContainer>
-  </Box>
+          <Markdown content={description} />
+        </CampaignDescription>
+        <Box paddingX={{ sm: space[5], md: space[2] }} alignSelf={{ _: 'center', sm: 'left' }}>
+          <CampaignIllustration src={'/images/Najaarsronde-coronaprik-hero.svg'} extension="svg" />
+        </Box>
+      </BannerContainer>
+    </Box>
+  </Tile>
 );
 
 type CampaignImageProps = {
