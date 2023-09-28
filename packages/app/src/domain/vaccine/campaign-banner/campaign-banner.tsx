635d2027b46013eba7497aa12645b39bf41496bb
VWSCoronaDashboard29 <B>
2023-09-28 11:56:02 +0200

    feat(COR-1784): Centralize styling

--- packages/app/src/domain/vaccine/campaign-banner/campaign-banner.tsx
+++ packages/app/src/domain/vaccine/campaign-banner/campaign-banner.tsx
@@ -1,6 +1,5 @@
 import { Box } from '~/components/base';
 import { sizes, space } from '~/style/theme';
-import styled from 'styled-components';
 import { MaxWidth } from '~/components/max-width';
 import { colors } from '@corona-dashboard/common';
 import { Heading } from '~/components/typography';
@@ -15,19 +14,21 @@ interface CampaignBannerProps {
 export const CampaignBanner = ({ title, description }: CampaignBannerProps) => (
   <Tile>
     <Box backgroundColor={colors.blue1} marginY={space[4]}>
-      <BannerContainer
+      <MaxWidth
         alignItems={{ _: 'flex-start', sm: 'center' }}
         display="flex"
         flexDirection={{ _: 'column', sm: 'row' }}
         justifyContent="center"
+        gridColumnGap={space[4]}
         paddingX={{ _: space[3], sm: space[4] }}
         paddingY={space[4]}
       >
-        <CampaignDescription
+        <Box
           maxWidth={sizes.maxWidthText}
           display="flex"
           flexDirection="column"
           justifyContent="space-between"
+          gridRowGap={space[4]}
           paddingX={{ sm: space[3], md: space[5] }}
           paddingY={space[4]}
         >
@@ -36,11 +37,11 @@ export const CampaignBanner = ({ title, description }: CampaignBannerProps) => (
           </Heading>
 
           <Markdown content={description} />
-        </CampaignDescription>
-        <Box paddingX={{ sm: space[5], md: space[2] }} alignSelf={{ _: 'center', sm: 'left' }}>
-          <CampaignIllustration src={'/images/Najaarsronde-coronaprik-hero.svg'} extension="svg" />
         </Box>
-      </BannerContainer>
+        <Box paddingX={{ sm: space[5], md: space[2] }} alignSelf={{ _: 'center', sm: 'left' }}>
+          <CampaignImage src={'/images/Najaarsronde-coronaprik-hero.svg'} extension="svg" />
+        </Box>
+      </MaxWidth>
     </Box>
   </Tile>
 );
@@ -56,21 +57,11 @@ const CampaignImage = (props: CampaignImageProps) => {
   const { src, extension, ...imageProps } = props;
 
   return (
-    <picture className={props.className}>
-      <source type={`image/${extension}`} />
-      <Image loading="lazy" src={src} {...imageProps} />
-    </picture>
+    <Box alignSelf="center">
+      <picture className={props.className}>
+        <source type={`image/${extension}`} />
+        <Image loading="lazy" src={src} {...imageProps} />
+      </picture>
+    </Box>
   );
 };
-
-const BannerContainer = styled(MaxWidth)`
-  column-gap: ${space[4]};
-`;
-
-const CampaignDescription = styled(Box)`
-  row-gap: ${space[4]};
-`;
-
-const CampaignIllustration = styled(CampaignImage)`
-  align-self: center;
-`;
