6d252719b7626a25fb74ccd1b806e72703197ff6
VWSCoronaDashboard29 <B>
2023-10-02 14:55:56 +0200

    fix(COR-1784/COR-1801): Fix feature flags

--- packages/common/src/feature-flags/features.ts
+++ packages/common/src/feature-flags/features.ts
@@ -4,13 +4,13 @@ export const features: Feature[] = [
   {
     name: 'nlVaccinationCoveragePerAgeGroupAutumn2022',
     isEnabled: false,
-    metricName: 'vaccine_coverage_per_age_group_estimated_autumn_2022_archived_202310xx',
+    metricName: 'vaccine_coverage_per_age_group_estimated_autumn_2022_archived_20231004',
     dataScopes: ['archived_nl'],
   },
   {
     name: 'nlVaccinationCoveragePerAgeGroupFullyVaccinated',
     isEnabled: false,
-    metricName: 'vaccine_coverage_per_age_group_estimated_fully_vaccinated_archived_202310xx',
+    metricName: 'vaccine_coverage_per_age_group_estimated_fully_vaccinated_archived_20231004',
     dataScopes: ['archived_nl'],
   },
 ];
