6e64c0957b8011cffd2c7dd154b0a13352c35f7c
VWSCoronaDashboard29 <B>
2023-10-10 14:55:30 +0200

    fix(COR-1784): Seperate sanity keys for vaccine coverage

--- packages/app/src/pages/landelijk/vaccinaties.tsx
+++ packages/app/src/pages/landelijk/vaccinaties.tsx
@@ -290,8 +290,8 @@ function VaccinationPage(props: StaticProps<typeof getStaticProps>) {
 
           <PrimarySeriesShotCoveragePerAgeGroup
             text={textNl.vaccination_coverage}
-            title={textNl.vaccination_coverage.title}
-            description={textNl.vaccination_coverage.description_autumn_2022_shot}
+            title={textNl.vaccination_coverage.title_primary_series}
+            description={textNl.vaccination_coverage.description_primary_shot}
             sortingOrder={['80+', '70-79', '60-69', '50-59', '40-49', '30-39', '18-29', '12-17', '5-11']}
             metadata={{
               datumsText: textNl.dates,
@@ -376,7 +376,7 @@ function VaccinationPage(props: StaticProps<typeof getStaticProps>) {
 
               <Autumn2022ShotCoveragePerAgeGroup
                 text={textNl.vaccination_coverage}
-                title={textNl.vaccination_coverage.title}
+                title={textNl.vaccination_coverage.title_autumn_2022_shot}
                 description={textNl.vaccination_coverage.description_autumn_2022_shot}
                 sortingOrder={['80+', '70-79', '60-69', '50-59', '40-49', '30-39', '18-29', '12-17', '5-11']}
                 metadata={{
@@ -389,8 +389,8 @@ function VaccinationPage(props: StaticProps<typeof getStaticProps>) {
 
               <BoosterShotCoveragePerAgeGroup
                 text={textNl}
-                title={textNl.vaccination_coverage.title}
-                description={textNl.archived.vaccination_coverage.top_level_description_booster_shot}
+                title={textNl.vaccination_coverage.title_archived}
+                description={textNl.vaccination_coverage.description_archived}
                 sortingOrder={['80+', '70-79', '60-69', '50-59', '40-49', '30-39', '18-29', '12-17', '5-11']}
                 metadata={{
                   datumsText: textNl.datums,
