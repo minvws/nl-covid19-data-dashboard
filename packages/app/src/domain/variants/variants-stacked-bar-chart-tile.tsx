17e90ab6aa32d22ca6c9056e7a145a2cfd4061c5
VWSCoronaDashboard29 <B>
2023-10-19 14:46:22 +0200

    refactor: Split useSeriesConfig into seperate file

--- packages/app/src/domain/variants/variants-stacked-bar-chart-tile.tsx
+++ packages/app/src/domain/variants/variants-stacked-bar-chart-tile.tsx
@@ -1,11 +1,23 @@
-import { MetadataProps } from '~/components';
+import { ChartTile, Markdown, MetadataProps } from '~/components';
+import { Box } from '~/components/base';
 
 interface VariantsStackedBarChartTileProps {
   title: string;
   description: string;
+  _helpText: string;
   metadata: MetadataProps;
 }
 
-export const VariantsStackedBarChartTile = (_props: VariantsStackedBarChartTileProps) => {
-  return <></>;
+export const VariantsStackedBarChartTile = ({ title, description, _helpText, metadata }: VariantsStackedBarChartTileProps) => {
+  return (
+    <ChartTile title={title} description={description} metadata={metadata}>
+      <Box>
+        <Box>
+          <Markdown content="placeholder"></Markdown>
+        </Box>
+      </Box>
+      {/*<InteractiveLegend helpText={helpText} selectOptions={} selection={} onToggleItem={} />*/}
+      {/*<StackedChart accessibility={{key: 'variants_stacked_area_over_time_chart'}} values={} config={} />*/}
+    </ChartTile>
+  );
 };
